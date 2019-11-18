b167bbc6f36f2bba58f146b7aae7718f89ae65a7
Vlad Mishchenko <vladislav.a.mishchenko@gmail.com>
2019-11-18 15:13:53 +0200

    develop

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -67,24 +67,25 @@ function transformStateWithClones(state, transforms) {
   const answer = [];
   const stateClone = { ...state };
   for (let i = 0; i < transforms.length; i++) {
-    if (transforms[i].operation === 'addProperties') {
-      for (const key in transforms[i].properties) {
-        stateClone[key] = transforms[i].properties[key];
-      }
-    }
-
-    if (transforms[i].operation === 'removeProperties') {
-      for (let j = 0; j < transforms[i].properties.length; j++) {
-        if (transforms[i].properties[j] in stateClone) {
-          delete stateClone[transforms[i].properties[j]];
+    switch (transforms[i].operation) {
+      case ('addProperties'):
+        for (const key in transforms[i].properties) {
+          stateClone[key] = transforms[i].properties[key];
         }
-      }
-    }
+        break;
 
-    if (transforms[i].operation === 'clear') {
-      for (const key in stateClone) {
-        delete stateClone[key];
-      }
+      case ('removeProperties'):
+        for (let j = 0; j < transforms[i].properties.length; j++) {
+          if (transforms[i].properties[j] in stateClone) {
+            delete stateClone[transforms[i].properties[j]];
+          }
+        }
+        break;
+
+      default:
+        for (const key in stateClone) {
+          delete stateClone[key];
+        }
     }
     answer.push({ ...stateClone });
   }
