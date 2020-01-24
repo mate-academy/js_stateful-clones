e7e340a7e8400179606017d9665510691fcd71f8
Yaroslav Magrelo <slava.magrelo@gmail.com>
2020-01-24 16:24:47 +0200

    fix repeated resetObj assign

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -68,31 +68,27 @@ function transformStateWithClones(state, transforms) {
   const resultingObj = Object.assign({}, state);
 
   for (const item of transforms) {
-    let resetObj;
-
     if (item.operation === 'addProperties') {
       for (const key in item.properties) {
         resultingObj[key] = item.properties[key];
       }
-      resetObj = Object.assign({}, resultingObj);
-      resultingArr.push(resetObj);
     }
 
     if (item.operation === 'removeProperties') {
       for (let i = 0; i < item.properties.length; i++) {
         delete resultingObj[item.properties[i]];
       }
-      resetObj = Object.assign({}, resultingObj);
-      resultingArr.push(resetObj);
     }
 
     if (item.operation === 'clear') {
       for (const key in resultingObj) {
         delete resultingObj[key];
       }
-      resetObj = Object.assign({}, resultingObj);
-      resultingArr.push(resetObj);
     }
+
+    const resetObj = Object.assign({}, resultingObj);
+
+    resultingArr.push(resetObj);
   }
 
   return resultingArr;
