3dfa87eac3ee6efb85475f6f2d37dd0d7e3a2f7f
Stanislav Mochyr <delenjer@gmail.com>
2020-01-24 14:44:06 +0200

    js_stateful-clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -64,7 +64,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, transforms) {
-  // write code here
+  const newArr = [];
+  const cloning = { ...state };
+
+  for (const obj of transforms) {
+    const properties = obj.properties;
+
+    switch (obj.operation) {
+      case 'addProperties':
+        Object.assign(cloning, properties);
+        break;
+      case 'removeProperties':
+        for (const key of properties) {
+          delete cloning[key];
+        }
+        break;
+
+      default:
+        for (const key in cloning) {
+          delete cloning[key];
+        }
+    }
+
+    newArr.push({ ...cloning });
+  }
+
+  return newArr;
 }
 
 module.exports = transformStateWithClones;
