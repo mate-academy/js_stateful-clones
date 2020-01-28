9850b0fc2334fc2941bf6998738da6c6125884f0
Maximka <maximkacmm@gmail.com>
2020-01-28 09:51:26 +0200

    fix commit

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -66,6 +66,7 @@
 function transformStateWithClones(state, transforms) {
   const newArr = [];
   const newObject = Object.assign({}, state);
+  let transitObj = {};
 
   for (let i = 0; i < transforms.length; i++) {
     const operations = transforms[i].operation;
@@ -73,32 +74,30 @@ function transformStateWithClones(state, transforms) {
 
     switch (operations) {
       case 'addProperties' :
-        let transitObj = {};
 
         for (const key in properties) {
           newObject[key] = properties[key];
         }
+
         transitObj = Object.assign({}, newObject);
         newArr.push(Object.assign({}, transitObj));
         break;
-    }
 
-    switch (operations) {
       case 'removeProperties' :
         for (let f = 0; f < properties.length; f++) {
           delete newObject[properties[f]];
         }
+
         newArr.push(Object.assign({}, newObject));
         break;
-    }
 
-    switch (operations) {
       case 'clear' :
         for (const key in newObject) {
           if (newObject.hasOwnProperty(key)) {
             delete newObject[key];
           }
         }
+
         newArr.push(Object.assign({}, newObject));
         break;
     }
