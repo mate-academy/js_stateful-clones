63fda6ca3b90cca09eac4a29d3640c2ec12de057
BuildTools <unconfigured@null.spigotmc.org>
2020-04-01 15:06:46 +0300

    HOORAY!!!

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -64,7 +64,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, transforms) {
-  // write code here
+  const _coolState = { ...state };
+  const _cloneDetect = [];
+
+  transforms.forEach((item) => {
+    const _operation = item.operation;
+
+    switch (_operation) {
+      case 'addProperties':
+        Object.assign(_coolState, item['properties']);
+        break;
+      case 'removeProperties':
+        item.properties.forEach(_delItem => {
+          delete _coolState[_delItem];
+        });
+        break;
+      case 'clear':
+        for (const _delKey in _coolState) {
+          delete _coolState[_delKey];
+        }
+        break;
+    }
+
+    _cloneDetect.push({ ..._coolState });
+  });
+
+  return _cloneDetect;
 }
 
 module.exports = transformStateWithClones;
