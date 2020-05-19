26d3cfb69e99df32a77844480178aa6ceb864663
Anatoliy Pokhylko <pokhilko.av@gmail.com>
2020-05-19 13:38:13 +0300

    Fixes after code review

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -9,36 +9,41 @@
 function transformStateWithClones(state, transforms) {
   // write code here
 
-  const arrResult = [];
+  const history = [];
   const cloneState = {
     ...state,
   };
+  const OPERATION = {
+    add: 'addProperties',
+    remove: 'removeProperties',
+    clear: 'clear',
+  };
 
   for (const key of transforms) {
-    if (key.operation === 'addProperties') {
+    if (key.operation === OPERATION.add) {
       for (const keyAdd in key.properties) {
         cloneState[keyAdd] = key.properties[keyAdd];
       };
     }
 
-    if (key.operation === 'removeProperties') {
+    if (key.operation === OPERATION.remove) {
       for (const keyRemove in key.properties) {
         delete cloneState[key.properties[keyRemove]];
       }
     }
 
-    if (key.operation === 'clear') {
+    if (key.operation === OPERATION.clear) {
       for (const keyRemove in cloneState) {
         delete cloneState[keyRemove];
       }
     }
 
-    arrResult.push({
+    history.push({
       ...cloneState,
     });
   }
 
-  return arrResult;
+  return history;
 }
 
 module.exports = transformStateWithClones;
