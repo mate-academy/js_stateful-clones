933537c249c895486df838c4e505b579246b1d86
Roman <romikkus@gmail.com>
2020-08-04 16:57:49 +0300

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -66,6 +66,41 @@
  */
 function transformStateWithClones(state, transforms) {
   // write code here
+  const result = [];
+  const copy = { ...state };
+
+  for (const object of transforms) {
+    switch (object['operation']) {
+      case 'addProperties': {
+        for (const prop in object['properties']) {
+          copy[prop] = object['properties'][prop];
+        }
+        break;
+      }
+
+      case 'removeProperties': {
+        for (const prop of object['properties']) {
+          delete copy[prop];
+        }
+        break;
+      }
+
+      case 'clear': {
+        for (const prop in copy) {
+          delete copy[prop];
+        }
+        break;
+      }
+
+      default: {
+        return 'Invalid case';
+      }
+    }
+
+    result.push({ ...copy });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
