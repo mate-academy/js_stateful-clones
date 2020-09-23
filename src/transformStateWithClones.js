f59cfa31cb93c2ad932fc9b8659f67b60d959be4
Sofiia Kucherenko <kucherenko.sv13@gmail.com>
2020-09-23 11:38:12 +0300

    solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -66,6 +66,32 @@
  */
 function transformStateWithClones(state, transforms) {
   // write code here
+  const cloneState = { ...state };
+  const result = [];
+
+  for (const transform of transforms) {
+    const { operation, properties } = transform;
+
+    switch (operation) {
+      case 'addProperties':
+        Object.assign(cloneState, properties);
+        break;
+
+      case 'removeProperties':
+        for (const property of properties) {
+          delete cloneState[property];
+        };
+        break;
+
+      case 'clear':
+        for (const key in cloneState) {
+          delete cloneState[key];
+        };
+    }
+    result.push({ ...cloneState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
