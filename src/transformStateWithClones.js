533d7702a0f8b9ad25e333c6a434d4e8d325497c
Yevgeniy Podobreev <podobr_2018@ukr.net>
2020-12-03 20:45:04 +0200

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -66,6 +66,24 @@
  */
 function transformStateWithClones(state, transforms) {
   // write code here
-}
+  const result = [];
+  const stateClone = { ...state };
 
+  for (const transform of transforms) {
+    if (transform.operation === 'addProperties') {
+      Object.assign(stateClone, transform.properties);
+    } else if (transform.operation === 'removeProperties') {
+      for (const property of transform.properties) {
+        delete stateClone[property];
+      }
+    } else if (transform.operation === 'clear') {
+      for (const key in stateClone) {
+        delete stateClone[key];
+      }
+    }
+    result.push({ ...stateClone });
+  }
+
+  return result;
+}
 module.exports = transformStateWithClones;
