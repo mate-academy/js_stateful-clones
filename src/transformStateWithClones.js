11cad330eb1fa6ac67391430e5bc10cb730893d0
Your Name <mogolgogol.90@gmail.com>
2020-12-10 20:03:02 +0200

    solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -65,7 +65,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, transforms) {
-  // write code here
+  const stateClone = { ...state };
+  const states = [];
+
+  for (const { operation, properties } of transforms) {
+    switch (operation) {
+      case 'addProperties':
+        Object.assign(stateClone, properties);
+        break;
+
+      case 'removeProperties':
+        for (const empty of properties) {
+          delete stateClone[empty];
+        };
+        break;
+
+      case 'clear':
+        for (const erased in stateClone) {
+          delete stateClone[erased];
+        };
+    }
+    states.push({ ...stateClone });
+  }
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
