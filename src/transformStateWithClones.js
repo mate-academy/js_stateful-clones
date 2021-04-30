7230c715e5a663df890dbce65bb0369ec6604a39
Андрій Наливайко <andrijnalivajko@MacBook-Pro-Andrij.local>
2021-04-30 16:44:56 +0300

    Revolution after review

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -66,6 +66,32 @@
  */
 function transformStateWithClones(state, transforms) {
   // write code here
+  let copyOfState = Object.assign({}, state);
+  const result = [];
+
+  for (const run of transforms) {
+    const { operation, properties } = run;
+
+    switch (operation) {
+      case 'addProperties':
+        Object.assign(copyOfState, properties);
+        break;
+
+      case 'removeProperties':
+        for (const remove of properties) {
+          delete copyOfState[remove];
+        }
+        break;
+
+      case 'clear':
+        copyOfState = {};
+        break;
+    }
+
+    result.push(Object.assign({}, copyOfState));
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
