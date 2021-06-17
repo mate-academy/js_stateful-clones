4d9ceb65d86afd00a9061c4acb5247b4af5ca5ba
Oleksii Lymonov <alex25_92@ukr.net>
2021-06-17 16:06:02 +0300

    first_try

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,32 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const newObj = Object.assign({}, state);
+  const newArray = [];
+
+  for (const item of actions) {
+    if (item.type === 'addProperties') {
+      Object.assign(newObj, item.extraData);
+    }
+
+    if (item.type === 'removeProperties') {
+      for (const index of item.keysToRemove) {
+        delete newObj[index];
+      }
+    }
+
+    if (item.type === 'clear') {
+      for (const key in newObj) {
+        delete newObj[key];
+      }
+    }
+
+    const FinalObj = { ...newObj };
+
+    newArray.push(FinalObj);
+  }
+
+  return newArray;
 }
 
 module.exports = transformStateWithClones;
