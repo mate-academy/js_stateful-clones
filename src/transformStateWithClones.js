aba6d2fed7eb4f378856cc6c570b1434b8762baa
Oleksii Lymonov <alex25_92@ukr.net>
2021-06-17 19:22:17 +0300

    switch_added

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,29 +8,29 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
-  const copy = Object.assign({}, state);
+  const copyState = { ...state };
   const transformedState = [];
 
-  for (const item of actions) {
-    if (item.type === 'addProperties') {
-      Object.assign(copy, item.extraData);
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(copyState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete copyState[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in copyState) {
+          delete copyState[key];
+        }
+        break;
     }
 
-    if (item.type === 'removeProperties') {
-      for (const index of item.keysToRemove) {
-        delete copy[index];
-      }
-    }
-
-    if (item.type === 'clear') {
-      for (const key in copy) {
-        delete copy[key];
-      }
-    }
-
-    const finalCopy = Object.assign({}, copy);
-
-    transformedState.push(finalCopy);
+    transformedState.push({ ...copyState });
   }
 
   return transformedState;
