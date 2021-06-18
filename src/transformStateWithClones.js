c2c615d57154233642e5b96de284097a24b31dd7
vdzhyranov <v.dzhyranov@gmail.com>
2021-06-18 11:01:55 +0300

    fix

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let clone = { ...state };
+  const arr = [];
+
+  for (const item of actions) {
+    clone = { ...clone };
+    arr.push(clone);
+
+    switch (actions.type) {
+      case 'addProperties':
+        clone = (Object.assign(clone, item.extraData));
+        break;
+
+      case 'removeProperties':
+        for (const toDel of item.keysToRemove) {
+          delete clone[toDel];
+        }
+        break;
+
+      case 'clear':
+        for (const clear in clone) {
+          if (clone.hasOwnProperty(clear)) {
+            delete clone[clear];
+          }
+        }
+    }
+  }
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
