a8eace0fc53fb139ceb2a84a79f815f4a056220c
Oleksii Blezniuk <oleksii.blezniuk@gmail.com>
2021-06-21 17:35:13 +0300

    task is completed

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,34 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const result = [];
+  const currentState = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(currentState, action.extraData);
+
+      result.push({ ...currentState });
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete currentState[key];
+      }
+
+      result.push({ ...currentState });
+    }
+
+    if (action.type === 'clear') {
+      for (const key in currentState) {
+        delete currentState[key];
+      }
+
+      result.push({ ...currentState });
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
