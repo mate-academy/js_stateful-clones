39fd6921ccc6e868f6a8ffb0e75c9741332e6ee0
David Dohru <tronsmartmy@gmail.com>
2021-06-21 15:51:55 +0300

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,35 @@
  *
  * @return {Object[]}
  */
-function transformStateWithClones(state, actions) {
-  // write code here
+function transformStateWithClones(state, action) {
+  const arr = [];
+  const obj = { ...state };
+
+  for (let i = 0; i < action.length; i++) {
+    if (action[i].type === 'addProperties') {
+      for (const key in action[i].extraData) {
+        obj[key] = action[i].extraData[key];
+      }
+    }
+
+    if (action[i].type === 'removeProperties') {
+      for (let j = 0; j < action[i].keysToRemove.length; j++) {
+        for (let key in action[i].keysToRemove[j]) {
+          key = [key];
+          delete obj[action[i].keysToRemove[j]];
+        }
+      }
+    }
+
+    if (action[i].type === 'clear') {
+      for (const key in obj) {
+        delete obj[key];
+      }
+    }
+    arr.push({ ...obj });
+  }
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
