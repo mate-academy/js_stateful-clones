2b13fe8905b9e511861054f9d3924fbeaff017e0
Aleksandr Semerenko <sanya96088475@gmail.com>
2021-06-22 15:29:54 +0300

    transform state clone version 1.0

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateClone = { ...state };
+  const transformState = [];
+
+  for (const char of actions) {
+    if (char.type === 'addProperties') {
+      Object.assign(stateClone, char.extraData);
+    }
+
+    if (char.type === 'removeProperties') {
+      for (const key of char.keysToRemove) {
+        delete stateClone[key];
+      }
+    }
+
+    if (char.type === 'clear') {
+      for (const key of Object.keys(stateClone)) {
+        delete stateClone[key];
+      }
+    }
+
+    transformState.push({ ...stateClone });
+  }
+
+  return transformState;
 }
 
 module.exports = transformStateWithClones;
