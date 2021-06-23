a11b69498e7772b56fc0ae628bc9e5156dd94078
Yaroslav <gso218695@gmail.com>
2021-06-23 23:18:48 +0300

    test

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let stateCopy = { ...state };
+  const actionChanges = [];
+
+  for (const action of actions) {
+    stateCopy = { ...stateCopy };
+    actionChanges.push(stateCopy);
+
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete stateCopy[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
+        break;
+    }
+  }
+
+  return actionChanges;
 }
 
 module.exports = transformStateWithClones;
