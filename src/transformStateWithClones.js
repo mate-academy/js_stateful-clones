9ae1a8b199edc2bf563b145fcbc25e81f1a661a7
Stas Pechenyi <spechenuy@gmail.com>
2021-06-23 14:24:52 +0300

    Homework was done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const transformState = [];
+  const tempState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(tempState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete (tempState[key]);
+        }
+        break;
+
+      case 'clear':
+        for (const key in tempState) {
+          delete (tempState[key]);
+        }
+        break;
+    }
+
+    transformState.push({ ...tempState });
+  }
+
+  return transformState;
 }
 
 module.exports = transformStateWithClones;
