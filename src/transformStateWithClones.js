7ac2370a57c7dc856f60ce86cb17c28ef642b840
Artem Krulykovskyi <krulikovskiy.a@gmail.com>
2021-06-23 16:34:47 +0300

    i love js

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const states = [];
+  let prevState = { ...state };
+
+  for (let i = 0; i < actions.length; i++) {
+    states.push(prevState);
+
+    if (actions[i].type === 'addProperties') {
+      for (const property in actions[i].extraData) {
+        states[i][property] = actions[i].extraData[property];
+      }
+    }
+
+    if (actions[i].type === 'removeProperties') {
+      for (const property of actions[i].keysToRemove) {
+        delete states[i][property];
+      }
+    }
+
+    if (actions[i].type === 'clear') {
+      states[i] = {};
+    }
+
+    prevState = { ...states[i] };
+  }
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
