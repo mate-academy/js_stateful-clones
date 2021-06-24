ccb04c6e1e7709b473ae17fb6456f4f15e282411
Ruslan Vyvdyuk <84183046+Vivdyuk@users.noreply.github.com>
2021-06-24 15:11:41 +0300

    Optimize v1.1

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -16,24 +16,26 @@ function transformStateWithClones(state, actions) {
     switch (action.type) {
       case 'addProperties':
         Object.assign(newState, action.extraData);
-        transformClones.push({ ...newState });
         break;
+
       case 'removeProperties':
         for (const key of action.keysToRemove) {
           delete newState[key];
         }
-        transformClones.push({ ...newState });
+
         break;
 
       case 'clear':
         for (const key in newState) {
           delete newState[key];
         }
-        transformClones.push({ ...newState });
+
         break;
 
       default:
     }
+
+    transformClones.push({ ...newState });
   }
 
   return transformClones;
