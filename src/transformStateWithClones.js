73b06fab114941ca2c550b6b7e7dff637350e3ea
coyotious <coyotious@gmail.com>
2021-06-27 11:44:09 +0300

    added function transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newState = Object.assign({}, state);
+  const actionsStory = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'removeProperties':
+        for (const removeData of action.keysToRemove) {
+          delete newState[removeData];
+        };
+        break;
+      case 'addProperties':
+        Object.assign(newState, action.extraData);
+        break;
+      default:
+        for (const key in newState) {
+          delete newState[key];
+        }
+        break;
+    }
+
+    actionsStory.push({ ...newState });
+  }
+
+  return actionsStory;
 }
 
 module.exports = transformStateWithClones;
