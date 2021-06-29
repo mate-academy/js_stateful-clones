f7de1ee797f6f843030a1e14ab53066d7a272641
Svyatoslav Victor <victorsvyatoslav@gmail.com>
2021-06-29 09:04:26 +0300

    Task Done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,49 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateClone = {};
+
+  Object.assign(stateClone, state);
+
+  const actionsLog = new Array(actions.length);
+
+  for (let i = 0; i < actionsLog.length; i++) {
+    actionsLog[i] = {};
+  }
+
+  for (let i = 0; i < actionsLog.length; i++) {
+    if (actions[i].type === 'addProperties') {
+      if (i === 0) {
+        Object.assign(actionsLog[i], stateClone);
+        Object.assign(actionsLog[i], actions[i].extraData);
+      } else {
+        Object.assign(actionsLog[i], actionsLog[i - 1]);
+        Object.assign(actionsLog[i], actions[i].extraData);
+      }
+    }
+
+    if (actions[i].type === 'removeProperties') {
+      if (i === 0) {
+        Object.assign(actionsLog[i], stateClone);
+
+        for (const keys of actions[i].keysToRemove) {
+          delete actionsLog[i][keys];
+        }
+      } else {
+        Object.assign(actionsLog[i], actionsLog[i - 1]);
+
+        for (const keys of actions[i].keysToRemove) {
+          delete actionsLog[i][keys];
+        }
+      }
+    }
+
+    if (actions[i].type === 'clear') {
+      actionsLog[i] = {};
+    }
+  }
+
+  return actionsLog;
 }
 
 module.exports = transformStateWithClones;
