f87827dbd09f3c12f8dc2ae8b9caf36394ee057e
Roman Minchenko <minchenko.poma@gmail.com>
2021-07-16 17:01:16 +0300

    stateful-clones-done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let copy = { ...state };
+
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'addProperties') {
+      copy = Object.assign(copy, actions[i].extraData);
+      result.push(Object.assign({}, copy));
+    }
+
+    if (actions[i].type === 'removeProperties') {
+      for (let y = 0; y < actions[i].keysToRemove.length; y++) {
+        delete copy[actions[i].keysToRemove[y]];
+      }
+      result.push(Object.assign({}, copy));
+    }
+
+    if (actions[i].type === 'clear') {
+      for (const key in copy) {
+        delete copy[key];
+      }
+      result.push(Object.assign({}, copy));
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
