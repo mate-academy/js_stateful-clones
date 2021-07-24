c83b9b870f5c941b65bf6d48103835ee9a68bcd9
Dima Cherednyk <dicherednyk@gmail.com>
2021-07-24 17:35:59 +0300

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,67 +7,41 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const add0 = {};
-  const rem0 = {};
-  const clear0 = {};
-  const result = {};
+  let copy = {
+    ...state,
+  };
   const resultMassive = [];
 
   for (const key of actions) {
     switch (key.type) {
       case 'addProperties':
-        if (resultMassive.length > 0) {
-          Object.assign(result, key.extraData);
-
-          const add = Object.assign({}, result);
-
-          resultMassive.push(add);
-        } else {
-          Object.assign(result, state, key.extraData);
-          Object.assign(add0, result);
-          resultMassive.push(add0);
+        copy = {
+          ...copy,
+          ...key.extraData,
         };
         break;
 
       case 'removeProperties':
-        if (resultMassive.length > 0) {
-          const rem = Object.assign({}, result);
-
-          for (const f of key.keysToRemove) {
-            delete result[f];
-            delete rem[f];
-          };
-          resultMassive.push(rem);
-        } else {
-          Object.assign(result, state);
-          Object.assign(rem0, result);
-
-          for (const f of key.keysToRemove) {
-            delete rem0[f];
-            delete result[f];
-          };
-          resultMassive.push(rem0);
+        for (const f of key.keysToRemove) {
+          delete copy[f];
         };
         break;
 
       case 'clear':
-        if (resultMassive.length > 0) {
-          const clear = Object.assign({}, result);
-
-          for (const y in clear) {
-            delete result[y];
-            delete clear[y];
-          };
-          resultMassive.push(clear);
-        } else {
-          Object.assign(clear0, result);
-          resultMassive.push(clear0);
+        for (const y in copy) {
+          delete copy[y];
         };
         break;
-    }
-  }
+    };
+
+    const result = {
+      ...copy,
+    };
+
+    resultMassive.push(result);
+  };
 
   return resultMassive;
-}
+};
 
 module.exports = transformStateWithClones;
