ff5d64215a54db9d5544f043c9e64357c385ba71
Ihor Nahnybida <igor653279@gmail.com>
2021-08-02 23:25:02 +0300

    Cloned and transformed

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arr = [];
+  const temp = Object.assign({}, state);
+
+  for (const obj of actions) {
+    switch (obj.type) {
+      case 'addProperties':
+        for (const i in obj.extraData) {
+          temp[i] = obj.extraData[i];
+        }
+        arr.push(Object.assign({}, temp));
+        break;
+
+      case 'removeProperties':
+        for (const i in obj.keysToRemove) {
+          delete temp[obj.keysToRemove[i]];
+        }
+        arr.push(Object.assign({}, temp));
+        break;
+
+      default:
+        for (const i in temp) {
+          delete temp[i];
+        }
+        arr.push(Object.assign({}, temp));
+    }
+  }
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
