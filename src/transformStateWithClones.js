87568307154853ec077aa6e460288740c0a62fa2
Anton Forzun <anton_forzun@ukr.net>
2021-08-02 14:53:57 +0300

    tranform clone

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const newState = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      for (const key in action.extraData) {
+        newState[key] = action.extraData[key];
+      }
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete newState[key];
+      }
+    }
+
+    if (action.type === 'clear') {
+      for (const key in newState) {
+        delete newState[key];
+      }
+    }
+
+    const resultObj = { ...newState };
+
+    result.push(resultObj);
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
