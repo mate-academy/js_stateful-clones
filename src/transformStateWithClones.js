0b3dd503f16508f8e37517047f8e141ad2c50e36
Illia Vovchenko <ilya.vovchenko@gmail.com>
2021-08-03 21:10:22 +0300

    task was done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,39 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+
+  for (const action of actions) {
+    let stateToCopy = stateHistory[stateHistory.length - 1];
+    const { type: actionType } = action;
+
+    if (actionType === 'clear') {
+      stateHistory.push({});
+      continue;
+    }
+
+    if (stateHistory.length === 0) {
+      stateToCopy = state;
+    }
+
+    const tempState = { ...stateToCopy };
+
+    if (actionType === 'addProperties') {
+      Object.assign(tempState, action.extraData);
+    }
+
+    if (actionType === 'removeProperties') {
+      if (action.keysToRemove.length !== 0) {
+        for (const key of action.keysToRemove) {
+          delete tempState[key];
+        }
+      }
+    }
+
+    stateHistory.push(tempState);
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
