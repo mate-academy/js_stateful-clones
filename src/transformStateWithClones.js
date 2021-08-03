cb3fa627b687354dea110377fabd3a01dcbfa09e
Sasha Lifashkin <alifashkin@outlook.com>
2021-08-03 18:32:13 +0300

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,42 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  // let stateClone =
+  // Object.defineProperties({}, Object.getOwnPropertyDescriptors(state));
+  const stateClone = Object.assign({}, state);
+  const resultStatesArray = [];
+
+  for (const item of actions) {
+    switch (item.type) {
+      case 'addProperties':
+        for (const key in item.extraData) {
+          stateClone[key] = item.extraData[key];
+        }
+        resultStatesArray.push(Object.assign({}, stateClone));
+
+        break;
+
+      case 'removeProperties':
+        for (const keyRemove of item.keysToRemove) {
+          if (keyRemove in stateClone) {
+            delete stateClone[keyRemove];
+          }
+        }
+        resultStatesArray.push(Object.assign({}, stateClone));
+
+        break;
+
+      case 'clear':
+        for (const key in stateClone) {
+          delete stateClone[key];
+        }
+        resultStatesArray.push(Object.assign({}, stateClone));
+
+        break;
+    }
+  }
+
+  return resultStatesArray;
 }
 
 module.exports = transformStateWithClones;
