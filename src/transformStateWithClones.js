14ce55c3a2cf89bc9b90aade81cd719fa455a28c
Eugene Gnedenko <deadup4@gmail.com>
2021-08-04 14:28:32 +0300

    tranform state with clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const cloneOfState = [];
+  const copyOfState = { ...state };
+
+  for (let i = 0; i < actions.length; i++) {
+    switch (actions[i].type) {
+      case 'addProperties':
+        Object.assign(copyOfState, actions[i].extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of actions[i].keysToRemove) {
+          delete copyOfState[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in copyOfState) {
+          delete copyOfState[key];
+        }
+        break;
+    }
+
+    cloneOfState.push({ ...copyOfState });
+  }
+
+  return cloneOfState;
 }
 
 module.exports = transformStateWithClones;
