b12b2113d94ed626ff031b009d7102ae5938032d
karolina-nadDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD <westlinka20@gmail.com>
2021-08-04 21:05:36 +0300

    task done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arr = [];
+  const stateClone = { ...state };
+
+  for (const properties of actions) {
+    switch (properties.type) {
+      case 'addProperties':
+        Object.assign(stateClone, properties.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const keys in properties.keysToRemove) {
+          delete stateClone[properties.keysToRemove[keys]];
+        }
+        break;
+
+      case 'clear':
+        for (const properties2 in stateClone) {
+          delete stateClone[properties2];
+        };
+        break;
+    }
+
+    arr.push(Object.assign({}, stateClone));
+  }
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
