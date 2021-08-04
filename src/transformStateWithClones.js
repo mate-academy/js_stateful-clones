6cf1f0298c889320f49a8ad559a22442c1e06b54
dmytro_mykoliv <amigo.del.ua@gmail.com>
2021-08-04 11:32:13 +0200

    simplified code

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,50 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
-  const cloneState = [];
+  const changeList = [];
+  let stateCopy = { ...state };
 
-  for (let i = 0; i < actions.length; i++) {
-    switch (actions[i].type) {
+  for (const object of actions) {
+    switch (object.type) {
       case 'addProperties':
-
-        if (i === 0) {
-          cloneState.push(Object.assign({}, state));
-        } else {
-          cloneState.push(Object.assign({}, cloneState[i - 1]));
-        }
-
-        Object.assign(cloneState[i], actions[i].extraData);
+        Object.assign(stateCopy, object.extraData);
         break;
 
       case 'removeProperties':
-
-        if (i === 0) {
-          cloneState.push(Object.assign({}, state));
-        } else {
-          cloneState.push(Object.assign({}, cloneState[i - 1]));
-        }
-
-        actions[i].keysToRemove.forEach(element => {
-          delete cloneState[i][element];
+        object.keysToRemove.forEach(element => {
+          delete stateCopy[element];
         });
         break;
 
       case 'clear':
-
-        if (i === 0) {
-          cloneState.push(Object.assign({}, state));
-        } else {
-          cloneState.push(Object.assign({}, cloneState[i - 1]));
-        }
-
-        Object.keys(cloneState[i]).forEach(element => {
-          delete cloneState[i][element];
-        });
+        stateCopy = {};
     }
+
+    changeList.push({ ...stateCopy });
   }
 
-  return cloneState;
+  return changeList;
 }
 
 module.exports = transformStateWithClones;
