42d5f0ab0b041f18d583ea4946175c1a02baf500
Clon <clonvhs@gmail.com>
2021-08-04 15:49:20 +0300

    Solution
    v1.0

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+  const copyOfState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        for (const addPropertiesData in action.extraData) {
+          copyOfState[addPropertiesData] = action.extraData[addPropertiesData];
+        }
+        break;
+      case 'removeProperties':
+        for (const newKey of action.keysToRemove) {
+          delete copyOfState[newKey];
+        }
+        break;
+      case 'clear':
+        for (const newKey in copyOfState) {
+          delete copyOfState[newKey];
+        }
+        break;
+    }
+    stateHistory.push({ ...copyOfState });
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
