70a6b6c7d31278f4fd215614b21b4cd34a19eabb
Andrii Serhovetc <serhovetc88@gmail.com>
2021-08-04 16:24:10 +0300

    Clone state done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const changeList = [];
+  const cloneState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties' :
+        Object.assign(cloneState, action.extraData);
+        break;
+
+      case 'removeProperties' :
+        for (const key of action.keysToRemove) {
+          delete cloneState[key];
+        }
+
+        break;
+
+      case 'clear' : {
+        for (const key in cloneState) {
+          delete cloneState[key];
+        }
+
+        break;
+      }
+    }
+    changeList.push({ ...cloneState });
+  }
+
+  return changeList;
 }
 
 module.exports = transformStateWithClones;
