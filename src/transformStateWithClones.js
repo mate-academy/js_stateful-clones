e2311ceb373f3f7bac038f7a91c5d8ce1599287e
Volodymyr Polishchuk <rot010011@gmail.com>
2021-08-04 23:07:51 +0300

    work is done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const changeList = [];
+  let newState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(newState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const keyArr of action.keysToRemove) {
+          if (newState.hasOwnProperty(keyArr)) {
+            delete newState[keyArr];
+          }
+        }
+        break;
+
+      case 'clear':
+        newState = {};
+    }
+
+    changeList.push({ ...newState });
+  }
+
+  return changeList;
 }
 
 module.exports = transformStateWithClones;
