7bf24ce595eb1c05ddabda6f9b319b9b4b236530
Sasha Havryliuk <akwaccc@gmail.com>
2021-08-04 15:04:18 +0300

    the construction was changed to switch

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,18 +11,23 @@ function transformStateWithClones(state, actions) {
   let objectStep = { ...state };
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      for (const key in action.extraData) {
-        objectStep[key] = action.extraData[key];
-      }
-    } else if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete objectStep[key];
-      }
-    } else if (action.type === 'clear') {
-      for (const key in objectStep) {
-        delete objectStep[key];
-      }
+    switch (action.type) {
+      case 'addProperties':
+        for (const key in action.extraData) {
+          objectStep[key] = action.extraData[key];
+        }
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete objectStep[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in objectStep) {
+          delete objectStep[key];
+        }
     }
 
     collectionObjects.push(objectStep);
