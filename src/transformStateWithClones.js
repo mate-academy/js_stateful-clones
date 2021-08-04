bef256338958cf7318c748e8885bc05a941ab11f
unknown <hado.roman.ihorovych@gmail.com>
2021-08-04 17:10:21 +0300

    solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const clone = { ...state };
+  const array = [];
+
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'addProperties') {
+      Object.assign(clone, actions[i].extraData);
+    }
+
+    if (actions[i].type === 'removeProperties') {
+      for (const key of actions[i].keysToRemove) {
+        delete clone[key];
+      }
+    }
+
+    if (actions[i].type === 'clear') {
+      for (const key in clone) {
+        delete clone[key];
+      }
+    }
+
+    array.push({ ...clone });
+  }
+
+  return array;
 }
 
 module.exports = transformStateWithClones;
