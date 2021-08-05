91d161f2fbcd9748eff3d4cd4ed72031c9e24d97
Vsevolod Maslak <vsevolodmaslak@gmail.com>
2021-08-05 08:50:22 +0300

    final

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateLogs = [];
+  const stateCopy = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        break;
+      case 'removeProperties':
+        for (const entriesToRemove of action.keysToRemove) {
+          delete stateCopy[entriesToRemove];
+        }
+        break;
+      case 'clear':
+        for (const entriesToRemove in stateCopy) {
+          delete stateCopy[entriesToRemove];
+        }
+        break;
+    }
+    stateLogs.push({ ...stateCopy });
+  }
+
+  return stateLogs;
 }
 
 module.exports = transformStateWithClones;
