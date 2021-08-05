11e4637c9e99820986a43d2b9047c4d3467924a7
Oleksandr Strilchuk <virass.m@gmail.com>
2021-08-05 14:20:09 +0300

    Task done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let newState = { ...state };
+  const changeListArray = [];
+
+  for (const key of actions) {
+    switch (key.type) {
+      case 'clear':
+        newState = {};
+        break;
+
+      case 'addProperties':
+        Object.assign(newState, key.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const arr of key.keysToRemove) {
+          if (arr in newState) {
+            delete newState[arr];
+          }
+        }
+    }
+    changeListArray.push({ ...newState });
+  }
+
+  return changeListArray;
 }
 
 module.exports = transformStateWithClones;
