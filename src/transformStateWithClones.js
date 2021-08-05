96f7de132c2a63e959e9783f7cdfae053f84899e
Oleksandr Makohon <sashamakogon94@gmail.com>
2021-08-05 13:55:51 +0300

    done state clone

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const copy = Object.assign({}, state);
+  const allVersions = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(copy, action.extraData);
+        allVersions.push({ ...copy });
+        break;
+
+      case 'removeProperties':
+        for (const element of action.keysToRemove) {
+          if (element in copy) {
+            delete copy[element];
+          }
+        }
+        allVersions.push({ ...copy });
+        break;
+
+      case 'clear' :
+        for (const key in copy) {
+          delete copy[key];
+        }
+        allVersions.push({ ...copy });
+    }
+  }
+
+  return allVersions;
 }
 
 module.exports = transformStateWithClones;
