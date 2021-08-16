7e45a617dcf25026350903f9d346188c501f1f0f
Illia Ostrovskiy <aleanrenuar@gmail.com>
2021-08-16 23:37:42 +0300

    Initial commit

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,41 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateReport = [];
+  const currentState = { ...state };
+
+  for (const i in actions) {
+    if (actions[i].type === 'clear') {
+      for (const key in currentState) {
+        delete currentState[key];
+      }
+
+      stateReport.push({});
+      continue;
+    }
+
+    if (actions[i].type === 'addProperties') {
+      for (const key in actions[i].extraData) {
+        currentState[key] = actions[i].extraData[key];
+      }
+
+      stateReport.push({ ...currentState });
+      continue;
+    }
+
+    if (actions[i].type === 'removeProperties') {
+      for (const key in actions[i].keysToRemove) {
+        if (currentState[actions[i].keysToRemove[key]]) {
+          delete currentState[actions[i].keysToRemove[key]];
+        }
+      }
+
+      stateReport.push({ ...currentState });
+      continue;
+    }
+  }
+
+  return stateReport;
 }
 
 module.exports = transformStateWithClones;
