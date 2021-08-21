31402bcb56b6ccef155f50b5f7357e7e6d28b481
Dmytro Semenenko <dmitriy.semenenko@gmail.com>
2021-08-21 12:20:41 +0300

    Solution2 js-stateful-clones ver. with switch

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,29 +11,32 @@ function transformStateWithClones(state, actions) {
   const state2 = [];
   let obj = { ...state };
   let obj1 = {};
-  let obj2 = {};
-  let obj3 = {};
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      obj = {
-        ...obj,
-        ...action.extraData,
-      };
-      obj1 = { ...obj };
-      state2.push(obj1);
-    } else if (action.type === 'removeProperties') {
-      for (const prop of action.keysToRemove) {
-        delete obj[prop];
-      }
-      obj2 = { ...obj };
-      state2.push(obj2);
-    } else if (action.type === 'clear') {
-      for (const key in obj) {
-        delete obj[key];
-      }
-      obj3 = { ...obj };
-      state2.push(obj3);
+    switch (action.type) {
+      case 'addProperties':
+        obj = {
+          ...obj,
+          ...action.extraData,
+        };
+        obj1 = { ...obj };
+        state2.push(obj1);
+        break;
+
+      case 'removeProperties':
+        for (const prop of action.keysToRemove) {
+          delete obj[prop];
+        };
+        obj1 = { ...obj };
+        state2.push(obj1);
+        break;
+
+      case 'clear':
+        for (const key in obj) {
+          delete obj[key];
+        };
+        obj1 = { ...obj };
+        state2.push(obj1);
     }
   }
 
