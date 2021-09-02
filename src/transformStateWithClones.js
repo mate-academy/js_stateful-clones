a0ab2d93986fa79f5f84439d59a72fb25d36e35b
Serhii Dvorets <dvorets.sergey@gmail.com>
2021-09-02 10:08:58 +0300

    1

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,43 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        if (result.length > 0) {
+          result.push(Object.assign({},
+            result[result.length - 1],
+            action.extraData));
+          break;
+        }
+        result.push(Object.assign({}, state, action.extraData));
+        break;
+
+      case 'removeProperties':
+        if (result.length > 0) {
+          result.push(Object.assign({}, result[result.length - 1]));
+
+          for (const key of action.keysToRemove) {
+            delete result[result.length - 1][key];
+          }
+          break;
+        }
+        result.push(Object.assign({}, state));
+
+        for (const key of action.keysToRemove) {
+          delete result[result.length - 1][key];
+        }
+        break;
+
+      case `clear`:
+        result.push({});
+        break;
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
