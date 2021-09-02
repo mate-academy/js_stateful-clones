0f4b853e3d6795a32e467f10e0b0cc48f6a520f4
Rak Oleg <rakole86@gmail.com>
2021-09-02 14:56:37 +0300

    js_stateful-clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,40 @@
 'use strict';
 
 /**
- * @param {Object} state
+ * @param {Object} clone
  * @param {Object[]} actions
  *
  * @return {Object[]}
  */
-function transformStateWithClones(state, actions) {
-  // write code here
+function transformcloneWithClones(state, actions) {
+  const arr = [];
+  const clone = Object.assign({}, state);
+
+  for (const actionKey of actions) {
+    for (const key in actionKey) {
+      switch (actionKey[key]) {
+        case ('addProperties'):
+          Object.assign(clone, actionKey.extraData);
+
+          break;
+
+        case ('removeProperties') :
+          for (const prop of actionKey.keysToRemove) {
+            delete clone[prop];
+          }
+          break;
+
+        case ('clear'):
+          for (const cloneKey in clone) {
+            delete clone[cloneKey];
+          }
+          break;
+      }
+    }
+    arr.push({ ...clone });
+  }
+
+  return arr;
 }
 
-module.exports = transformStateWithClones;
+module.exports = transformcloneWithClones;
