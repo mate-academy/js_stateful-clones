882886f64c0f77e3b8539b42e18e60881c338095
Liliia Kuleba <kuleba.lilia.1872003@gmail.com>
2021-09-15 23:47:56 +0300

    clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arrayResult = [];
+  const dynamicObject = { ...state };
+
+  for (const i of actions) {
+    if (i.type === 'addProperties') {
+      Object.assign(dynamicObject, i.extraData);
+    }
+
+    if (i.type === 'removeProperties') {
+      for (const j of i.keysToRemove) {
+        delete dynamicObject[j];
+      }
+    }
+
+    if (i.type === 'clear') {
+      for (const a in dynamicObject) {
+        delete dynamicObject[a];
+      }
+    }
+
+    arrayResult.push({ ...dynamicObject });
+  }
+
+  return arrayResult;
 }
 
 module.exports = transformStateWithClones;
