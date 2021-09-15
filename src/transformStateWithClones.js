7b9bb51b17df2046e483d2829bb32437c47d23f4
Vitalii Strohush <vitalikstrog2003v@gmail.com>
2021-09-15 22:50:56 +0300

    transformStateWithClone solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,32 @@
  *
  * @return {Object[]}
  */
+// eslint-disable-next-line no-shadow
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arr = [];
+  const obj = { ...state };
+
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'addProperties') {
+      Object.assign(obj, actions[i].extraData);
+    }
+
+    if (actions[i].type === 'removeProperties') {
+      for (const c of actions[i].keysToRemove) {
+        delete obj[c];
+      }
+    }
+
+    if (actions[i].type === 'clear') {
+      for (const key in obj) {
+        delete obj[key];
+      }
+    }
+
+    arr.push({ ...obj });
+  }
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
