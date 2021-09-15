ee0348b671ad1b1b9f0bb6d216063091ec0116b7
Vladyslav Banul <vladyslav.banul@gmail.com>
2021-09-15 21:19:21 +0300

    aboba16

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const clone = [];
+
+  const buf = { ...state };
+
+  for (let i = 0; i < actions.length; i++) {
+    switch (actions[i].type) {
+      case 'addProperties':
+        Object.assign(buf, actions[i].extraData);
+        clone.push({ ...buf });
+        break;
+      case 'removeProperties':
+        for (const key in actions[i].keysToRemove) {
+          delete buf[actions[i].keysToRemove[key]];
+        }
+        clone.push({ ...buf });
+        break;
+      case 'clear':
+        for (const key in buf) {
+          delete buf[key];
+        }
+        clone.push({ ...buf });
+        break;
+    }
+  }
+
+  return clone;
 }
 
 module.exports = transformStateWithClones;
