14d4a961484d48bc3b8c2a592161ac9281a2611d
Roman Toritsyn <romantoritsyn@gmail.com>
2021-09-16 18:08:17 +0300

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let newState = Object.assign({}, state);
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(newState, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const whitchKey of action.keysToRemove) {
+        delete newState[whitchKey];
+      }
+    }
+
+    if (action.type === 'clear') {
+      for (const key in newState) {
+        delete newState[key];
+      }
+    }
+    result.push(newState);
+    newState = Object.assign({}, newState);
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
