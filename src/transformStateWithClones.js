bf6cc053455fbcee28f2f9449384e79253ab2854
anastasiia <theyashkindzhus@gmail.com>
2021-09-16 09:51:24 +0300

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,30 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const clone = { ...state };
+  const resArr = [];
+
+  for (const key of actions) {
+    if (key.type === 'addProperties') {
+      Object.assign(clone, key.extraData);
+    }
+
+    if (key.type === 'clear') {
+      for (const stateKey in clone) {
+        delete clone[stateKey];
+      }
+    }
+
+    if (key.type === 'removeProperties') {
+      for (const removeKey of key.keysToRemove) {
+        delete clone[removeKey];
+      }
+    }
+
+    resArr.push({ ...clone });
+  }
+
+  return resArr;
 }
 
 module.exports = transformStateWithClones;
