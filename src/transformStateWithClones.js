b4e1d94f6ad0515e2b791b83830708b6f4aabc4b
Mariia Torkaienko <mary_dob@hotmail.com>
2021-09-16 16:03:08 +0300

    Stateful clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,28 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const allCloneStates = [];
+  let cloneState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(cloneState, action.extraData);
+        break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete cloneState[key];
+        }
+        break;
+      case 'clear':
+        cloneState = {};
+        break;
+    }
+
+    allCloneStates.push({ ...cloneState });
+  }
+
+  return allCloneStates;
 }
 
 module.exports = transformStateWithClones;
