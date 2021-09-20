1d686e93c174d0a5061939d01e59790a82cc9252
Michael Strazhevski <work.acc.com@gmail.com>
2021-09-20 13:22:22 +0300

    Made over on switch\case

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,21 +7,47 @@
  * @return {Object[]}
  */
 
+// function transformStateWithClones1(state, actions) {
+//   const states = [];
+//   const stateCopy = { ...state };
+
+//   for (const action of actions) {
+//     if (action.type === 'addProperties') {
+//       Object.assign(stateCopy, action.extraData);
+//     } else if (action.type === 'removeProperties') {
+//       for (const key of action.keysToRemove) {
+//         delete stateCopy[key];
+//       }
+//     } else if (action.type === 'clear') {
+//       for (const prop in stateCopy) {
+//         delete stateCopy[prop];
+//       }
+//     }
+//     states.push({ ...stateCopy });
+//   }
+
+//   return states;
+// }
+
 function transformStateWithClones(state, actions) {
   const states = [];
   const stateCopy = { ...state };
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(stateCopy, action.extraData);
-    } else if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete stateCopy[key];
-      }
-    } else if (action.type === 'clear') {
-      for (const prop in stateCopy) {
-        delete stateCopy[prop];
-      }
+    switch (true) {
+      case (action.type === 'addProperties'):
+        Object.assign(stateCopy, action.extraData);
+        break;
+      case (action.type === 'removeProperties'):
+        for (const key of action.keysToRemove) {
+          delete stateCopy[key];
+        };
+        break;
+      case (action.type === 'clear'):
+        for (const prop in stateCopy) {
+          delete stateCopy[prop];
+        };
+        break;
     }
     states.push({ ...stateCopy });
   }
