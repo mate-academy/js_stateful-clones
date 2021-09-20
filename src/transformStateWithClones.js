a14e60d5879af5e8243affa54fabf7d6c0ab39a4
Michael Strazhevski <work.acc.com@gmail.com>
2021-09-20 13:24:03 +0300

    Switch\case clean code

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,28 +7,6 @@
  * @return {Object[]}
  */
 
-// function transformStateWithClones1(state, actions) {
-//   const states = [];
-//   const stateCopy = { ...state };
-
-//   for (const action of actions) {
-//     if (action.type === 'addProperties') {
-//       Object.assign(stateCopy, action.extraData);
-//     } else if (action.type === 'removeProperties') {
-//       for (const key of action.keysToRemove) {
-//         delete stateCopy[key];
-//       }
-//     } else if (action.type === 'clear') {
-//       for (const prop in stateCopy) {
-//         delete stateCopy[prop];
-//       }
-//     }
-//     states.push({ ...stateCopy });
-//   }
-
-//   return states;
-// }
-
 function transformStateWithClones(state, actions) {
   const states = [];
   const stateCopy = { ...state };
