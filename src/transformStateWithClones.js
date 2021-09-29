56a9e6da17ec71b43b6217cdb51b82ab0db124b1
Vladyslav <vladyslav3177@gmail.com>
2021-09-29 17:47:17 +0300

    switch case

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,16 +11,25 @@ function transformStateWithClones(state, actions) {
   const result = [];
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(copyState, action.extraData);
-    } else if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete copyState[key];
-      }
-    } else if (action.type === 'clear') {
-      for (const key in copyState) {
-        delete copyState[key];
-      }
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(copyState, action.extraData);
+
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete copyState[key];
+        }
+
+        break;
+
+      case 'clear':
+        for (const key in copyState) {
+          delete copyState[key];
+        }
+
+        break;
     }
 
     result.push({ ...copyState });
