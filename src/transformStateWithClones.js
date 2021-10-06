b90c915f5705cd6d8108537f3c95c9d1e646b6e6
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2021-10-06 15:16:32 +0300

    rewrote to switch

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,35 +6,75 @@
  *
  * @return {Object[]}
  */
+// function transformStateWithClones(state, actions) {
+//   const versionsOfState = [];
+//   const copyState = { ...state };
+
+//   for (const obj of actions) {
+//     for (const key in obj) {
+//       if (key === 'extraData') {
+//         for (const keyIn in obj[key]) {
+//           copyState[keyIn] = obj[key][keyIn];
+//         }
+
+//         versionsOfState.push({ ...copyState });
+//       }
+
+//       if (key === 'keysToRemove') {
+//         for (const keyIn of obj[key]) {
+//           delete copyState[keyIn];
+//         }
+
+//         versionsOfState.push({ ...copyState });
+//       }
+
+//       if (key === 'type' && obj[key] === 'clear') {
+//         for (const keyIn in copyState) {
+//           delete copyState[keyIn];
+//         }
+
+//         versionsOfState.push({ ...copyState });
+//       }
+//     }
+//   }
+
+//   return versionsOfState;
+// }
+
 function transformStateWithClones(state, actions) {
   const versionsOfState = [];
   const copyState = { ...state };
 
-  for (const obj of actions) {
-    for (const key in obj) {
-      if (key === 'extraData') {
-        for (const keyIn in obj[key]) {
-          copyState[keyIn] = obj[key][keyIn];
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        for (const keyToAdd in action.extraData) {
+          copyState[keyToAdd] = action.extraData[keyToAdd];
         }
 
         versionsOfState.push({ ...copyState });
-      }
 
-      if (key === 'keysToRemove') {
-        for (const keyIn of obj[key]) {
-          delete copyState[keyIn];
+        break;
+
+      case 'removeProperties':
+        for (const keyToRemove of action.keysToRemove) {
+          delete copyState[keyToRemove];
         }
 
         versionsOfState.push({ ...copyState });
-      }
 
-      if (key === 'type' && obj[key] === 'clear') {
-        for (const keyIn in copyState) {
-          delete copyState[keyIn];
+        break;
+
+      case 'clear':
+        for (const key in copyState) {
+          delete copyState[key];
         }
 
         versionsOfState.push({ ...copyState });
-      }
+
+        break;
+
+      default:
     }
   }
 
