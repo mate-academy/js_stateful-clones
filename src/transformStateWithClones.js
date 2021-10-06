acdd590320dbd31eb6a408755aed83c76f83912a
Volodymyr Taranin <jaffplay@gmail.com>
2021-10-06 23:40:56 +0300

    add variables

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const getObjectCopy = { ...state };
+  const result = [];
+
+  for (let objectIndex = 0; objectIndex < actions.length; objectIndex++) {
+    const getArraysObject = actions[objectIndex];
+
+    if (actions[objectIndex].type === 'addProperties') {
+      for (const key in getArraysObject.extraData) {
+        getObjectCopy[key] = getArraysObject.extraData[key];
+      }
+      result.push({ ...getObjectCopy });
+    } else if (getArraysObject.type === 'removeProperties') {
+      for (let keyIndex = 0; keyIndex < getArraysObject.keysToRemove.length;
+        keyIndex++) {
+        delete getObjectCopy[getArraysObject.keysToRemove[keyIndex]];
+      }
+      result.push({ ...getObjectCopy });
+    } else if (getArraysObject.type === 'clear') {
+      for (const key in getObjectCopy) {
+        delete getObjectCopy[key];
+      }
+      result.push({ ...getObjectCopy });
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
