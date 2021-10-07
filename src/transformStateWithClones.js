cd67f01c0cd6da7d78727513989610944a32eb60
Max <gorodtira51@gmail.com>
2021-10-07 16:16:10 +0300

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,35 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const stateClone = { ...state };
+  const stateChangeLog = [];
+
+  for (const key of actions) {
+    const { type, extraData, keysToRemove } = key;
+
+    switch (type) {
+      case 'addProperties':
+        Object.assign(stateClone, extraData);
+        stateChangeLog.push({ ...stateClone });
+        break;
+      case 'removeProperties':
+        for (const item of keysToRemove) {
+          delete stateClone[item];
+        }
+        stateChangeLog.push({ ...stateClone });
+        break;
+      case 'clear':
+        for (const item in stateClone) {
+          delete stateClone[item];
+        }
+        stateChangeLog.push({ ...stateClone });
+        break;
+      default:
+        return 'Error';
+    }
+  }
+
+  return stateChangeLog;
 }
 
 module.exports = transformStateWithClones;
