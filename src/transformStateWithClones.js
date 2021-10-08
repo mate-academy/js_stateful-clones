d5e857ba68a8b1856f8860e963bd091c575a80a1
Volodymyr Taranin <jaffplay@gmail.com>
2021-10-08 17:46:19 +0300

    replace push after switch/case block

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -16,26 +16,24 @@ function transformStateWithClones(state, actions) {
         for (const key in actionObject.extraData) {
           getObjectCopy[key] = actionObject.extraData[key];
         }
-        result.push({ ...getObjectCopy });
         break;
 
       case 'removeProperties':
         for (const propertyForRemove of actionObject.keysToRemove) {
           delete getObjectCopy[propertyForRemove];
         }
-        result.push({ ...getObjectCopy });
         break;
 
       case 'clear':
         for (const key in getObjectCopy) {
           delete getObjectCopy[key];
         }
-        result.push({ ...getObjectCopy });
         break;
 
       default:
         return 'Error';
     }
+    result.push({ ...getObjectCopy });
   }
 
   return result;
