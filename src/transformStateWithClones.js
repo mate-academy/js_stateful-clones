a94eb2742c44e661cd20bc952ae10a4350a065c7
Vitaly <vitaly.pasechnik.11@gmail.com>
2021-10-12 15:26:36 +0300

    last saved 12/10

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,37 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arr = [];
+
+  for (const i of actions) {
+    let item = {};
+
+    if (arr[0] === undefined) {
+      item = { ...state };
+    } else {
+      item = { ...arr[arr.length - 1] };
+    };
+
+    if (i.type === 'addProperties') {
+      Object.assign(item, i.extraData);
+    }
+
+    if (i.type === 'removeProperties') {
+      for (const x in i.keysToRemove) {
+        delete item[i.keysToRemove[x]];
+      }
+    }
+
+    if (i.type === 'clear') {
+      for (const x in item) {
+        delete item[x];
+      }
+    }
+
+    arr.push(item);
+  }
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
