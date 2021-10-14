3b4e8963ee55d1881592da318e9e23fae9c5d844
Artem Motalov <artemmmmmotalov@gmail.com>
2021-10-14 19:16:28 +0300

    2nd commit - remove comments

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arrayResult = [];
+  let stateNew = { ...state };
+  let stateTemp = {};
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      stateNew = Object.assign(stateNew, action.extraData);
+    } else if (action.type === 'removeProperties') {
+      for (const keyRemoved of action.keysToRemove) {
+        delete stateNew[keyRemoved];
+      }
+    } else if (action.type === 'clear') {
+      for (const keyCleared in stateNew) {
+        delete stateNew[keyCleared];
+      }
+    } else {
+      continue;
+    }
+
+    stateTemp = { ...stateNew };
+    arrayResult.push(stateTemp);
+  }
+
+  return arrayResult;
 }
 
 module.exports = transformStateWithClones;
