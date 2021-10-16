b7cedaa787e468a60c49c174d120bab8be9d1057
Oleg Kozak <kozakoleg77@ukr.net>
2021-10-16 17:45:13 +0300

    Fixes after code review

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,32 +8,27 @@
  */
 function transformStateWithClones(state, actions) {
   const arr = [];
-  let i = 0;
+  const copy = { ...state };
 
   for (const key of actions) {
-    const copy = {};
-
-    if (i === 0) {
-      Object.assign(copy, state);
-    } else {
-      Object.assign(copy, arr[i - 1]);
-    }
-
-    for (const lit in key) {
-      if (key[lit] === 'addProperties') {
+    switch (key.type) {
+      case 'addProperties':
         Object.assign(copy, key.extraData);
-      } else if (key[lit] === 'removeProperties') {
+        break;
+
+      case 'removeProperties':
         for (const y of key.keysToRemove) {
           delete copy[y];
         }
-      } else if (key[lit] === 'clear') {
+        break;
+
+      case 'clear':
         for (const part in copy) {
           delete copy[part];
         }
-      }
+        break;
     }
-    arr.push(copy);
-    i++;
+    arr.push({ ...copy });
   }
 
   return arr;
