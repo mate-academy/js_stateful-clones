e91d338d3b917ace53ae961612c9161f5c9260d6
Dmytro Hubych <dh.hubych@gmail.com>
2021-10-19 17:11:07 +0300

    solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -12,31 +12,24 @@ function transformStateWithClones(state, actions) {
   const stateModified = [];
 
   for (const index of actions) {
-    if (index.type === 'addProperties') {
-      const localTemporaryObject = { ...objectKeeper };
-
-      for (const key in index.extraData) {
-        localTemporaryObject[key] = index.extraData[key];
-      }
-
-      objectKeeper = { ...localTemporaryObject };
-      stateModified.push(localTemporaryObject);
+    switch (index.type) {
+      case 'addProperties':
+        for (const key in index.extraData) {
+          objectKeeper[key] = index.extraData[key];
+        }
+        break;
+      case 'removeProperties':
+        for (const key of index.keysToRemove) {
+          delete objectKeeper[key];
+        }
+        break;
+      case 'clear':
+        objectKeeper = {};
+        break;
+      default:
     }
 
-    if (index.type === 'removeProperties') {
-      for (const key of index.keysToRemove) {
-        delete objectKeeper[key];
-      }
-
-      const localTemporaryObject = { ...objectKeeper };
-
-      stateModified.push(localTemporaryObject);
-    }
-
-    if (index.type === 'clear') {
-      objectKeeper = {};
-      stateModified.push({ ...objectKeeper });
-    }
+    stateModified.push({ ...objectKeeper });
   }
 
   return stateModified;
