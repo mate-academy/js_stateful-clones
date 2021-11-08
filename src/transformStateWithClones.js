4f0e2ac6c72fda04472f0a8bbc03a1bea2a54aec
Mira Tyshchenko <tyshchenkomira@gmail.com>
2021-11-08 23:26:54 +0200

    implemented task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newState = { ...state };
+  const result = [];
+
+  for (const obj of actions) {
+    if (obj.type === 'addProperties') {
+      Object.assign(newState, obj.extraData);
+      result.push({ ...newState });
+    }
+
+    if (obj.type === 'removeProperties') {
+      for (const key of obj.keysToRemove) {
+        if (newState[key] !== undefined) {
+          delete newState[key];
+        }
+      }
+      result.push({ ...newState });
+    }
+
+    if (obj.type === 'clear') {
+      for (const key in newState) {
+        delete newState[key];
+      }
+      result.push({});
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
