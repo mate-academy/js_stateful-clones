a82a8f86c051f476a40047f022489e583f4de937
TatianaMorozova04 <morozovatatiana04@gmail.com>
2021-11-09 17:29:05 +0200

    transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,41 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const allStateVersions = [];
+  const newState = { ...state };
+
+  for (const obj in actions) {
+    const type = actions[obj].type;
+
+    if (type === 'addProperties') {
+      const addProp = actions[obj].extraData;
+
+      for (const prop in addProp) {
+        newState[prop] = addProp[prop];
+      }
+      allStateVersions.push({ ...newState });
+    }
+
+    if (type === 'removeProperties') {
+      const keys = actions[obj].keysToRemove;
+
+      for (const key in keys) {
+        delete newState[keys[key]];
+      }
+      allStateVersions.push({ ...newState });
+    }
+
+    if (type === 'clear') {
+      for (const prop in newState) {
+        delete newState[prop];
+      }
+      allStateVersions.push({ ...newState });
+    }
+  }
+
+  return allStateVersions;
 }
 
 module.exports = transformStateWithClones;
