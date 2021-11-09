e43548e27958809029c9e84f83809fc14b57546c
Yevhen Khmelnytskyi <summerssscrystal@gmail.com>
2021-11-09 08:26:10 +0200

    task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,58 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let newObj;
+  let count = 0;
+
+  for (const obj of actions) {
+    for (const key in obj) {
+      if (key === 'type' && obj[key] === 'addProperties') {
+        if (count === 0) {
+          newObj = [{}];
+          Object.assign(newObj[0], state);
+          Object.assign(newObj[count], obj.extraData);
+          count++;
+        } else {
+          newObj.push({});
+          Object.assign(newObj[count], newObj[count - 1]);
+          Object.assign(newObj[count], obj.extraData);
+          count++;
+        }
+      }
+
+      if (key === 'type' && obj[key] === 'removeProperties') {
+        if (count === 0) {
+          newObj = [{}];
+          Object.assign(newObj[0], state);
+        } else {
+          newObj.push({});
+          Object.assign(newObj[count], newObj[count - 1]);
+        }
+
+        for (const props of obj.keysToRemove) {
+          delete newObj[count][props];
+        }
+        count++;
+      }
+
+      if (key === 'type' && obj[key] === 'clear') {
+        if (count === 0) {
+          newObj = [{}];
+          Object.assign(newObj[0], state);
+        } else {
+          newObj.push({});
+          Object.assign(newObj[count], newObj[count - 1]);
+        }
+
+        for (const keys in newObj[count]) {
+          delete newObj[count][keys];
+        }
+        count++;
+      }
+    }
+  }
+
+  return newObj;
 }
 
 module.exports = transformStateWithClones;
