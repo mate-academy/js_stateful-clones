9a880a2664d816899e532fb6afed7268b60f91a9
Ihor Poliakov <junglqq@gmail.com>
2021-11-09 16:42:58 +0200

    init

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const obj = { ...state };
+  const arr = [];
+
+  for (const key of actions) {
+    if (key.type === 'addProperties') {
+      for (const add in key.extraData) {
+        obj[add] = key.extraData[add];
+      }
+      arr.push({ ...obj });
+    }
+
+    if (key.type === 'clear') {
+      for (const del in obj) {
+        delete obj[del];
+      }
+      arr.push({});
+    }
+
+    if (key.type === 'removeProperties') {
+      for (const remove of key.keysToRemove) {
+        delete obj[remove];
+      }
+      arr.push({ ...obj });
+    }
+  }
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
