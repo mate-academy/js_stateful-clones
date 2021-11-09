3732bc0e5b0654f55e3f9dcd9196da16d9a5ddfc
Dariia Romanova <dariia.romanova@gmail.com>
2021-11-09 11:52:36 +0200

    Switch was used

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,17 +8,25 @@
  */
 function transformStateWithClones(state, actions) {
   const allStates = [];
-  let currentState = { ...state };
+  const currentState = { ...state };
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(currentState, action.extraData);
-    } else if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete currentState[key];
-      }
-    } else if (action.type === 'clear') {
-      currentState = {};
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(currentState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete currentState[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in currentState) {
+          delete currentState[key];
+        }
+        break;
     }
 
     allStates.push({ ...currentState });
