d5e7cfb0d4f92031f2ef193014498afd43b65311
Mariia Bila <freiheit.maria@gmail.com>
2021-11-09 12:38:41 +0200

    switch case was implemented

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -10,15 +10,18 @@ function transformStateWithClones(state, actions) {
   const transformHistory = [];
   let transformStep = { ...state };
 
-  for (const element of actions) {
-    if (element.type === 'addProperties') {
-      Object.assign(transformStep, element.extraData);
-    } else if (element.type === 'removeProperties') {
-      for (const removeKeys of element.keysToRemove) {
-        delete transformStep[removeKeys];
-      }
-    } else if (element.type === 'clear') {
-      transformStep = {};
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(transformStep, action.extraData);
+        break;
+      case 'removeProperties':
+        for (const removeKeys of action.keysToRemove) {
+          delete transformStep[removeKeys];
+        };
+        break;
+      case 'clear':
+        transformStep = {};
     }
     transformHistory.push({ ...transformStep });
   }
