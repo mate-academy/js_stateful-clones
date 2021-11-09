29214b5e96f8909c5465163aeb8603327312c8da
Lera Permiakova <vv.permiakova@gmail.com>
2021-11-10 00:24:06 +0200

    transform

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newObj = Object.assign({}, state);
+  const resArr = [];
+
+  for (const prop of actions) {
+    if (prop.type === 'addProperties') {
+      Object.assign(newObj, prop.extraData);
+    }
+
+    if (prop.type === 'removeProperties') {
+      for (const rem of prop.keysToRemove) {
+        for (const key in newObj) {
+          if (key === rem) {
+            delete newObj[key];
+          }
+        }
+      }
+    }
+
+    if (prop.type === 'clear') {
+      for (const key in newObj) {
+        delete newObj[key];
+      }
+    }
+
+    resArr.push({ ...newObj });
+  }
+
+  return resArr;
 }
 
 module.exports = transformStateWithClones;
