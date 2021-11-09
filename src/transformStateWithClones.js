687b4f7cd1b716a7f920936a8068f55f80bb4a6b
Oleksiy Pochkayev <oleksiypochkayev@gmail.com>
2021-11-09 15:48:36 +0200

    solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,28 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let transormItem = { ...state };
+
+  for (const i of actions) {
+    if (i.type === 'addProperties') {
+      transormItem = ({
+        ...transormItem, ...i.extraData,
+      });
+    } else if (i.type === 'removeProperties') {
+      for (const key of i.keysToRemove) {
+        delete transormItem[key];
+      }
+    } else if (i.type === 'clear') {
+      transormItem = {};
+    }
+
+    result.push({ ...transormItem });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
