17be30b8b1b9b3e0295678a474e716228109b074
Skidan Alexander(notebook) <xpromt@gmail.com>
2021-11-18 14:00:03 +0200

    result

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,29 @@
  *
  * @return {Object[]}
  */
+
+// write code here
 function transformStateWithClones(state, actions) {
-  // write code here
+  const resAction = [];
+  let tempState = { ...state };
+
+  for (const act of actions) {
+    switch (act.type) {
+      case 'addProperties':
+        Object.assign(tempState, act.extraData);
+        break;
+      case 'removeProperties':
+        for (const removeKeys of act.keysToRemove) {
+          delete tempState[removeKeys];
+        };
+        break;
+      case 'clear':
+        tempState = {};
+    }
+    resAction.push({ ...tempState });
+  }
+
+  return resAction;
 }
 
 module.exports = transformStateWithClones;
