69c0383ac5e88d15f7b5c0de309b34502fde40e2
Vova Alputov <alputov1998@gmail.com>
2021-11-22 19:40:37 +0200

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [{ ...state }];
+
+  for (const obj in actions) {
+    const { type, extraData, keysToRemove } = actions[obj];
+    let count = 0;
+    const arr = result[count];
+
+    if (type === 'addProperties') {
+      Object.assign(arr, extraData);
+    } else if (type === 'removeProperties') {
+      if (keysToRemove.length > 1) {
+        for (const key of keysToRemove) {
+          delete arr[key];
+        }
+      } else {
+        delete arr[keysToRemove];
+      }
+    } else if (type === 'clear') {
+      for (const key in arr) {
+        delete arr[key];
+      }
+    }
+    result.push({ ...arr });
+    count++;
+  }
+  result.shift();
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
