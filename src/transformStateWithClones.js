b97e07b7e5ed7358174f146783f7065151144593
Ihor Barban <barbanihor@gmial.com>
2021-11-23 20:33:53 +0200

    I do not understand where the error is

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arr = [];
+  const objToPushArr = { ...state };
+
+  for (const obj of actions) {
+    if (obj['type'] === 'clear') {
+      for (const key in objToPushArr) {
+        delete objToPushArr[key];
+      }
+      arr.push(objToPushArr);
+    } else if (obj['type'] === 'addProperties') {
+      Object.assign(objToPushArr, obj.extraData);
+      arr.push(objToPushArr);
+    } else if (obj['type'] === 'removeProperties') {
+      for (const key of obj['keysToRemove']) {
+        if (key in objToPushArr) {
+          delete objToPushArr[key];
+        }
+      }
+      arr.push(objToPushArr);
+    }
+  }
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
