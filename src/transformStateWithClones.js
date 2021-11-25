b8ae62921ef92d98d569208d31a9c33472d54267
Vova Alputov <alputov1998@gmail.com>
2021-11-25 20:58:53 +0200

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -9,32 +9,40 @@
 function transformStateWithClones(state, actions) {
   const result = [{ ...state }];
 
-  for (const obj in actions) {
-    const { type, extraData, keysToRemove } = actions[obj];
-    let count = 0;
-    const arr = result[count];
+  for (let i = 0; i < actions.length; i++) {
 
-    if (type === 'addProperties') {
-      Object.assign(arr, extraData);
-    } else if (type === 'removeProperties') {
-      if (keysToRemove.length > 1) {
-        for (const key of keysToRemove) {
-          delete arr[key];
-        }
-      } else {
-        delete arr[keysToRemove];
+    for (const action of actions) {
+      const { type, extraData, keysToRemove } = action;
+      const arr = result[i];
+
+      switch (type) {
+        case 'addProperties':
+          Object.assign(arr, extraData);
+          break;
+
+        case 'removeProperties':
+          if (keysToRemove.length > 1) {
+            for (const key of keysToRemove) {
+              delete arr[key];
+            }
+          } else {
+            delete arr[keysToRemove];
+          }
+          break;
+
+        case 'clear':
+          for (const key in arr) {
+            delete arr[key];
+          }
+          break;
       }
-    } else if (type === 'clear') {
-      for (const key in arr) {
-        delete arr[key];
-      }
+      result.push({ ...arr });
     }
-    result.push({ ...arr });
-    count++;
-  }
-  result.shift();
 
-  return result;
+    result.shift();
+
+    return result;
+  }
 }
 
 module.exports = transformStateWithClones;
