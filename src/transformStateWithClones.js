f43a41c7297bf8ba5fcd709aa3ab32e3ed4ae9e2
Andrey Aleksandrov <a.s.aleksandrov89@gmail.com>
2021-11-26 10:00:27 +0100

    Create state with clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateArray = [];
+  const tempState = {};
+  // copy state object to new temporary object
+
+  for (const item in state) {
+    tempState[item] = state[item];
+  }
+
+  for (const action of actions) {
+    switch (action['type']) {
+      case 'clear':
+        for (const key in tempState) {
+          delete tempState[key];
+        }
+        break;
+      case 'addProperties':
+        Object.assign(tempState, action['extraData']);
+        break;
+      case 'removeProperties':
+        for (const key of action['keysToRemove']) {
+          delete tempState[key];
+        }
+        break;
+      default: break;
+    }
+
+    stateArray.push(Object.assign({}, tempState));
+  }
+
+  return stateArray;
 }
 
 module.exports = transformStateWithClones;
