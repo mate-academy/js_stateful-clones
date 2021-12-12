5ff37f1d82d4f19315496367fadd730010911d79
Oleksandr Leshchenko <bulochkin72@gmail.com>
2021-12-12 18:09:25 +0200

    commit added

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const obj = { ...state };
+  const arr = [];
+
+  for (let i = 0; i < actions.length; i++) {
+    for (const key in actions[i]) {
+      if (actions[i][key] === 'removeProperties') {
+        for (let j = 0; j < actions[i].keysToRemove.length; j++) {
+          delete obj[actions[i].keysToRemove[j]];
+        }
+        arr[i] = { ...obj };
+      }
+
+      if (actions[i][key] === 'addProperties') {
+        Object.assign(obj, actions[i].extraData);
+        arr[i] = { ...obj };
+      }
+
+      if (actions[i][key] === 'clear') {
+        for (const item in obj) {
+          delete obj[item];
+        }
+        arr[i] = { ...obj };
+      }
+    }
+  }
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
