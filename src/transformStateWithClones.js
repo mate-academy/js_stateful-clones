bd1ad9fa64a4c007c429be29210df04adb9f7814
Niki <tes.kolia@gmail.com>
2021-12-13 20:55:38 +0200

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,36 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const clone = { ...state };
+  const arrClone = [];
+
+  for (const obj of actions) {
+    if (obj.type === 'addProperties') {
+      Object.assign(clone, obj.extraData);
+
+      const tempCLone = { ...clone };
+
+      arrClone.push(tempCLone);
+    } else if (obj.type === 'removeProperties') {
+      for (const delKey of obj.keysToRemove) {
+        delete clone[delKey];
+      }
+
+      const tempCLone = { ...clone };
+
+      arrClone.push(tempCLone);
+    } else if (obj.type === 'clear') {
+      for (const x in clone) {
+        delete clone[x];
+      }
+
+      const tempCLone = { ...clone };
+
+      arrClone.push(tempCLone);
+    }
+  }
+
+  return arrClone;
 }
 
 module.exports = transformStateWithClones;
