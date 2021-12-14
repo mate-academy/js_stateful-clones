af3ac87c348562dd30d69a32042aa7ebc63961ac
Artem <tema.maksimovich@gmail.com>
2021-12-14 19:03:34 +0200

    Task is done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const listOfStates = [];
+  const copyState = { ...state };
+
+  actions.forEach((elem) => {
+    if (elem.type === 'addProperties') {
+      Object.assign(copyState, elem.extraData);
+      listOfStates.push({ ...copyState });
+    }
+
+    if (elem.type === 'removeProperties') {
+      for (const key of elem.keysToRemove) {
+        delete copyState[key];
+      }
+      listOfStates.push({ ...copyState });
+    }
+
+    if (elem.type === 'clear') {
+      for (const key in copyState) {
+        delete copyState[key];
+      }
+      listOfStates.push({ ...copyState });
+    }
+  });
+
+  return listOfStates;
 }
 
 module.exports = transformStateWithClones;
