d13eefade34d1a1c9d4ff55a560b680757c146d0
izzilowe <izzilowe7@gmail.com>
2021-12-14 18:17:05 +0200

    first ry

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const stateCopy = { ...state };
+
+  for (const el of actions) {
+    if (el.type === 'addProperties') {
+      Object.assign(stateCopy, el.extraData);
+    }
+
+    if (el.type === 'removeProperties') {
+      for (const item of el.keysToRemove) {
+        delete stateCopy[item];
+      }
+    }
+
+    if (el.type === 'clear') {
+      for (const item in stateCopy) {
+        delete stateCopy[item];
+      }
+    }
+
+    result.push({ ...stateCopy });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
