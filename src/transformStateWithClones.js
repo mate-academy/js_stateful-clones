2b9f0d9333d01dc252006b1e01bff1858012c8b3
Maksym Shymanskij <makskampod@gmail.com>
2021-12-15 16:08:05 +0200

    v1-clone

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newState = { ...state };
+  const result = [];
+
+  for (const command of actions) {
+    switch (command.type) {
+      case 'addProperties':
+        Object.assign(newState, command.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const names of command.keysToRemove) {
+          delete newState[names];
+        }
+        break;
+
+      case 'clear':
+        for (const allPropretys in newState) {
+          delete newState[allPropretys];
+        }
+    }
+    result.push({ ...newState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
