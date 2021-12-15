0b50016b8d377f5254c9d42dcc5f1620ca874a4b
michael valiahmetov <michael.valiahmetov@gmail.com>
2021-12-15 12:03:43 +0200

    Stateful-clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,24 +8,17 @@
  */
 function transformStateWithClones(state, actions) {
   const clones = [];
-  let stateClone = { ...state };
+  const stateClone = { ...state };
 
-  for (let i = 0; i < actions.length; i++) {
-    switch (actions[i].type) {
+  for (const action of actions) {
+    switch (action.type) {
       case 'addProperties':
-        const keysAdd = Object.keys(actions[i].extraData);
-        const valuesAdd = Object.values(actions[i].extraData);
-
-        for (let y = 0; y < keysAdd.length; y++) {
-          stateClone[keysAdd[y]] = valuesAdd[y];
-        };
+        Object.assign(stateClone, action.extraData);
         break;
 
       case 'removeProperties':
-        const valuesRemove = Object.values(actions[i].keysToRemove);
-
-        for (let j = 0; j < valuesRemove.length; j++) {
-          delete stateClone[valuesRemove[j]];
+        for (const keyRemove of action.keysToRemove) {
+          delete stateClone[keyRemove];
         };
         break;
 
@@ -34,10 +27,12 @@ function transformStateWithClones(state, actions) {
           delete stateClone[keyClear];
         };
         break;
+
+      default:
+        return null;
     };
 
-    clones.push(stateClone);
-    stateClone = { ...clones[i] };
+    clones.push({ ...stateClone });
   };
 
   return clones;
