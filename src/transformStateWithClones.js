0813b877dad7b36fc026c32f73f0e891ec879490
Yevhenii Kriuchek <evhenii.kriuchek@gmail.com>
2022-01-19 14:21:50 +0200

    hm

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,38 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const tempState = { ...state };
+  const stateArr = [];
+
+  for (const i in actions) {
+    switch (actions[i].type) {
+      case 'addProperties': {
+        for (const ent of Object.entries(actions[i].extraData)) {
+          tempState[ent[0]] = ent[1];
+        }
+        stateArr.push({ ...tempState });
+        break;
+      }
+
+      case 'removeProperties': {
+        for (const key of actions[i].keysToRemove) {
+          delete tempState[key];
+        }
+        stateArr.push({ ...tempState });
+        break;
+      }
+
+      case `clear`: {
+        for (const key in tempState) {
+          delete tempState[key];
+        }
+        stateArr.push({ ...tempState });
+        break;
+      }
+    }
+  }
+
+  return stateArr;
 }
 
 module.exports = transformStateWithClones;
