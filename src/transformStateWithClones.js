6ed61c895e0fc4c1275a33fc1e0a4011e6969d87
Kostya Kovbel <dovkerdota@gmail.com>
2022-01-20 18:24:13 +0200

    sol

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,33 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const allChanges = [];
+  const stateCopy = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        allChanges.push({ ...stateCopy });
+        break;
+
+      case 'removeProperties':
+        action.keysToRemove.forEach(element => {
+          delete stateCopy[element];
+        });
+        allChanges.push({ ...stateCopy });
+        break;
+
+      case 'clear' :
+        for (const properties in stateCopy) {
+          delete stateCopy[properties];
+        }
+        allChanges.push({ ...stateCopy });
+        break;
+    }
+  }
+
+  return allChanges;
 }
 
 module.exports = transformStateWithClones;
