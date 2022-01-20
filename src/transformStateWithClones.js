3c440a368fb12c6765c68ca99abdca42c6fe661a
Misha Novikov <novikov1807@gmail.com>
2022-01-20 15:30:23 +0200

    Complite

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,34 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const stateCopy = { ...state };
+  const statesVers = [];
+
+  for (let i = 0; i < actions.length; i++) {
+    const action = actions[i];
+
+    switch (action.type) {
+      case 'clear':
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
+        break;
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete stateCopy[key];
+        }
+        break;
+
+      default:
+        break;
+    }
+    statesVers.push({ ...stateCopy });
+  }
+
+  return statesVers;
 }
 
 module.exports = transformStateWithClones;
