845c197ff93290fa71eebee47ae6df022f9561c0
trtskvalerie <trtsk.tea.valerie@gmail.com>
2022-01-20 18:09:14 +0200

    stateful clones solution v1

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,41 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateVersions = [{ ...state }];
+
+  actions.forEach(action => {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
+      case 'addProperties':
+        stateVersions.push({
+          ...stateVersions[stateVersions.length - 1],
+          ...extraData,
+        });
+        break;
+
+      case 'removeProperties':
+        stateVersions.push({ ...stateVersions[stateVersions.length - 1] });
+
+        Object.keys(stateVersions[stateVersions.length - 1]).forEach(key => {
+          if (keysToRemove.includes(key)) {
+            delete stateVersions[stateVersions.length - 1][key];
+          }
+        });
+        break;
+
+      case 'clear':
+        stateVersions.push({});
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  stateVersions.shift();
+
+  return stateVersions;
 }
 
 module.exports = transformStateWithClones;
