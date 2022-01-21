906b5b60d2d7fea1f5d2fc4bb2c82aa76bb492ca
Heorhii Plugoyi <gladiator943work@gmail.com>
2022-01-21 18:27:55 +0200

    save

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,37 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const copyState = { ...state };
+
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
+      case ('addProperties') :
+        Object.assign(copyState, extraData);
+        break;
+
+      case ('removeProperties') :
+        for (const key of keysToRemove) {
+          delete copyState[key];
+        }
+        break;
+
+      case ('clear') :
+        Object.keys(copyState).map(key => delete copyState[key]);
+        // for (const key in state) {
+        //   delete state[key];
+        // }
+        break;
+
+      default : ;
+    }
+
+    result.push({ ...copyState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
