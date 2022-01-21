c5f6b5ed8dcbe8610c268faf477c59326248b9d2
unknown <m23a09x96@gmail.com>
2022-01-21 19:27:58 +0200

    clone__arr

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let clone = { ...state };
+  const arrClone = [];
+
+  for (const x of actions) {
+    if (x.type === 'addProperties') {
+      Object.assign(clone, x.extraData);
+      arrClone.push(clone);
+      clone = { ...arrClone[arrClone.length - 1] };
+    }
+
+    if (x.type === 'removeProperties') {
+      for (const i of x.keysToRemove) {
+        if (clone.hasOwnProperty(i)) {
+          delete clone[i];
+        }
+      }
+      arrClone.push(clone);
+      clone = { ...arrClone[arrClone.length - 1] };
+    }
+
+    if (x.type === `clear`) {
+      for (const key in clone) {
+        delete clone[key];
+      }
+      arrClone.push(clone);
+      clone = { ...arrClone[arrClone.length - 1] };
+    }
+  }
+
+  return arrClone;
 }
 
 module.exports = transformStateWithClones;
