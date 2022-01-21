d3b7271a0253a93d0aa22f4851a3ebc79a59baa9
Mykhailo Ianush <yanushmisha@gmail.com>
2022-01-21 09:38:18 +0200

    change if on switch

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -15,30 +15,30 @@ function transformStateWithClones(state, actions) {
   const extraData = 'extraData';
   const keysToRemove = 'keysToRemove';
 
-  for (const object of actions) {
-    if (object[type] === 'addProperties') {
-      const objectExtraData = object[extraData];
+  for (const action of actions) {
+    switch (action[type]) {
+      case 'addProperties':
+        const objectExtraData = action[extraData];
 
-      for (const key in objectExtraData) {
-        newState[key] = objectExtraData[key];
-      }
-      clonedObject.push(createClone(newState));
-    }
+        for (const key in objectExtraData) {
+          newState[key] = objectExtraData[key];
+        }
+        clonedObject.push(createClone(newState));
+        break;
+      case 'removeProperties':
+        for (const key of action[keysToRemove]) {
+          delete newState[key];
+        }
 
-    if (object[type] === 'removeProperties') {
-      for (const key of object[keysToRemove]) {
-        delete newState[key];
-      }
+        clonedObject.push(createClone(newState));
+        break;
+      case 'clear':
+        for (const key in newState) {
+          delete newState[key];
+        }
 
-      clonedObject.push(createClone(newState));
-    }
-
-    if (object[type] === 'clear') {
-      for (const key in newState) {
-        delete newState[key];
-      }
-
-      clonedObject.push({});
+        clonedObject.push({});
+        break;
     }
   }
 
