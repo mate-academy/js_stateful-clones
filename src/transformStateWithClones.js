489eb84dcd972478c47d447124e8b661ada436db
Dorosh13 <dorosh.d.13@gmail.com>
2022-01-23 17:26:04 +0200

    created stateful-clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,41 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  const stateArr = [];
+
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
+      case 'addProperties':
+        Object.assign(stateCopy, extraData);
+
+        stateArr.push({ ...stateCopy });
+        break;
+
+      case 'removeProperties':
+        for (const key of keysToRemove) {
+          delete stateCopy[key];
+        }
+
+        stateArr.push({ ...stateCopy });
+        break;
+
+      case 'clear':
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
+
+        stateArr.push({ ...stateCopy });
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  return stateArr;
 }
 
 module.exports = transformStateWithClones;
