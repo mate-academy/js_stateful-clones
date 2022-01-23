af83b0d0a9e4d07a8ebf6573ebb0a0359a9875c5
Maksym Mishchanchuk <yssam.nn@gmail.com>
2022-01-23 20:24:53 +0200

    sol

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const copyState = { ...state };
+  const result = [];
+
+  for (const action of actions) {
+    const typeOfObject = action.type;
+    const keysToRemove = action.keysToRemove;
+
+    switch (typeOfObject) {
+      case 'addProperties':
+        Object.assign(copyState, action.extraData);
+        break;
+
+      case 'removeProperties' :
+        for (const property of keysToRemove) {
+          delete copyState[property];
+        }
+        break;
+
+      case 'clear':
+        for (const key in copyState) {
+          delete copyState[key];
+        }
+        break;
+      default:
+        return ('Error action');
+    }
+    result.push({ ...copyState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
