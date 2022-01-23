38324df3dff116b38e04a96167bc85f51734109b
unknown <m23a09x96@gmail.com>
2022-01-23 15:07:27 +0200

    Solution fixed

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,35 +8,35 @@
  */
 function transformStateWithClones(state, actions) {
   let clone = { ...state };
-  const arrClone = [];
+  const result = [];
 
   for (const x of actions) {
     if (x.type === 'addProperties') {
       Object.assign(clone, x.extraData);
-      arrClone.push(clone);
-      clone = { ...arrClone[arrClone.length - 1] };
+      result.push(clone);
+      clone = { ...result[result.length - 1] };
     }
 
     if (x.type === 'removeProperties') {
-      for (const i of x.keysToRemove) {
-        if (clone.hasOwnProperty(i)) {
-          delete clone[i];
+      for (const item of x.keysToRemove) {
+        if (clone.hasOwnProperty(item)) {
+          delete clone[item];
         }
       }
-      arrClone.push(clone);
-      clone = { ...arrClone[arrClone.length - 1] };
+      result.push(clone);
+      clone = { ...result[result.length - 1] };
     }
 
     if (x.type === `clear`) {
       for (const key in clone) {
         delete clone[key];
       }
-      arrClone.push(clone);
-      clone = { ...arrClone[arrClone.length - 1] };
+      result.push(clone);
+      clone = { ...result[result.length - 1] };
     }
   }
 
-  return arrClone;
+  return result;
 }
 
 module.exports = transformStateWithClones;
