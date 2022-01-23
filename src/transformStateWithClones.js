7703ebf131d4a4b75706526387a3d2ceee4b1a14
Hlib Schebalkov <glebusha228@gmail.com>
2022-01-24 00:51:44 +0300

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,51 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const clone = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(clone, action.extraData);
+        result.push({ ...clone });
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete clone[key];
+        }
+        result.push({ ...clone });
+        break;
+
+      case 'clear':
+        for (const key in clone) {
+          delete clone[key];
+        }
+        result.push({ ...clone });
+        break;
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
+
+// if (actions[i].type === 'addProperties') {
+//   for (const addKey in actions[i].extraData) {
+//     if (!state[addKey] || state[addKey] !== actions[i].extraData[addKey]) {
+//       state[addKey] = actions[i].extraData[addKey];
+//     }
+//   }
+// } else if (actions[i].type === 'removeProperties') {
+//   for (let del = 0; del < actions[i].keysToRemove.length; del++) {
+//     if (state[actions[i].keysToRemove[del]]) {
+//       delete state[actions[i].keysToRemove[del]];
+//     }
+//   }
+// } else if (actions[i].type === 'clear') {
+//   for (const key in state) {
+//     delete state[key];
+//   }
+// }
