3e02abbc603977986ea7b1c3878f0cc28440884d
Serhii Vorona <seregavorona94@gmail.com>
2022-01-24 14:46:09 +0200

    clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,36 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const result = [];
+  const copyState = Object.assign({}, state);
+
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
+      case 'addProperties':
+        Object.assign(copyState, extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of keysToRemove) {
+          delete copyState[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in copyState) {
+          delete copyState[key];
+        }
+        break;
+
+      default:
+        return [];
+    }
+    result.push({ ...copyState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
