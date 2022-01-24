bd487c67b8b64f3381f878fa0a1b943751910c6b
Oleksandr Polotniuk <oleksandr.polotniuk@gmail.com>
2022-01-24 18:03:40 +0200

    Second try

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,40 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const action = Object.values(actions);
-
   const stateCopy = { ...state };
   const stateVersions = [];
 
-  for (const key of action) {
-    switch (key.type) {
-      case 'addProperties':
-        for (const property in key.extraData) {
-          stateCopy[property] = key.extraData[property];
-        }
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
 
-        stateVersions.push({ ...stateCopy });
+    switch (type) {
+      case 'addProperties':
+        for (const property in extraData) {
+          stateCopy[property] = extraData[property];
+        }
         break;
 
       case 'removeProperties':
-        for (const property of key.keysToRemove) {
+        for (const property of keysToRemove) {
           delete stateCopy[property];
         }
-
-        stateVersions.push({ ...stateCopy });
         break;
 
       case 'clear':
         for (const property in stateCopy) {
           delete stateCopy[property];
         }
-
-        stateVersions.push({ ...stateCopy });
         break;
 
       default:
         break;
     }
+
+    stateVersions.push({ ...stateCopy });
   }
 
   return stateVersions;
