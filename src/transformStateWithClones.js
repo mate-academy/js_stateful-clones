93c14d17268a0574033ed78e93686db4a89d6539
Hlib Schebalkov <glebusha228@gmail.com>
2022-01-25 23:38:36 +0300

    only one push

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -14,23 +14,24 @@ function transformStateWithClones(state, actions) {
     switch (action.type) {
       case 'addProperties':
         Object.assign(clone, action.extraData);
-        result.push({ ...clone });
         break;
 
       case 'removeProperties':
         for (const key of action.keysToRemove) {
           delete clone[key];
         }
-        result.push({ ...clone });
+
         break;
 
       case 'clear':
         for (const key in clone) {
           delete clone[key];
         }
-        result.push({ ...clone });
+
         break;
     }
+
+    result.push({ ...clone });
   }
 
   return result;
