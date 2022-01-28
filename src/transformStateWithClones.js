d183b7f85e4878d06afb62f665ed60af8d24538a
dant0n <dmibuc@gmail.com>
2022-01-28 21:02:52 +0200

    Finish hw.

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  const stateObjectsHistory = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        stateObjectsHistory.push({ ...stateCopy });
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete stateCopy[key];
+        }
+        stateObjectsHistory.push({ ...stateCopy });
+        break;
+
+      case 'clear':
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
+        stateObjectsHistory.push({});
+        break;
+    }
+  }
+
+  return stateObjectsHistory;
 }
 
 module.exports = transformStateWithClones;
