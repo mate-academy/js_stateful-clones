9bb45adf96f121dd22352ceec6423a418dc209c9
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-01-31 20:28:31 +0200

    Loops for were changed

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,46 +6,13 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
   const modifiedState = { ...state };
   const stateVersions = [];
 
-  for (let i = 0; i < actions.length; i++) {
-    stateVersions.push({ ...changeProperties(modifiedState, actions[i]) });
-  }
-
-  return stateVersions;
-}
-
-function changeProperties(currentState, changes) {
-  switch (changes.type) {
-    case 'clear':
-      for (const key in currentState) {
-        delete currentState[key];
-      }
-      break;
-    case 'addProperties':
-      Object.assign(currentState, changes.extraData);
-      break;
-    case 'removeProperties':
-      for (let j = 0; j < changes.keysToRemove.length; j++) {
-        if (changes.keysToRemove[j] in currentState) {
-          delete currentState[changes.keysToRemove[j]];
-        }
-      }
-      break;
-  }
-
-  return currentState;
-}
-
-// There is another way of solving the task below
-/* function transformStateWithClones(state, actions) {
-  const modifiedState = { ...state };
-  const stateVersions = [];
-
-  for (let i = 0; i < actions.length; i++) {
-    switch (actions[i].type) {
+  for (const action of actions) {
+    switch (action.type) {
       case 'clear':
         for (const key in modifiedState) {
           delete modifiedState[key];
@@ -53,14 +20,12 @@ function changeProperties(currentState, changes) {
         break;
 
       case 'addProperties':
-        Object.assign(modifiedState, actions[i].extraData);
+        Object.assign(modifiedState, action.extraData);
         break;
 
       case 'removeProperties':
-        for (let j = 0; j < actions[i].keysToRemove.length; j++) {
-          if (actions[i].keysToRemove[j] in modifiedState) {
-            delete modifiedState[actions[i].keysToRemove[j]];
-          }
+        for (const remove of action.keysToRemove) {
+          delete modifiedState[remove];
         }
         break;
     }
@@ -68,6 +33,6 @@ function changeProperties(currentState, changes) {
   }
 
   return stateVersions;
-} */
+}
 
 module.exports = transformStateWithClones;
