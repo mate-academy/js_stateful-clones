aafc43421748d238496fce4e20eb71637ae5f6ae
Yaroslav Rehulskyi <barsinvest84@gmail.com>
2022-02-08 15:27:53 +0200

    Making clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,35 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const stateModified = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(stateModified, action.extraData);
+      result.push({ ...stateModified });
+    }
+
+    if (action.type === 'removeProperties') {
+      const arr = action.keysToRemove;
+
+      for (const key of arr) {
+        delete stateModified[key];
+      }
+
+      result.push({ ...stateModified });
+    }
+
+    if (action.type === 'clear') {
+      for (const key in stateModified) {
+        delete stateModified[key];
+      }
+
+      result.push({ ...stateModified });
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
