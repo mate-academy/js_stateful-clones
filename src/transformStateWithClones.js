390006437d3a03800a6adad0001ff883a4651e8a
Alexandr Trubin-Koshyk <alexandr.koshyk@gmail.com>
2022-02-09 17:34:01 +0200

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const resultArr = [];
+  const stateOnAction = { ...state };
+
+  for (const item of actions) {
+    switch (item.type) {
+      case 'addProperties':
+        Object.assign(stateOnAction, item.extraData);
+        resultArr.push({ ...stateOnAction });
+        break;
+
+      case 'removeProperties':
+        for (const key of item.keysToRemove) {
+          delete stateOnAction[key];
+        }
+        resultArr.push({ ...stateOnAction });
+        break;
+
+      case 'clear':
+        for (const key in stateOnAction) {
+          delete stateOnAction[key];
+        }
+        resultArr.push({ ...stateOnAction });
+        break;
+    }
+  }
+
+  return resultArr;
 }
 
 module.exports = transformStateWithClones;
