b0969770dfbc3f379a73c40bb416843cd2a7e3fd
Max Shlikhta <maxok.vibe@gmail.com>
2022-02-09 18:20:44 +0200

    Clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const newState = { ...state };
+
+  for (const prop of actions) {
+    switch (prop.type) {
+      case 'clear':
+        for (const keys in newState) {
+          delete newState[keys];
+        }
+        break;
+
+      case 'removeProperties':
+        for (const key of prop.keysToRemove) {
+          delete newState[key];
+        }
+        break;
+
+      case 'addProperties':
+        Object.assign(newState, prop.extraData);
+        break;
+    }
+
+    result.push({ ...newState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
