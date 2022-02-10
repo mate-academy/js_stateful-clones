3d7adb5df7f09eb6a41bd0258a2c8b71e468a195
Inna <dragotaiv@gmail.com>
2022-02-10 23:34:34 +0200

    clonese

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,33 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const copy = { ...state };
+  const result = [];
+
+  for (const obj of actions) {
+    const { type, extraData, keysToRemove } = obj;
+
+    switch (type) {
+      case 'addProperties':
+        Object.assign(copy, extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of keysToRemove) {
+          delete copy[key];
+        }
+        break;
+
+      case 'clear':
+        for (const property in copy) {
+          delete copy[property];
+        }
+        break;
+    }
+    result.push({ ...copy });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
