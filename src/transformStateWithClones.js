d6f15eda7dea4408bcdfc06940fbf2ab2a8bfd54
artemboby <artem.bobyr@jamesandjames.com>
2022-02-22 19:21:38 +0200

    task is done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,35 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const objects = [];
+  let newObj = { ...state };
+
+  for (const obj of actions) {
+    switch (obj.type) {
+      case 'addProperties':
+        newObj = { ...newObj };
+        Object.assign(newObj, obj.extraData);
+        objects.push(newObj);
+        break;
+      case 'removeProperties':
+        newObj = { ...newObj };
+
+        for (const key of obj.keysToRemove) {
+          if (key in newObj) {
+            delete newObj[key];
+          }
+        }
+        objects.push(newObj);
+        break;
+      case 'clear':
+        newObj = { ...newObj };
+        Object.keys(newObj).forEach(key => delete newObj[key]);
+        objects.push(newObj);
+        break;
+    }
+  }
+
+  return objects;
 }
 
 module.exports = transformStateWithClones;
