46cd942abb121e1f18ce2d9baba246b5ae6d335b
Roman Nikitenko <Lomkaaa@ukr.net>
2022-03-13 18:36:14 +0200

    Task complete

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,45 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const results = [];
+
+  actions.forEach(action => {
+    let result;
+
+    if (!results.length) {
+      result = {
+        ...state,
+      };
+    } else {
+      result = { ...results[results.length - 1] };
+    }
+
+    switch (action.type) {
+      case 'addProperties':
+        Object.keys(action.extraData).forEach(dataKey => {
+          result[dataKey] = action.extraData[dataKey];
+        });
+        break;
+      case 'removeProperties':
+        action.keysToRemove.forEach(keyRemove => {
+          delete result[keyRemove];
+        });
+
+        break;
+      case 'clear':
+        for (const key in result) {
+          delete result[key];
+        }
+        break;
+      default:
+        break;
+    }
+    results.push(result);
+  });
+
+  return results;
 }
 
 module.exports = transformStateWithClones;
