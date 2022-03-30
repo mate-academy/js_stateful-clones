0f526a2302782a8e37367a6c0767434c42fa715d
Oksana Tyshchenko <oksanasergeevna30@gmail.com>
2022-03-30 13:31:12 +0300

    transformStateWithClones function was created

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const clone = { ...state };
+
+  for (const item of actions) {
+    if (item.type === 'addProperties') {
+      const { extraData } = item;
+
+      Object.assign(clone, extraData);
+    } else if (item.type === 'removeProperties') {
+      const { keysToRemove } = item;
+
+      for (const key in clone) {
+        if (keysToRemove.includes(key)) {
+          delete clone[key];
+        }
+      }
+    } else if (item.type === 'clear') {
+      for (const key in clone) {
+        delete clone[key];
+      }
+    }
+    result.push({ ...clone });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
