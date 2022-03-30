befcde186de7c5cdcdd897e64d535d3ed2b8acc9
Helen Petrovska <paznachka@gmail.com>
2022-03-30 19:08:19 +0300

    state was added

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,34 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const resultArr = [];
+  const clone = { ...state };
+
+  for (const obj of actions) {
+    if (obj.extraData) {
+      for (const key in obj.extraData) {
+        clone[key] = obj.extraData[key];
+      }
+    }
+
+    if (obj.type === 'clear') {
+      for (const key in clone) {
+        delete clone[key];
+      }
+    }
+
+    if (obj.keysToRemove) {
+      for (const key of obj.keysToRemove) {
+        delete clone[key];
+      }
+    }
+
+    resultArr.push({ ...clone });
+  }
+
+  return resultArr;
 }
 
 module.exports = transformStateWithClones;
