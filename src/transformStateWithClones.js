5fc629ab4943e6b5e492384a77e9a34d1032176d
Anastasiia Sharkova <sharkova.nastik@gmail.com>
2022-03-31 18:04:59 +0200

    new array

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const cloneState = { ...state };
+  const actionsInfo = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(cloneState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const extra of action.keysToRemove) {
+          delete cloneState[extra];
+        };
+        break;
+
+      default:
+        for (const item in cloneState) {
+          delete cloneState[item];
+        }
+        break;
+    }
+    actionsInfo.push({ ...cloneState });
+  }
+
+  return actionsInfo;
 }
 
 module.exports = transformStateWithClones;
