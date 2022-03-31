3f70bedf97e06e99fbfd8d97ea25f34aeee5105c
Vlad Skoromnyi <VectraSton@gmail.com>
2022-03-31 16:39:02 +0300

    remake on switch

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -10,27 +10,27 @@ function transformStateWithClones(state, actions) {
   const firstState = { ...state };
   const changeState = [];
 
-  function assigned(index, to, from) {
-    changeState[index] = Object.assign(to, from);
-  }
-
   for (let i = 0; i < actions.length; i++) {
-    if (actions[i].type === 'addProperties') {
-      for (const key in actions[i].extraData) {
-        firstState[key] = actions[i].extraData[key];
-      }
-      assigned(i, {}, firstState);
-    } else if (actions[i].type === 'removeProperties') {
-      for (const key of actions[i].keysToRemove) {
-        delete firstState[key];
-      }
-      assigned(i, {}, firstState);
-    } else if (actions[i].type === 'clear') {
-      for (const key in firstState) {
-        delete firstState[key];
-      }
-      assigned(i, {}, firstState);
+    switch (actions[i].type) {
+      case 'addProperties':
+        const { extraData } = actions[i];
+
+        Object.assign(firstState, extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of actions[i].keysToRemove) {
+          delete firstState[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in firstState) {
+          delete firstState[key];
+        }
+        break;
     }
+    changeState[i] = Object.assign({}, firstState);
   }
 
   return changeState;
