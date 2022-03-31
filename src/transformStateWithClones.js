14ca24c6ec754e7b6984115cd0ea2a87c240c24e
Max Nikiforov <maxxnikiforov@gmail.com>
2022-04-01 00:10:53 +0700

    small changes

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,27 +7,27 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const clone = { ...state };
+  const cloneState = { ...state };
   const result = [];
 
   for (let i = 0; i < actions.length; i++) {
     if (actions[i]['type'] === 'addProperties') {
-      Object.assign(clone, actions[i]['extraData']);
+      Object.assign(cloneState, actions[i]['extraData']);
     }
 
     if (actions[i]['type'] === 'removeProperties') {
       for (let x = 0; x < actions[i]['keysToRemove'].length; x++) {
-        delete clone[actions[i]['keysToRemove'][x]];
+        delete cloneState[actions[i]['keysToRemove'][x]];
       }
     }
 
     if (actions[i]['type'] === `clear`) {
-      for (const key in clone) {
-        delete clone[key];
+      for (const key in cloneState) {
+        delete cloneState[key];
       }
     }
 
-    const clone2 = { ...clone };
+    const clone2 = { ...cloneState };
 
     result.push(clone2);
   }
