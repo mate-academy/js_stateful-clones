ea256a4a6c3d6bb806534d12ce195ed65c05ec67
Anatolii Vlasiuk <vlasyuk_a@ukr.net>
2022-03-31 12:08:31 +0300

    Clonestate solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
-}
+  const res = [];
+  const clone = Object.assign({}, state);
 
+  for (let i = 0; i < actions.length; i++) {
+    switch (true) {
+      case actions[i].type === 'addProperties':
+        for (const key in actions[i].extraData) {
+          clone[key] = actions[i].extraData[key];
+        }
+        res[i] = Object.assign({}, clone);
+        break;
+
+      case actions[i].type === 'removeProperties':
+        for (const key of actions[i].keysToRemove) {
+          delete clone[key];
+        }
+        res[i] = Object.assign({}, clone);
+        break;
+
+      case actions[i].type === 'clear':
+        for (const key in state) {
+          delete clone[key];
+        }
+        res[i] = Object.assign({}, clone);
+        break;
+    }
+
+    return res;
+  }
+}
 module.exports = transformStateWithClones;
