f28891d90b6a476b98c288aff18acf7e14379b28
SeKaM0 <polozhentsew02@gmail.com>
2022-03-31 22:08:26 +0300

    Finish task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
-}
+  const mas = [];
+  const stateV2 = { ...state };
 
+  for (let i = 0; i < actions.length; i++) {
+    switch (actions[i].type) {
+      case 'addProperties' :
+        Object.assign(stateV2, actions[i].extraData);
+        break;
+      case 'removeProperties' :
+        for (const key in stateV2) {
+          if (actions[i].keysToRemove.includes(key)) {
+            delete stateV2[key];
+          }
+        }
+
+        break;
+
+      case 'clear':
+        for (const key in stateV2) {
+          delete stateV2[key];
+        }
+
+        break;
+    }
+
+    const result = { ...stateV2 };
+
+    mas.push(result);
+  }
+
+  return mas;
+}
 module.exports = transformStateWithClones;
