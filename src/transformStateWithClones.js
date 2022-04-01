5202e8de446cc7250a164826f489b7c89c35d0b3
Dima Kotovich <kotovih950@gmail.com>
2022-04-01 14:01:07 +0300

    finish

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,35 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const stateCopy = { ...state };
+  const NewState = [];
+
+  for (let index = 0; index < actions.length; index++) {
+    switch (actions[index].type) {
+      case 'addProperties':
+        Object.assign(stateCopy, actions[index].extraData);
+        break;
+
+      case 'clear':
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
+        break;
+
+      case 'removeProperties' :
+        for (const key of actions[index].keysToRemove) {
+          delete stateCopy[key];
+        }
+        break;
+
+      default:
+        break;
+    }
+
+    NewState.push({ ...stateCopy });
+  }
+
+  return NewState;
 }
 
 module.exports = transformStateWithClones;
