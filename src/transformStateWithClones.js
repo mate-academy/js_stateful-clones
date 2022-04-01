6058c6db511c91653c34499f5ce7bc8237de78cf
Pavlo Stasiv <99363993+StasivPavlo@users.noreply.github.com>
2022-04-01 19:50:01 +0200

    transforStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,59 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const res = [];
+
+  for (let index = 0; index < actions.length; index++) {
+    let newState = {};
+
+    const actionType = actions[index].type;
+    const actionExtraDate = actions[index].extraData;
+
+    switch (actionType) {
+      case 'addProperties': {
+        if (index) {
+          newState = {
+            ...res[index - 1],
+            ...actionExtraDate,
+          };
+        } else {
+          newState = {
+            ...state,
+            ...actionExtraDate,
+          };
+        }
+
+        res.push(newState);
+        break;
+      }
+
+      case 'removeProperties': {
+        if (index) {
+          newState = { ...res[index - 1] };
+        } else {
+          newState = { ...state };
+        }
+
+        for (let key = 0; key < actions[index].keysToRemove.length; key++) {
+          delete newState[actions[index].keysToRemove[key]];
+        }
+
+        res.push(newState);
+        break;
+      }
+
+      case 'clear': {
+        res.push({ });
+        break;
+      }
+
+      default: {
+        throw new Error(`Unsopported action type: ${actionType}`);
+      }
+    }
+  }
+
+  return res;
 }
 
 module.exports = transformStateWithClones;
