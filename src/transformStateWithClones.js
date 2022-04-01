133dc56e09e31cc664c371704edc1388ba4bcda5
Nikita Prepelytsia <prep.nikita@gmail.com>
2022-04-01 09:00:56 +0300

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,37 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const likeState = { ...state };
+  let copyObj = {};
+  const result = [];
+
+  for (let i = 0; i < actions.length; i++) {
+    switch (actions[i].type) {
+      case 'addProperties':
+        for (const key in actions[i].extraData) {
+          likeState[key] = actions[i].extraData[key];
+        }
+        copyObj = { ...likeState };
+        result.push(copyObj);
+        break;
+      case 'removeProperties':
+        for (let item = 0; item < actions[i].keysToRemove.length; item++) {
+          delete likeState[actions[i].keysToRemove[item]];
+        }
+        copyObj = { ...likeState };
+        result.push(copyObj);
+        break;
+      case 'clear':
+        for (const item in likeState) {
+          delete likeState[item];
+        }
+        copyObj = { ...likeState };
+        result.push(copyObj);
+        break;
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
