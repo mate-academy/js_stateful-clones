95bb402358b0d6d35e4b043fe61bbd7bd63161e2
Kostiantyn <havryshkostya@gmail.com>
2022-04-03 19:28:10 +0400

    12 green outof 12

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
-}
+  const result = [];
+  let mutatedClone = Object.assign(state);
 
+  for (const key of actions) {
+    if (key.type === 'clear') {
+      mutatedClone = {};
+    }
+
+    if (key.type === 'addProperties') {
+      mutatedClone = Object.assign({}, mutatedClone, key.extraData);
+    }
+
+    if (key.type === 'removeProperties') {
+      mutatedClone = Object.assign({}, mutatedClone);
+
+      for (const removeItem of key.keysToRemove) {
+        if (removeItem in mutatedClone) {
+          delete mutatedClone[removeItem];
+        }
+      }
+    }
+
+    result.push(mutatedClone);
+  }
+
+  return result;
+}
 module.exports = transformStateWithClones;
