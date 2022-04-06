0d110ba99bc58306c1d1198c449a0be1dfdd8625
Andrii Rutkovskii <greateman94@gmail.com>
2022-04-06 14:23:25 +0300

    complited

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,50 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arr = [];
+  let objBox = {};
+  let objBox2 = {};
+
+  for (const key in state) {
+    objBox2[key] = state[key];
+  }
+
+  for (let i = 0; i < actions.length; i++) {
+    objBox = {};
+
+    for (const key in objBox2) {
+      objBox[key] = objBox2[key];
+    }
+
+    if (actions[i].type === 'addProperties') {
+      for (const key in actions[i].extraData) {
+        objBox[key] = actions[i].extraData[key];
+      }
+
+      arr.push(objBox);
+      objBox2 = objBox;
+    } else if (actions[i].type === 'removeProperties') {
+      for (const key of actions[i].keysToRemove) {
+        if (!objBox[key]) {
+          continue;
+        }
+
+        delete objBox[key];
+      }
+
+      arr.push(objBox);
+      objBox2 = objBox;
+    } else if (actions[i].type === 'clear' && Object.keys(state).length !== 0) {
+      for (const keyOfState in objBox) {
+        delete objBox[keyOfState];
+      }
+
+      arr.push(objBox);
+      objBox2 = objBox;
+    }
+  }
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
