93bf2cb3c992ba4effe64710204e99c54b42fd6d
Александр Погребняк <pogrebnyak19999@gmail.com>
2022-04-08 20:39:57 +0300

    Refactor solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,43 +8,38 @@
  */
 function transformStateWithClones(state, actions) {
   const result = [];
-  let stateVersions = state;
-  let variantsCount = 0;
+  const stateVersion = { ...state };
 
   for (const variants of actions) {
     const { type, extraData, keysToRemove } = variants;
 
     switch (type) {
       case 'addProperties':
-        if (result[variantsCount - 1]) {
-          stateVersions = result[variantsCount - 1];
-        }
-
-        result.push(Object.assign({}, stateVersions));
-
         for (const data in extraData) {
-          result[variantsCount][data] = extraData[data];
+          stateVersion[data] = extraData[data];
         }
+
         break;
 
       case 'removeProperties':
-        if (result[variantsCount - 1]) {
-          stateVersions = result[variantsCount - 1];
-        }
-
-        result.push(Object.assign({}, stateVersions));
-
         for (const key of keysToRemove) {
-          delete result[variantsCount][key];
+          delete stateVersion[key];
         }
+
         break;
 
       case 'clear':
-        result.push({});
+        for (const key in stateVersion) {
+          delete stateVersion[key];
+        }
+
+        break;
+
+      default:
         break;
     }
 
-    variantsCount++;
+    result.push({ ...stateVersion });
   }
 
   return result;
