a21a17cd6459ee6a1c3adddcabd64722a4db799d
Roman Lipatov <rlipatov96@gmail.com>
2022-04-11 12:05:22 +0300

    stareful-clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,30 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  let copy = { ...state };
+  const result = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(copy, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete copy[key];
+        }
+        break;
+
+      case 'clear':
+        copy = {};
+        break;
+    }
+
+    result.push({ ...copy });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
