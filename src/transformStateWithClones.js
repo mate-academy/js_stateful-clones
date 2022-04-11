1ac9ab57db6320f1e09f6a23399c8cbd8316c2d9
Yura Dikanev <yuriydikanev@gmail.com>
2022-04-11 16:30:30 +0300

    stateful_clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,27 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const result = [];
+  const clonedState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(clonedState, action.extraData);
+        break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete clonedState[key];
+        }
+        break;
+      case 'clear':
+        Object.keys(clonedState).forEach(key => delete
+        clonedState[key]);
+    }
+    result.push({ ...clonedState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
