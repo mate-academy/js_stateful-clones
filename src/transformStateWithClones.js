f38082f9e5d9e46497cabe6445312ea0f768e8d7
Bogdan Prokopenko <pb.dvlpr@gmail.com>
2022-04-13 16:11:10 +0300

    Firs Run Stateful Clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const total = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(total, action.extraData);
+        break;
+    }
+
+    switch (action.type) {
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete total[key];
+        }
+        break;
+    }
+
+    switch (action.type) {
+      case 'clear': {
+        for (const key in total) {
+          delete total[key];
+        }
+        break;
+      }
+    }
+    result.push({ ...total });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
