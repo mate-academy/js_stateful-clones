6c9ec28a8b27a3532b612a9594fd3525d7492c9d
Bogdan Prokopenko <pb.dvlpr@gmail.com>
2022-04-14 17:34:01 +0300

    Second Run Stateful Clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,32 +8,32 @@
  */
 function transformStateWithClones(state, actions) {
   const result = [];
-  const total = { ...state };
+  const copyState = { ...state };
 
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties':
-        Object.assign(total, action.extraData);
+        Object.assign(copyState, action.extraData);
         break;
     }
 
     switch (action.type) {
       case 'removeProperties':
         for (const key of action.keysToRemove) {
-          delete total[key];
+          delete copyState[key];
         }
         break;
     }
 
     switch (action.type) {
       case 'clear': {
-        for (const key in total) {
-          delete total[key];
+        for (const key in copyState) {
+          delete copyState[key];
         }
         break;
       }
     }
-    result.push({ ...total });
+    result.push({ ...copyState });
   }
 
   return result;
