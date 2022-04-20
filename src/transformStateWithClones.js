f730b5adaf00972dfafe653b7573a492d5f26db8
Andrei Sucman <sucmanandrei@gmail.com>
2022-04-20 23:05:22 +0300

    task completed

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const clone = { ...state };
+  const result = [];
+
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i]['type'] === 'addProperties') {
+      Object.assign(clone, actions[i]['extraData']);
+    }
+
+    if (actions[i]['type'] === 'removeProperties') {
+      for (const property in actions[i]['keysToRemove']) {
+        delete clone[actions[i]['keysToRemove'][property]];
+      }
+    }
+
+    if (actions[i]['type'] === 'clear') {
+      for (const property in clone) {
+        delete clone[property];
+      }
+    }
+    result.push({ ...clone });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
