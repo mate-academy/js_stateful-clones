c7df567b59413e1d44504299f50527998cc64327
Hubert Wolak <29hihub@gmail.com>
2022-04-25 13:21:20 +0200

    solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,37 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const objectCopy = { ...state };
+  const statesArray = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(objectCopy, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const removeKey of action.keysToRemove) {
+          delete objectCopy[removeKey];
+        }
+        break;
+
+      case 'clear':
+        for (const clearKey in objectCopy) {
+          delete objectCopy[clearKey];
+        }
+        break;
+
+      default:
+        throw new Error('Invalid paramater');
+    }
+
+    const updatedState = { ...objectCopy };
+
+    statesArray.push(updatedState);
+  }
+
+  return statesArray;
 }
 
 module.exports = transformStateWithClones;
