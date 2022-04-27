e752c4ba73cecc8cc8b79d5ad4592abcf6a6cc81
MabuzaM <mabuzamfano@gmail.com>
2022-04-27 12:55:22 +0200

    Started the function and created the block for addProperties

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,28 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateClone = { ...state };
+  const stateClones = [];
+
+  for (const action of actions) {
+    const { type, extraData } = action;
+
+    switch (type) {
+      case 'addProperties':
+        Object.assign(stateClone, extraData);
+        stateClones.push(stateClone);
+        break;
+
+      default:
+
+        throw new Error();
+
+        // return 'Something went worng with your inputs'
+        //   + ', Please check and enter again';
+    }
+  }
+
+  return stateClones;
 }
 
 module.exports = transformStateWithClones;
