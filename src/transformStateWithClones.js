efb10531aa85f9f9ab76a7e0f077fcec46bc5c8a
Andriyk0 <andriy22221@gmail.com>
2022-04-27 15:02:16 +0300

    create

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arr = [];
+  const a = { ...state };
+
+  for (const key of actions) {
+    if (key.type === 'addProperties') {
+      Object.assign(a, key.extraData);
+      arr.push({ ...a });
+    }
+
+    if (key.type === 'removeProperties') {
+      for (const j of key.keysToRemove) {
+        delete a[j];
+      }
+      arr.push({ ...a });
+    }
+
+    if (key.type === 'clear') {
+      for (const k in a) {
+        delete a[k];
+      }
+      arr.push({ ...a });
+    }
+  }
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
