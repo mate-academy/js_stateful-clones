db86d0bf5f3b99315cb703b5e0b8f8b7049e0420
Alyona Pisotska <pisotska.o@gmail.com>
2022-04-29 13:41:19 +0300

    Transform state with clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
-}
+  const states = [];
+  const newState = { ...state };
 
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(newState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete newState[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in newState) {
+          delete newState[key];
+        }
+    }
+
+    states.push({ ...newState });
+  }
+
+  return states;
+}
 module.exports = transformStateWithClones;
