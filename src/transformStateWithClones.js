388ac90b5b25d96af9d553c032a671cd237fb5d6
Pavlo Pokryvets <pavelpokryvets@gmail.com>
2022-04-30 11:36:10 +0300

    State_with_Clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,36 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  let countKeyState;
+  const copyState = {
+    ...state,
+  };
+  const mainArray = [];
+  let clearObject = {};
+
+  for (countKeyState = 0; countKeyState < actions.length; countKeyState++) {
+    if (actions[countKeyState].type === 'addProperties') {
+      Object.assign(copyState, actions[countKeyState].extraData);
+    } else if (actions[countKeyState].type === 'removeProperties') {
+      for (const keyForRemove of actions[countKeyState].keysToRemove) {
+        delete copyState[keyForRemove];
+      }
+    } else if (actions[countKeyState].type === 'clear') {
+      const keysState = Object.keys(copyState);
+
+      for (const keyForState of keysState) {
+        delete copyState[keyForState];
+      }
+    }
+
+    clearObject = {
+      ...copyState,
+    };
+    mainArray.push(clearObject);
+    clearObject = {};
+  }
+
+  return mainArray;
 }
 
 module.exports = transformStateWithClones;
