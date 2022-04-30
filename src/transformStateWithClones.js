d4969a6b93be1bdd9c98ffb88f49136781d553ab
Server <isr.lviv2021@gmail.com>
2022-04-30 16:51:29 +0300

    transform state

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,25 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const res = [];
+  const newState = { ...state };
+
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'addProperties') {
+      Object.assign(newState, actions[i].extraData);
+    } else if (actions[i].type === 'removeProperties') {
+      for (const key of actions[i].keysToRemove) {
+        delete newState[key];
+      }
+    } else if (actions[i].type === 'clear') {
+      for (const key1 in newState) {
+        delete newState[key1];
+      }
+    }
+    res.push({ ...newState });
+  }
+
+  return res;
 }
 
 module.exports = transformStateWithClones;
