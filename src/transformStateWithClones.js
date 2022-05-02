692d7132cf7f5933c9852131d813e24baba88f36
Server <isr.lviv2021@gmail.com>
2022-05-02 20:12:06 +0300

    ransform state

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -10,17 +10,22 @@ function transformStateWithClones(state, actions) {
   const res = [];
   const newState = { ...state };
 
-  for (let i = 0; i < actions.length; i++) {
-    if (actions[i].type === 'addProperties') {
-      Object.assign(newState, actions[i].extraData);
-    } else if (actions[i].type === 'removeProperties') {
-      for (const key of actions[i].keysToRemove) {
-        delete newState[key];
-      }
-    } else if (actions[i].type === 'clear') {
-      for (const key1 in newState) {
-        delete newState[key1];
-      }
+  for (let j = 0; j < actions.length; j++) {
+    switch (actions[j].type) {
+      case 'addProperties':
+        Object.assign(newState, actions[j].extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of actions[j].keysToRemove) {
+          delete newState[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key1 in newState) {
+          delete newState[key1];
+        }
     }
     res.push({ ...newState });
   }
