c5090ff6d53507b9cd03d0f72c907dc8aa4b5741
Roman Marunkevich <vinnibea@gmail.com>
2022-05-09 04:13:17 +0300

    sol

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,31 @@
 'use strict';
 
-/**
- * @param {Object} state
- * @param {Object[]} actions
- *
- * @return {Object[]}
- */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const current = { ...state };
+  const resultingArray = [];
+  let newr;
+
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'addProperties') {
+      for (const prop in actions[i].extraData) {
+        current[prop] = actions[i].extraData[prop];
+      }
+    }
+
+    if (actions[i].type === 'removeProperties') {
+      for (const word in actions[i].keysToRemove) {
+        delete current[actions[i].keysToRemove[word]];
+      }
+    }
+
+    if (actions[i].type === 'clear') {
+      for (const key in current) {
+        delete current[key];
+      }
+    } newr = { ...current }; resultingArray[i] = newr;
+  }
+
+  return resultingArray;
 }
 
 module.exports = transformStateWithClones;
