7ef55083701793e97181e6b0cf8888d6810374b2
Kamil Witek <kamilprogramuje@gmail.com>
2022-05-12 19:18:49 +0200

    task almost done, need to handle when input is large

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,40 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newState = [];
+  let recentState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        recentState = {
+          ...recentState,
+          ...action.extraData,
+        };
+
+        newState.push({
+          ...recentState,
+        });
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete recentState[key];
+        }
+        newState.push({ ...recentState });
+        break;
+
+      case 'clear':
+        newState.push({});
+        break;
+
+      default:
+        newState.push({});
+        break;
+    }
+  }
+
+  return newState;
 }
 
 module.exports = transformStateWithClones;
