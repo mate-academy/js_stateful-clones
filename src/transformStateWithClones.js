66bbad4bd19efa46a31046b7951aecbd404a41c6
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-05-17 12:36:43 +0300

    solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,60 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const res = [];
+  const copy = { ...state };
+
+  for (const action of actions) {
+    for (const key in action) {
+      if (action[key] === 'clear') {
+        for (const keyState in copy) {
+          delete copy[keyState];
+        }
+
+        const innerCopy = { ...copy };
+
+        res.push(innerCopy);
+      }
+
+      if (action[key] === 'removeProperties') {
+        const values = [];
+
+        for (const keyAct in action) {
+          values.push(action[keyAct]);
+        }
+
+        for (let j = 0; j < values[1].length; j++) {
+          for (const keyState in copy) {
+            if (keyState === values[1][j]) {
+              delete copy[keyState];
+            }
+          }
+        }
+
+        const innerCopy = { ...copy };
+
+        res.push(innerCopy);
+      }
+
+      if (action[key] === 'addProperties') {
+        const values = [];
+
+        for (const keyAct in action) {
+          values.push(action[keyAct]);
+        }
+
+        for (const addKey in values[1]) {
+          copy[addKey] = values[1][addKey];
+        }
+
+        const innerCopy = { ...copy };
+
+        res.push(innerCopy);
+      }
+    }
+  }
+
+  return res;
 }
 
 module.exports = transformStateWithClones;
