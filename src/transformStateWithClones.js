834fe9a13a2975c4f2962d22300d4429f3062e7f
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-05-18 18:29:53 +0300

    fix

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -9,54 +9,29 @@
 function transformStateWithClones(state, actions) {
   const res = [];
   const copy = { ...state };
+  let innerCopy;
 
   for (const action of actions) {
-    for (const key in action) {
-      if (action[key] === 'clear') {
+    switch (action.type) {
+      case 'clear':
         for (const keyState in copy) {
           delete copy[keyState];
         }
-
-        const innerCopy = { ...copy };
-
+        innerCopy = { ...copy };
         res.push(innerCopy);
-      }
-
-      if (action[key] === 'removeProperties') {
-        const values = [];
-
-        for (const keyAct in action) {
-          values.push(action[keyAct]);
+        break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete copy[key];
         }
-
-        for (let j = 0; j < values[1].length; j++) {
-          for (const keyState in copy) {
-            if (keyState === values[1][j]) {
-              delete copy[keyState];
-            }
-          }
-        }
-
-        const innerCopy = { ...copy };
-
+        innerCopy = { ...copy };
         res.push(innerCopy);
-      }
-
-      if (action[key] === 'addProperties') {
-        const values = [];
-
-        for (const keyAct in action) {
-          values.push(action[keyAct]);
-        }
-
-        for (const addKey in values[1]) {
-          copy[addKey] = values[1][addKey];
-        }
-
-        const innerCopy = { ...copy };
-
+        break;
+      case 'addProperties':
+        Object.assign(copy, action.extraData);
+        innerCopy = { ...copy };
         res.push(innerCopy);
-      }
+        break;
     }
   }
 
