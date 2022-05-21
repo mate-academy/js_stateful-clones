dda47b77d5d13d03c98bd823071f93e1ce4895a9
Artem Furdela <furdela.artem@gmail.com>
2022-05-21 19:00:15 +0300

    tranformStateWithClones first commit

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,36 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const logArray = [];
+  const newState = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(newState, action.extraData);
+      logArray.push({ ...newState });
+      continue;
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete newState[key];
+      }
+
+      logArray.push({ ...newState });
+      continue;
+    }
+
+    if (action.type === 'clear') {
+      for (const key in newState) {
+        delete newState[key];
+      }
+
+      logArray.push({});
+      continue;
+    }
+  }
+
+  return logArray;
 }
 
 module.exports = transformStateWithClones;
