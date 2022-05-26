2ac3ed4a6db2950514b564a017679f6684fb4b8d
Denys Okhremenko <den.okhremenko@gmail.com>
2022-05-26 12:55:34 +0300

    test 2 faild

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,90 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+
+  for (const i in actions) {
+    if (actions[i].type === 'addProperties') {
+      result[i] = { ...state };
+
+      for (const key in actions[i].extraData) {
+        result[i][key] = actions[i].extraData[key];
+      }
+    }
+
+    if (actions[i].type === 'removeProperties') {
+      result[i] = { ...state };
+
+      for (const key in actions[i].keysToRemove) {
+        delete result[i][actions[i].keysToRemove[key]];
+      }
+    }
+
+    if (actions[i].type === 'clear') {
+      result[i] = {};
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
+
+/* function transformStateWithClones(state, actions) {
+  const result = [];
+
+  for (const i in actions) {
+    if (actions[i].type === 'addProperties') {
+      result[i] = { ...state };
+
+      for (const key in actions[i].extraData) {
+        result[i][key] = actions[i].extraData[key];
+      }
+    }
+
+    if (actions[i].type === 'removeProperties') {
+      result[i] = { ...state };
+
+      for (const key in actions[i].keysToRemove) {
+        delete result[i][actions[i].keysToRemove[key]];
+      }
+    }
+
+    if (actions[i].type === 'clear') {
+      result[i] = {};
+    }
+  }
+
+  return result;
+} */
+
+/*
+function transformStateWithClones(state, actions) {
+  const result = [];
+
+  result[0] = { ...state };
+
+  for (const i in actions) {
+    if (actions[i].type === 'addProperties') {
+      result[i] = { ...result[i - 1] };
+
+      for (const key in actions[i].extraData) {
+        result[i][key] = actions[i].extraData[key];
+      }
+    }
+
+    if (actions[i].type === 'removeProperties') {
+      result[i] = { ...result[i - 1] };
+
+      for (const key in actions[i].keysToRemove) {
+        delete result[i][actions[i].keysToRemove[key]];
+      }
+    }
+
+    if (actions[i].type === 'clear') {
+      result[i] = {};
+    }
+  }
+
+  return result;
+} */
