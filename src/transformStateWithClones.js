2c37f08ef00f189a1e1e2ba4bbc1d25a94eddbee
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2022-05-26 12:54:24 +0300

    statefulCloneFirstCommit

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,36 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const newState = [];
+  const clone = { ...state };
+  let action = {};
+  let removeObject = '';
+
+  for (const ch of actions) {
+    action = ch;
+
+    if (action.type === 'addProperties') {
+      Object.assign(clone, action.extraData);
+    }
+
+    for (const remove in action.keysToRemove) {
+      removeObject = action.keysToRemove[remove];
+
+      if (action.type === 'removeProperties') {
+        delete clone[removeObject];
+      }
+    }
+
+    for (const key in clone) {
+      if (action.type === 'clear') {
+        delete clone[key];
+      }
+    }
+
+    newState.push({ ...clone });
+  }
+
+  return newState;
 }
 
 module.exports = transformStateWithClones;
