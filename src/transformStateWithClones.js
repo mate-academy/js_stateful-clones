8453ae06320c2f6b3f28847f5f1e5a419cc20a61
Denys Okhremenko <den.okhremenko@gmail.com>
2022-05-27 10:16:17 +0300

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,89 +8,30 @@
  */
 function transformStateWithClones(state, actions) {
   const result = [];
+  let currentObj = { ...state };
 
-  for (const i in actions) {
+  for (let i = 0; i < actions.length; i++) {
     if (actions[i].type === 'addProperties') {
-      result[i] = { ...state };
-
       for (const key in actions[i].extraData) {
-        result[i][key] = actions[i].extraData[key];
+        currentObj[key] = actions[i].extraData[key];
       }
     }
 
     if (actions[i].type === 'removeProperties') {
-      result[i] = { ...state };
-
       for (const key in actions[i].keysToRemove) {
-        delete result[i][actions[i].keysToRemove[key]];
+        delete currentObj[actions[i].keysToRemove[key]];
       }
     }
 
     if (actions[i].type === 'clear') {
-      result[i] = {};
+      currentObj = {};
     }
+
+    result[i] = currentObj;
+    currentObj = { ...result[i] };
   }
 
   return result;
 }
 
 module.exports = transformStateWithClones;
-
-/* function transformStateWithClones(state, actions) {
-  const result = [];
-
-  for (const i in actions) {
-    if (actions[i].type === 'addProperties') {
-      result[i] = { ...state };
-
-      for (const key in actions[i].extraData) {
-        result[i][key] = actions[i].extraData[key];
-      }
-    }
-
-    if (actions[i].type === 'removeProperties') {
-      result[i] = { ...state };
-
-      for (const key in actions[i].keysToRemove) {
-        delete result[i][actions[i].keysToRemove[key]];
-      }
-    }
-
-    if (actions[i].type === 'clear') {
-      result[i] = {};
-    }
-  }
-
-  return result;
-} */
-
-/*
-function transformStateWithClones(state, actions) {
-  const result = [];
-
-  result[0] = { ...state };
-
-  for (const i in actions) {
-    if (actions[i].type === 'addProperties') {
-      result[i] = { ...result[i - 1] };
-
-      for (const key in actions[i].extraData) {
-        result[i][key] = actions[i].extraData[key];
-      }
-    }
-
-    if (actions[i].type === 'removeProperties') {
-      result[i] = { ...result[i - 1] };
-
-      for (const key in actions[i].keysToRemove) {
-        delete result[i][actions[i].keysToRemove[key]];
-      }
-    }
-
-    if (actions[i].type === 'clear') {
-      result[i] = {};
-    }
-  }
-
-  return result;
-} */
