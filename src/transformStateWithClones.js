8caa7497ae294681242b08b04a31007f02a363d8
Volodymyr Shymanskyi <v.shimanskyi@bnb.net.ua>
2022-06-02 16:43:53 +0300

    1-st commit

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let result = { ...state };
+  const resArray = [];
+
+  for (const act of actions) {
+    if (act.type === 'addProperties') {
+      Object.assign(result, act.extraData);
+      resArray.push({ ...result });
+      continue;
+    }
+
+    if (act.type === 'removeProperties') {
+      for (const del of act.keysToRemove) {
+        delete result[del];
+      }
+
+      resArray.push({ ...result });
+      continue;
+    }
+
+    result = {};
+    resArray.push({ ...result });
+  }
+
+  return resArray;
 }
 
 module.exports = transformStateWithClones;
