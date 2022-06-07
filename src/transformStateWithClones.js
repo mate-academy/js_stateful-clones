bc1ebf34ec3c1c5cc5dbe8b23c6dbf5c8a12487a
Julia Makarenko <julys.makarenko@gmail.com>
2022-06-07 17:33:25 +0300

    Add stateful clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newState = [];
+  const cloneState = { ...state };
+
+  actions.forEach((action) => {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(cloneState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const keys of action.keysToRemove) {
+          delete cloneState[keys];
+        }
+        break;
+
+      case 'clear':
+        for (const keys in cloneState) {
+          delete cloneState[keys];
+        }
+        break;
+    }
+
+    newState.push({ ...cloneState });
+  });
+
+  return newState;
 }
 
 module.exports = transformStateWithClones;
