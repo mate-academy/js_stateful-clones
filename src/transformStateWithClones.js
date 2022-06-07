47cd64cd44e1b91958e65d930adac1a8d0cd8b95
Yaroslav Baiduk <byaa99@gmail.com>
2022-06-07 18:18:11 +0300

    Killme

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,40 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let newObj = {
+    ...state,
+  };
+  const newArray = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(newObj, action.extraData);
+
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          if (newObj[key]) {
+            delete newObj[key];
+          }
+        }
+
+        break;
+
+      case 'clear':
+        newObj = {};
+
+        break;
+
+      default:
+        return null;
+    }
+
+    newArray.push({ ...newObj });
+  }
+
+  return newArray;
 }
 
 module.exports = transformStateWithClones;
