5bc621e8f5785f4dd97710c0a82a7fbc4cd8f1f8
Max Dumiak <maxdumiak39@gmail.com>
2022-06-07 16:56:23 +0300

    Clones - ready

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,33 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const obj = { ...state };
+  const arrayResult = [];
+
+  for (const prop of actions) {
+    switch (prop.type) {
+      case 'addProperties':
+        (Object.assign(obj, prop.extraData));
+        break;
+
+      case 'removeProperties':
+        for (const value of prop.keysToRemove) {
+          delete obj[value];
+        }
+        break;
+
+      case 'clear':
+        for (const key in obj) {
+          delete obj[key];
+        }
+        break;
+    }
+    arrayResult.push({ ...obj });
+  }
+
+  return arrayResult;
 }
 
 module.exports = transformStateWithClones;
