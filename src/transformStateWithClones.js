ae11246039a544c8b6470e939104dfecbc7427ee
Denys Politov <dpolitov2001@gmail.com>
2022-06-08 12:23:46 +0300

    Completed the task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,41 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const resultStates = [];
+
+  for (let i = 0; i < actions.length; i++) {
+    if (i === 0) {
+      resultStates.push(
+        {
+          ...state,
+        }
+      );
+    } else {
+      resultStates.push(
+        {
+          ...resultStates[i - 1],
+        }
+      );
+    }
+
+    if (actions[i].type === 'addProperties') {
+      Object.assign(resultStates[i], actions[i].extraData);
+    }
+
+    if (actions[i].type === 'removeProperties') {
+      for (const key of actions[i].keysToRemove) {
+        delete resultStates[i][key];
+      }
+    }
+
+    if (actions[i].type === 'clear') {
+      for (const key in resultStates[i]) {
+        delete resultStates[i][key];
+      }
+    }
+  }
+
+  return resultStates;
 }
 
 module.exports = transformStateWithClones;
