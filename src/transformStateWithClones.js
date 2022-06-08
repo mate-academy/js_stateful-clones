b8fe319f4528f3c07305b9c91bbbe5814ec8e5d8
Kirill Yarin <logisticyarin@gmail.com>
2022-06-08 23:33:27 +0300

    not all

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,37 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const states = [];
+  let prevState = state;
+
+  for (let index = 0; index < actions.length; index++) {
+    const action = actions[index];
+    let newState;
+
+    if (actions.type === 'addProperties') {
+      newState = {
+        ...prevState,
+        ...action.extraData,
+      };
+    }
+
+    if (action.type === 'removeProperties') {
+      newState = { ...prevState };
+
+      for (const key of action.keysToRemove) {
+        delete newState[key];
+      }
+    }
+
+    if (action.type === 'clear') {
+      newState = {};
+    }
+
+    prevState = newState;
+    states.push(newState);
+  }
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
