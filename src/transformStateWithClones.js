4e3e7c0b0b1d1aef4e12e31ed1e4012cdddd442b
Luidmyla Omelchenko <lui.omelika@gmail.com>
2022-06-08 20:32:01 +0300

    finished transform function

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,42 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let prevState = state;
+
+  for (const action of actions) {
+    let newState = transform (prevState, action);
+    result.push(newState);
+    prevState = newState;
+  }
+
+  return result;
+}
+
+function transform (prevState, action) {
+  switch (action.type) {
+    case 'clear':
+      return {};
+
+    case 'addProperties':
+      return {
+        ...prevState,
+        ...action.extraData,
+      };
+
+    case 'removeProperties': {
+      const newState = { ...prevState };
+
+      for (let keys of action.keysToRemove) {
+        delete newState[keys];
+      }
+
+      return newState;
+    }
+
+    default:
+      return prevState;
+  }
 }
 
 module.exports = transformStateWithClones;
