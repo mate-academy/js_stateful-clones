c70bb07bc2cb280952c3f83791d6ad014a674572
Andriy Hamernyk <0986758477and@gmail.com>
2022-06-08 19:41:56 +0300

    Clone created

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,35 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const copy = { ...state };
+  const newState = [];
+
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
+      case 'addProperties':
+        Object.assign(copy, extraData);
+
+        break;
+
+      case 'removeProperties':
+        for (const prop of keysToRemove) {
+          delete copy[prop];
+        }
+
+        break;
+
+      case 'clear':
+        for (const elm in copy) {
+          delete copy[elm];
+        }
+    }
+
+    newState.push({ ...copy });
+  }
+
+  return newState;
 }
 
 module.exports = transformStateWithClones;
