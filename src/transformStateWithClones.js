cbaa9a300002dfa76e38cd7663a3d4bbab09557b
PaulRodriguez2049 <paul.rodriguez.2049@gmail.com>
2022-06-14 14:40:19 +0300

    first commit

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,33 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const newState = { ...state };
+  const array = [];
+
+  for (const object of actions) {
+    if (object.type === `addProperties`) {
+      Object.assign(newState, object.extraData);
+      array.push({ ...newState });
+    }
+
+    if (object.type === `clear`) {
+      for (const key in newState) {
+        delete newState[key];
+      }
+      array.push({ ...newState });
+    }
+
+    if (object.type === `removeProperties`) {
+      object.keysToRemove.forEach((el) => {
+        if (newState[el]) {
+          delete newState[el];
+        }
+      });
+      array.push({ ...newState });
+    }
+  }
+
+  return array;
 }
 
 module.exports = transformStateWithClones;
