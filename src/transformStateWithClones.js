8f8a34b5c921eda9dc6cd7b1a3497937b9598900
IvanG6 <64322485+IvanG6@users.noreply.github.com>
2022-06-17 13:27:12 +0300

    switched

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,23 +11,27 @@ function transformStateWithClones(state, actions) {
   const res = [];
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      for (const key of Object.keys(action.extraData)) {
-        stateClone[key] = action.extraData[key];
-      }
-    }
-
-    if (action.type === 'removeProperties') {
-      for (const keyToRemove of action.keysToRemove) {
-        if (stateClone[keyToRemove]) {
-          delete stateClone[keyToRemove];
+    switch (action.type) {
+      case 'addProperties': {
+        for (const key of Object.keys(action.extraData)) {
+          stateClone[key] = action.extraData[key];
         }
+        break;
       }
-    }
 
-    if (action.type === 'clear') {
-      for (const keyToRm of Object.keys(stateClone)) {
-        delete stateClone[keyToRm];
+      case 'removeProperties': {
+        for (const keyToRemove of action.keysToRemove) {
+          if (stateClone[keyToRemove]) {
+            delete stateClone[keyToRemove];
+          }
+        }
+        break;
+      }
+
+      case 'clear': {
+        for (const keyToRm of Object.keys(stateClone)) {
+          delete stateClone[keyToRm];
+        }
       }
     }
     res.push({ ...stateClone });
