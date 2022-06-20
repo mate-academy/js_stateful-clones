ac77ba5655a27abde3392fc292e4614866b6e4f5
Max Shylenko <maxshylenko@gmail.com>
2022-06-20 21:11:34 +0200

    stateful clones done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,39 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const copyOfState = { ...state };
+  const result = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(copyOfState, action.extraData);
+        result.push({ ...copyOfState });
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          if (!copyOfState[key]) {
+            continue;
+          }
+
+          delete copyOfState[key];
+        }
+
+        result.push({ ...copyOfState });
+        break;
+
+      case 'clear':
+        for (const key in copyOfState) {
+          delete copyOfState[key];
+        }
+
+        result.push({ ...copyOfState });
+        break;
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
