7b793481cf8dca2a8bc96a5437b829751e0e29ba
Max Shylenko <maxshylenko@gmail.com>
2022-06-21 21:30:59 +0200

    corrected

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -14,29 +14,22 @@ function transformStateWithClones(state, actions) {
     switch (action.type) {
       case 'addProperties':
         Object.assign(copyOfState, action.extraData);
-        result.push({ ...copyOfState });
         break;
 
       case 'removeProperties':
         for (const key of action.keysToRemove) {
-          if (!copyOfState[key]) {
-            continue;
-          }
-
           delete copyOfState[key];
         }
-
-        result.push({ ...copyOfState });
         break;
 
       case 'clear':
         for (const key in copyOfState) {
           delete copyOfState[key];
         }
-
-        result.push({ ...copyOfState });
         break;
     }
+
+    result.push({ ...copyOfState });
   }
 
   return result;
