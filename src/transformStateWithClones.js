2ac14875cc76a1c411b139a2b4326091ddba99ee
Leonid Levtseniuk <llevtseniuk@gmail.com>
2022-06-21 14:20:12 +0300

    Clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const clone = { ...state };
+  const cloneStatus = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(clone, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete clone[key];
+        }
+
+        break;
+
+      case 'clear':
+        for (const prop in clone) {
+          delete clone[prop];
+        }
+
+        break;
+    }
+    cloneStatus.push(Object.assign({ ...clone }, action.extraData));
+  }
+
+  return cloneStatus;
 }
 
 module.exports = transformStateWithClones;
