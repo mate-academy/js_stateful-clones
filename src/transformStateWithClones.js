5e406e87c8716f7829a34198e19609bb4bba87d5
Anton Chetveryk <chetverykantonma@gmail.com>
2022-06-22 13:49:41 +0300

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let curState = { ...state };
+  const statesArr = [];
+
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    if (type === 'addProperties') {
+      curState = {
+        ...curState,
+        ...extraData,
+      };
+    }
+
+    if (type === 'clear') {
+      curState = {};
+    }
+
+    if (type === 'removeProperties') {
+      for (const key of keysToRemove) {
+        if (key in curState) {
+          delete curState[key];
+        }
+      }
+    }
+    statesArr.push({ ...curState });
+  }
+
+  return statesArr;
 }
 
 module.exports = transformStateWithClones;
