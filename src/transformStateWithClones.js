b92242a4c5324cdcd0f42d293a3fbfb2b1ebe7d9
Aleksandr Kovalenko <dias.koval@gmail.com>
2022-07-01 17:49:18 +0300

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,28 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const sum = [];
+  let total = { ...state };
+
+  for (const key of actions) {
+    if (key.type === 'addProperties') {
+      Object.assign(total, key.extraData);
+    }
+
+    if (key.type === 'removeProperties') {
+      for (const i of key.keysToRemove) {
+        delete total[i];
+      }
+    }
+
+    if (key.type === 'clear') {
+      total = {};
+    }
+
+    sum.push({ ...total });
+  }
+
+  return sum;
 }
 
 module.exports = transformStateWithClones;
