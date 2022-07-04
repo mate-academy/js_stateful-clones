bdfbbe40d214a8fd69b7d0ee189fc17ceee0df40
Denys Danyliuk <denysdanyliuuk@gmail.com>
2022-07-04 15:45:30 +0300

    solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,4 +1,5 @@
 'use strict';
+/* eslint-disable */
 
 /**
  * @param {Object} state
@@ -7,7 +8,38 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  const newArr = [];
+
+  for (const { type, extraData, keysToRemove } of actions) {
+    switch (type) {
+      case 'addProperties':
+        Object.assign(stateCopy, extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of keysToRemove) {
+          delete stateCopy[key];
+        }
+
+        break;
+
+      case 'clear':
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
+
+        break;
+
+      default:
+        break;
+    }
+
+    newArr.push(Object.assign({}, stateCopy));
+  }
+
+
+  return newArr;
 }
 
 module.exports = transformStateWithClones;
