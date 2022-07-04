7e1fdcf25aec478ba206a2ec5d715566748257ee
Denys Korotkoruchko <denenergii@gmail.com>
2022-07-04 16:16:38 +0300

    ready

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,29 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  const arr = [];
+
+  for (const element of actions) {
+    if (element.type === 'addProperties') {
+      arr.push(Object.assign({}, stateCopy, element['extraData']));
+      Object.assign(stateCopy, element['extraData']);
+    } else if (element.type === 'removeProperties') {
+      for (const key of element['keysToRemove']) {
+        delete stateCopy[key];
+      }
+      arr.push(Object.assign({}, stateCopy));
+    } else if (element.type === 'clear') {
+      for (const key in stateCopy) {
+        delete stateCopy[key];
+      }
+      arr.push({});
+    }
+  }
+
+  return (arr);
 }
 
 module.exports = transformStateWithClones;
