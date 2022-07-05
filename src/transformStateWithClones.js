f1599d4440111a231f7ba9eb3bbabdf1b7077d72
Mykhailo Oblat <moblat890@gmail.com>
2022-07-05 15:12:36 +0300

    task js_stateful-clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const newState = { ...state };
+
+  for (const key of actions) {
+    if (key.type === 'addProperties') {
+      Object.assign(newState, key.extraData);
+    }
+
+    if (key.type === 'removeProperties') {
+      for (const keyToDel of key.keysToRemove) {
+        delete newState[keyToDel];
+      }
+    }
+
+    if (key.type === 'clear') {
+      for (const keyNewState in newState) {
+        delete newState[keyNewState];
+      }
+    }
+
+    result.push({ ...newState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
