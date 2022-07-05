609369dd0596cd58543d9f3607ccbafe6daaaedb
Dmitriy Teteryn <dmitriy.teteryn@gmail.com>
2022-07-05 15:58:49 +0300

    fix const name

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,28 +7,28 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const copy = { ...state };
+  const copyState = { ...state };
   const arrOfClone = [];
 
   actions.forEach(action => {
     switch (action.type) {
       case 'addProperties':
-        Object.assign(copy, action.extraData);
+        Object.assign(copyState, action.extraData);
         break;
 
       case 'removeProperties':
         action.keysToRemove.forEach(key => {
-          delete copy[key];
+          delete copyState[key];
         });
         break;
 
       case 'clear':
-        for (const key in copy) {
-          delete copy[key];
+        for (const key in copyState) {
+          delete copyState[key];
         }
         break;
     }
-    arrOfClone.push({ ...copy });
+    arrOfClone.push({ ...copyState });
   });
 
   return arrOfClone;
