67e4ff8af368c756a5a2382780811afadd4bd80e
Oleksandr-Pustovit <Pustovit.o2020@gmail.com>
2022-07-05 12:30:27 +0300

    refactored code with switch

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,20 +11,20 @@ function transformStateWithClones(state, actions) {
   const statesHistory = [];
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(prototype, action.extraData);
-    }
-
-    if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete prototype[key];
-      }
-    }
-
-    if (action.type === 'clear') {
-      for (const key in prototype) {
-        delete prototype[key];
-      }
+    switch (true) {
+      case action.type === 'addProperties':
+        Object.assign(prototype, action.extraData);
+        break;
+      case action.type === 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete prototype[key];
+        }
+        break;
+      case action.type === 'clear':
+        for (const key in prototype) {
+          delete prototype[key];
+        }
+        break;
     }
 
     statesHistory.push(Object.assign({}, prototype));
