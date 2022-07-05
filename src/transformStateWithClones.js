80a41e871a6e1d140e86e58c63d87d1176b4a4b5
Denys Korotkoruchko <denenergii@gmail.com>
2022-07-05 14:24:23 +0300

    fixed

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,28 +8,28 @@
  */
 
 function transformStateWithClones(state, actions) {
-  const stateCopy = { ...state };
+  const objectForUse = { ...state };
   const arr = [];
 
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties':
-        Object.assign(stateCopy, action['extraData']);
+        Object.assign(objectForUse, action.extraData);
         break;
 
       case 'removeProperties':
-        for (const key of action['keysToRemove']) {
-          delete stateCopy[key];
+        for (const key of action.keysToRemove) {
+          delete objectForUse[key];
         };
         break;
 
       case 'clear':
-        for (const key in stateCopy) {
-          delete stateCopy[key];
+        for (const key in objectForUse) {
+          delete objectForUse[key];
         };
         break;
     }
-    arr.push(Object.assign({}, stateCopy));
+    arr.push(Object.assign({}, objectForUse));
   }
 
   return (arr);
