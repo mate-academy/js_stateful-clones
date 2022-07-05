4bc41b61b11234e9a5a1364876d7c34257cadf42
Yaroslav Lizogub <lizogub.yar@gmail.com>
2022-07-05 20:06:44 +0300

    added throw error in default case

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -32,7 +32,7 @@ function transformStateWithClones(state, actions) {
         break;
 
       default:
-        break;
+        throw new Error('Wrong action type');
     }
 
     states.push({ ...stateCopy });
