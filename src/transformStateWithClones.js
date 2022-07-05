9fcbfd684224281ff909869cd5eae95c7557c408
DashaPoliazz <dasha.poliazzz@gmail.com>
2022-07-05 13:39:49 +0300

    refactored to 'swich case'

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -14,18 +14,23 @@ function transformStateWithClones(state, actions) {
   actions.forEach(action => {
     currentState = { ...currentState };
 
-    if (action.type === 'addProperties') {
-      Object.assign(currentState, action.extraData);
-    } else if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        if (key in currentState) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(currentState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          if (key in currentState) {
+            delete currentState[key];
+          }
+        }
+        break;
+
+      case 'clear':
+        for (const key of Object.keys(currentState)) {
           delete currentState[key];
         }
-      }
-    } else if (action.type === 'clear') {
-      for (const key of Object.keys(currentState)) {
-        delete currentState[key];
-      }
     }
 
     stateHistory.push(currentState);
