af012bbcc29c01ddb658de84c1257b943d227682
Gena Kupriiuk <gamesgolden04@gmail.com>
2022-07-06 17:46:22 +0300

    destructuring in loop added

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,14 +11,14 @@ function transformStateWithClones(state, actions) {
 
   const stateCopy = { ...state };
 
-  for (const action of actions) {
-    switch (action.type) {
+  for (const { type, extraData, keysToRemove } of actions) {
+    switch (type) {
       case 'addProperties':
-        Object.assign(stateCopy, action.extraData);
+        Object.assign(stateCopy, extraData);
         break;
 
       case 'removeProperties':
-        for (const keyToRemove of action.keysToRemove) {
+        for (const keyToRemove of keysToRemove) {
           delete stateCopy[keyToRemove];
         }
         break;
