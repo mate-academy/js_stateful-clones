54cce8fb5b2fb17bcba212aba05b65f9409e7f0e
fenderukr <priststoreua@gmail.com>
2022-07-08 02:00:55 +0300

    transformStateWithClones0.1

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = Object.assign({}, state);
+  const newArr = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        newArr.push({ ...stateCopy });
+        break;
+      case 'removeProperties':
+        for (const keyToRemove of action.keysToRemove) {
+          delete stateCopy[keyToRemove];
+        }
+        newArr.push({ ...stateCopy });
+        break;
+
+      case 'clear':
+        for (const key in stateCopy) {
+          if (stateCopy.hasOwnProperty(key)) {
+            delete stateCopy[key];
+          }
+        }
+        newArr.push({ ...stateCopy });
+    }
+  }
+
+  return newArr;
 }
 
 module.exports = transformStateWithClones;
