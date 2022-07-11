a93b4e13ffae668eda9a9e8d3761bca3b42e1566
Yura Atamaniuk <atam3905@gmail.com>
2022-07-11 14:00:42 +0300

    Clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,35 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const previousStates = [];
+  const copyState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(copyState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete copyState[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in copyState) {
+          delete copyState[key];
+        }
+        break;
+
+      default:
+        return null;
+    }
+
+    previousStates.push({ ...copyState });
+  }
+
+  return previousStates;
 }
 
 module.exports = transformStateWithClones;
