617e07714eb763fcdfbd22390555c55a4ac404c2
Boris Kerekesha <kerekesha90@ukr.net>
2022-07-15 10:49:29 +0300

    state clone

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,48 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let cloneState = { ...state };
+  const arrayState = [];
+
+  for (const property of actions) {
+    if (property.type === 'addProperties') {
+      const addPropertiesObject = {};
+
+      for (const key in property.extraData) {
+        addPropertiesObject[key] = property.extraData[key];
+      }
+
+      const addCloneState = {
+        ...cloneState,
+        ...addPropertiesObject,
+      };
+
+      arrayState.push(addCloneState);
+      cloneState = { ...addCloneState };
+    }
+
+    if (property.type === 'removeProperties') {
+      const removeObject = { ...cloneState };
+
+      for (const keyremove of property.keysToRemove) {
+        if (keyremove in removeObject) {
+          delete removeObject[keyremove];
+        }
+      }
+
+      arrayState.push(removeObject);
+      cloneState = { ...removeObject };
+    }
+
+    if (property.type === 'clear') {
+      const emplyObject = {};
+
+      arrayState.push(emplyObject);
+      cloneState = {};
+    }
+  }
+
+  return arrayState;
 }
 
 module.exports = transformStateWithClones;
