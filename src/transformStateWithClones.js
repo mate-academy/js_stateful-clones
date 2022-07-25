d66d3da3101329f9ca3876c3982d6350716f9f57
Dmytro Sheremetiev <dmytriy.sheremetiev@gmail.com>
2022-07-25 09:12:52 +0300

    First commit

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,35 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const cloneArray = [];
+  let cloneState = { ...state };
+
+  for (const item of actions) {
+    switch (item.type) {
+      case 'addProperties':
+        for (const key in item.extraData) {
+          cloneState[key] = item.extraData[key];
+        }
+
+        cloneArray.push({ ...cloneState });
+        break;
+
+      case 'removeProperties':
+        for (const key of item.keysToRemove) {
+          delete cloneState[key];
+        }
+
+        cloneArray.push({ ...cloneState });
+        break;
+
+      case 'clear':
+        cloneState = {};
+        cloneArray.push({ ...cloneState });
+        break;
+    }
+  }
+
+  return cloneArray;
 }
 
 module.exports = transformStateWithClones;
