f8a01bb517be4c0556b005025cae5ab90eae4b85
Yevhen Pavlovskyi <pavlovskiy07@gmail.com>
2022-07-26 16:52:43 +0300

    all done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,27 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const copyState = [];
+  let totalObject = Object.assign({}, state);
+
+  actions.forEach(action => {
+    if (action.type === 'addProperties') {
+      Object.assign(totalObject, action.extraData);
+    }
+
+    if (action.type === 'clear') {
+      totalObject = {};
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete totalObject[key];
+      };
+    }
+    copyState.push({ ...totalObject });
+  });
+
+  return copyState;
 }
 
 module.exports = transformStateWithClones;
