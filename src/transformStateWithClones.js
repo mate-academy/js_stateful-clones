07b10bd6aab478942771cbcede08ef353669d7dd
Lesya Korniychuk <korniychuklesya@ukr.net>
2022-08-03 22:06:41 +0300

    create Stateful_clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let objCopy = Object.assign({}, state);
+  const newState = [];
+
+  for (let i = 0; i < actions.length; i++) {
+    switch (actions[i].type) {
+      case 'addProperties':
+        Object.assign(objCopy, actions[i].extraData);
+        break;
+
+      case 'removeProperties':
+        for (const keyRemove of actions[i].keysToRemove) {
+          delete objCopy[keyRemove];
+        }
+        break;
+
+      case 'clear':
+        for (const keyClear in objCopy) {
+          delete objCopy[keyClear];
+        }
+        break;
+    }
+    newState.push(objCopy);
+    objCopy = Object.assign({}, objCopy);
+  }
+
+  return newState;
 }
 
 module.exports = transformStateWithClones;
