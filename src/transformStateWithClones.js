07377c25909508e1d8f92f09a3ecfbf025094f85
Adrian Faryniuk <forever0754@gmail.com>
2022-08-04 17:27:57 +0300

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const clone = { ...state };
+  const resArr = [];
+
+  for (const obj of actions) {
+    if (obj.type === 'addProperties') {
+      Object.assign(clone, obj.extraData);
+      resArr.push({ ...clone });
+    }
+
+    if (obj.type === 'removeProperties') {
+      const toRemove = obj.keysToRemove;
+
+      for (const rem of toRemove) {
+        delete clone[rem];
+      }
+      resArr.push({ ...clone });
+    }
+
+    if (obj.type === 'clear') {
+      for (const clear in clone) {
+        delete clone[clear];
+      }
+      resArr.push({ ...clone });
+    }
+  }
+
+  return resArr;
 }
 
 module.exports = transformStateWithClones;
