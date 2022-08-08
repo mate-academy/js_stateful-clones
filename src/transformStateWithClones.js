e37c0630fb9464ef5d544125e34da54848f0bf9f
Kostya Kosyuk <80011474+Kostya7337@users.noreply.github.com>
2022-08-09 00:43:40 +0300

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,42 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arr = [];
+
+  for (let i = 0; i < actions.length; i++) {
+    if (i === 0) {
+      arr.push({ ...state });
+    } else {
+      arr.push({ ...arr[i - 1] });
+    }
+
+    switch (actions[i][`type`]) {
+      case 'addProperties':
+        for (const property in actions[i][`extraData`]) {
+          arr[i][property] = actions[i][`extraData`][property];
+        }
+        break;
+
+      case 'removeProperties':
+        for (const property in actions[i][`keysToRemove`]) {
+          if (Object.keys(arr[i]).includes(actions[i][`keysToRemove`][property])) {
+            delete arr[i][actions[i][`keysToRemove`][property]];
+          }
+        }
+        break;
+
+      case 'clear':
+        for (const stateKey in arr[i]) {
+          delete arr[i][stateKey];
+        }
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
