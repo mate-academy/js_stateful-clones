39975f016e00f1b8c8b3688d8687ee2a30cb1322
Andrii Stiebalov <stiebalov@gmail.com>
2022-08-08 16:38:14 +0300

    transformStateWithClones was added

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,35 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arrOfChandes = [];
+  const newObj = { ...state };
+
+  for (const obj of actions) {
+    switch (obj.type) {
+      case 'addProperties' :
+        for (const key in obj.extraData) {
+          newObj[key] = obj.extraData[key];
+        }
+        arrOfChandes.push({ ...newObj });
+        break;
+
+      case 'removeProperties':
+        for (const j of obj.keysToRemove) {
+          delete newObj[j];
+        }
+        arrOfChandes.push({ ...newObj });
+        break;
+
+      case 'clear':
+        for (const key in newObj) {
+          delete newObj[key];
+        }
+        arrOfChandes.push({ ...newObj });
+        break;
+    }
+  }
+
+  return arrOfChandes;
 }
 
 module.exports = transformStateWithClones;
