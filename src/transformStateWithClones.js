984acc41c84cc8c2092c1cb789ebc45fad04f5dd
Maxim Gumeniuk <gumeniukmaks0510@gmail.com>
2022-08-08 17:54:41 +0300

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = {};
+
+  Object.assign(stateCopy, state);
+
+  const stateArr = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete stateCopy[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
+        break;
+    }
+    stateArr.push(Object.assign({}, stateCopy));
+  }
+
+  return stateArr;
 }
 
 module.exports = transformStateWithClones;
