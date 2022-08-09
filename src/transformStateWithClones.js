5ff3d1739b6cd61489f4e07fcde3c0445e8ab567
Vadls <vadim_tanasienko54541@ukr.net>
2022-08-09 20:22:31 +0300

    Reserve copy

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,81 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const history = [];
+  let lastState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties': {
+        // console.log('action:', action);
+
+        const { extraData } = action;
+
+        // console.log(extraData);
+
+        const stateCopy = {
+          ...lastState, ...extraData,
+        };
+
+        // console.log("lastState:", lastState);
+        // console.log('extraData:', extraData);
+        // console.log(stateCopy);
+        lastState = stateCopy;
+        break;
+      }
+
+      case 'removeProperties': {
+        const { keysToRemove } = action;
+
+        const stateCopy = { ...lastState };
+
+        for (const value of keysToRemove) {
+          delete stateCopy[value];
+        }
+        lastState = stateCopy;
+
+        break;
+      }
+
+      case 'clear': {
+        const stateEmpty = {};
+
+        lastState = stateEmpty;
+        break;
+      }
+
+      default: {
+        throw new Error(`Unknown action type: ${action.type}`);
+      }
+    }
+    history.push(lastState);
+  }
+  // console.log(lastState);
+  // console.log(history);
+
+  return history;
 }
+
+// const state = {
+//   foo: 'bar', bar: 'foo',
+// };
+
+// transformStateWithClones(state, [
+//   {
+//     type: 'addProperties',
+//     extraData: {
+//       name: 'Jim', hello: 'world',
+//     },
+//   },
+//   {
+//     type: 'clear',
+//   },
+//   {
+//     type: 'removeProperties', keysToRemove: ['bar', 'hello'],
+//   },
+//   {
+//     type: 'addProperties', extraData: { another: 'one' },
+//   },
+// ]);
 
 module.exports = transformStateWithClones;
