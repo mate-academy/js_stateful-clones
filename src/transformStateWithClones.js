3ef9210480a1da4f6a8bcbb41bc2597f6d82451e
Shmelyov Oleg <shmelyov.oleg13@gmail.com>
2022-08-09 14:49:49 +0300

    implemented a function with history

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const history = [];
+  const lastState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(lastState, action.extraData);
+        history.push({ ...lastState });
+        break;
+
+      case 'removeProperties' :
+        action.keysToRemove.forEach(key => {
+          delete lastState[key];
+        });
+        history.push({ ...lastState });
+        break;
+
+      case 'clear' :
+        Object.keys(lastState).forEach(key => {
+          delete lastState[key];
+        });
+        history.push({ ...lastState });
+        break;
+
+      default :
+        break;
+    }
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
