73bf6bd40b3a77bf157a62dfba44b60eb51197df
Oleh Mykolaichuk <ooolehmy@gmail.com>
2022-08-09 08:58:00 +0300

    Added object desructuring

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -10,22 +10,22 @@ function transformStateWithClones(state, actions) {
   const newState = { ...state };
   const newStateArr = [];
 
-  for (const i of actions) {
-    if (i.type === 'addProperties') {
-      for (const key in i.extraData) {
-        newState[key] = i.extraData[key];
+  for (const { type, extraData, keysToRemove } of actions) {
+    if (type === 'addProperties') {
+      for (const key in extraData) {
+        newState[key] = extraData[key];
       }
       newStateArr.push({ ...newState });
     }
 
-    if (i.type === 'removeProperties') {
-      for (const key of i.keysToRemove) {
+    if (type === 'removeProperties') {
+      for (const key of keysToRemove) {
         delete newState[key];
       }
       newStateArr.push({ ...newState });
     }
 
-    if (i.type === 'clear') {
+    if (type === 'clear') {
       for (const key in newState) {
         delete newState[key];
       }
