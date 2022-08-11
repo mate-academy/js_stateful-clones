0a5c84759b8b8c56e75595a270795e37c0ddb1a7
Oleksandr Sobchuk <osobchuk1@gmail.com>
2022-08-11 22:02:46 +0300

    StateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const resultArr = [];
+
+  const obj = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(obj, action.extraData);
+        resultArr.push({ ...obj });
+        break;
+
+      case 'removeProperties':
+        for (const remove of action.keysToRemove) {
+          delete obj[remove];
+        }
+        resultArr.push({ ...obj });
+        break;
+
+      case 'clear':
+        for (const key in obj) {
+          delete obj[key];
+        }
+        resultArr.push({ ...obj });
+        break;
+    }
+  }
+
+  return resultArr;
 }
 
 module.exports = transformStateWithClones;
