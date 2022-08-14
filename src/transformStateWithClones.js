3e6c03861263999c29ee733a56f34579ca91fe5e
Oleksandr Babmyndra <alphamonik1@gmail.com>
2022-08-14 13:49:39 +0300

    Transform state with clones was created

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,28 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const resultArr = [];
+  let localState = { ...state };
+
+  for (const action of actions) {
+    let stateAfterAction = { ...localState };
+
+    if (action.type === 'addProperties') {
+      resultArr.push(Object.assign(stateAfterAction, action.extraData));
+    } else if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete stateAfterAction[key];
+      }
+      resultArr.push(stateAfterAction);
+    } else if (action.type === 'clear') {
+      stateAfterAction = {};
+      resultArr.push(stateAfterAction);
+    }
+
+    localState = stateAfterAction;
+  }
+
+  return resultArr;
 }
 
 module.exports = transformStateWithClones;
