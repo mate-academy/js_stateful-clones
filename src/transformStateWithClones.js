c0c2b9e9947be776bd2efcbf6c9d1d135222f8d5
Anna Grechana <grechanaanna@gmial.com>
2022-08-14 14:55:46 +0300

    1st version

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let newstate = Object.assign({}, state);
+  const stateArray = [];
+
+  for (let i = 0; i < actions.length; i++) {
+    switch (actions[i].type) {
+      case 'addProperties':
+        stateArray.push(newstate);
+        Object.assign(newstate, actions[i].extraData);
+        newstate = Object.assign({}, newstate);
+        break;
+
+      case 'removeProperties':
+        stateArray.push(newstate);
+
+        for (const del of actions[i].keysToRemove) {
+          delete newstate[del];
+        }
+        newstate = Object.assign({}, newstate);
+        break;
+
+      case 'clear':
+        const clear = {};
+
+        stateArray.push(clear);
+        newstate = {};
+        break;
+    }
+  }
+
+  return stateArray;
 }
 
 module.exports = transformStateWithClones;
