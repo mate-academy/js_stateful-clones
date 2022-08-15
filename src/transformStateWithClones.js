4800009209a11adef13a45db20a1504135c15df6
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-08-15 16:02:26 +0300

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,55 @@
 'use strict';
 
 /**
- * @param {Object} state
+ * @param {Object} newState
  * @param {Object[]} actions
  *
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newState = { ...state };
+  const returnObjects = [];
+  let container;
+
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'clear') {
+      toClear();
+      container = Object.assign({}, newState);
+      returnObjects.push(container);
+    }
+
+    if (actions[i].type === 'removeProperties') {
+      toRemove(actions[i].keysToRemove);
+      container = Object.assign({}, newState);
+      returnObjects.push(container);
+    }
+
+    if (actions[i].type === 'addProperties') {
+      toAdd(actions[i].extraData);
+      container = Object.assign({}, newState);
+      returnObjects.push(container);
+    }
+  }
+
+  function toClear() {
+    for (const key in newState) {
+      delete newState[key];
+    }
+  }
+
+  function toRemove(x) {
+    for (const key in newState) {
+      if (x.includes(key)) {
+        delete newState[key];
+      }
+    }
+  }
+
+  function toAdd(x) {
+    Object.assign(newState, x);
+  }
+
+  return returnObjects;
 }
 
 module.exports = transformStateWithClones;
