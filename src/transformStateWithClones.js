783368603b94a0b7e1f10eeabe111e376f27410f
Oleksandr Malyshev <sanyokmalyshev@gmail.com>
2022-08-17 12:42:59 +0800

    transformStateWithClones done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,100 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const newObj = { ...state };
+  let secObj = {};
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      for (const [key, value] of Object.entries(action.extraData)) {
+        newObj[key] = value;
+      }
+      secObj = { ...newObj };
+      result.push(secObj);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete newObj[key];
+      }
+      secObj = { ...newObj };
+      result.push(secObj);
+    }
+
+    if (action.type === 'clear') {
+      for (const key of Object.keys(newObj)) {
+        delete newObj[key];
+      }
+      secObj = { ...newObj };
+      result.push(secObj);
+    }
+  }
+  // eslint-disable-next-line no-console
+  console.log(secObj);
+  // eslint-disable-next-line no-console
+  console.log(result);
+
+  return result;
 }
+
+// [
+//   {foo: 'bar', bar: 'foo', name: 'Jim', hello: 'world'},
+//   {foo: 'bar', name: 'Jim'},
+//   {foo: 'bar', name: 'Jim', another: 'one'}
+// ].
+
+// const states = {
+//   foo: 'bar',
+//   bar: 'foo',
+// };
+
+// const actionss = [
+//   {
+//     type: 'addProperties',
+//     extraData: {
+//       name: 'Jim',
+//       hello: 'world',
+//     },
+//   },
+//   {
+//     type: 'removeProperties',
+//     keysToRemove: ['bar', 'hello'],
+//   },
+//   {
+//     type: 'addProperties',
+//     extraData: {
+//       another: 'one',
+//     },
+//   },
+// ];
+
+// transformStateWithClones(states, actionss);
+
+// const states = {
+//   foo: 'bar', name: 'Jim', another: 'one',
+// };
+
+// transformStateWithClones(states, [
+//   {
+//     type: 'removeProperties', keysToRemove: ['another'],
+//   },
+//   { type: 'clear' },
+//   { type: 'clear' },
+//   { type: 'clear' },
+//   {
+//     type: 'addProperties', extraData: { yet: 'another property' },
+//   },
+//   { type: 'clear' },
+//   {
+//     type: 'addProperties',
+//     extraData: {
+//       foo: 'bar', name: 'Jim',
+//     },
+//   },
+//   {
+//     type: 'removeProperties', keysToRemove: ['name', 'hello'],
+//   },
+// ]);
 
 module.exports = transformStateWithClones;
