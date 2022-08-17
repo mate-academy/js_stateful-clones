682eb57ea413c98dae36030935ef13d5437708d1
Oleksandr <Khabailiuk@gmail.com>
2022-08-17 22:41:12 +0200

    commit message

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,32 @@
  *
  * @return {Object[]}
  */
-function transformStateWithClones(state, actions) {
-  // write code here
-}
 
+function transformStateWithClones(state, actions) {
+  const clone = [];
+  const copyState = { ...state };
+
+  for (const act of actions) {
+    switch (act.type) {
+      case 'addProperties':
+        Object.assign(copyState, act.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of act.keysToRemove) {
+          delete copyState[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in copyState) {
+          delete copyState[key];
+        }
+    }
+
+    clone.push({ ...copyState });
+  }
+
+  return clone;
+}
 module.exports = transformStateWithClones;
