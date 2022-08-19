7adce9236cf7879e3740d2347f6302102f68925d
EhorSaprykin <astralik1993@gmail.com>
2022-08-19 17:07:44 +0300

    Task done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const resultArr = [];
+  const clone = Object.assign({}, state);
+
+  for (const action in actions) {
+    const actionType = actions[action].type;
+    const removeArr = actions[action].keysToRemove;
+    const extraData = actions[action].extraData;
+
+    if (actionType === 'addProperties') {
+      Object.assign(clone, extraData);
+    } else if (actionType === 'removeProperties') {
+      for (const removeProp of removeArr) {
+        delete clone[removeProp];
+      }
+    } else if (actionType === 'clear') {
+      for (const key in clone) {
+        delete clone[key];
+      }
+    }
+    resultArr.push({ ...clone });
+  }
+
+  return resultArr;
 }
 
 module.exports = transformStateWithClones;
