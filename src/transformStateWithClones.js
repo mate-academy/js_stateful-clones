08411b2f12097a775793b5608a2a1dfa8cdd4c49
Vladislav Gogol <gogolsimon95@gmail.com>
2022-08-19 15:01:10 +0300

    Clone solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const states = [];
+  let rollingState = { ...state };
+
+  for (const action of actions) {
+    switch (action['type']) {
+      case 'addProperties':
+        Object.assign(rollingState, action['extraData']);
+        break;
+
+      case 'removeProperties':
+        for (const prop of action['keysToRemove']) {
+          delete rollingState[prop];
+        };
+        break;
+
+      default:
+        rollingState = {};
+        break;
+    }
+    states.push({ ...rollingState });
+  }
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
