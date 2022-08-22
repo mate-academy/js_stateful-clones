e232c0e51d35ddf16789ac059dde559de58cce7b
Aleks <offsset@gmail.com>
2022-08-22 20:44:32 +0300

    new

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,20 +11,26 @@ function transformStateWithClones(state, actions) {
   const resultStateM = [];
 
   for (const result of actions) {
-    if (result.type === 'addProperties') {
-      const mas = Object.keys(result.extraData);
+    switch (true) {
+      case (result.type === 'addProperties'):
+        const mas = Object.keys(result.extraData);
 
-      for (const key of mas) {
-        resultStateO[key] = result.extraData[key];
-      }
-    } else if (result.type === 'removeProperties') {
-      for (const key of result.keysToRemove) {
-        delete resultStateO[key];
-      }
-    } else if (result.type === 'clear') {
-      for (const key in resultStateO) {
-        delete resultStateO[key];
-      }
+        for (const key of mas) {
+          resultStateO[key] = result.extraData[key];
+        }
+        break;
+
+      case (result.type === 'removeProperties'):
+        for (const key of result.keysToRemove) {
+          delete resultStateO[key];
+        }
+        break;
+
+      case (result.type === 'clear'):
+        for (const key in resultStateO) {
+          delete resultStateO[key];
+        }
+        break;
     }
 
     const copy = { ...resultStateO };
