3eec084b0288633e0bbf62e2373cc577b3eed5a7
Mitriy Che <mitriyche@gmail>
2022-08-25 18:32:55 +0300

    Transform clone Objects

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const copyState = { ...state };
+  const unionInArray = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(copyState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete copyState[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key of Object.keys(copyState)) {
+          delete copyState[key];
+        }
+        break;
+    }
+    unionInArray.push({ ...copyState });
+  }
+
+  return unionInArray;
 }
 
 module.exports = transformStateWithClones;
