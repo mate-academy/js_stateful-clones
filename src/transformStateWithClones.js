e9716ae5e2fcc811bdbbdd8b9c3d8ea551cee565
EhorSaprykin <astralik1993@gmail.com>
2022-08-25 11:14:25 +0300

    remove if...else, add switch

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -10,21 +10,26 @@ function transformStateWithClones(state, actions) {
   const resultArr = [];
   const clone = Object.assign({}, state);
 
-  for (const action in actions) {
-    const actionType = actions[action].type;
-    const removeArr = actions[action].keysToRemove;
-    const extraData = actions[action].extraData;
+  for (const action of actions) {
+    const [actionType, removeArr, extraData] = [
+      action.type, action.keysToRemove, action.extraData];
 
-    if (actionType === 'addProperties') {
-      Object.assign(clone, extraData);
-    } else if (actionType === 'removeProperties') {
-      for (const removeProp of removeArr) {
-        delete clone[removeProp];
-      }
-    } else if (actionType === 'clear') {
-      for (const key in clone) {
-        delete clone[key];
-      }
+    switch (actionType) {
+      case 'addProperties':
+        Object.assign(clone, extraData);
+        break;
+
+      case 'removeProperties':
+        for (const removeProp of removeArr) {
+          delete clone[removeProp];
+        };
+        break;
+
+      case 'clear':
+        for (const key in clone) {
+          delete clone[key];
+        }
+        break;
     }
     resultArr.push({ ...clone });
   }
