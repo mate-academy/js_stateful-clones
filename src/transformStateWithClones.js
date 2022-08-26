3de95f9ec841b628677a7075d93502bcbc9256e5
naveed-shoukat <shoukat.naveed@gmail.com>
2022-08-26 22:36:52 +0300

    stateful_clones solution done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,39 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateClone = [];
+  const currentState = { ...state };
+
+  for (let i = 0; i < actions.length; i++) {
+    const { type } = actions[i];
+
+    switch (type) {
+      case 'addProperties':
+        for (const [key, value] of Object.entries(actions[i].extraData)) {
+          currentState[key] = value;
+        }
+        break;
+
+      case 'removeProperties':
+        for (const key of actions[i].keysToRemove) {
+          if (currentState[key]) {
+            delete currentState[key];
+          }
+        }
+        break;
+
+      default:
+        if (currentState) {
+          for (const key of Object.keys(currentState)) {
+            delete currentState[key];
+          }
+        }
+    }
+
+    stateClone.push({ ...currentState });
+  }
+
+  return stateClone;
 }
 
 module.exports = transformStateWithClones;
