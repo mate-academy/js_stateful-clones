1ce02486f79c42bb71d3777a7382987c444789c5
Kamal Tufekcic <kamal.tufekcic@gmail.com>
2022-08-31 02:12:26 +0300

    I see what you meant by clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const statePaste = { ...state };
+  const objState = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(statePaste, action.extraData);
+        break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete statePaste[key];
+        }
+        break;
+
+      default:
+        for (const property in statePaste) {
+          delete statePaste[property];
+        }
+    }
+    objState.push({ ...statePaste });
+  }
+
+  return objState;
 }
 
 module.exports = transformStateWithClones;
