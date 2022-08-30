de521d2ce9287b7c52d3553bd1c8b8d9d5eb645f
Mykola Chernysh <nanoteck7@gmail.com>
2022-08-30 16:34:13 +0300

    Cloning and transforming state Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,37 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newState = { ...state };
+  const result = [];
+
+  for (let i = 0; i < actions.length; i++) {
+    const { type } = actions[i];
+
+    switch (type) {
+      case 'addProperties':
+        Object.assign(newState, actions[i].extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of actions[i].keysToRemove) {
+          delete newState[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in newState) {
+          delete newState[key];
+        }
+        break;
+
+      default:
+        break;
+    }
+
+    result.push({ ...newState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
