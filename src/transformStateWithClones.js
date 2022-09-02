fed6ad7b9030f2780703b62b60db507976db450b
Ruslan Nedaiborshch <rnedaiborshch@gmail.com>
2022-09-02 11:19:17 +0300

    my cod stateful clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newAction = [];
+  const copyState = { ...state };
+
+  for (const action of actions) {
+    const { extraData, keysToRemove, type } = action;
+
+    switch (type) {
+      case 'addProperties':
+        Object.assign(copyState, extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of keysToRemove) {
+          delete copyState[key];
+        }
+        break;
+
+      case 'clear':
+        for (const j in copyState) {
+          delete copyState[j];
+        }
+        break;
+
+      default:
+        return [];
+    }
+    newAction.push({ ...copyState });
+  }
+
+  return newAction;
 }
 
 module.exports = transformStateWithClones;
