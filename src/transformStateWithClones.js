ff51105b3e286f9e16eed95187da7bf1b4aebd13
Yeva Drach <evadrach1408@gmail.com>
2022-09-03 00:41:34 +0300

    Help me to fix 2 mistakes

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,35 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arrayWithCopies = [];
+
+  for (const object of actions) {
+    const copy = { ...state };
+
+    switch (object.type) {
+      case 'addProperties':
+        Object.assign(copy, object.extraData);
+
+        arrayWithCopies.push(copy);
+        break;
+
+      case 'removeProperties':
+        for (const values of object.keysToRemove) {
+          delete copy[values];
+        }
+
+        arrayWithCopies.push(copy);
+        break;
+
+      case 'clear':
+        for (const key in copy) {
+          delete copy[key];
+        }
+        arrayWithCopies.push(copy);
+    }
+  }
+
+  return arrayWithCopies;
 }
 
 module.exports = transformStateWithClones;
