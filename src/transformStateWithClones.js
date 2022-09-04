369425aa0c46f525e62c23616940b1e6d4b67bc6
Dmytro Yankovskyi <yankovdimon@gmail.com>
2022-09-04 14:45:53 +0300

    switch/case

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -14,22 +14,27 @@ function transformStateWithClones(state, actions) {
     const operation = actions[act];
     const newClones = Object.assign({}, clones);
 
-    if (operation.type === 'addProperties') {
-      container.push(Object.assign(newClones, operation.extraData));
-    };
+    switch (operation.type) {
+      case 'addProperties':
+        container.push(Object.assign(newClones, operation.extraData));
+        break;
 
-    if (operation.type === 'removeProperties') {
-      for (const property in operation.keysToRemove) {
-        delete newClones[operation.keysToRemove[property]];
-      };
-      container.push(newClones);
-    };
+      case 'removeProperties':
+        for (const property in operation.keysToRemove) {
+          delete newClones[operation.keysToRemove[property]];
+        };
+        container.push(newClones);
+        break;
 
-    if (operation.type === 'clear') {
-      for (const property in newClones) {
-        delete newClones[property];
-      };
-      container.push(newClones);
+      case 'clear':
+        for (const property in newClones) {
+          delete newClones[property];
+        };
+        container.push(newClones);
+        break;
+
+      default:
+        break;
     };
 
     clones = newClones;
