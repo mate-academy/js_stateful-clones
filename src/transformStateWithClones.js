ccb3ecb00f285e29edb904a2ffb71b69e6924675
Anastasia Titarenko <titarenkoap123@gmail.com>
2022-09-06 20:45:01 +0300

    if else were deleted

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,42 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const resultArray = [];
-  let removePropertiesClone = {};
+  const resultArray = [state];
+  let removePropertiesClone = { ...state };
 
   for (let i = 0; i < actions.length; i++) {
     switch (actions[i].type) {
       case 'addProperties':
-        if (resultArray.length === 0) {
-          resultArray.push(Object.assign({}, state, actions[i].extraData));
-        } else {
-          resultArray.push(Object.assign({},
-            resultArray[i - 1], actions[i].extraData));
-        }
+        resultArray.push(Object.assign({},
+          resultArray[i], actions[i].extraData));
         break;
       case 'removeProperties':
-        if (resultArray.length === 0) {
-          removePropertiesClone = Object.assign({}, state);
+        removePropertiesClone = Object.assign({}, resultArray[i]);
 
-          for (const keyToRemove of actions[i].keysToRemove) {
-            delete removePropertiesClone[keyToRemove];
-          }
-          resultArray.push(removePropertiesClone);
-        } else {
-          removePropertiesClone = Object.assign({}, resultArray[i - 1]);
-
-          for (const keyToRemove of actions[i].keysToRemove) {
-            delete removePropertiesClone[keyToRemove];
-          }
-          resultArray.push(removePropertiesClone);
+        for (const keyToRemove of actions[i].keysToRemove) {
+          delete removePropertiesClone[keyToRemove];
         }
+        resultArray.push(removePropertiesClone);
         break;
       default:
         resultArray.push({});
     }
   }
 
-  return resultArray;
+  return resultArray.slice(1);
 }
 
 module.exports = transformStateWithClones;
