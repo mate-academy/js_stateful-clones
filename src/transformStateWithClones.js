e9ce2b77dc35b7051815b75049c877dbbf04b5f2
Sergii Iatsenko <sergii.iatsenko@gmail.com>
2022-09-09 16:46:46 +0300

    Solution after review 3.0

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -9,37 +9,28 @@
 function transformStateWithClones(state, actions) {
   const resultArr = [];
 
-  resultArr.push(Object.assign({}, state));
+  let previousState = {};
 
-  for (let i = 0; i < actions.length; i++) {
-    for (const key in actions[i]) {
-      switch (actions[i][key]) {
+  Object.assign(previousState, state);
+
+  for (const anAction of actions) {
+    for (const typeValue in anAction) {
+      switch (anAction[typeValue]) {
         case 'addProperties':
-          Object.assign(resultArr[i], actions[i].extraData);
-
-          if (i < (actions.length - 1)) {
-            resultArr.push(Object.assign({}, resultArr[i]));
-          }
+          Object.assign(previousState, anAction.extraData);
+          resultArr.push(Object.assign({}, previousState));
           break;
 
         case 'removeProperties':
-          for (let propRemove of actions[i].keysToRemove) {
-            delete resultArr[i][propRemove];
+          for (const propRemove of anAction.keysToRemove) {
+            delete previousState[propRemove];
           }
-
-          if (i < (actions.length - 1)) {
-            resultArr.push(Object.assign({}, resultArr[i]));
-          }
+          resultArr.push(Object.assign({}, previousState));
           break;
 
         case 'clear':
-          for (const propClear in resultArr[i]) {
-            delete resultArr[i][propClear];
-          }
-
-          if (i < (actions.length - 1)) {
-            resultArr.push(Object.assign({}, resultArr[i]));
-          }
+          previousState = {};
+          resultArr.push(Object.assign({}, previousState));
           break;
       }
     }
