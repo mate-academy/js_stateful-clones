dd903dc5fa7c731e836a030779dfed67d9ae2467
grafit-off <graf.off.it@gmail.com>
2022-09-12 18:26:04 +0300

    feat: reducer completed

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,37 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const initialState = { ...state };
+  const stateHistory = [];
+
+  actions.forEach((action) => {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
+      case 'addProperties':
+        Object.assign(initialState, extraData);
+        break;
+
+      case 'removeProperties':
+        keysToRemove.forEach((key) => {
+          delete initialState[key];
+        });
+        break;
+
+      case 'clear':
+        for (const key in initialState) {
+          delete initialState[key];
+        }
+        break;
+
+      default:
+        break;
+    }
+
+    stateHistory.push({ ...initialState });
+  });
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
