417d78387fa580d8183e8e3b3cbd727301d75723
Killdex <kdcompany2018@gmail.com>
2022-09-13 00:28:55 +0300

    implemented task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,27 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const res = [];
+  const newObj = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(newObj, action.extraData);
+      res.push({ ...newObj });
+    } else if (action.type === 'removeProperties') {
+      for (let i = 0; i < action.keysToRemove.length; i++) {
+        delete newObj[action.keysToRemove[i]];
+      }
+      res.push({ ...newObj });
+    } else if (action.type === 'clear') {
+      for (const key in newObj) {
+        delete newObj[key];
+      }
+      res.push({ ...newObj });
+    }
+  }
+
+  return res;
 }
 
 module.exports = transformStateWithClones;
