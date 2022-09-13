b5fe1c55a83569b8a193bd796a0bcd925b855f2b
Anton Martyniuk <antonmartyniuk@gmail.com>
2022-09-13 08:21:55 +0300

    Solution with fix 2

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,48 +8,44 @@
  */
 function transformStateWithClones(state, actions) {
   const result = [];
-  const copy = { ...state };
+  const stateCopy = { ...state };
 
   for (const action of actions) {
-    let resultObject = { ...copy };
+    let resultObject = { ...stateCopy };
 
     switch (action.type) {
       case ('addProperties'):
-        Object.assign(copy, action.extraData);
+        Object.assign(stateCopy, action.extraData);
 
-        resultObject = { ...copy };
-
-        result.push(resultObject);
+        resultObject = { ...stateCopy };
 
         break;
 
       case ('removeProperties'):
-        for (let i = 0; i < action.keysToRemove.length; i++) {
-          const propertieToDelete = action.keysToRemove[i];
+        const { keysToRemove } = action;
 
-          delete copy[propertieToDelete];
+        for (let i = 0; i < action.keysToRemove.length; i++) {
+          delete stateCopy[keysToRemove[i]];
         };
 
-        resultObject = { ...copy };
-
-        result.push(resultObject);
+        resultObject = { ...stateCopy };
 
         break;
 
       case ('clear'):
-        for (const key in copy) {
-          delete copy[key];
+        for (const key in stateCopy) {
+          delete stateCopy[key];
         };
 
-        resultObject = { ...copy };
-
-        result.push(resultObject);
+        resultObject = { ...stateCopy };
 
         break;
 
       default:
         return 'Error';
     }
+
+    result.push(resultObject);
   }
 
   return result;
