d320ae19827adaf534671dfaff95681f1e55aecd
Serhii Romaniuk <deepspark@gmail.com>
2022-09-13 17:01:55 +0300

    add js code who decides task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,39 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const resultArray = [];
+  let addObj = { ...state };
+
+  for (const { type, extraData, keysToRemove } of actions) {
+    if (type === 'addProperties') {
+      addObj = {
+        ...addObj,
+        ...extraData,
+      };
+
+      resultArray.push({
+        ...addObj, ...extraData,
+      });
+    } else if (type === 'removeProperties') {
+      for (const item of keysToRemove) {
+        if (Object.keys(addObj).filter(elem => elem === item)) {
+          delete addObj[item];
+        }
+      }
+
+      resultArray.push({
+        ...addObj, ...extraData,
+      });
+    } else {
+      for (const bit of Object.keys(addObj)) {
+        delete addObj[bit];
+      }
+
+      resultArray.push(addObj);
+    }
+  }
+
+  return resultArray;
 }
 
 module.exports = transformStateWithClones;
