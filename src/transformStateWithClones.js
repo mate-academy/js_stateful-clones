15f6d198834b2eb612e08459d6dad2a00b2a4822
Vladimir Dyblia <vladimirdublya@gmail.com>
2022-09-14 10:14:18 +0300

    added switch insted if

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -13,29 +13,29 @@ function transformStateWithClones(state, actions) {
   let container;
 
   for (let i = 0; i < actions.length; i++) {
-    if (actions[i].type === 'clear') {
-      for (const key in newState) {
-        delete newState[key];
-      }
-      container = Object.assign({}, newState);
-      returnObjects.push(container);
-    }
-
-    if (actions[i].type === 'removeProperties') {
-      for (const key in newState) {
-        if (actions[i].keysToRemove.includes(key)) {
+    switch (actions[i].type) {
+      case 'clear':
+        for (const key in newState) {
           delete newState[key];
         }
-      }
-      container = Object.assign({}, newState);
-      returnObjects.push(container);
-    }
+        container = { ...newState };
+        break;
 
-    if (actions[i].type === 'addProperties') {
-      Object.assign(newState, actions[i].extraData);
-      container = Object.assign({}, newState);
-      returnObjects.push(container);
+      case 'removeProperties':
+        for (const key in newState) {
+          if (actions[i].keysToRemove.includes(key)) {
+            delete newState[key];
+          }
+        }
+        container = Object.assign({}, newState);
+        break;
+
+      case 'addProperties' :
+        Object.assign(newState, actions[i].extraData);
+        container = Object.assign({}, newState);
+        break;
     }
+    returnObjects.push(container);
   }
 
   return returnObjects;
