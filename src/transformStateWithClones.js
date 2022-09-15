a4a9aa7be506fdbbe01cbaeb368852af1356db73
Olena Shevchenko <shevochkas@gmail.com>
2022-09-15 14:03:21 +0300

    function transformStateWithClones was added

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,35 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = {
+    ...state,
+  };
+
+  const result = [];
+
+  for (const step of actions) {
+    if (step.type === 'addProperties') {
+      Object.assign(stateCopy, step.extraData);
+    }
+
+    if (step.type === 'removeProperties') {
+      const keysToDelete = step.keysToRemove;
+
+      for (const key of keysToDelete) {
+        delete stateCopy[key];
+      }
+    }
+
+    if (step.type === 'clear') {
+      for (const key in stateCopy) {
+        delete stateCopy[key];
+      }
+    }
+
+    result.push({ ...stateCopy });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
