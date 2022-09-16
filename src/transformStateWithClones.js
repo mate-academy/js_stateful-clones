dfa8810b0be495169c3283a0a5931f85511afe74
Yuri Gatskovskyi <yurimoreau@gmail.com>
2022-09-16 12:55:46 +0300

    switch-case added

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,28 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
   const newArray = [];
   const newState = { ...state };
   let index = 0;
 
   for (const actionsObject of actions) {
-    if (actionsObject.type === 'addProperties') {
-      Object.assign(newState, actionsObject.extraData);
-      newArray[index] = { ...newState };
-      index++;
-    } else if (actionsObject.type === 'removeProperties') {
-      for (const toRemoveProperty of actionsObject.keysToRemove) {
-        if (newState.hasOwnProperty(toRemoveProperty)) {
-          delete newState[toRemoveProperty];
-        }
-      }
-      newArray[index] = { ...newState };
-      index++;
-    } else if (actionsObject.type === 'clear') {
-      Object.keys(newState).forEach(key => delete newState[key]);
-      newArray[index] = { ...newState };
-      index++;
+    switch (actionsObject.type) {
+      case 'addProperties':
+        Object.assign(newState, actionsObject.extraData);
+        newArray[index] = { ...newState };
+        index++;
+        break;
+      case 'removeProperties':
+        for (const toRemoveProperty of actionsObject.keysToRemove) {
+          if (newState.hasOwnProperty(toRemoveProperty)) {
+            delete newState[toRemoveProperty];
+          }
+        };
+        newArray[index] = { ...newState };
+        index++;
+        break;
+      case 'clear':
+        Object.keys(newState).forEach(key => delete newState[key]);
+        newArray[index] = { ...newState };
+        index++;
+        break;
     }
   };
 
