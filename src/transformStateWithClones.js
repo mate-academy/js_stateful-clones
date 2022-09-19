e92a81af425a57c5d9116ec075fc590191f72111
Kalmykov Mikhail <kalmykov.mikhail2@gmail.com>
2022-09-19 15:17:54 +0300

    task was completed

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const resultArr = [];
+  const copyState = { ...state };
+
+  for (const obj of actions) {
+    if (obj.type === 'addProperties') {
+      Object.assign(copyState, obj.extraData);
+
+      resultArr.push({ ...copyState });
+    }
+
+    if (obj.type === 'removeProperties') {
+      for (const word of obj.keysToRemove) {
+        delete copyState[word];
+      }
+
+      resultArr.push({ ...copyState });
+    }
+
+    if (obj.type === 'clear') {
+      for (const key in copyState) {
+        delete copyState[key];
+      }
+
+      resultArr.push({ ...copyState });
+    }
+  }
+
+  return resultArr;
 }
 
 module.exports = transformStateWithClones;
