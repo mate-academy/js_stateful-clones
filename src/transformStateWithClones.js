cef5c53756be1ce6efa1c669470098ebd906ab31
stetsenko_viktoriia <stetsenko.viktoriia@gmail.com>
2022-09-21 18:02:12 +0300

    task was done with clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,35 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const array = [];
+  const StateCopy = Object.assign({}, state);
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        for (const key in action.extraData) {
+          StateCopy[key] = action.extraData[key];
+        }
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete StateCopy[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in StateCopy) {
+          delete StateCopy[key];
+        }
+        break;
+    }
+    array.push(Object.assign({}, StateCopy));
+  }
+
+  return array;
 }
 
 module.exports = transformStateWithClones;
