ad787f56fc7204927f36fe8a23e75e2a8f04f63a
Kalmykov Mikhail <kalmykov.mikhail2@gmail.com>
2022-09-25 12:22:26 +0300

    fix dublicated push

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -14,26 +14,24 @@ function transformStateWithClones(state, actions) {
     switch (obj.type) {
       case ('addProperties'):
         Object.assign(copyState, obj.extraData);
-        resultArr.push({ ...copyState });
         break;
 
       case ('removeProperties'):
         for (const word of obj.keysToRemove) {
           delete copyState[word];
         }
-        resultArr.push({ ...copyState });
         break;
 
       case ('clear'):
         for (const key in copyState) {
           delete copyState[key];
         }
-        resultArr.push({ ...copyState });
         break;
 
       default:
         break;
     }
+    resultArr.push({ ...copyState });
   }
 
   return resultArr;
