08caeb7ed62d6e46d2213888019ac06a9d1ec3f9
metin <metinbicaksiz@gmail.com>
2022-09-27 09:11:52 -0700

    modified

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -10,37 +10,26 @@
 function transformStateWithClones(state, actions) {
   // write code here
   const newState = { ...state };
-  let newArray = [];
+  const newArray = [];
 
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties':
         Object.assign(newState, action.extraData);
-
-        const newState1 = { ...newState };
-
-        newArray.push(newState1);
         break;
       case 'removeProperties':
         for (const keytoRemove of action.keysToRemove) {
           delete newState[keytoRemove];
         }
-
-        const state2 = { ...newState };
-
-        newArray.push(state2);
         break;
       case 'clear':
         for (const key in newState) {
           delete newState[key];
         }
-
-        const state3 = { ...newState };
-
-        newArray.push(state3);
         break;
       default:
     }
+    newArray.push({ ...newState });
   };
 
   return newArray;
