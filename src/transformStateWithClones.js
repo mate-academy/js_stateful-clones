47378bbb66fd184bae2f3bc946dbbc0b33930e14
Vladislav Gogol <gogolsimon95@gmail.com>
2022-09-27 18:23:26 +0300

    Solution 2

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,13 +11,13 @@ function transformStateWithClones(state, actions) {
   let rollingState = { ...state };
 
   for (const action of actions) {
-    switch (action['type']) {
+    switch (action.type) {
       case 'addProperties':
-        Object.assign(rollingState, action['extraData']);
+        Object.assign(rollingState, action.extraData);
         break;
 
       case 'removeProperties':
-        for (const prop of action['keysToRemove']) {
+        for (const prop of action.keysToRemove) {
           delete rollingState[prop];
         };
         break;
