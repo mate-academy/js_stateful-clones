ce5fa2226f2ad8fc8b0b4c8b04aaadd8c535c829
christian m wolf <108151633+devWizHub@users.noreply.github.com>
2022-10-07 15:58:46 +0200

    commit message

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,35 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+
+  const stateCopy = { ...state };
+  const arrOfActions = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          if (stateCopy[key]) {
+            delete stateCopy[key];
+          }
+        }
+        break;
+
+      default:
+        if (stateCopy) {
+          for (const key in stateCopy) {
+            delete stateCopy[key];
+          }
+        }
+    }
+    arrOfActions.push({ ...stateCopy });
+  }
+
+  return arrOfActions;
 }
 
 module.exports = transformStateWithClones;
