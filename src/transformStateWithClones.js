b26721128fb8baba922bcacd2893b4323d4b8a3b
Vlad Fedorishchev <fedorischev23@gmail.com>
2022-10-10 19:35:34 +0300

    Task done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const resultArr = [];
+  const copyMainArray = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(copyMainArray, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete copyMainArray[key];
+      }
+    }
+
+    if (action.type === 'clear') {
+      for (const key in copyMainArray) {
+        delete copyMainArray[key];
+      }
+    }
+    resultArr.push({ ...copyMainArray });
+  }
+
+  return resultArr;
 }
 
 module.exports = transformStateWithClones;
