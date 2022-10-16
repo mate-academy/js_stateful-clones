5856298648ef8cf58d9ae7be911c974dda1b5ba4
Roman-Malysh <jnecbk201@gmail.com>
2022-10-16 19:28:53 +0200

    turnedIntoSwitchStatement

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,20 +11,26 @@ function transformStateWithClones(state, actions) {
   const arr = [];
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      for (const key in action.extraData) {
-        newObj[key] = action.extraData[key];
-      }
-    } else if (action.type === 'removeProperties') {
-      for (const i in action.keysToRemove) {
-        delete newObj[action.keysToRemove[i]];
-      }
-    } else if (action.type === 'clear') {
-      for (const u in newObj) {
-        delete newObj[u];
-      }
+    switch (action.type) {
+      case 'addProperties':
+        for (const key in action.extraData) {
+          newObj[key] = action.extraData[key];
+        }
+        break;
+
+      case 'removeProperties':
+        for (const i in action.keysToRemove) {
+          delete newObj[action.keysToRemove[i]];
+        }
+        break;
+
+      case 'clear':
+        for (const u in newObj) {
+          delete newObj[u];
+        }
+        break;
     }
-    arr.push({ ...newObj });
+    arr.push(Object.assign({}, newObj));
   }
 
   return arr;
