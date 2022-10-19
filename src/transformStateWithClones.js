29f15b7128929e34233663a99a662772952b1807
Max Fedak <max.fedak@gmail.com>
2022-10-19 13:15:47 +0300

    checklist correction

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,36 +7,40 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const res = [];
+  const stateCopy = [];
 
-  res.push(Object.assign({}, state));
+  stateCopy.push(Object.assign({}, state));
 
   for (let i = 0; i < Object.values(actions).length; i++) {
     if (i > 0) {
-      res.push(Object.assign({}, res[i - 1]));
+      stateCopy.push(Object.assign({}, stateCopy[i - 1]));
     }
 
     switch (actions[i].type) {
       case 'clear':
-        Object.keys(res[i]).forEach(key => {
-          delete res[i][key];
+        Object.keys(stateCopy[i]).forEach(key => {
+          delete stateCopy[i][key];
         });
         break;
 
       case 'removeProperties':
         actions[i].keysToRemove.forEach(key => {
-          delete res[i][key];
+          delete stateCopy[i][key];
         });
         break;
 
       case 'addProperties':
         for (const [key, value] of Object.entries(actions[i].extraData)) {
-          res[i][key] = value;
+          stateCopy[i][key] = value;
         }
+        break;
+
+      default:
+        return 'Error occured';
     }
   }
 
-  return res;
+  return stateCopy;
 }
 
 module.exports = transformStateWithClones;
