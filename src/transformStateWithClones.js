a1f9d0f1fe2d631ed6d968ba3ddee54280f46ee9
Spirit <baolt.king@gmail.com>
2022-10-19 23:50:41 +0300

    complete

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateVirsions = [];
+  const copyState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(copyState, action.extraData);
+        stateVirsions.push(Object.assign({}, copyState));
+        break;
+      case 'removeProperties':
+        for (const property of action.keysToRemove) {
+          delete copyState[property];
+        };
+        stateVirsions.push(Object.assign({}, copyState));
+        break;
+      case 'clear':
+        Object.keys(copyState).forEach(key => delete copyState[key]);
+        stateVirsions.push({});
+        break;
+      default:
+        break;
+    }
+  }
+
+  return stateVirsions;
 }
 
 module.exports = transformStateWithClones;
