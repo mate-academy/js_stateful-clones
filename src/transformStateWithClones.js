819a42eaab079010249d03e4990a5d8e8ec16781
Kukharuk <its.up.to.you.dk@gmail.com>
2022-10-20 08:20:16 +0300

    stateClonesDK

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,42 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let resultObj = { ...state };
+
+  for (const elem of actions) {
+    switch (elem.type) {
+      case 'addProperties':
+        Object.assign(resultObj, elem.extraData);
+
+        const resultObj2 = { ...resultObj };
+
+        result.push(resultObj2);
+
+        break;
+
+      case 'removeProperties':
+        for (const key of elem.keysToRemove) {
+          delete resultObj[key];
+        }
+
+        const resultObj3 = { ...resultObj };
+
+        result.push(resultObj3);
+        break;
+
+      case 'clear':
+        resultObj = {};
+
+        const resultObj4 = { ...resultObj };
+
+        result.push(resultObj4);
+
+        break;
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
