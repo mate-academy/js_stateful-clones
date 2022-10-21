47d48737c518642113ce15c203bc073e1e45ee26
Vadym Baranov <vadym.baranov@hotmail.com>
2022-10-21 20:04:30 +0300

    Task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,40 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const allActionsState = [];
+  const stateCopy = { ...state };
+
+  for (const part of actions) {
+    const { type } = part;
+    const { extraData } = part;
+    const { keysToRemove } = part;
+
+    switch (type) {
+      case 'addProperties':
+        Object.assign(stateCopy, extraData);
+        break;
+
+      case 'removeProperties':
+        for (const removeKeys of keysToRemove) {
+          delete stateCopy[removeKeys];
+        }
+
+        break;
+
+      case 'clear':
+        for (const keys in stateCopy) {
+          delete stateCopy[keys];
+        }
+        break;
+
+      default:
+        throw new Error('Invalid action');
+    }
+
+    allActionsState.push({ ...stateCopy });
+  }
+
+  return allActionsState;
 }
 
 module.exports = transformStateWithClones;
