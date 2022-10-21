c3f50284d326bed999500a13e944c344727e3e8e
Andrii Bassarab <andrijbassarab@gmail.com>
2022-10-21 23:50:32 +0300

    fix solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,32 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const clones = [ { ...state } ];
+  const clones = [];
+  const stateCopy = { ...state };
 
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties':
-        const copy = { ...clones[clones.length - 1] };
-
-        clones.push(Object.assign(copy, action.extraData));
+        Object.assign(stateCopy, action.extraData);
         break;
 
       case 'removeProperties':
-        const lastObj = { ...clones[clones.length - 1] };
-
         for (const key of action.keysToRemove) {
-          delete lastObj[key];
+          delete stateCopy[key];
         }
-        clones.push(lastObj);
         break;
 
       case 'clear':
-        clones.push({});
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
         break;
-    }
-  }
 
-  clones.shift();
+      default:
+        throw new Error('Wrong type of state');
+    }
+    clones.push({ ...stateCopy });
+  }
 
   return clones;
 }
