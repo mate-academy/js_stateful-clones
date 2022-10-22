1295c1e2852ea2c83f4c6ccf86c62b389c14ba82
Max Skorokhod <mrzandras@gmail.com>
2022-10-22 20:57:54 +0300

    shadow clone jutsu

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,34 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const array = [];
+  const stateCopy = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const keyRemove of action.keysToRemove) {
+          delete stateCopy[keyRemove];
+        }
+        break;
+
+      case 'clear':
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
+        break;
+
+      default:
+        throw new Error('Error is happend ;c(wrong incoming type of clone)');
+    }
+    array.push({ ...stateCopy });
+  }
+
+  return array;
 }
 
 module.exports = transformStateWithClones;
