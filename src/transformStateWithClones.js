a73619871c8f60ac69481c84f95a951d216e478e
Roman Maksymuk <romamaksymuk@gmail.com>
2022-10-23 12:22:32 +0300

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,38 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  let copyState = { ...state };
+  const arr = [];
+
+  for (const obj of actions) {
+    if (obj.type === 'addProperties') {
+      copyState = {
+        ...copyState,
+        ...obj.extraData,
+      };
+
+      arr.push({
+        ...copyState,
+        ...obj.extraData,
+      });
+    }
+
+    if (obj.type === 'removeProperties') {
+      for (const key of obj.keysToRemove) {
+        delete copyState[key];
+      }
+      arr.push({ ...copyState });
+    }
+
+    if (obj.type === 'clear') {
+      for (const key in copyState) {
+        delete copyState[key];
+      }
+      arr.push({ ...copyState });
+    }
+  }
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
