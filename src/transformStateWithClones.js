5095e036ceb5c5b98e343e84991a817748d07eaa
Alex Tsimba <alexander.tsimba@gmail.com>
2022-10-24 17:53:02 +0300

    yet another fixes after review

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -12,24 +12,24 @@ function transformStateWithClones(state, actions) {
 
   for (const action of actions) {
     switch (action.type) {
-      case 'clear' :
+      case 'clear':
         for (const property in stateClone) {
           delete stateClone[property];
         }
         break;
 
       case 'removeProperties':
-        for (const property of action.keysToRemove) {
-          delete stateClone[property];
+        for (const keyToRemove of action.keysToRemove) {
+          delete stateClone[keyToRemove];
         }
         break;
 
-      case 'addProperties' :
+      case 'addProperties':
         Object.assign(stateClone, action.extraData);
         break;
 
       default: {
-        throw new Error('Error. Check input');
+        throw new Error('Error. Action type missmatch');
       }
     }
     resultState.push({ ...stateClone });
