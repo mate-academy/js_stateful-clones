13bc47e84070c2affee6bbd65466b92c1fd32636
kryvets-vadym <57111524+kryvets-vadym@users.noreply.github.com>
2022-10-25 20:27:53 +0300

    Update transformStateWithClones.js

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -27,7 +27,7 @@ function transformStateWithClones(state, actions) {
         break;
 
       default:
-        break;
+        throw new Error('Error occured');
     }
 
     objectStates.push({ ...stateCopy });
