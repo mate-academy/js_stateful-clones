730d1ee75ac2fd049634c19d4b0b46a195884064
Sofiia Dovhan <dovgan014@gmail.com>
2022-10-25 15:39:32 +0300

    fixed details

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,38 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arrayOfStates = [];
+  const copyOfState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(copyOfState, action.extraData);
+
+        break;
+
+      case 'removeProperties':
+        for (const keyToRemove of action.keysToRemove) {
+          delete copyOfState[keyToRemove];
+        }
+
+        break;
+
+      case 'clear':
+        for (const key in copyOfState) {
+          delete copyOfState[key];
+        }
+
+        break;
+
+      default:
+        throw new Error('Error');
+    }
+
+    arrayOfStates.push({ ...copyOfState });
+  }
+
+  return arrayOfStates;
 }
 
 module.exports = transformStateWithClones;
