7f20c2743b3e0af8317ef3aa2f86ce966c943ccf
Anastasiia Shyptia <shyptianastya27@gmail.com>
2022-10-29 12:53:55 +0300

    Rewrite code using switch

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,35 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const resultObject = [];
+  const resultStates = [];
   let clone = Object.assign({}, state);
 
   for (let i = 0; i < actions.length; i++) {
-    if (actions[i].type === 'addProperties') {
-      clone = Object.assign({}, clone, actions[i].extraData);
-      resultObject.push(clone);
+    switch (actions[i].type) {
+      case 'addProperties':
+        clone = Object.assign({}, clone, actions[i].extraData);
+        break;
+
+      case 'removeProperties':
+        for (let a = 0; a < actions[i].keysToRemove.length; a++) {
+          delete clone[actions[i].keysToRemove[a]];
+        }
+        break;
+
+      case 'clear':
+        for (const key in clone) {
+          delete clone[key];
+        }
+        break;
     }
 
-    if (actions[i].type === 'removeProperties') {
-      for (let a = 0; a < actions[i].keysToRemove.length; a++) {
-        delete clone[actions[i].keysToRemove[a]];
-      }
-
-      resultObject.push(clone);
-    }
-
-    if (actions[i].type === 'clear') {
-      for (const key in clone) {
-        delete clone[key];
-      }
-
-      resultObject.push(clone);
-    }
-
-    clone = Object.assign({}, resultObject[i]);
+    resultStates.push(clone);
+    clone = Object.assign({}, resultStates[i]);
   }
 
-  return resultObject;
+  return resultStates;
 }
 
 module.exports = transformStateWithClones;
