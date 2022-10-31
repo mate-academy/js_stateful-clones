c42117c6d0cf1fa428967d4d59cc8eaefa838127
Konstantin Burnazaki <kostas.burnazaki@gmail.com>
2022-10-31 18:28:41 +0200

    initial

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,47 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = Array(actions.length);
+
+  for (const action in actions) {
+    let newState = {};
+
+    if (result[action - 1]) {
+      newState = {
+        ...result[action - 1],
+      };
+    } else {
+      newState = {
+        ...state,
+      };
+    }
+
+    switch (true) {
+      case actions[action]['type'] === 'addProperties':
+        Object.assign(newState, actions[action]['extraData']);
+
+        result[action] = newState;
+        break;
+
+      case actions[action]['type'] === 'removeProperties':
+        for (const keyToRemove in actions[action]['keysToRemove']) {
+          const propToRemove = actions[action]['keysToRemove'][keyToRemove];
+
+          if (newState.hasOwnProperty(propToRemove)) {
+            delete newState[propToRemove];
+          }
+        }
+
+        result[action] = newState;
+        break;
+
+      case actions[action]['type'] === 'clear':
+        result[action] = {};
+        break;
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
