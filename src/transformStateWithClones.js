e7cffaa04329b5589db9942969a8d17a642a69fe
Demut <uddarendeli1@gmail.com>
2022-11-03 14:56:01 +0300

    transformStateWithClones implemented

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,39 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const backupObj = Object.assign({}, state);
+
+  actions.forEach(action => {
+    switch (action.type) {
+      case 'addProperties':
+        const obj = Object.entries(action.extraData);
+
+        for (let i = 0; i < obj.length; i++) {
+          const element = obj[i];
+
+          backupObj[element[0]] = element[1];
+        }
+        break;
+      case 'removeProperties':
+        for (let i = 0; i < action.keysToRemove.length; i++) {
+          const element = action.keysToRemove[i];
+
+          if (element in backupObj) {
+            delete backupObj[element];
+          }
+        }
+        break;
+
+      case 'clear':
+        for (const key in backupObj) {
+          delete backupObj[key];
+        }
+    }
+    result.push(Object.assign({}, backupObj));
+  });
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
