082f34d168d3a07de7ded910db8b2127393f3005
Oleg Bonchuk <bonchuk@MacBook-Air-Oleg.local>
2022-11-03 11:57:29 +0200

    rewriting code with switch cases

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,16 +11,20 @@ function transformStateWithClones(state, actions) {
   let stateB = { ...state };
 
   for (const key in actions) {
-    if (actions[key].type === 'addProperties') {
-      Object.assign(stateB, actions[key].extraData);
-    } else if (actions[key].type === 'removeProperties') {
-      const keysToRemove = actions[key].keysToRemove;
+    switch (actions[key].type) {
+      case 'addProperties':
+        Object.assign(stateB, actions[key].extraData);
+        break;
+      case 'removeProperties':
+        const keysToRemove = actions[key].keysToRemove;
 
-      for (const each in keysToRemove) {
-        delete stateB[keysToRemove[each]];
-      }
-    } else if (actions[key].type === 'clear') {
-      stateB = {};
+        for (const each in keysToRemove) {
+          delete stateB[keysToRemove[each]];
+        }
+        break;
+      case 'clear':
+        stateB = {};
+        break;
     }
     secondState.push({ ...stateB });
   };
