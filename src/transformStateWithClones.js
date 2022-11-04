18543be53a7ea98d81bc6eee52ab76642d4aace7
Nick Busarhin <busarginkolia@gmail.com>
2022-11-04 23:39:43 +0200

    clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,49 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let copy = { ...state };
+  const versions = [];
+
+  for (const a of actions) {
+    switch (a.type) {
+      case 'addProperties':
+        const steps = a.extraData;
+
+        for (const key in steps) {
+          if (!copy[key]) {
+            copy[key] = steps[key];
+          }
+
+          if (copy[key]) {
+            copy[key] = steps[key];
+          }
+        };
+        versions.push(copy);
+        break;
+
+      case 'removeProperties':
+        const takeAway = a.keysToRemove;
+
+        for (const name of takeAway) {
+          if (copy[name]) {
+            delete copy[name];
+          }
+        };
+        versions.push(copy);
+        break;
+
+      case 'clear':
+        for (const n in copy) {
+          delete copy[n];
+        };
+        versions.push(copy);
+        break;
+    }
+
+    copy = { ...copy };
+  };
+
+  return versions;
 }
 
 module.exports = transformStateWithClones;
