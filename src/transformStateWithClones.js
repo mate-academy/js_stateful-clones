3468c0f0a3a0ce9642e1263ce2edb2cd066f4efb
Artem Hirzhev <a.hirzhev@gmail.com>
2022-11-15 10:18:37 +0200

    Initial commit

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const nextState = { ...state };
+  const result = [];
+
+  for (let i = 0; i < actions.length; i++) {
+    const { type, extraData, keysToRemove } = actions[i];
+
+    switch (type) {
+      case 'addProperties':
+        Object.assign(nextState, extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key in keysToRemove) {
+          delete nextState[keysToRemove[key]];
+        }
+        break;
+
+      case 'clear':
+        for (const clear in nextState) {
+          delete nextState[clear];
+        }
+    }
+
+    result.push({ ...nextState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
