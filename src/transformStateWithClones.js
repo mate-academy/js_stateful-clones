11d922f7c6ff7bf01bda3abf7fce887300e23370
Iorga Nicu <ioghigithublab@gmail.com>
2022-11-16 19:48:59 +0200

    Job Done!

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const currentState = { ...state };
+
+  for (const obj of actions) {
+    if (obj.type === 'addProperties') {
+      Object.assign(currentState, obj.extraData);
+    }
+
+    if (obj.type === 'removeProperties') {
+      for (const key of obj.keysToRemove) {
+        if (currentState.hasOwnProperty(key)) {
+          delete currentState[key];
+        }
+      }
+    }
+
+    if (obj.type === 'clear') {
+      if (Object.keys(currentState).length !== 0) {
+        for (const key in currentState) {
+          delete currentState[key];
+        }
+      }
+    }
+
+    result.push(Object.assign({}, currentState));
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
