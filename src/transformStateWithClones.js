74e197fab2b6a59907cf81021c3cd9f4bbaaba65
mykolachupyak <chupyak@gmail.com>
2022-11-19 07:53:05 +0200

    improved solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,28 +11,32 @@ function transformStateWithClones(state, actions) {
   const resultArray = [];
   const newState = Object.assign({}, state);
 
-  for (let i = 0; i < actions.length; i++) {
-    if (actions[i].type === 'addProperties') {
-      for (const key in actions[i].extraData) {
-        newState[key] = actions[i].extraData[key];
-      }
-      resultArray.push(Object.assign({}, newState));
-    }
-
-    if (actions[i].type === 'removeProperties') {
-      for (let j = 0; j < actions[i].keysToRemove.length; j++) {
-        if (newState[actions[i].keysToRemove[j]]) {
-          delete newState[actions[i].keysToRemove[j]];
+  for (const i in actions) {
+    switch (actions[i].type) {
+      case 'addProperties' : {
+        for (const key in actions[i].extraData) {
+          newState[key] = actions[i].extraData[key];
         }
+        resultArray.push(Object.assign({}, newState));
+        break;
       }
-      resultArray.push(Object.assign({}, newState));
-    }
 
-    if (actions[i].type === 'clear') {
-      for (const key in newState) {
-        delete newState[key];
+      case 'removeProperties' : {
+        for (let j = 0; j < actions[i].keysToRemove.length; j++) {
+          if (newState[actions[i].keysToRemove[j]]) {
+            delete newState[actions[i].keysToRemove[j]];
+          }
+        }
+        resultArray.push(Object.assign({}, newState));
+        break;
       }
-      resultArray.push(Object.assign({}, newState));
+
+      default : {
+        for (const key in newState) {
+          delete newState[key];
+        }
+        resultArray.push(Object.assign({}, newState));
+      }
     }
   }
 
