7814e9813b84d970880ca39c73a9f2808d606549
mr-valentos <nerpavlad@gmail.com>
2022-11-20 23:39:41 +0200

    state copi

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,37 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+
+  const arr = actions.map((el) => {
+    switch (el.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, el.extraData);
+
+        return { ...stateCopy };
+
+      case 'removeProperties':
+        for (const key of el.keysToRemove) {
+          if (stateCopy.hasOwnProperty(key)) {
+            delete stateCopy[key];
+          }
+        }
+
+        return { ...stateCopy };
+
+      case 'clear':
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
+
+        return { ...stateCopy };
+
+      default:
+        break;
+    }
+  });
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
