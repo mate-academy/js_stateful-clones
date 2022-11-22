ede3a2990410357ffbcf7b57a24b0f9e3ad5f2ad
Sergey Mironenko <serg1prime@gmail.com>
2022-11-23 00:44:24 +0200

    Task was done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const clones = [];
+  let clone = { ...state };
+
+  actions.forEach((action) => {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
+      case 'addProperties':
+        Object.assign(clone, extraData);
+        break;
+
+      case 'removeProperties':
+        for (const k of keysToRemove) {
+          delete clone[k];
+        }
+        break;
+
+      case 'clear':
+        clone = {};
+        break;
+    }
+
+    clones.push({ ...clone });
+  });
+
+  return clones;
 }
 
 module.exports = transformStateWithClones;
