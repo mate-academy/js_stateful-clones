5adfb903e08989760aea64342c95e558c3ba3cb0
Artem Pinchuk <aviktorovich193@gmail.com>
2022-11-23 14:00:34 +0200

    Solution

--- readme.md
+++ readme.md
@@ -29,7 +29,7 @@ If `state` is {foo: 'bar', bar: 'foo'}, then
 ```
 transformStateWithClones(state, [
   {type: 'addProperties', extraData: {name: 'Jim', hello: 'world'}},
-  {type: 'removeProperties', keysToRemove: ['bar', 'hello']},
+  {type: ', keysToRemove: ['bar', 'hello']},
   {type: 'addProperties', extraData: {another: 'one'}}
 ])
 ```
@@ -44,7 +44,7 @@ must return the following array:
 ].
 ```
 
-**The `state` object itself should not be modified and must remain {foo: 'bar', bar: 'foo'}.**
+*The `state` object itself should not be modified and must remain {foo: 'bar', bar: 'foo'}.***
 
 Then after calling
 
--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
-}
+  const result = [];
+  let stateClone = { ...state };
 
+  for (let i = 0; i <= actions.length - 1; i++) {
+    if (actions[i].type === 'addProperties') {
+      Object.assign(stateClone, actions[i].extraData);
+      result.push({ ...stateClone });
+    }
+
+    if (actions[i].type === 'removeProperties') {
+      for (const value of actions[i].keysToRemove) {
+        if (value in stateClone) {
+          delete stateClone[value];
+        }
+      }
+      result.push({ ...stateClone });
+    }
+
+    if (actions[i].type === 'clear') {
+      result.push({});
+      stateClone = {};
+    }
+  }
+
+  return result;
+}
 module.exports = transformStateWithClones;
