583fb8a64737615124b6ef2aadef1742f093cd71
Anton Martynes <martynes1997@gmail.com>
2022-11-27 18:16:38 +0200

    spaces added

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -15,12 +15,15 @@ function transformStateWithClones(state, actions) {
       case 'addProperties':
         Object.assign(stateCopy, action.extraData);
         break;
+
       case 'clear':
         Object.keys(stateCopy).forEach(key => delete stateCopy[key]);
         break;
+
       case 'removeProperties':
         action.keysToRemove.forEach(key => delete stateCopy[key]);
         break;
+
       default:
         break;
     }
