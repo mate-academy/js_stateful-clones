62eeca2d0205f61aae588c459fdec52cfacd8316
Viktoriia Kravchenko <kravchenkovictoriya9@gmail.com>
2022-11-28 07:46:57 +0200

    Added transformStateWithClones function

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,35 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const versionsOfState = [];
+  const stateCopy = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        break;
+
+      case 'removeProperties':
+        action.keysToRemove.forEach((keyToRemove) => {
+          delete stateCopy[keyToRemove];
+        });
+        break;
+
+      case 'clear':
+        Object.keys(stateCopy).forEach((keytoClear) => {
+          delete stateCopy[keytoClear];
+        });
+        break;
+
+      default:
+        break;
+    }
+
+    versionsOfState.push({ ...stateCopy });
+  }
+
+  return versionsOfState;
 }
 
 module.exports = transformStateWithClones;
