c182e9b1af46f81da203776abf15c1c89682cdbd
Mykhailo Orlov <mihalikmihalik85@gmail.com>
2022-12-01 19:50:24 +0200

    all tests good

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arr = [];
+  const obj = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(obj, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete obj[key];
+      }
+    }
+
+    if (action.type === 'clear') {
+      for (const key1 in obj) {
+        delete obj[key1];
+      }
+    }
+    arr.push({ ...obj });
+  }
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
