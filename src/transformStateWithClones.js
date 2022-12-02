085ac662adcc4cb755ff3173237448e26dcefa73
Vitalii Skomskiy <skomskiy@ukr.net>
2022-12-02 19:26:35 +0200

    StateClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const copyState = { ...state };
+  const cloneArray = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'clear':
+        for (const del in copyState) {
+          delete copyState[del];
+        };
+        cloneArray.push({ ...copyState });
+        break;
+
+      case 'addProperties':
+        Object.assign(copyState, action.extraData);
+        cloneArray.push({ ...copyState });
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete copyState[key];
+        }
+        cloneArray.push({ ...copyState });
+    }
+  }
+
+  return cloneArray;
 }
 
 module.exports = transformStateWithClones;
