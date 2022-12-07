b5a7b79d654738344b9b1b8cb9399c07181120ed
Ashton <nonlocality@gmail.com>
2022-12-07 17:20:04 +0200

    Initial commit

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const _state = { ...state };
+  const states = [];
+
+  for (const action in actions) {
+    switch (actions[action].type) {
+      case 'addProperties':
+        Object.assign(_state, actions[action].extraData);
+        break;
+      case 'removeProperties':
+        for (const key of actions[action].keysToRemove) {
+          delete _state[key];
+        }
+        break;
+      case 'clear':
+        for (const key in _state) {
+          delete _state[key];
+        }
+        break;
+    }
+    states.push({ ..._state });
+  }
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
