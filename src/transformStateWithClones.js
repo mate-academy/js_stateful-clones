1a06107fab882fea43c116b84aba789c5efbf260
ihorivna007 <emmakorovnik@gmail.com>
2022-12-10 20:20:08 +0200

    initial commit

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,27 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const array = [];
+  const initial = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      for (const data in action.extraData) {
+        initial[data] = action.extraData[data];
+      }
+    } else if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete initial[key];
+      }
+    } else {
+      for (const item in initial) {
+        delete initial[item];
+      }
+    }
+    array.push({ ...initial });
+  }
+
+  return array;
 }
 
 module.exports = transformStateWithClones;
