c40e98ff33e58b5e2867247c7611334e464bd229
yana-kotsulym <yanak7170@gmail.com>
2022-12-16 21:56:21 +0100

    Fixes after code review

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -9,68 +9,74 @@
 function transformStateWithClones(state, actions) {
   const result = [];
 
-  for (let i = 0; i < actions.length; i++) {
-    const actionValues = Object.values(actions[i]);
-    const actionKeys = Object.keys(actions[i]);
+  for (const part of actions) {
+    const actionValues = Object.values(part);
+    const actionKeys = Object.keys(part);
 
     for (let n = 0; n < actionKeys.length; n++) {
-      if (actionKeys[n] === 'type') {
-        if (actionValues[n] === 'addProperties') {
-          if (i === 0) {
-            result.push(Object.assign({}, state, actionValues[n + 1]));
-          } else {
-            result.push(Object.assign({}, result[i - 1], actionValues[n + 1]));
-          }
-        }
+      switch (actionKeys[n]) {
+        case 'type':
+          switch (actionValues[n]) {
+            case 'addProperties':
+              if (result.length === 0) {
+                result.push(Object.assign({}, state, actionValues[n + 1]));
+              } else {
+                result.push(Object.assign(
+                  {},
+                  result[result.length - 1],
+                  actionValues[n + 1]));
+              }
+              break;
 
-        if (actionValues[n] === 'removeProperties') {
-          let copy = {};
+            case 'removeProperties':
+              let copy = {};
 
-          if (i === 0) {
-            if (actionValues[n + 1].length === 0) {
-              copy = {
-                ...state,
-              };
-            }
-
-            for (const key in state) {
-              for (const item of actionValues[n + 1]) {
-                if (item !== key) {
-                  copy[key] = state[key];
+              if (result.length === 0) {
+                if (actionValues[n + 1].length === 0) {
+                  copy = {
+                    ...state,
+                  };
                 }
 
-                if (item === key) {
-                  delete copy[key];
-                  break;
+                for (const key in state) {
+                  for (const item of actionValues[n + 1]) {
+                    if (item !== key) {
+                      copy[key] = state[key];
+                    }
+
+                    if (item === key) {
+                      delete copy[key];
+                      break;
+                    }
+                  }
+                }
+              } else {
+                if (actionValues[n + 1].length === 0) {
+                  copy = {
+                    ...result[result.length - 1],
+                  };
+                }
+
+                for (const key in result[result.length - 1]) {
+                  for (const item of actionValues[n + 1]) {
+                    if (item !== key) {
+                      copy[key] = result[result.length - 1][key];
+                    }
+
+                    if (item === key) {
+                      delete copy[key];
+                      break;
+                    }
+                  }
                 }
               }
-            }
-          } else {
-            if (actionValues[n + 1].length === 0) {
-              copy = {
-                ...result[i - 1],
-              };
-            }
+              result.push(copy);
+              break;
 
-            for (const key in result[i - 1]) {
-              for (const item of actionValues[n + 1]) {
-                if (item !== key) {
-                  copy[key] = result[i - 1][key];
-                }
-
-                if (item === key) {
-                  delete copy[key];
-                  break;
-                }
-              }
-            }
+            case 'clear':
+              result.push({});
+              break;
           }
-          result.push(copy);
-        }
-
-        if (actionValues[n] === 'clear') {
-          result.push({});
-        }
       }
     }
   }
