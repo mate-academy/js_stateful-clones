23ab0ef3956e0f7cb77d48ce87a588a7d141955c
yana-kotsulym <yanak7170@gmail.com>
2022-12-17 18:54:52 +0100

    fixed after code review

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -9,76 +9,30 @@
 function transformStateWithClones(state, actions) {
   const result = [];
 
-  for (const part of actions) {
-    const actionValues = Object.values(part);
-    const actionKeys = Object.keys(part);
+  const copyState = { ...state };
 
-    for (let n = 0; n < actionKeys.length; n++) {
-      switch (actionKeys[n]) {
-        case 'type':
-          switch (actionValues[n]) {
-            case 'addProperties':
-              if (result.length === 0) {
-                result.push(Object.assign({}, state, actionValues[n + 1]));
-              } else {
-                result.push(Object.assign(
-                  {},
-                  result[result.length - 1],
-                  actionValues[n + 1]));
-              }
-              break;
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(copyState, action.extraData);
+        break;
 
-            case 'removeProperties':
-              let copy = {};
-
-              if (result.length === 0) {
-                if (actionValues[n + 1].length === 0) {
-                  copy = {
-                    ...state,
-                  };
-                }
-
-                for (const key in state) {
-                  for (const item of actionValues[n + 1]) {
-                    if (item !== key) {
-                      copy[key] = state[key];
-                    }
-
-                    if (item === key) {
-                      delete copy[key];
-                      break;
-                    }
-                  }
-                }
-              } else {
-                if (actionValues[n + 1].length === 0) {
-                  copy = {
-                    ...result[result.length - 1],
-                  };
-                }
-
-                for (const key in result[result.length - 1]) {
-                  for (const item of actionValues[n + 1]) {
-                    if (item !== key) {
-                      copy[key] = result[result.length - 1][key];
-                    }
-
-                    if (item === key) {
-                      delete copy[key];
-                      break;
-                    }
-                  }
-                }
-              }
-              result.push(copy);
-              break;
-
-            case 'clear':
-              result.push({});
-              break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          if ([key] in copyState) {
+            delete copyState[key];
           }
-      }
+        }
+        break;
+
+      case 'clear':
+        for (const item in copyState) {
+          delete copyState[item];
+        }
+        break;
     }
+
+    result.push({ ...copyState });
   }
 
   return result;
