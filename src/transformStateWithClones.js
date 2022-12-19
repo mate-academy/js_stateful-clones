dc5960ce1261f242935d8e977ed062b4eca2a5b0
Ilona Romaniuk <ilona09041983@gmail.com>
2022-12-19 22:10:30 +0200

    Function transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,43 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateModifications = [];
+  const copy = { ...state };
+
+  for (const object of actions) {
+    for (const key in object) {
+      switch (object[key]) {
+        case 'addProperties':
+          Object.assign(copy, object.extraData);
+
+          const copy1 = { ...copy };
+
+          stateModifications.push(copy1);
+          break;
+
+        case 'removeProperties':
+          for (const value of object.keysToRemove) {
+            delete copy[value];
+          }
+
+          const copy2 = { ...copy };
+
+          stateModifications.push(copy2);
+          break;
+
+        case 'clear':
+          for (const i in copy) {
+            delete copy[i];
+          }
+
+          const copy3 = { ...copy };
+
+          stateModifications.push(copy3);
+      }
+    }
+  }
+
+  return stateModifications;
 }
 
 module.exports = transformStateWithClones;
