8f5c6929e1d5e8503b5e77d56210199a3e9fe5d8
Taras Baran <baranprivat22@gmail.com>
2022-12-20 21:10:07 +0200

    finished

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newObj = { ...state };
+  const result = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(newObj, action.extraData);
+        break;
+      case 'removeProperties':
+        for (const prop of action.keysToRemove) {
+          if (newObj[prop]) {
+            delete newObj[prop];
+          }
+        }
+        break;
+      case 'clear':
+        Object.keys(newObj).forEach(v => delete newObj[v]);
+        break;
+    }
+
+    result.push({ ...newObj });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
