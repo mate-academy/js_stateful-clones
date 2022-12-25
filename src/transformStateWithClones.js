8dfc9a6dbf516945fce083b67615a8e10a2bb54c
Denys <kolbasin1990@gmail.com>
2022-12-25 17:34:21 +0200

    task_stateful_clones_v01

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const obj = { ...state };
+  const arrObj = [];
+
+  for (const key of actions) {
+    const { extraData, keysToRemove, type } = key;
+
+    switch (type) {
+      case 'addProperties':
+        Object.assign(obj, extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key2 of keysToRemove) {
+          delete obj[key2];
+        }
+        break;
+
+      case 'clear':
+        for (const key3 in obj) {
+          delete obj[key3];
+        }
+        break;
+    }
+
+    arrObj.push({ ...obj });
+  }
+
+  return arrObj;
 }
 
 module.exports = transformStateWithClones;
