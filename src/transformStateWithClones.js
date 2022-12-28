fb61a9d33519fb7e13a8f1e336ad422379a80d2e
Ihor Bondarenko <garryrocksmanjs@gmail.com>
2022-12-28 22:19:42 +0200

    Cloning and transforming state - completed

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let nextState = { ...state };
+
+  for (const action of actions) {
+    switch (action['type']) {
+      case 'addProperties':
+        for (const data in action.extraData) {
+          nextState[data] = action.extraData[data];
+        }
+        break;
+
+      case 'removeProperties':
+        for (const toRemove of action.keysToRemove) {
+          delete nextState[toRemove];
+        }
+        break;
+
+      case 'clear':
+        nextState = {};
+        break;
+
+      default:
+        throw new Error('Unexpected state');
+    }
+    result.push({ ...nextState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
