e36b83419912f9c2b3bc599f5fccbb5c58ba8e21
Andrii <kurash.andrii@gmail.com>
2022-12-28 18:46:16 +0200

    finished Stateful clone task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,25 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateClones = [];
+  const stateClone = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(stateClone, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      action.keysToRemove.forEach(key => delete stateClone[key]);
+    }
+
+    if (action.type === 'clear') {
+      Object.keys(stateClone).forEach(key => delete stateClone[key]);
+    }
+    stateClones.push({ ...stateClone });
+  }
+
+  return stateClones;
 }
 
 module.exports = transformStateWithClones;
