885191f1d6a99ac034702641745d0b3fc3cace6e
Andrey Berezyanskiy <andrey.berezyanskiy@gmail.com>
2022-12-28 22:20:01 +0200

    v-01

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,37 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const currentState = { ...state };
+  const stateArr = [];
+
+  for (const el of actions) {
+    switch (el.type) {
+      case 'addProperties':
+        Object.assign(currentState, el.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const keysRemove of el.keysToRemove) {
+          delete currentState[keysRemove];
+        };
+        break;
+
+      case 'clear':
+        for (const key in currentState) {
+          if (currentState.hasOwnProperty([key])) {
+            delete currentState[key];
+          }
+        };
+        break;
+
+      default:
+        break;
+    }
+
+    stateArr.push({ ...currentState });
+  }
+
+  return stateArr;
 }
 
 module.exports = transformStateWithClones;
