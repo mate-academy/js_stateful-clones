054eb9cb09afc2795695b5640a181ffacad4c28c
Oleksandr Prokopenkop <alex.prokop2206@gmail.com>
2022-12-29 12:37:43 +0200

    solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,33 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
   // write code here
+  let stateNew = { ...state };
+  const stateChanges = [];
+
+  for (const act of actions) {
+    switch (act.type) {
+      case 'addProperties':
+        Object.assign(stateNew, act.extraData);
+        break;
+      case 'removeProperties':
+        for (const key of act.keysToRemove) {
+          delete stateNew[key];
+        };
+        break;
+      case 'clear':
+        stateNew = {};
+        break;
+      default:
+        throw new Error('function dot found');
+    }
+
+    stateChanges.push({ ...stateNew });
+  }
+
+  return stateChanges;
 }
 
 module.exports = transformStateWithClones;
