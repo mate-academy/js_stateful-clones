a1fa1063c474bbda52e877642fd1264f88afa7ea
karaedd <karaed.ml@mail.ru>
2022-12-29 15:01:39 +0200

    first commit

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
-}
+  const obj = Object.assign({}, state);
+  const array = [];
 
+  for (const action of actions) {
+    if (action.type === 'clear') {
+      for (const key in obj) {
+        delete obj[key];
+      }
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete obj[key];
+      }
+    }
+
+    if (action.type === 'addProperties') {
+      Object.assign(obj, action.extraData);
+    }
+
+    array.push(Object.assign({}, obj));
+  }
+
+  return array;
+}
 module.exports = transformStateWithClones;
