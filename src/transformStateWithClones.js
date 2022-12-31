62555c6b46466bc6feb80a53a6310aec2e148048
Mykola Serbin <serbin.nick@gmail.com>
2022-12-31 17:09:27 +0100

    clones task commit

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,34 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const result = [];
+  const clone = { ...state };
+
+  for (let i = 0; i < actions.length; i++) {
+    switch (actions[i].type) {
+      case 'addProperties':
+        Object.assign(clone, actions[i].extraData);
+        break;
+
+      case 'removeProperties':
+        for (let j = 0; j < actions[i].keysToRemove.length; j++) {
+          delete clone[actions[i].keysToRemove[j]];
+        }
+        break;
+
+      case 'clear':
+        for (const key in clone) {
+          delete clone[key];
+        }
+        break;
+
+      default:
+        break;
+    }
+    result.push({ ...clone });
+  }
+
+  return (result);
 }
 
 module.exports = transformStateWithClones;
