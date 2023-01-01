f36a6f56093d92977f6a6c24be3f084fc88b7050
Vadym Tsyndra <Mr.Tsyndra@gmail.com>
2023-01-01 18:43:41 +0200

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,67 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arr = [];
+
+  for (const action of actions) {
+    if (action.type === 'clear') {
+      if (arr.length === 0) {
+        const obj = Object.assign({}, state);
+
+        for (const prop in obj) {
+          delete obj[prop];
+        }
+
+        arr.push(obj);
+      } else {
+        const obj = Object.assign({}, arr[arr.length - 1]);
+
+        for (const prop in obj) {
+          delete obj[prop];
+        }
+
+        arr.push(obj);
+      }
+    }
+
+    if (action.type === 'addProperties') {
+      if (arr.length === 0) {
+        const obj = Object.assign({}, state);
+
+        Object.assign(obj, action.extraData);
+
+        arr.push(obj);
+      } else {
+        const obj = Object.assign({}, arr[arr.length - 1]);
+
+        Object.assign(obj, action.extraData);
+
+        arr.push(obj);
+      }
+    }
+
+    if (action.type === 'removeProperties') {
+      if (arr.length === 0) {
+        const obj = Object.assign({}, state);
+
+        for (const prop of action.keysToRemove) {
+          delete obj[prop];
+        }
+
+        arr.push(obj);
+      } else {
+        const obj = Object.assign({}, arr[arr.length - 1]);
+
+        for (const prop of action.keysToRemove) {
+          delete obj[prop];
+        }
+
+        arr.push(obj);
+      }
+    }
+  }
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
