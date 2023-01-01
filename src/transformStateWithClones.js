5d916e01cb7c2b0950a5946e3094bb0a512ab3b3
Dashaverz <dashaverzhanskayaa@gmail.com>
2023-01-01 18:53:50 +0100

    remove if statement

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -19,9 +19,7 @@ function transformStateWithClones(state, actions) {
 
       case `removeProperties`:
         for (const key of action.keysToRemove) {
-          if (stateCopy.hasOwnProperty(key)) {
-            delete stateCopy[key];
-          }
+          delete stateCopy[key];
         }
         break;
 
