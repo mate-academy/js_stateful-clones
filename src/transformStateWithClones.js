f9c8a92545d64a0a93b9534b0e61be22653a3264
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-01-05 15:49:06 +0200

    Finish task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,36 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const states = [];
+  const newState = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      for (const prop in action.extraData) {
+        newState[prop] = action.extraData[prop];
+      }
+
+      states.push({ ...newState });
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const prop of action.keysToRemove) {
+        delete newState[prop];
+      }
+
+      states.push({ ...newState });
+    }
+
+    if (action.type === 'clear') {
+      for (const prop in newState) {
+        delete newState[prop];
+      }
+
+      states.push({ ...newState });
+    }
+  }
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
