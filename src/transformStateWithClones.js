b689344657c3679a6d82d410bd89c3e5802fc5c7
Oleksandr Prykhodko <olexandr.prykhodko@gmail.com>
2023-01-11 12:31:13 +0100

    Stateful_Clones were implemented

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,27 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let currentState = JSON.parse(JSON.stringify(state));
+  const states = [];
+
+  for (let i = 0; i < actions.length; i++) {
+    const { type, extraData, keysToRemove } = actions[i];
+
+    if (type === 'addProperties') {
+      Object.assign(currentState, extraData);
+    } else if (type === 'removeProperties') {
+      for (let j = 0; j < keysToRemove.length; j++) {
+        if (currentState.hasOwnProperty(keysToRemove[j])) {
+          delete currentState[keysToRemove[j]];
+        }
+      }
+    } else if (type === 'clear') {
+      currentState = {};
+    }
+    states.push(JSON.parse(JSON.stringify(currentState)));
+  };
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
