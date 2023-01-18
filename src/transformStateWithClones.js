64b6214968a9aab643e6c11e6d364dfa35f0d7d9
Oleksandr Maslo <98667890az@gmail.com>
2023-01-18 15:44:36 +0200

    statefulClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,39 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateClone = {...state};
+  const cloneHistory = [];
+
+  for (const action of actions) {
+    const type = action.type;
+
+    switch (type) {
+      case 'addProperties':
+        Object.assign(stateClone, action.extraData);
+
+        cloneHistory.push({ ...stateClone })
+        break;
+
+      case 'removeProperties':
+        for (const keysRemove of action.keysToRemove) {
+          delete stateClone[keysRemove];
+        }
+
+        cloneHistory.push({ ...stateClone })
+        break;
+
+      case 'clear':
+        for (const key in stateClone) {
+          delete stateClone[key];
+        }
+
+        cloneHistory.push({ ...stateClone })
+        break;
+      default: return 'Unknown action type';
+    }
+  }
+
+  return cloneHistory;
 }
 
 module.exports = transformStateWithClones;
