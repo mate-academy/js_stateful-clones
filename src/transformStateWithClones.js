9382cb81b267f595d469c21ca87147f3fbba1070
Nataliia Batiukova <batiukova.nataliia@gmail.com>
2023-01-19 20:09:12 +0000

    the task was solved

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const array = [];
+  const copy = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(copy, action.extraData);
+
+        break;
+
+      case 'removeProperties':
+        for (const remove of action.keysToRemove) {
+          delete copy[remove];
+        }
+
+        break;
+
+      case 'clear':
+        for (const deleteAll in copy) {
+          delete copy[deleteAll];
+        }
+        break;
+
+      default:
+        throw Error('this type does not exist');
+    }
+    array.push({ ...copy });
+  }
+
+  return array;
 }
 
 module.exports = transformStateWithClones;
