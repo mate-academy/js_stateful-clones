1f6e91f6a9d9fc6adf9aa95824839ae55fbee496
Roman Hrytsiuk <vasylguk7@gmail.com>
2023-01-20 00:57:25 +0200

    Stateful clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let copy = { ...state };
+
+  for (const property of actions) {
+    switch (property.type) {
+      case 'addProperties':
+        Object.assign(copy, property.extraData);
+        result.push({ ...copy });
+        break;
+
+      case 'removeProperties':
+        for (const item of property.keysToRemove) {
+          delete copy[item];
+        }
+        result.push({ ...copy });
+        break;
+
+      case 'clear':
+        copy = {};
+        result.push({ ...copy });
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
