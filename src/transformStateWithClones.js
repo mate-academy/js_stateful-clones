be16db786642cd256384fb3e6039149f7fc77856
Nazarii Piatnochka <nazar.piatnochka@gmail.com>
2023-01-23 13:08:58 +0200

    The tests were passed successfully

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const array = [];
+  let tempSTate = { ...state };
+
+  for (const item of actions) {
+    for (const key in item) {
+      if (key === 'extraData') {
+        Object.assign(tempSTate, item[key]);
+        array.push({ ...tempSTate });
+      }
+
+      if (key === 'keysToRemove') {
+        for (const k in item[key]) {
+          delete tempSTate[item[key][k]];
+        }
+        array.push({ ...tempSTate });
+      }
+
+      if (item[key] === 'clear') {
+        tempSTate = {};
+        array.push({ ...tempSTate });
+      }
+    }
+  }
+
+  return array;
 }
 
 module.exports = transformStateWithClones;
