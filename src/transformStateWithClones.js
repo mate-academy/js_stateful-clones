2edc25bc9f5a96efedc83b6895c0e1940852f81b
Yevhenii Hura <yevheniihura@gmail.com>
2023-01-23 18:11:16 +0200

    function was created

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const clonedState = { ...state };
+
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'addProperties') {
+      Object.assign(clonedState, actions[i].extraData);
+      result.push(Object.assign({}, clonedState));
+    } else if (actions[i].type === 'removeProperties') {
+      const arr = actions[i].keysToRemove;
+
+      for (let k = 0; k < arr.length; k++) {
+        delete clonedState[arr[k]];
+      }
+
+      result.push(Object.assign({}, clonedState));
+    } else if (actions[i].type === 'clear') {
+      for (const key in clonedState) {
+        if (clonedState.hasOwnProperty(key)) {
+          delete clonedState[key];
+        }
+      }
+
+      result.push(Object.assign({}, clonedState));
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
