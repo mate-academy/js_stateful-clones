aaa6d3be286dc64259ea17afd6b32c0cc4b1e110
Anastasiia Em <ema199625@gmail.com>
2023-01-25 22:34:12 +0200

    Removed variable, moved push  out of switch

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,26 +11,22 @@ function transformStateWithClones(state, actions) {
   const modifiedStates = { ...state };
 
   for (const key of actions) {
-    const type = key.type;
-
-    switch (type) {
+    switch (key.type) {
       case 'addProperties':
         Object.assign(modifiedStates, key.extraData);
-        statesArray.push({ ...modifiedStates });
         break;
       case 'removeProperties':
         for (let x = 0; x < key.keysToRemove.length; x++) {
           delete modifiedStates[key.keysToRemove[x]];
         };
-        statesArray.push({ ...modifiedStates });
         break;
       case 'clear':
         for (const item in modifiedStates) {
           delete modifiedStates[item];
         };
-        statesArray.push({ ...modifiedStates });
         break;
     }
+    statesArray.push({ ...modifiedStates });
   }
 
   return statesArray;
