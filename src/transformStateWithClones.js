a49a277fb9dcd5a810ecdc703c970970a2ab3add
Peti51 <galpeter31@gmail.com>
2023-01-26 09:57:34 +0100

    commit message

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const copy = [];
+  const copyObj = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(copyObj, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const keys of action.keysToRemove) {
+          if (copyObj.hasOwnProperty(keys)) {
+            delete copyObj[keys];
+          }
+        }
+        break;
+
+      case 'clear':
+        for (const keys in copyObj) {
+          delete copyObj[keys];
+        }
+        break;
+    }
+
+    copy.push({ ...copyObj });
+  }
+
+  return copy;
 }
 
 module.exports = transformStateWithClones;
