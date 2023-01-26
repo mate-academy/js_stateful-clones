a43656ba6b36ec25db4ca1e0830fe5d14f7de713
Anastasiia Em <ema199625@gmail.com>
2023-01-26 19:36:22 +0200

    Added changes

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -10,14 +10,16 @@ function transformStateWithClones(state, actions) {
   const statesArray = [];
   const modifiedStates = { ...state };
 
-  for (const key of actions) {
-    switch (key.type) {
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
       case 'addProperties':
-        Object.assign(modifiedStates, key.extraData);
+        Object.assign(modifiedStates, extraData);
         break;
       case 'removeProperties':
-        for (let x = 0; x < key.keysToRemove.length; x++) {
-          delete modifiedStates[key.keysToRemove[x]];
+        for (let x = 0; x < keysToRemove.length; x++) {
+          delete modifiedStates[keysToRemove[x]];
         };
         break;
       case 'clear':
@@ -25,6 +27,8 @@ function transformStateWithClones(state, actions) {
           delete modifiedStates[item];
         };
         break;
+      default:
+        throw Error('There is no such action');
     }
     statesArray.push({ ...modifiedStates });
   }
