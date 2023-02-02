2ff6fe70e1e5c0e286b53735bd072af639b869b2
Yevhen-Ivanchuk <e.ivanchyk@gmail.com>
2023-02-02 12:00:06 +0200

    Clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,37 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateArray = [];
+
+  let stateCopy = {
+    ...state,
+  };
+
+  for (const part of actions) {
+    switch (part.type) {
+      case 'addProperties':
+        stateCopy = {
+          ...stateCopy,
+          ...part.extraData,
+        };
+        stateArray.push({ ...stateCopy });
+        break;
+
+      case 'removeProperties':
+        for (const removeElement of part.keysToRemove) {
+          delete stateCopy[removeElement];
+        }
+        stateArray.push({ ...stateCopy });
+        break;
+
+      case 'clear':
+        stateCopy = {};
+        stateArray.push({});
+        break;
+    }
+  }
+
+  return stateArray;
 }
 
 module.exports = transformStateWithClones;
