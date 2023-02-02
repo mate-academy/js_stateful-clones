27880a531257a60a77d05e9cf86e38ecd1ff60eb
Hanna Balabukha <anna.aniskovets1992@ukr.net>
2023-02-02 17:09:32 +0300

    'task'

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const array = [];
+  let newObj = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(newObj, action.extraData);
+        array.push({...newObj});
+        break;
+
+      case 'removeProperties':
+        for (const type of action.keysToRemove) {
+          if (newObj[type]) {
+            delete newObj[type];
+          }
+        }
+        array.push({...newObj});
+        break;
+
+      case 'clear':
+        newObj = {};
+        array.push({});
+      }
+    }
+
+  return array;
 }
 
 module.exports = transformStateWithClones;
