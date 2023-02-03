57866aa1af41d96c8b69ca93c7b6171b5cae610a
Yevhen Prymushko <yevhenpetrov6@gmail.com>
2023-02-03 10:10:08 +0200

    Stateful clones created

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const res = [];
+  const obj = Object.assign({}, state);
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(obj, action.extraData);
+
+      res.push(Object.assign({}, obj));
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const remove of action.keysToRemove) {
+        delete obj[remove];
+      }
+
+      res.push(Object.assign({}, obj));
+    }
+
+    if (action.type === 'clear') {
+      for (const prop in obj) {
+        delete obj[prop];
+      }
+
+      res.push({});
+    }
+  }
+
+  return res;
 }
 
 module.exports = transformStateWithClones;
