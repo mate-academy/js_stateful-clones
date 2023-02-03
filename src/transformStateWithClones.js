c3e09ffe4b7c97621c789ffb912f02aaf7e3528c
Yevhen Prymushko <yevhenpetrov6@gmail.com>
2023-02-03 13:42:30 +0200

    chenges made

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,30 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
+  const add = 'addProperties';
+  const remove = 'removeProperties';
   const res = [];
-  const obj = Object.assign({}, state);
+  const stateCopy = Object.assign({}, state);
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(obj, action.extraData);
-
-      res.push(Object.assign({}, obj));
-    }
-
-    if (action.type === 'removeProperties') {
-      for (const remove of action.keysToRemove) {
-        delete obj[remove];
-      }
-
-      res.push(Object.assign({}, obj));
-    }
-
-    if (action.type === 'clear') {
-      for (const prop in obj) {
-        delete obj[prop];
-      }
-
-      res.push({});
+    switch (action.type) {
+      case add:
+        Object.assign(stateCopy, action.extraData);
+        res.push(Object.assign({}, stateCopy));
+        break;
+      case remove:
+        for (const key of action.keysToRemove) {
+          delete stateCopy[key];
+        }
+        res.push(Object.assign({}, stateCopy));
+        break;
+      default:
+        for (const prop in stateCopy) {
+          delete stateCopy[prop];
+        }
+        res.push({});
+        break;
     }
   }
 
