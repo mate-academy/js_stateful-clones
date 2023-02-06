c1ba892deb68bba83ba9365d40b74160951059c1
Rufat Mammadov <rufat.mammadov.m@gmail.com>
2023-02-06 18:31:45 +0200

    stateful-clones - first try

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,41 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arrayOfStates = [];
+  const copyOfState = { ...state };
+
+  for (const action of actions) {
+    const {
+      type,
+      extraData,
+      keysToRemove,
+    } = action;
+
+    switch (type) {
+      case 'addProperties': {
+        Object.assign(copyOfState, extraData);
+        arrayOfStates.push({ ...copyOfState });
+        break;
+      }
+
+      case 'removeProperties': {
+        for (const key of keysToRemove) {
+          delete copyOfState[key];
+        }
+        arrayOfStates.push({ ...copyOfState });
+        break;
+      }
+
+      case 'clear': {
+        for (const key in copyOfState) {
+          delete copyOfState[key];
+        }
+        arrayOfStates.push({ ...copyOfState });
+      }
+    }
+  }
+
+  return arrayOfStates;
 }
 
 module.exports = transformStateWithClones;
