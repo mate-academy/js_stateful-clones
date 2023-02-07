c8e35d3378474b5b9b97b84093b1aeffebb24c85
ivanm2706 <ivanmorhun664@gmail.com>
2023-02-07 08:34:10 +0200

    added swich, destructurisation

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -10,32 +10,32 @@ function transformStateWithClones(state, actions) {
   const resultState = [{ ...state }];
   let count = 0;
 
-  for (const char of actions) {
+  for (const action of actions) {
+    const { type, keysToRemove, extraData } = action;
+
     if (count > 0) {
       Object.assign(resultState[count] = {}, resultState[count - 1]);
     }
 
-    if (char.type === 'addProperties') {
-      Object.assign(resultState[count], char.extraData);
-    }
+    switch (type) {
+      case 'addProperties':
+        Object.assign(resultState[count], extraData);
+        count++;
+        break;
 
-    if (char.type === 'removeProperties') {
-      for (const charRemove of char.keysToRemove) {
-        for (const key in resultState[count]) {
-          if (charRemove === key) {
-            delete resultState[count][key];
-          }
+      case 'removeProperties':
+        for (const key of keysToRemove) {
+          delete resultState[count][key];
         }
-      }
-    }
+        count++;
+        break;
 
-    if (char.type === 'clear') {
-      for (const key in resultState[count]) {
-        delete resultState[count][key];
-      }
+      case 'clear':
+        for (const key in resultState[count]) {
+          delete resultState[count][key];
+        }
+        count++;
     }
-
-    count++;
   }
 
   return resultState;
