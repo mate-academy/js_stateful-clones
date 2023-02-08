f07336969edc4a42cd40c0571d12406dfe7e32f3
Serhii Nikolin <sergeynikolin@gmail.com>
2023-02-08 19:14:20 +0200

    Task done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const supportObj = {};
+  const result = [];
+
+  Object.assign(supportObj, state);
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(supportObj, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const remProp of action.keysToRemove) {
+          if (supportObj[remProp]) {
+            delete supportObj[remProp];
+          }
+        };
+        break;
+
+      case 'clear':
+        for (const prop in supportObj) {
+          delete supportObj[prop];
+        }
+        break;
+    }
+
+    result.push(Object.assign({}, supportObj));
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
