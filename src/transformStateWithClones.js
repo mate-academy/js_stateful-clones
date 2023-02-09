c6a6e9bc70cc746365d31adc8c89f59c9a7daaac
luka4ok <vitalik.shostak@gmail.com>
2023-02-09 18:46:21 +0200

    -action- desctructured. improved -clear- case. -throw new Error- added to -default-

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,31 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const newState = { ...state };
+  let newState = { ...state };
   const result = [];
 
-  for (const action of actions) {
-    switch (action.type) {
+  for (const { extraData, keysToRemove, type } of actions) {
+    switch (type) {
       case 'addProperties':
-        Object.assign(newState, action.extraData);
+        Object.assign(newState, extraData);
         break;
 
       case 'removeProperties':
-        for (const key of action.keysToRemove) {
+        for (const key of keysToRemove) {
           delete newState[key];
         }
 
         break;
 
       case 'clear':
-        for (const key in newState) {
-          delete newState[key];
-        };
+        newState = {};
 
         break;
 
       default:
-        return 'This type of action cannot be found.';
+        throw new Error('Wrong type of action!');
     }
 
     result.push(Object.assign({}, newState));
