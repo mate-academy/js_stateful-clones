692b986591bc31be262454691ee08f58971b4ee5
Vadim Drobyazko <volandemortuwka@gmail.com>
2023-02-11 14:41:26 +0200

    state add

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,35 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  const massive = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        massive.push({ ...stateCopy });
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete stateCopy[key];
+        }
+
+        massive.push({ ...stateCopy });
+        break;
+
+      case 'clear':
+        for (const keys in stateCopy) {
+          delete stateCopy[keys];
+        }
+
+        massive.push({ ...stateCopy });
+        break;
+    }
+  }
+
+  return massive;
 }
 
 module.exports = transformStateWithClones;
