501a1d1274efa1dfe9c9b49433d8bb0a66d3306f
Anton-Tretiak <tretiak.ap@gmail.com>
2023-02-13 12:40:30 +0200

    fix name of variable

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,27 +8,27 @@
  */
 function transformStateWithClones(state, actions) {
   const resultArr = [];
-  const copy = { ...state };
+  const stateCopy = { ...state };
 
   for (const action of actions) {
     if (action.type === 'clear') {
-      for (const key of Object.keys(copy)) {
-        delete copy[key];
+      for (const key of Object.keys(stateCopy)) {
+        delete stateCopy[key];
       }
     }
 
     if (action.type === 'addProperties') {
       for (const data in action.extraData) {
-        copy[data] = action.extraData[data];
+        stateCopy[data] = action.extraData[data];
       }
     }
 
     if (action.type === 'removeProperties') {
       for (const data in action.keysToRemove) {
-        delete copy[action.keysToRemove[data]];
+        delete stateCopy[action.keysToRemove[data]];
       }
     }
-    resultArr.push({ ...copy });
+    resultArr.push({ ...stateCopy });
   }
 
   return resultArr;
