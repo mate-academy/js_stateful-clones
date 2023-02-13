ee6c9b325972753da0b37f6ecc5118db05c5b155
gretoq <dovhun3d@gmail.com>
2023-02-13 19:23:01 +0200

    Created modify solution which does not edit input values

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,52 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const copyState = Object.assign({}, state);
+  const resultArray = [];
+
+  for (const action of actions) {
+    const whatWeMustDo = action.type;
+    const isAdding = whatWeMustDo === 'addProperties';
+    const isDeleting = whatWeMustDo === 'removeProperties';
+    const isClearing = whatWeMustDo === 'clear';
+
+    switch (true) {
+      case (isAdding):
+        Object.assign(copyState, action.extraData);
+        break;
+
+      case (isDeleting):
+        deletProp(copyState, action.keysToRemove);
+        break;
+
+      case (isClearing):
+        clearingTheObject(copyState);
+        break;
+
+      default:
+        return 'Unknow error';
+    }
+
+    resultArray.push(Object.assign({}, copyState));
+  }
+
+  return resultArray;
+}
+
+function clearingTheObject(object) {
+  for (const key in object) {
+    delete object[key];
+  }
+
+  return object;
+}
+
+function deletProp(object, props) {
+  for (const prop of props) {
+    delete object[prop];
+  }
+
+  return object;
 }
 
 module.exports = transformStateWithClones;
