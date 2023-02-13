c19bab23c5380e0ef63396419d5695d443768deb
Rashyd <rashydlezgi@gmail.com>
2023-02-13 19:24:50 +0200

    transformStateWithClones is done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,39 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const states = [];
+
+  for (const action of actions) {
+    let stateCopy = {};
+
+    if (states.length === 0) {
+      stateCopy = { ...state };
+    } else {
+      stateCopy = { ...states[states.length - 1] };
+    }
+
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const keyToRemove of action.keysToRemove) {
+          delete stateCopy[keyToRemove];
+        }
+        break;
+
+      case 'clear':
+        for (const keyToRemove of Object.keys(stateCopy)) {
+          delete stateCopy[keyToRemove];
+        }
+        break;
+    }
+
+    states.push(stateCopy);
+  }
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
