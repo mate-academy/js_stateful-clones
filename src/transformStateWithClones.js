0dc6538d4053a9970504a5a25afe2a911f410e95
kirill-vypirovskyi <kirill.vypirovsky@gmail.com>
2023-02-14 13:07:53 +0200

    code refactored

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,46 +7,26 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const addProps = (current, props) => {
-    const newObject = { ...current };
-
-    Object.assign(newObject, props);
-
-    return newObject;
-  };
-
-  const removeProps = (current, props) => {
-    const newObject = { ...current };
-
-    for (const key of props) {
-      delete newObject[key];
-    }
-
-    return newObject;
-  };
-
   const resultArray = [];
-  const actionsToDo = [ ...actions ];
-  let currentState = state;
+  let currentState = { ...state };
 
-  for (const action of actionsToDo) {
+  for (const action of [...actions]) {
     switch (action.type) {
       case 'addProperties':
-        const added = addProps(currentState, action.extraData);
-
-        resultArray.push(added);
-        currentState = added;
+        Object.assign(currentState, action.extraData);
         break;
+
       case 'removeProperties':
-        const removed = removeProps(currentState, action.keysToRemove);
-
-        resultArray.push(removed);
-        currentState = removed;
+        for (const key of action.keysToRemove) {
+          delete currentState[key];
+        }
         break;
+
       default:
-        resultArray.push({});
         currentState = {};
     }
+
+    resultArray.push({ ...currentState });
   }
 
   return resultArray;
