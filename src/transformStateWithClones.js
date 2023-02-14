d729abd5cbff22ac267974ccbc7838a785657457
Bozhena Bodnar <bozhena2705bodnar@gmail.com>
2023-02-14 18:28:17 +0100

    create clone

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arrayOfObj = [];
+  const copy = {};
+
+  Object.assign(copy, state);
+
+  for (const act of actions) {
+    if (act.type === 'addProperties') {
+      Object.assign(copy, act.extraData);
+    }
+
+    if (act.type === 'removeProperties') {
+      const arr = act.keysToRemove;
+
+      for (const key of arr) {
+        delete copy[key];
+      }
+    }
+
+    if (act.type === 'clear') {
+      for (const key in copy) {
+        delete copy[key];
+      }
+    }
+
+    arrayOfObj.push({ ...copy });
+
+    return arrayOfObj;
+  }
 }
 
 module.exports = transformStateWithClones;
