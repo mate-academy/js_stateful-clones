f85e72e38afe97256858f2855df0320c466df6ea
andrej <andersontwentytwo@gmail.com>
2023-02-14 19:03:35 +0200

    task implemented

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,40 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [
+    { ...state },
+  ];
+
+  for (const action of actions) {
+    const tempState = {
+      ...result[result.length - 1],
+    };
+
+    const { type, extraData, keysToRemove } = action;
+
+    switch (true) {
+      case type === 'addProperties':
+        Object.assign(tempState, extraData);
+        break;
+
+      case type === 'clear':
+        for (const key in tempState) {
+          delete tempState[key];
+        }
+        break;
+
+      case type === 'removeProperties':
+        for (const key of keysToRemove) {
+          delete tempState[key];
+        }
+        break;
+    }
+    result.push(tempState);
+  }
+
+  result.shift();
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
