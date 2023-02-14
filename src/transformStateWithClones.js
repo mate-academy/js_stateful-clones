f004a53ba868018de2b66d342659f2cb2f8f14a4
Mark Babich <morak.bobama@gmail.com>
2023-02-14 20:11:31 +0200

    Fixed default

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -12,18 +12,21 @@ function transformStateWithClones(state, actions) {
 
   for (const action of actions) {
     switch (action.type) {
-      case '':
-        return '';
       case 'addProperties':
         Object.assign(stateClone, action.extraData);
         break;
+
       case 'removeProperties':
         for (const keyToRemove of action.keysToRemove) {
           delete stateClone[keyToRemove];
         }
         break;
+
       case 'clear':
         stateClone = {};
+        break;
+
+      default:
         break;
     }
 
