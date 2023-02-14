3053f5a3a9fcd8720cf6e3a25a3274406f962a9a
Mark Babich <morak.bobama@gmail.com>
2023-02-14 15:28:23 +0200

    Stateful clones task done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let stateClone = { ...state };
+  const changeHistoryArray = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case '':
+        return '';
+      case 'addProperties':
+        Object.assign(stateClone, action.extraData);
+        break;
+      case 'removeProperties':
+        for (const keyToRemove of action.keysToRemove) {
+          delete stateClone[keyToRemove];
+        }
+        break;
+      case 'clear':
+        stateClone = {};
+        break;
+    }
+
+    changeHistoryArray.push({ ...stateClone });
+  }
+
+  return changeHistoryArray;
 }
 
 module.exports = transformStateWithClones;
