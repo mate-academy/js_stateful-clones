df5ede413bfd3c31cb0151dfb8db6958b902a9b6
Bogdan <bodia-00wot@ukr.net>
2023-02-14 17:51:25 +0200

    sol

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let stateCopy = Object.assign({}, state);
+  const stateVersions = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties' :
+        Object.assign(stateCopy, action.extraData);
+        break;
+
+      case 'removeProperties' :
+        for (const key in action.keysToRemove) {
+          if (stateCopy.hasOwnProperty(action.keysToRemove[key])) {
+            delete stateCopy[action.keysToRemove[key]];
+          }
+        }
+        break;
+
+      case 'clear':
+        stateCopy = {};
+        break;
+
+      default:
+        return 'err';
+    }
+    stateVersions.push({ ...stateCopy });
+  }
+
+  return stateVersions;
 }
 
 module.exports = transformStateWithClones;
