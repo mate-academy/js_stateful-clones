c6a38c5d447d367beada48f5d87150bdbc7bb4e0
Mikhieiev Ivan <navigator79@gmail.com>
2023-02-15 13:18:40 +0200

    clone

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const cloneState = { ...state };
+  const array = [];
+
+  for (const obj of actions) {
+    switch (obj.type) {
+      case 'addProperties':
+        Object.assign(cloneState, obj.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of obj.keysToRemove) {
+          delete cloneState[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in cloneState) {
+          delete cloneState[key];
+        }
+        break;
+    }
+    array.push({ ...cloneState });
+  }
+
+  return array;
 }
 
 module.exports = transformStateWithClones;
