08fac3f64a04df8b2b1755072732eefeda2f5837
vadosik15 <vadim.bratus@gmail.com>
2023-02-15 17:15:01 +0300

    checks

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arrayActions = [];
+  const newState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(newState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        action.keysToRemove.forEach((index) => delete newState[index]);
+        break;
+
+      case 'clear':
+        for (const keys in newState) {
+          delete newState[keys];
+        }
+        break;
+
+      default:
+        break;
+    }
+    arrayActions.push({ ...newState });
+  }
+
+  return arrayActions;
 }
 
 module.exports = transformStateWithClones;
