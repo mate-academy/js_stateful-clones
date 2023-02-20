fb7bce1adcb610e10ed6a979ae1ed84ed6743c8e
Ultramarin <000100010111ultramrin@gmail.com>
2023-02-20 02:07:08 +0200

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,31 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const result = [];
+  const tempState = { ...state };
+
+  for (const action of actions) {
+    if (action['type'] === 'addProperties') {
+      Object.keys(action['extraData']).forEach(key => {
+        tempState[key] = action['extraData'][key];
+      });
+    }
+
+    if (action['type'] === 'removeProperties') {
+      action['keysToRemove'].forEach(key => {
+        delete tempState[key];
+      });
+    }
+
+    if (action['type'] === 'clear') {
+      Object.keys(tempState).forEach(key => {
+        delete tempState[key];
+      });
+    }
+    result.push({ ...tempState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
