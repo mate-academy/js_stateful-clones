977150c94f42c54f6ae104c0ccd4ba178f6cd92c
Oleg <rakolegit@gmail.com>
2023-02-20 15:48:35 +0200

    transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,31 @@
  *
  * @return {Object[]}
  */
-function transformStateWithClones(state, actions) {
-  // write code here
+function transformStateWithClones(state = {}, actions) {
+  const arr = [];
+  const nevobj = Object.assign({}, state);
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'clear':
+        for (const key in nevobj) {
+          delete nevobj[key];
+        }
+        arr.push({ ...nevobj });
+        break;
+      case 'addProperties':
+        Object.assign(nevobj, action.extraData);
+        arr.push({ ...nevobj });
+        break;
+      case 'removeProperties':
+        for (const remove of action.keysToRemove) {
+          delete nevobj[remove];
+        }
+        arr.push({ ...nevobj });
+    }
+  }
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
