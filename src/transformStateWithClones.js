0850271e67a2958777f0ec2c4346b8ff6500891c
Oleksandr Lytovchenko <jaster93him@gmail.com>
2023-02-22 14:15:50 +0200

    created clone object

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newState = { ...state };
+  const resalt = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(newState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete newState[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key of Object.keys(newState)) {
+          delete newState[key];
+        }
+        break;
+    }
+
+    resalt.push({ ...newState });
+  }
+
+  return resalt;
 }
 
 module.exports = transformStateWithClones;
