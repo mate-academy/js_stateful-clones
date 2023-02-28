23dc2408444f1a3f5ae5da5e107baa631767d14e
Yurii Hlushenko <yauglushenko@gmail.com>
2023-02-28 16:18:40 +0200

    switch/case used

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -10,21 +10,22 @@ function transformStateWithClones(state, actions) {
   const result = [];
   let stateClone = { ...state };
 
-  for (const obj of actions) {
-    if (obj.type === 'addProperties') {
-      Object.assign(stateClone, obj.extraData);
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateClone, action.extraData);
+        break;
+      case 'removeProperties':
+        for (const keys of action.keysToRemove) {
+          delete stateClone[keys];
+        }
+        break;
+      case 'clear':
+        stateClone = {};
+        break;
+      default:
+        result.push({ ...stateClone });
     }
-
-    if (obj.type === 'removeProperties') {
-      for (const keys of obj.keysToRemove) {
-        delete stateClone[keys];
-      }
-    }
-
-    if (obj.type === 'clear') {
-      stateClone = {};
-    }
-
     result.push({ ...stateClone });
   }
 
