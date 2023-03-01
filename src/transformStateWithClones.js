eceadcbc05c9eea58f513b72ee0708b70ba9f3c6
Vladyslav Kulybaba <106146104+VladGraund@users.noreply.github.com>
2023-03-01 12:48:27 +0100

    complete task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,35 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const mutableState = { ...state };
+
+  for (const key of actions) {
+    switch (key.type) {
+      case 'addProperties':
+        Object.assign(mutableState, key.extraData);
+
+        result.push({ ...mutableState });
+        break;
+
+      case 'clear':
+        for (const prop of Object.getOwnPropertyNames(mutableState)) {
+          delete mutableState[prop];
+        }
+        result.push({ ...mutableState });
+        break;
+
+      case 'removeProperties':
+        for (const val of key.keysToRemove) {
+          delete mutableState[val];
+        }
+        result.push({ ...mutableState });
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
