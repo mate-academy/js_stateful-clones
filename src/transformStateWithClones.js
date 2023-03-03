43471a7cf482c7cd4f466773d3c41ea68e317450
Julia <julia@ocuhealthcare.com>
2023-03-02 20:22:40 -0600

    Solution for Cloning and transforming state

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newState = Object.assign({}, state);
+  const result = [];
+
+  for (const action of actions) {
+    if (action.type === 'clear') {
+      const stateKeys = Object.keys(newState);
+
+      for (const key of stateKeys) {
+        delete newState[key];
+      }
+
+      result.push({ ...newState });
+    } else if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete newState[key];
+      }
+
+      result.push({ ...newState });
+    } else if (action.type === 'addProperties') {
+      Object.assign(newState, action.extraData);
+
+      result.push({ ...newState });
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
