6a7426d93d26c05b76f7f52c9a10e93b3992ac4c
Volodymyr Zadorozhnyi <Torsida@gmail.com>
2023-03-04 22:10:37 +0200

    first version

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,40 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const clonedStates = [];
+  let currentState = { ...state };
+
+  for (const { type, extraData, keysToRemove } of actions) {
+    let nextState;
+
+    switch (type) {
+      case 'addProperties':
+        nextState = {
+          ...currentState, ...extraData,
+        };
+        break;
+      case 'removeProperties':
+        nextState = {};
+
+        for (const [key, value] of Object.entries(currentState)) {
+          if (!keysToRemove.includes(key)) {
+            nextState[key] = value;
+          }
+        }
+
+        break;
+      case 'clear':
+        nextState = {};
+        break;
+      default:
+        return 'Unknown action type';
+    }
+
+    clonedStates.push(nextState);
+    currentState = nextState;
+  }
+
+  return clonedStates;
 }
 
 module.exports = transformStateWithClones;
