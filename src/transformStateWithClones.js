760712c934b1e32312947b28256c2b02b004c9e4
Vladyslav Fedenko <vadondrummer@gmail.com>
2023-03-07 00:10:28 +0200

    Fedenko-Stateful-clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let newState = { ...state };
+
+  for (const actionProperties of actions) {
+    const { type, extraData, keysToRemove } = actionProperties;
+
+    if (type === 'addProperties') {
+      Object.assign(newState, extraData);
+      result.push({ ...newState });
+    }
+
+    if (type === 'removeProperties') {
+      for (const key of keysToRemove) {
+        delete newState[key];
+      }
+      result.push({ ...newState });
+    }
+
+    if (type === 'clear') {
+      for (const key in newState) {
+        delete newState[key];
+      }
+      result.push({ ...newState });
+      newState = {};
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
