87749c0358da517a07b7c686e38c377dcdfccf49
Pavlo Reutskiy <pavel.reutskiy@gmail.com>
2023-03-13 20:12:09 +0200

    added transformStateWithClones function

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  const stateVersions = [];
+
+  for (const version of actions) {
+    switch (version.type) {
+      case 'clear':
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
+        stateVersions.push({ ...stateCopy });
+        break;
+
+      case 'addProperties':
+        Object.assign(stateCopy, version.extraData);
+        stateVersions.push({ ...stateCopy });
+        break;
+
+      case 'removeProperties':
+        for (const removable of version.keysToRemove) {
+          delete stateCopy[removable];
+        }
+        stateVersions.push({ ...stateCopy });
+        break;
+    }
+  }
+
+  return stateVersions;
 }
 
 module.exports = transformStateWithClones;
