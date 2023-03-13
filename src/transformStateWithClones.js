5330c9d9916afcbef1673490fae152efc9084b60
victor buldenko <gookidoo@gmail.com>
2023-03-13 20:00:04 +0300

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,27 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const obj = Object.assign({}, state);
+  const result = [];
+
+  actions.forEach(element => {
+    if (element['type'] === 'clear') {
+      for (const key in obj) {
+        delete obj[key];
+      }
+      result.push({ ...obj });
+    } else if (element['type'] === 'removeProperties') {
+      element['keysToRemove'].forEach(prop => {
+        delete obj[prop];
+      });
+      result.push({ ...obj });
+    } else {
+      Object.assign(obj, element['extraData']);
+      result.push({ ...obj });
+    }
+  });
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
