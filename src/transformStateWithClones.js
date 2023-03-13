c494e5e1ebe3a223e2a3550414931be47d313ce6
Mark Boiko <boikomark555@gmail.com>
2023-03-13 20:30:30 +0200

    create transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,42 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arrOfObjects = [];
+  let copyState = {};
+  let objectOfEveryProperty = {};
+
+  Object.assign(copyState, state);
+
+  for (const obj of actions) {
+    switch (obj.type) {
+      case 'addProperties':
+        objectOfEveryProperty = Object.assign({}, copyState, obj.extraData);
+        arrOfObjects.push(objectOfEveryProperty);
+        copyState = {};
+        Object.assign(copyState, objectOfEveryProperty);
+        break;
+      case 'removeProperties':
+        obj.keysToRemove.forEach(elem => delete copyState[elem]);
+        objectOfEveryProperty = Object.assign({}, copyState);
+        copyState = {};
+        Object.assign(copyState, objectOfEveryProperty);
+        arrOfObjects.push(objectOfEveryProperty);
+        break;
+
+      case 'clear':
+        for (const elem in copyState) {
+          delete copyState[elem];
+        }
+        objectOfEveryProperty = Object.assign({}, copyState);
+        arrOfObjects.push(objectOfEveryProperty);
+        break;
+
+      default:
+        return null;
+    }
+  }
+
+  return arrOfObjects;
 }
 
 module.exports = transformStateWithClones;
