15850f4a0c8611f7e02caa4ef37491f916c4bd29
Vasyl Zhyliakov <snak871@gmail.com>
2023-03-14 13:19:59 +0200

    transform clones function was created

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,35 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const array = [];
+  const stateClone = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateClone, action.extraData);
+        array.push({ ...stateClone });
+        break;
+
+      case 'removeProperties':
+        for (const character of action.keysToRemove) {
+          if (stateClone.hasOwnProperty(character)) {
+            delete stateClone[character];
+          }
+        }
+        array.push({ ...stateClone });
+        break;
+
+      case 'clear':
+        for (const key in stateClone) {
+          delete stateClone[key];
+        }
+        array.push({ ...stateClone });
+        break;
+    }
+  }
+
+  return array;
 }
 
 module.exports = transformStateWithClones;
