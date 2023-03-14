c11b3ec6d51d47fcac33257b1bef10b87ea307ea
Ihor <ikruzhynskyy@gmail.com>
2023-03-14 15:42:00 +0200

    changed variables name

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,30 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const newState = { ...state };
+  const stateCopy = { ...state };
   const result = [];
 
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties':
-        Object.assign(newState, action.extraData);
+        Object.assign(stateCopy, action.extraData);
         break;
 
       case 'removeProperties':
-        for (const key of action.keysToRemove) {
-          delete newState[key];
+        for (const property of action.keysToRemove) {
+          delete stateCopy[property];
         }
         break;
 
       case 'clear':
-        for (const data in newState) {
-          delete newState[data];
+        for (const property in stateCopy) {
+          delete stateCopy[property];
         }
         break;
       default: throw new Error('invalid action type');
     }
 
-    result.push({ ...newState });
+    result.push({ ...stateCopy });
   }
 
   return result;
