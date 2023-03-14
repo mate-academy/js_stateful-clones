6565671902b8419c1cdf845a5a2df127373466f3
Anna Latusha <annapop03072701@gmail.com>
2023-03-14 14:54:16 +0200

    Create function transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const cloneStates = [];
+  const cloneState = { ...state };
+
+  actions.forEach(action => {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(cloneState, action.extraData);
+        break;
+      case 'removeProperties':
+        action.keysToRemove.forEach(property => delete cloneState[property]);
+        break;
+      case 'clear':
+        for (const property in cloneState) {
+          delete cloneState[property];
+        }
+        break;
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
+    }
+
+    cloneStates.push({ ...cloneState });
+  });
+
+  return cloneStates;
 }
 
 module.exports = transformStateWithClones;
