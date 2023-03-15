f0aa925b409ee58e588880d66d09f85df4d4e41a
Oleh Kolodii <bigknife4@gmail.com>
2023-03-15 14:04:42 +0200

    fixes after checklist

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -9,30 +9,34 @@
 function transformStateWithClones(state, actions) {
   // write code here
   const resultArr = [];
-  const tempObj = { ...state };
+  const stateCopy = { ...state };
 
   for (const item of actions) {
     switch (item.type) {
       case 'addProperties':
-        Object.assign(tempObj, item.extraData);
+        Object.assign(stateCopy, item.extraData);
         break;
 
       case 'removeProperties':
         for (const keyToRemove of item.keysToRemove) {
-          if (tempObj.hasOwnProperty(keyToRemove)) {
-            delete tempObj[keyToRemove];
+          if (stateCopy.hasOwnProperty(keyToRemove)) {
+            delete stateCopy[keyToRemove];
           }
         }
         break;
 
       case 'clear':
-        for (const prop in tempObj) {
-          delete tempObj[prop];
+        for (const prop in stateCopy) {
+          delete stateCopy[prop];
         }
         break;
+
+      default: {
+        return 'An error occured';
+      }
     }
 
-    resultArr.push({ ...tempObj });
+    resultArr.push({ ...stateCopy });
   }
 
   return resultArr;
