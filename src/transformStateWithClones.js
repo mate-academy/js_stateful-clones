a4cfa4f11ae4326c87b6e4c312b80dd927b82ab4
BondarenkoDmitriy <bondarenko.dima.kiev@gmail.com>
2023-03-15 16:10:53 +0200

    function works

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const resultObj = [];
+  const phantomState = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(phantomState, action.extraData);
+    }
+
+    if (action.type === 'clear') {
+      for (const key in phantomState) {
+        delete phantomState[key];
+      }
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const keyForState of action.keysToRemove) {
+        if (phantomState[keyForState]) {
+          delete phantomState[keyForState];
+        }
+      }
+    }
+
+    resultObj.push({ ...phantomState });
+  }
+
+  return resultObj;
 }
 
 module.exports = transformStateWithClones;
