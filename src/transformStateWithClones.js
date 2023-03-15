96d14748b93ed58d4f622c59f70ae671e5d003fd
Maria Shmakova <masha.shmakova@gmail.com>
2023-03-15 15:47:17 +0200

    transformStateWithClones function were created

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const clone = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(clone, action.extraData);
+      result.push(Object.assign({}, clone));
+    }
+
+    if (action.type === 'removeProperties') {
+      action.keysToRemove.forEach(property => {
+        delete clone[property];
+      });
+      result.push(Object.assign({}, clone));
+    }
+
+    if (action.type === 'clear') {
+      Object.keys(clone).forEach(key => {
+        delete clone[key];
+      });
+      result.push(Object.assign({}, clone));
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
