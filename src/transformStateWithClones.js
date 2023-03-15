8f29c0041d080508f4f422803a46cf4aa0a27743
Hamuud <artemlask2014@gmail.com>
2023-03-15 15:47:13 +0200

    Solve the task using spread operator

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,38 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const copyState = { ...state };
+  const multipleStateArr = [];
+
+  actions.forEach(action => {
+    switch (action.type) {
+      case 'addProperties': {
+        Object.assign(copyState, action.extraData);
+        break;
+      }
+
+      case 'removeProperties': {
+        action.keysToRemove.forEach(key => {
+          delete copyState[key];
+        });
+        break;
+      }
+
+      case 'clear': {
+        for (const data in copyState) {
+          delete copyState[data];
+        }
+        break;
+      }
+
+      default:
+        return `Sorry i don't know this action`;
+    }
+
+    multipleStateArr.push({ ...copyState });
+  });
+
+  return multipleStateArr;
 }
 
 module.exports = transformStateWithClones;
