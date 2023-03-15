6e7cfe9600f56047007a0bdedb4bf85964b5fd7b
Anna Kuzmenko <mogilevs.anna@gmail.com>
2023-03-15 20:17:12 +0200

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const cloneState = JSON.parse(JSON.stringify(state));
+  const clonArr = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case ('addProperties'):
+        Object.assign(cloneState, action.extraData);
+        break;
+
+      case ('clear'):
+        for (const key in cloneState) {
+          delete cloneState[key];
+        }
+        break;
+
+      case ('removeProperties'):
+        for (const keyToRemove of action.keysToRemove) {
+          delete cloneState[keyToRemove];
+        }
+        break;
+
+      default:
+        return cloneState;
+    }
+    clonArr.push({ ...cloneState });
+  }
+
+  return clonArr;
 }
 
 module.exports = transformStateWithClones;
