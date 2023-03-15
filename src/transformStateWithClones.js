a1990fcf452b3bc2a4d24a3a96738858ab2a9e2e
Rostyslav Hirin <hirin.rostyslav@gmail.com>
2023-03-15 11:41:59 +0200

    Transformed and cloned

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const statesArray = [];
+  let stateCopy = { ...state };
+
+  actions.forEach(action => {
+    stateCopy = { ...stateCopy };
+
+    switch (action.type) {
+      case 'addProperties':
+        for (const key in action.extraData) {
+          stateCopy[key] = action.extraData[key];
+        }
+        break;
+
+      case 'removeProperties':
+        action.keysToRemove.forEach(key => delete stateCopy[key]);
+        break;
+
+      case 'clear':
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
+        break;
+
+      default:
+        throw new Error('Something went wrong!');
+    }
+    statesArray.push(stateCopy);
+  });
+
+  return statesArray;
 }
 
 module.exports = transformStateWithClones;
