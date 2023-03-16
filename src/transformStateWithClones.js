20b026b000927a5927726bb1d826a2ac2574361b
Oleksandr Fenchuk <ofenchuk@proton.me>
2023-03-16 10:59:07 +0200

    stateful clones initial

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,40 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
   // write code here
+  const transformed = [];
+  let newState = { ...state };
+
+  for (const object of actions) {
+    switch (object.type) {
+      case 'addProperties': {
+        Object.assign(newState, object.extraData);
+        transformed.push({ ...newState });
+        break;
+      }
+
+      case 'clear': {
+        newState = {};
+        transformed.push({});
+        break;
+      }
+
+      case 'removeProperties': {
+        for (const remove of object.keysToRemove) {
+          delete newState[remove];
+        }
+        transformed.push({ ...newState });
+        break;
+      }
+
+      default:
+        return object.type;
+    }
+  }
+
+  return transformed;
 }
 
 module.exports = transformStateWithClones;
