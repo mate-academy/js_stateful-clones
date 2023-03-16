e1f95db87ac4210d913c52134937e68896e5f08d
Dima <dmitriy.movchan.02@gmail.com>
2023-03-16 12:33:18 +0200

    Solution task 3

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,40 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const states = [state];
+  let currentState = state;
+
+  for (let i = 0; i < actions.length; i++) {
+    const action = actions[i];
+
+    switch (action.type) {
+      case 'addProperties':
+        currentState = {
+          ...currentState,
+          ...action.extraData,
+        };
+        break;
+      case 'removeProperties':
+        const filteredState = {};
+
+        for (const [key, value] of Object.entries(currentState)) {
+          if (!action.keysToRemove.includes(key)) {
+            filteredState[key] = value;
+          }
+        }
+        currentState = filteredState;
+        break;
+      case 'clear':
+        currentState = {};
+        break;
+      default:
+        throw new Error(`Unsupported action type: ${action.type}`);
+    }
+
+    states.push({ ...currentState });
+  }
+
+  return states.slice(1);
 }
 
 module.exports = transformStateWithClones;
