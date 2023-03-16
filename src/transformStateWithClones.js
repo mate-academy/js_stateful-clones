879e9ce8b9d551d52d41a265044d35caaa5a8c3d
kukla1989 <kukla.1989@ukr.net>
2023-03-16 09:26:42 +0200

    create function transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,36 @@
 'use strict';
 
 /**
- * @param {Object} state
+ * @param {Object} copyState
  * @param {Object[]} actions
  *
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  const result = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        break;
+
+      case 'removeProperties':
+        action.keysToRemove.forEach(key => delete stateCopy[key]);
+        break;
+
+      case 'clear':
+        Object.keys(stateCopy).forEach(key => delete stateCopy[key]);
+        break;
+
+      default:
+        throw new Error('invalid action.type');
+    }
+    result.push({ ...stateCopy });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
