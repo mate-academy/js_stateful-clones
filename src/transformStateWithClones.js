fbfca2341f98ed12c68688c90360093250b7c5d0
Aleksandr <alexnovaklawyer@gmail.com>
2023-03-16 11:17:52 +0200

    try-catch replaced with error handler in default case

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -10,31 +10,29 @@ function transformStateWithClones(state, actions) {
   const copyOfState = JSON.parse(JSON.stringify(state));
   const result = [];
 
-  try {
-    for (const action of actions) {
-      switch (action['type']) {
-        case 'addProperties':
-          for (const fieldToAdd in action['extraData']) {
-            copyOfState[fieldToAdd] = action['extraData'][fieldToAdd];
-          };
-          result.push(JSON.parse(JSON.stringify(copyOfState)));
-          break;
-        case 'removeProperties':
-          for (const keyToRemove of action['keysToRemove']) {
-            delete copyOfState[keyToRemove];
-          };
-          result.push(JSON.parse(JSON.stringify(copyOfState)));
-          break;
-        case 'clear':
-          for (const field in copyOfState) {
-            delete copyOfState[field];
-          }
-          result.push({});
-          break;
-      }
+  for (const action of actions) {
+    switch (action['type']) {
+      case 'addProperties':
+        for (const fieldToAdd in action['extraData']) {
+          copyOfState[fieldToAdd] = action['extraData'][fieldToAdd];
+        };
+        result.push(JSON.parse(JSON.stringify(copyOfState)));
+        break;
+      case 'removeProperties':
+        for (const keyToRemove of action['keysToRemove']) {
+          delete copyOfState[keyToRemove];
+        };
+        result.push(JSON.parse(JSON.stringify(copyOfState)));
+        break;
+      case 'clear':
+        for (const field in copyOfState) {
+          delete copyOfState[field];
+        }
+        result.push({});
+        break;
+      default:
+        throw new Error('Invalid operation');
     }
-  } catch (error) {
-    throw new Error('Invalid operation');
   }
 
   return result;
