78a575647d8e4c48925611e18dff8b10cacccb0e
Alex Rog <sasharg303@gmail.com>
2023-03-16 12:10:43 +0100

    version2

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,31 +6,6 @@
  *
  * @return {Object[]}
  */
-// function transformStateWithClones(state, actions) {
-//   const objMass = [];
-//   let iteratorForMass = 0;
-
-//   for (const iterator of actions) {
-//     if (iterator.type === 'addProperties') {
-//       // for (const key in iterator.extraData) {
-//       //   objMass.push(...iterator.extraData[key]);
-//       // }
-//       objMass.push(state);
-//       objMass[iteratorForMass] = Object.assign({ ...iterator.extraData });
-//     } else if (iterator.type === 'removeProperties') {
-//       objMass.push(state);
-//       for (const iterator1 of iterator.keysToRemove) {
-//         delete objMass[iteratorForMass][iterator1];
-//       }
-//     } else if (iterator.type === 'clear') {
-//       objMass.push(state);
-//       for (const key1 in objMass[iteratorForMass]) {
-//         delete objMass[iteratorForMass][key1];
-//       }
-//     }
-//     iteratorForMass++;
-//   }
-// }
 
 function transformStateWithClones(state, actions) {
   const stateCopy = JSON.parse(JSON.stringify(state));
