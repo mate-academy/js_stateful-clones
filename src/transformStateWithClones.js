72a90935685a8aeee8c32eb656fc162d45721db7
Natali <swetswet1234@gmail.com>
2023-03-20 10:51:51 +0200

    develop

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,39 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateClone = [{ ...state }];
+
+  for (const action of actions) {
+    const copyExtraData = { ...action.extraData };
+    const copyKeysToRemove = { ...action.keysToRemove };
+    const copyLastStateClone
+      = Object.assign({}, stateClone[stateClone.length - 1]);
+
+    switch (action.type) {
+      case 'addProperties' :
+        Object.assign(copyLastStateClone, copyExtraData);
+
+        stateClone.push(copyLastStateClone);
+
+        break;
+
+      case 'removeProperties' :
+        for (const variable in copyKeysToRemove) {
+          delete copyLastStateClone[copyKeysToRemove[variable]];
+        }
+
+        stateClone.push(copyLastStateClone);
+
+        break;
+
+      case 'clear' :
+        stateClone.push({});
+    }
+  }
+
+  const returnStateClone = stateClone.slice(1);
+
+  return returnStateClone;
 }
 
 module.exports = transformStateWithClones;
