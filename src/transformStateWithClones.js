95a892506e1ad537255cf02d165690304490f88f
Artem Kurinniy <artem.kyrinniy@gmail.com>
2023-03-21 23:21:53 +0200

    commit

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,27 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const array = [];
+  let stateCopy = {
+    ...state,
+  };
+
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i]['type'] === 'addProperties') {
+      Object.assign(stateCopy, actions[i]['extraData']);
+      array.push({ ...stateCopy });
+    } else if (actions[i]['type'] === 'removeProperties') {
+      for (const key of actions[i]['keysToRemove']) {
+        delete stateCopy[key];
+      }
+      array.push({ ...stateCopy });
+    } else if (actions[i]['type'] === 'clear') {
+      stateCopy = {};
+      array.push({ ...stateCopy });
+    }
+  }
+
+  return array;
 }
 
 module.exports = transformStateWithClones;
