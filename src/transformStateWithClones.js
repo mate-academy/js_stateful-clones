676cfe0fb5452bfe4a3b1b8712daf97ac0db315d
Antonio S <soi4.toxa@ukr.net>
2023-03-27 16:21:10 +0300

    Stateful-clone

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let stateClone = Object.assign({}, state);
+  const statesOfSteps = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateClone, action.extraData);
+        statesOfSteps.push({ ...stateClone });
+        continue;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          if (stateClone[key]) {
+            delete stateClone[key];
+          }
+        }
+        statesOfSteps.push({ ...stateClone });
+        continue;
+
+      case 'clear':
+        stateClone = {};
+        statesOfSteps.push({ ...stateClone });
+        continue;
+    }
+  }
+
+  return statesOfSteps;
 }
 
 module.exports = transformStateWithClones;
