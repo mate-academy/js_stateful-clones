0437a811b3f9d4dacabce6203536d180da7d4469
Daniel <henio09@wp.pl>
2023-03-30 16:32:18 +0200

    solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const actionsHistory = [];
+  const newState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(newState, action.extraData);
+        break;
+      case 'removeProperties':
+        action.keysToRemove.forEach(key => delete newState[key]);
+        break;
+      case 'clear':
+        Object.keys(newState).forEach(key => delete newState[key]);
+        break;
+
+      default:
+        return 'Error, probably the wrong parameter was specified.';
+    }
+
+    actionsHistory.push({ ...newState });
+  }
+
+  return actionsHistory;
 }
 
 module.exports = transformStateWithClones;
