8e3309edcac3fbff87d34499c58367974ed4363f
catgode <105614630+catgode@users.noreply.github.com>
2023-04-02 20:04:07 +0200

    solve

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,42 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const objectClones = [];
+  const clone = {};
+
+  for (const key in state) {
+    clone[key] = state[key];
+  }
+
+  for (const action of actions) {
+    switch (action['type']) {
+      case 'addProperties':
+        for (const value in action['extraData']) {
+          clone[value] = (action['extraData'])[value];
+        }
+        break;
+      case 'removeProperties':
+        for (const value of action['keysToRemove']) {
+          delete clone[value];
+        }
+        break;
+      case 'clear':
+        for (const value in clone) {
+          delete clone[value];
+        }
+        break;
+    }
+
+    const cloneMirror = {};
+
+    for (const key in clone) {
+      cloneMirror[key] = clone[key];
+    }
+
+    objectClones.push(cloneMirror);
+  }
+
+  return objectClones;
 }
 
 module.exports = transformStateWithClones;
