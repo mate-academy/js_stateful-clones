da4e5bd5d3c90afc4731931d1cd6acaf826172ec
Nazar Yants <125669761+stnayn@users.noreply.github.com>
2023-04-04 12:15:34 +0300

    Update solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -13,20 +13,20 @@ function transformStateWithClones(state, actions) {
   for (const i of actions) {
     const ToPush = { ...variableObj };
 
-    if (i.type === 'addProperties') {
-      Object.assign(ToPush, i.extraData);
-    }
-
-    if (i.type === 'removeProperties') {
-      for (const key of i.keysToRemove) {
-        delete ToPush[key];
-      }
-    }
-
-    if (i.type === 'clear') {
-      for (const key in variableObj) {
-        delete ToPush[key];
-      }
+    switch (i.type) {
+      case 'addProperties':
+        Object.assign(ToPush, i.extraData);
+        break;
+      case 'removeProperties':
+        for (const key of i.keysToRemove) {
+          delete ToPush[key];
+        }
+        break;
+      case 'clear':
+        for (const key in variableObj) {
+          delete ToPush[key];
+        }
+        break;
     }
 
     variableObj = { ...ToPush };
