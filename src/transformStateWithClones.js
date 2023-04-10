c513fd9de393f723c927c24753075866a3804ab8
Oksana Maruniak <syper.ksenka@gmail.com>
2023-04-10 23:05:06 +0300

    Stateful clones OkM

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arr = [];
+  const copyState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(copyState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete copyState[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in copyState) {
+          delete copyState[key];
+        };
+        break;
+
+      default:
+        return 'In nomine patris et filii et spiritus sancti. Amen';
+    }
+    arr.push({ ...copyState });
+  }
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
