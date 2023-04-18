4083cd26f5437edeaac17e81157f1478054e2db1
Leonid Krainik <rialleons@gmail.com>
2023-04-18 19:18:31 +0300

    Save clone states

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,41 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  const cloneStates = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        addProperties(stateCopy, action.extraData);
+        cloneStates.push({ ...stateCopy });
+        break;
+      case 'removeProperties':
+        removeProperties(stateCopy, action.keysToRemove);
+        cloneStates.push({ ...stateCopy });
+        break;
+      case 'clear':
+        removeProperties(stateCopy, Object.keys(stateCopy));
+        cloneStates.push({ ...stateCopy });
+        break;
+      default:
+        throw new Error('Error!');
+    }
+  }
+
+  return cloneStates;
+}
+
+function addProperties(state, data) {
+  for (const property in data) {
+    state[property] = data[property];
+  }
+}
+
+function removeProperties(state, properties) {
+  for (const property of properties) {
+    delete state[property];
+  }
 }
 
 module.exports = transformStateWithClones;
