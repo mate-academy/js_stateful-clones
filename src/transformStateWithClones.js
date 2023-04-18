694a63f4b60bbbcaf8a385ccf8a6c7f632f9867d
vladimirshub <vladimirscloud@icloud.com>
2023-04-18 20:38:18 +0300

    commit

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const currentState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(currentState, action.extraData);
+        break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete currentState[key];
+        }
+        break;
+      case 'clear':
+        for (const key in currentState) {
+          delete currentState[key];
+        }
+        break;
+      default:
+        throw new Error(`Invalid action type: ${action.type}`);
+    }
+
+    result.push({ ...currentState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
