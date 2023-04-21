bd5f144a850f1b867ba1f4ad2688387666bd2c75
Yuliia Kyrychenko <julina.wow@gmail.com>
2023-04-21 21:22:03 +0300

    v02

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateClones = [];
+
+  const stateCopy = { ...state };
+
+  for (let i = 0; i < actions.length; i++) {
+    const action = actions[i];
+
+    switch (action.type) {
+      case 'addProperties':
+        for (const addData in action.extraData) {
+          stateCopy[addData] = action.extraData[addData];
+        }
+        break;
+
+      case 'removeProperties':
+        for (const removeData of action.keysToRemove) {
+          delete stateCopy[removeData];
+        }
+        break;
+
+      case 'clear':
+        if (Object.keys(stateCopy).length !== 0) {
+          Object.keys(stateCopy).forEach(key => delete stateCopy[key]);
+        }
+        break;
+    }
+    stateClones.push({ ...stateCopy });
+  }
+
+  return stateClones;
 }
 
 module.exports = transformStateWithClones;
