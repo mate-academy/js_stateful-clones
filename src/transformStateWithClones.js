95ce86741fddda9173431dd4e4ad99fdec63a84d
Artem Pinchuk <aviktorovich193@gmail.com>
2023-04-23 14:51:22 +0300

    fixes after code review

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,23 +11,24 @@ function transformStateWithClones(state, actions) {
   let stateClone = { ...state };
 
   for (let i = 0; i <= actions.length - 1; i++) {
-    if (actions[i].type === 'addProperties') {
-      Object.assign(stateClone, actions[i].extraData);
-      result.push({ ...stateClone });
-    }
+    switch (actions[i].type) {
+      case 'addProperties':
+        Object.assign(stateClone, actions[i].extraData);
+        result.push({ ...stateClone });
+        break;
 
-    if (actions[i].type === 'removeProperties') {
-      for (const value of actions[i].keysToRemove) {
-        if (value in stateClone) {
-          delete stateClone[value];
+      case 'removeProperties':
+        for (const value of actions[i].keysToRemove) {
+          if (value in stateClone) {
+            delete stateClone[value];
+          }
         }
-      }
-      result.push({ ...stateClone });
-    }
+        result.push({ ...stateClone });
+        break;
 
-    if (actions[i].type === 'clear') {
-      result.push({});
-      stateClone = {};
+      case 'clear':
+        result.push({});
+        stateClone = {};
     }
   }
 
