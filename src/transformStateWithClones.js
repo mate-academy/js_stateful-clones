4d944bff4e0eb872057e0b7f302e0ef5babfc014
Iryna Khalniepiesova <karolinkagrey@gmail.com>
2023-04-26 17:03:16 +0300

    First commit

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,27 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let newState = { ...state };
+  const arr = [];
+
+  for (const key of actions) {
+    if (key.type === 'clear') {
+      newState = {};
+    }
+
+    if (key.type === 'addProperties') {
+      Object.assign(newState, key.extraData);
+    }
+
+    if (key.type === `removeProperties`) {
+      for (const type of key.keysToRemove) {
+        delete newState[type];
+      }
+    }
+    arr.push({ ...newState });
+  }
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
