0547a3cb2c0f32861cbff33b522c43db4a2b951f
Diana Tuz <dianatuz12@gmail.com>
2023-05-03 00:49:39 +0300

    clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const clonedState = Object.assign({}, state);
+
+  for (const key in actions) {
+    if (actions[key].type === 'addProperties') {
+      Object.assign(clonedState, actions[key].extraData);
+      result.push(Object.assign({}, clonedState));
+    }
+
+    if (actions[key].type === 'removeProperties') {
+      for (const del of actions[key].keysToRemove) {
+        delete clonedState[del];
+      }
+      result.push(Object.assign({}, clonedState));
+    }
+
+    if (actions[key].type === 'clear') {
+      for (const k in clonedState) {
+        delete clonedState[k];
+      }
+      result.push(Object.assign({}, clonedState));
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
