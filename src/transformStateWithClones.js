58ef0bb011fbc1547b7f46a32557241ff87fb637
Maksym Nemera <nickmitchell@ukr.net>
2023-05-04 16:58:00 +0300

    fix array return

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,41 +8,34 @@
  */
 function transformStateWithClones(state, actions) {
   const stateCopy = { ...state };
-  const stateArray = [stateCopy];
+  const stateArray = [];
 
   for (const action of actions) {
-    const values = Object.values(action);
-    const lastState = stateArray[stateArray.length - 1];
-    const clonedState = { ...lastState };
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        break;
 
-    if (values[0] === 'clear') {
-      for (const property in clonedState) {
-        delete clonedState[property];
-      }
-      stateArray.push(clonedState);
+      case 'removeProperties':
+        for (const property of action.keysToRemove) {
+          delete stateCopy[property];
+        }
+        break;
+
+      case 'clear':
+        for (const property in stateCopy) {
+          delete stateCopy[property];
+        }
+        break;
+
+      default:
+        break;
     }
 
-    for (const properties of values.slice(1)) {
-      switch (values[0]) {
-        case 'addProperties':
-          stateArray.push(clonedState);
-          Object.assign(clonedState, properties);
-          break;
-
-        case 'removeProperties':
-          for (const property of properties) {
-            delete clonedState[property];
-          }
-          stateArray.push(clonedState);
-          break;
-
-        default:
-          break;
-      }
-    }
+    stateArray.push({ ...stateCopy });
   }
 
-  return stateArray.slice(1);
+  return stateArray;
 }
 
 module.exports = transformStateWithClones;
