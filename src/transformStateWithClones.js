c6c3184b136d8e664ea34eda6465b5742452312d
6houston6 <olexiyhritsak@gmai.com>
2023-05-05 11:59:06 +0300

    add clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,24 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [Object.assign({}, state)];
+
+  for (const action of actions) {
+    let newState = Object.assign({}, result[result.length - 1]);
+
+    if (action.type === 'addProperties') {
+      Object.assign(newState, action.extraData);
+    } else if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete newState[key];
+      }
+    } else if (action.type === 'clear') {
+      newState = {};
+    }
+    result.push(newState);
+  }
+
+  return result.slice(1);
 }
 
 module.exports = transformStateWithClones;
