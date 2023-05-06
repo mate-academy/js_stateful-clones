af4cfcac6e0af1160135dae347a1de521161abbe
Andrii <zisuan.od@gmail.com>
2023-05-06 11:33:37 +0300

    looks like it works

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,39 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const arr = [];
+  const objectToEqual = {};
+
+  Object.assign(objectToEqual, state);
+
+  for (const action of actions) {
+    const whatToDo = action.type;
+
+    switch (whatToDo) {
+      case 'addProperties':
+        Object.assign(objectToEqual, action.extraData);
+        break;
+
+      case 'removeProperties':
+        action.keysToRemove.forEach((keyToRemove) =>
+          delete objectToEqual[keyToRemove]);
+        break;
+
+      case 'clear':
+        for (const key in objectToEqual) {
+          delete objectToEqual[key];
+        }
+        break;
+
+      default: return;
+    }
+
+    const copy = { ...objectToEqual };
+
+    arr.push(copy);
+  }
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
