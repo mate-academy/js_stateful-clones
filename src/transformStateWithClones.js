817c335f6f900b1065dbdc84dba6e93c2110ea0c
Iryna Balandiukh <balandiukh.i@gmail.com>
2023-05-09 17:39:45 +0300

    Add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,44 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateClone = { ...state };
+  const listOfStateVersions = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateClone, action.extraData);
+        listOfStateVersions.push({ ...stateClone });
+        break;
+
+      case 'removeProperties':
+        removeProperty(stateClone, action.keysToRemove);
+        listOfStateVersions.push({ ...stateClone });
+        break;
+
+      case 'clear':
+        clearObject(stateClone);
+        listOfStateVersions.push({ ...stateClone });
+        break;
+
+      default:
+        return stateClone;
+    }
+  }
+
+  return listOfStateVersions;
+}
+
+function removeProperty(object, properties) {
+  for (const property of properties) {
+    delete object[property];
+  }
+}
+
+function clearObject(object) {
+  for (const key in object) {
+    delete object[key];
+  }
 }
 
 module.exports = transformStateWithClones;
