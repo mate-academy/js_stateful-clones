33feb3429d3ba2962cdef00481eeec77550dfe09
Yuliya1907 <violet1907@ukr.net>
2023-05-10 16:59:09 +0300

    fixed name of variable

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,31 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const clonedObj = { ...state };
+  const stateCopy = { ...state };
   const result = [];
 
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties':
-        Object.assign(clonedObj, action['extraData']);
+        Object.assign(stateCopy, action['extraData']);
         break;
 
       case 'removeProperties':
         for (const key of action['keysToRemove']) {
-          delete clonedObj[key];
+          delete stateCopy[key];
         }
         break;
 
       case 'clear':
-        for (const key in clonedObj) {
-          delete clonedObj[key];
+        for (const key in stateCopy) {
+          delete stateCopy[key];
         }
         break;
 
       default: throw new Error(`This statement: ${action.type} is unknown`);
     }
 
-    result.push({ ...clonedObj });
+    result.push({ ...stateCopy });
   }
 
   return result;
