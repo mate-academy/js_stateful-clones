ab7c38ae0187120f468b14ec711b054d3ae00af5
Maksym Bobryk <booobryyyk@gmail.com>
2023-05-10 16:38:02 +0300

    add transformStateWithClones task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,37 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateClone = { ...state };
+  const transformedState = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        for (const data in action.extraData) {
+          stateClone[data] = action.extraData[data];
+        }
+        break;
+
+      case 'clear':
+        for (const key in stateClone) {
+          delete stateClone[key];
+        }
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete stateClone[key];
+        }
+        break;
+
+      default:
+        throw new Error(`There is no such action type: ${action.type}`);
+    }
+
+    transformedState.push({ ...stateClone });
+  }
+
+  return transformedState;
 }
 
 module.exports = transformStateWithClones;
