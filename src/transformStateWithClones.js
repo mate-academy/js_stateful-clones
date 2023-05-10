a6875b8bf26eedef0abee2119d099ec35419eedf
Liza Strykharchuk <ol220681vol@gmail.com>
2023-05-10 19:56:33 +0300

    code

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,37 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateClone = [Object.assign({}, state)];
+
+  for (const action of actions) {
+    let cloneObj = Object.assign({}, stateClone[stateClone.length - 1]);
+
+    switch (action.type) {
+      case 'addProperties':
+
+        cloneObj = {
+          ...cloneObj,
+          ...action.extraData,
+        };
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete cloneObj[key];
+        }
+        break;
+
+      case 'clear':
+        cloneObj = {};
+        break;
+
+      default:
+        break;
+    }
+    stateClone.push(cloneObj);
+  }
+
+  return stateClone.slice(1);
 }
 
 module.exports = transformStateWithClones;
