40f62ebd1cc36dc05349fe074935affbb3a0aa59
Vladyslav Lapshyn <lapshin.vlad96@gmail.com>
2023-05-11 01:48:53 +0300

    made js stateful clones task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const copyOfState = { ...state };
+  const arrayOfCopies = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(copyOfState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key in copyOfState) {
+          if (action.keysToRemove.includes(key)) {
+            delete copyOfState[key];
+          }
+        }
+        break;
+
+      case 'clear':
+        for (const key in copyOfState) {
+          delete copyOfState[key];
+        }
+    }
+
+    arrayOfCopies.push({ ...copyOfState });
+  }
+
+  return arrayOfCopies;
 }
 
 module.exports = transformStateWithClones;
