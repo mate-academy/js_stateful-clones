0629bfb0697e69fa589b2a87e4f8d272c9b59f28
Oleksandr Kosohor <kosogor2007@gmail.com>
2023-05-10 22:04:43 +0300

    solution with fixes

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,35 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const upgState = { ...state };
-  const result = [];
+  const copyState = { ...state };
+  const changeHistory = [];
 
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties' :
-        Object.assign(upgState, action.extraData);
-        result.push({ ...upgState });
+        Object.assign(copyState, action.extraData);
         break;
 
       case 'removeProperties' :
         for (const key of action.keysToRemove) {
-          delete upgState[key];
-        } result.push({ ...upgState });
+          delete copyState[key];
+        }
         break;
 
       case 'clear' :
-        for (const key in upgState) {
-          delete upgState[key];
+        for (const key in copyState) {
+          delete copyState[key];
         }
-        result.push({ ...upgState });
         break;
 
       default:
-        throw new Error('Error');
+        throw new Error('Invalid action type');
     }
+    changeHistory.push({ ...copyState });
   }
 
-  return result;
+  return changeHistory;
 }
 
 module.exports = transformStateWithClones;
