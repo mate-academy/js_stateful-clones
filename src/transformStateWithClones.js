0abe97e06fadac97b034ccf8892e554c0565e450
Roksolana Tylipska <roksolana1407@gmail.com>
2023-05-11 13:00:30 +0300

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,70 @@
  *
  * @return {Object[]}
  */
+
+// ____________for me_____________________________________
+// function transformStateWithClones(state, actions) {
+//   const stateVersion = [];
+//   let newState = {...state}
+
+//   for (const action of actions) {
+//     const { type, extraData, keysToRemove} = actions;
+
+//     if (type === 'addProperties') {
+//       newState = {...newState, ...extraData};
+//     }
+
+//     if (type === 'removeProperties') {
+//       for (const keyToRemove of keysToRemove) {
+//         delete newState[keyToRemove];
+//       }
+//     }
+
+//     if {type === 'clear'} {
+//       for (const keyToRemove in newState) {
+//         delete newState[keyToRemove]
+//       }
+//     }
+//   }
+
+//   return stateVersion;
+
+// }
+// ____________for me_____________________________________
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateVersion = [];
+  let newState = { ...state };
+
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
+      case 'addProperties':
+        newState = {
+          ...newState,
+          ...extraData,
+        };
+        break;
+
+      case 'removeProperties':
+        for (const keyToRemove of keysToRemove) {
+          delete newState[keyToRemove];
+        }
+        break;
+
+      case 'clear':
+        newState = {};
+        break;
+
+      default:
+        break;
+    }
+
+    stateVersion.push({ ...newState });
+  }
+
+  return stateVersion;
 }
 
 module.exports = transformStateWithClones;
