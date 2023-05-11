6c21b6003f7b1f7a529adf4d4dd918aae0bdc4d4
Roksolana Tylipska <roksolana1407@gmail.com>
2023-05-11 15:36:21 +0300

    deleted my comments in JS

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,37 +6,6 @@
  *
  * @return {Object[]}
  */
-
-// ____________for me_____________________________________
-// function transformStateWithClones(state, actions) {
-//   const stateVersion = [];
-//   let newState = {...state}
-
-//   for (const action of actions) {
-//     const { type, extraData, keysToRemove} = actions;
-
-//     if (type === 'addProperties') {
-//       newState = {...newState, ...extraData};
-//     }
-
-//     if (type === 'removeProperties') {
-//       for (const keyToRemove of keysToRemove) {
-//         delete newState[keyToRemove];
-//       }
-//     }
-
-//     if {type === 'clear'} {
-//       for (const keyToRemove in newState) {
-//         delete newState[keyToRemove]
-//       }
-//     }
-//   }
-
-//   return stateVersion;
-
-// }
-// ____________for me_____________________________________
-
 function transformStateWithClones(state, actions) {
   const stateVersion = [];
   let newState = { ...state };
