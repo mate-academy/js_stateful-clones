3d453f243a021fb08a464b03a7e456390d04a5a6
Olena Bielozorova <lenka3010@gmail.com>
2023-05-11 10:59:47 +0300

    SolutionClonesState

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,30 @@
  *
  * @return {Object[]}
  */
-function transformStateWithClones(state, actions) {
-  // write code here
+function transformStateWithClonees(state, actions) {
+  const result = [];
+  const clone = { ...state };
+  let multiClone = {};
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      for (const data in action.extraData) {
+        clone[data] = action.extraData[data];
+      }
+    } else if (action.type === 'removeProperties') {
+      for (const data of action.keysToRemove) {
+        delete clone[data];
+      }
+    } else if (action.type === 'clear') {
+      for (const data in clone) {
+        delete clone[data];
+      }
+    }
+    multiClone = { ...clone };
+    result.push(multiClone);
+  }
+
+  return result;
 }
 
-module.exports = transformStateWithClones;
+module.exports = transformStateWithClonees;
