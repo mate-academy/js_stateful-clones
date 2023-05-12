5e17eb0be7a2622b5df8c1bb62d50d7d121bc334
Dmytro Fefelov <reseneweb@gmail.com>
2023-05-12 15:48:35 +0300

    Added Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,39 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = actions;
+  let basis = { ...state };
+
+  for (const meaning in actions) {
+    const point = actions[meaning];
+
+    for (const keys in point) {
+      const intermediate = point[keys];
+
+      if (keys === 'extraData') {
+        result[meaning] = {
+          ...basis, ...intermediate,
+        };
+        basis = result[meaning];
+      }
+
+      if (keys === 'keysToRemove') {
+        result[meaning] = { ...basis };
+
+        for (let i = 0; i < intermediate.length; i++) {
+          delete result[meaning][intermediate[i]];
+        }
+        basis = result[meaning];
+      }
+
+      if (point[keys] === 'clear') {
+        result[meaning] = {};
+        basis = result[meaning];
+      }
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
