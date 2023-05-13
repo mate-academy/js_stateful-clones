2b6560768e861cd7c361b5bf27faed97db9293be
iv4ek <ivaskovila@gmail.com>
2023-05-13 08:58:29 +0200

    State was cloned

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,35 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateClone = { ...state };
+  const newState = [];
+
+  for (const act of actions) {
+    switch (act.type) {
+      case 'addProperties':
+        Object.assign(stateClone, act.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const prop of act.keysToRemove) {
+          delete stateClone[prop];
+        }
+        break;
+
+      case 'clear':
+        for (const key in stateClone) {
+          delete stateClone[key];
+        }
+        break;
+
+      default:
+        break;
+    }
+
+    newState.push({ ...stateClone });
+  }
+
+  return newState;
 }
 
 module.exports = transformStateWithClones;
