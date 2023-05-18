9ee9149b5c12b3edbe8d0e164b551cb6dcebc9ed
Aleksandr Svyrydov <s.sviridov89@gmail.com>
2023-05-18 23:45:34 +0200

    new Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -13,7 +13,7 @@ function transformStateWithClones(state, actions) {
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties':
-        newState = Object.assign({}, newState, action.extraData);
+        Object.assign(newState, action.extraData);
         break;
 
       case 'removeProperties':
