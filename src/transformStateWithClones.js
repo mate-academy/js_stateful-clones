5ff3d5fcee99c35b2b0b52e5c49e76b98c693dc3
Nazar Hotko <nazar.hotko@outlook.com>
2023-05-21 18:57:11 +0200

    task was sloved

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,28 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const obj = { ...state };
+  const resultArr = [];
+
+  for (const num in actions) {
+    const action = actions[num];
+
+    if (action.type === 'addProperties') {
+      Object.assign(obj, action.extraData);
+    } else if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete obj[key];
+      }
+    } else if (action.type === 'clear') {
+      for (const key in obj) {
+        delete obj[key];
+      };
+    }
+
+    resultArr.push({ ...obj });
+  }
+
+  return resultArr;
 }
 
 module.exports = transformStateWithClones;
