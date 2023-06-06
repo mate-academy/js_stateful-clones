2af5c71633140f08fe58a11dac366c9157189a35
Dmytro Lazarenko <lazarenkodmytro@gmail.com>
2023-06-06 11:17:24 +0200

    add task js stateful clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,44 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let clonedState = Object.assign({}, state);
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties': {
+        const nextState = Object.assign({}, clonedState, action.extraData);
+
+        result.push(nextState);
+        break;
+      }
+
+      case 'removeProperties': {
+        const keysToRemove = action.keysToRemove;
+        const nextState = Object.assign({}, clonedState);
+
+        for (const key of keysToRemove) {
+          if (nextState.hasOwnProperty(key)) {
+            delete nextState[key];
+          }
+        }
+        result.push(nextState);
+        break;
+      }
+
+      case 'clear': {
+        const nextState = {};
+
+        result.push(nextState);
+        break;
+      }
+      default:
+        break;
+    }
+    clonedState = Object.assign({}, result[result.length - 1]);
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
