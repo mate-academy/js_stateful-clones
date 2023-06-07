95651695c63629874eb63fd9745231ede25535e7
Dmytro Lazarenko <lazarenkodmytro@gmail.com>
2023-06-07 22:23:34 +0200

    add nextState before for

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,40 +8,38 @@
  */
 function transformStateWithClones(state, actions) {
   const result = [];
-  const clonedState = Object.assign({}, state);
+  let clonedState = Object.assign({}, state);
+  let nextState;
 
   for (const action of actions) {
     switch (action.type) {
-      case 'addProperties':
-        {
-          const nextState = Object.assign({}, clonedState, action.extraData);
-
-          result.push(nextState);
-        }
+      case 'addProperties': {
+        nextState = Object.assign({}, clonedState, action.extraData);
+        result.push(nextState);
         break;
+      }
 
-      case 'removeProperties':
-        {
-          const keysToRemove = action.keysToRemove;
-          const nextState = Object.assign({}, clonedState);
+      case 'removeProperties': {
+        const keysToRemove = action.keysToRemove;
 
-          for (const key of keysToRemove) {
-            if (nextState.hasOwnProperty(key)) {
-              delete nextState[key];
-            }
+        nextState = Object.assign({}, clonedState);
+
+        for (const key of keysToRemove) {
+          if (nextState.hasOwnProperty(key)) {
+            delete nextState[key];
           }
-          result.push(nextState);
         }
+        result.push(nextState);
         break;
+      }
 
-      case 'clear':
-      {
-        const nextState = {};
-
+      case 'clear': {
+        nextState = {};
         result.push(nextState);
         break;
       }
     }
+    clonedState = nextState;
   }
 
   return result;
