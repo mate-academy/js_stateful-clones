8d84d8cdd2265d5e6168b12bc18a722cd411a5a8
Dmytro Lazarenko <lazarenkodmytro@gmail.com>
2023-06-07 21:17:38 +0200

    move place breack after curly brace

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,37 +8,37 @@
  */
 function transformStateWithClones(state, actions) {
   const result = [];
-  let clonedState = Object.assign({}, state);
+  const clonedState = Object.assign({}, state);
 
   for (const action of actions) {
     switch (action.type) {
-      case 'addProperties': {
-        const nextState = Object.assign({}, clonedState, action.extraData);
+      case 'addProperties':
+        {
+          const nextState = Object.assign({}, clonedState, action.extraData);
 
-        result.push(nextState);
-        clonedState = nextState;
-        break;
-      }
-
-      case 'removeProperties': {
-        const keysToRemove = action.keysToRemove;
-        const nextState = Object.assign({}, clonedState);
-
-        for (const key of keysToRemove) {
-          if (nextState.hasOwnProperty(key)) {
-            delete nextState[key];
-          }
+          result.push(nextState);
         }
-        result.push(nextState);
-        clonedState = nextState;
         break;
-      }
 
-      case 'clear': {
+      case 'removeProperties':
+        {
+          const keysToRemove = action.keysToRemove;
+          const nextState = Object.assign({}, clonedState);
+
+          for (const key of keysToRemove) {
+            if (nextState.hasOwnProperty(key)) {
+              delete nextState[key];
+            }
+          }
+          result.push(nextState);
+        }
+        break;
+
+      case 'clear':
+      {
         const nextState = {};
 
         result.push(nextState);
-        clonedState = nextState;
         break;
       }
     }
