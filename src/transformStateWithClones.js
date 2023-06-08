718e41d8672aaec727300b80eaa5e9eca6e9a65d
MaxSchmide <kuznetsov.max.v@gmail.com>
2023-06-08 16:32:09 +0300

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,26 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  const result = [];
+
+  actions.map(action => {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, { ...action.extraData });
+        break;
+      case 'removeProperties':
+        action.keysToRemove.map(key => delete stateCopy[key]);
+        break;
+      case 'clear':
+        Object.keys(stateCopy).forEach(key => delete stateCopy[key]);
+        break;
+    }
+
+    result.push({ ...stateCopy });
+  });
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
