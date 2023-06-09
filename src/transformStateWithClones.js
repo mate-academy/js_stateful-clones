44ab3a9c6595c05a0bc2e89d3ea5353761f0a349
IlonaBorkovska <borkovska.io@gmail.com>
2023-06-09 11:06:57 +0300

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,53 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const STATE_COPY = Object.assign({}, state);
+  const STATE_CHANGES_LIST = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(STATE_COPY, action.extraData);
+
+        STATE_CHANGES_LIST.push({
+          ...{},
+          ...STATE_COPY,
+        });
+
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          if (STATE_COPY.hasOwnProperty(key)) {
+            delete STATE_COPY[key];
+          }
+        }
+
+        STATE_CHANGES_LIST.push({
+          ...{},
+          ...STATE_COPY,
+        });
+
+        break;
+
+      case 'clear':
+        for (const key in STATE_COPY) {
+          delete STATE_COPY[key];
+        }
+
+        STATE_CHANGES_LIST.push({
+          ...{},
+          ...STATE_COPY,
+        });
+
+        break;
+
+      default:
+        throw new Error(`Action type "${action.type}" does not exist`);
+    }
+  }
+
+  return STATE_CHANGES_LIST;
 }
 
 module.exports = transformStateWithClones;
