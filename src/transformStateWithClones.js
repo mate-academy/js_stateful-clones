5475ee283bf8cbd2ce69f3b6bec55f1908e13571
Leo <you@example.com>
2023-06-10 19:32:39 +0300

    create clone

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,35 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
-}
+  const resArr = [];
+  let copyState = { ...state };
+
+  for (const instr of actions) {
+    const { type, extraData, keysToRemove } = instr;
+
+    switch (type) {
+      case 'addProperties':
+        for (const [key, value] of Object.entries(extraData)) {
+          copyState[key] = value;
+        }
+        resArr.push({ ...copyState });
+        break;
+      case 'removeProperties':
+        for (const key of Object.values(keysToRemove)) {
+          if (key in copyState) {
+            delete copyState[key];
+          }
+        }
+        resArr.push({ ...copyState });
+        break;
+      case 'clear':
+        copyState = {};
+        resArr.push({ ...copyState });
+        break;
+    }
+  }
+
+  return resArr;
+};
 
 module.exports = transformStateWithClones;
