b9a56055f4644e04291ffe2c09a1b62524938ad7
alex <cebanoleksandr@gmail.com>
2023-06-11 13:06:41 +0300

    solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,46 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let initialState = { ...state };
+  const statesArray = [];
+
+  for (const action of actions) {
+    reducer(action);
+  }
+
+  function reducer(action) {
+    switch (action.type) {
+      case 'addProperties':
+        for (const key in action.extraData) {
+          initialState[key] = action.extraData[key];
+        }
+
+        statesArray.push({ ...initialState });
+
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete initialState[key];
+        }
+
+        statesArray.push({ ...initialState });
+
+        break;
+
+      case 'clear':
+        initialState = {};
+
+        statesArray.push({ ...initialState });
+
+        break;
+
+      default:
+        throw new Error('Unexpected type');
+    }
+  }
+
+  return statesArray;
 }
 
 module.exports = transformStateWithClones;
