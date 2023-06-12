cfba25cbc3c2ed19f211b709cc7628412f97e89c
Ihor Oksentiuk <winbka.lem@gmail.com>
2023-06-12 18:08:16 +0300

    Finished task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,38 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const resultArray = [];
+  let stateCopy = { ...state };
+
+  for (const action of actions) {
+    let newState = { ...stateCopy };
+
+    switch (action.type) {
+      case 'addProperties':
+        newState = {
+          ...newState, ...action.extraData,
+        };
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete newState[key];
+        }
+        break;
+
+      case 'clear':
+        newState = {};
+        break;
+
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
+    }
+
+    resultArray.push(newState);
+    stateCopy = newState;
+  }
+
+  return resultArray;
 }
 
 module.exports = transformStateWithClones;
