1731c47dedac3910643fd555e1fdff131a9c0f1b
Oleh Khalanskyi <okhalanskyi@gmail.com>
2023-06-12 22:58:18 +0300

    feat: transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,30 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const stateVersions = [{ ...state }];
+
+  for (const action of actions) {
+    let currentVersion = {
+      ...stateVersions[stateVersions.length - 1],
+    };
+
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(currentVersion, action.extraData);
+        break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete currentVersion[key];
+        }
+        break;
+      case 'clear':
+        currentVersion = {};
+        break;
+    }
+    stateVersions.push(currentVersion);
+  }
+
+  return stateVersions.slice(1);
 }
 
 module.exports = transformStateWithClones;
