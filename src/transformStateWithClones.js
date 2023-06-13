4d9221116d1778e65abd8f2aa0f86087c234b9ab
Diana Pinchuk <diana9360@gmail.com>
2023-06-13 14:22:29 +0300

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let stateCopy = { ...state };
+  const previuosVersions = [];
+
+  for (let i = 0; i < actions.length; i++) {
+    switch (actions[i].type) {
+      case 'addProperties':
+        Object.assign(stateCopy, actions[i].extraData);
+        previuosVersions.push({ ...stateCopy });
+        break;
+
+      case 'removeProperties':
+        for (const key in actions[i].keysToRemove) {
+          delete stateCopy[actions[i].keysToRemove[key]];
+        }
+        previuosVersions.push({ ...stateCopy });
+        break;
+
+      case 'clear':
+        stateCopy = {};
+        previuosVersions.push({});
+        break;
+
+      default:
+        throw new Error('Wrong type');
+    }
+  }
+
+  return previuosVersions;
 }
 
 module.exports = transformStateWithClones;
