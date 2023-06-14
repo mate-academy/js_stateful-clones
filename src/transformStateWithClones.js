2274079f482d30651641c5ddbf6d3908d245014b
Taras Bilas <tarasbilas01@gmail.com>
2023-06-14 21:38:43 +0300

    simplify function

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,36 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const stateCopy = [];
-  let allProperties = {};
+  const stateCopy = [{ ...state }];
 
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties':
-        if (stateCopy.length === 0) {
-          stateCopy.push(Object.assign({}, state, action['extraData']));
-        } else {
-          stateCopy.push(
-            Object.assign({},
-              stateCopy[stateCopy.length - 1],
-              action['extraData'])
-          );
-        }
+        stateCopy.push(
+          Object.assign(
+            {},
+            stateCopy[stateCopy.length - 1],
+            action['extraData']
+          )
+        );
         break;
 
       case 'removeProperties':
-        if (stateCopy.length === 0) {
-          Object.assign(allProperties, state);
-        } else {
-          Object.assign(allProperties, stateCopy[stateCopy.length - 1]);
-        }
+        const allProperties = {};
+
+        Object.assign(allProperties, stateCopy[stateCopy.length - 1]);
 
         for (const property of action['keysToRemove']) {
           delete allProperties[property];
         }
 
         stateCopy.push(allProperties);
-        allProperties = {};
         break;
 
       case 'clear':
@@ -47,6 +41,8 @@ function transformStateWithClones(state, actions) {
         throw new Error('Invalid Action Name');
     }
   }
+
+  stateCopy.shift();
 
   return stateCopy;
 }
