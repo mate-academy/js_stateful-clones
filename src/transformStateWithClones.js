32da2aa16046369de2d15ce157492386f8b2561b
Roman Tarasov <rtarasov1k90@gmail.com>
2023-06-16 17:23:43 +0300

    state with clines

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,41 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const copyState = { ...state };
+  const result = [];
+
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
+      case 'addProperties':
+        for (const data in extraData) {
+          copyState[data] = extraData[data];
+        }
+
+        break;
+
+      case 'removeProperties':
+        for (const keyToRemove of keysToRemove) {
+          delete copyState[keyToRemove];
+        }
+
+        break;
+
+      case 'clear':
+        for (const key in copyState) {
+          delete copyState[key];
+        }
+
+        break;
+    }
+
+    const copyTwoState = { ...copyState };
+
+    result.push(copyTwoState);
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
