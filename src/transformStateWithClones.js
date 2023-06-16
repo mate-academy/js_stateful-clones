a36fbd454c620f3e1f887b076afa2dc94151c319
zipic <zpc23041988@gmail.com>
2023-06-16 10:14:24 +0300

    transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const clearObject = [];
+  const modifiedObject = { ...state };
+
+  actions.forEach(action => {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
+      case 'addProperties':
+        Object.assign(modifiedObject, extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of keysToRemove) {
+          delete modifiedObject[key];
+        }
+        break;
+      case 'clear':
+        for (const key in modifiedObject) {
+          delete modifiedObject[key];
+        }
+        break;
+      default:
+    }
+    clearObject.push({ ...modifiedObject });
+  });
+
+  return clearObject;
 }
 
 module.exports = transformStateWithClones;
