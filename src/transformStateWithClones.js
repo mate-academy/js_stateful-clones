3cc13b7989f6b6d217e833ce0959550e83533fda
Lina Do <dykaya0916@gmail.com>
2023-06-20 14:59:37 +0300

    improving code

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /**
  * @param {Object} state
@@ -7,31 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  let result = [];
+  const result = [];
   const copyState = Object.assign({}, state);
+
   for (const obj of actions) {
     switch (obj.type) {
-      case "addProperties":
-        for (const key in obj.extraData) {
-          copyState[key] = obj.extraData[key];
-        }
-        result.push(Object.assign({}, copyState));
+      case 'addProperties':
+        Object.assign(copyState, obj.extraData);
+
         break;
 
-      case "removeProperties":
+      case 'removeProperties':
         for (const remove of obj.keysToRemove) {
           delete copyState[remove];
         }
-        result.push(Object.assign({}, copyState));
+
         break;
 
-      default:
+      case 'clear':
         for (const del in copyState) {
           delete copyState[del];
         }
-        result.push(Object.assign({}, copyState));
+
+        break;
     }
+    result.push(Object.assign({}, copyState));
   }
+
   return result;
 }
 
