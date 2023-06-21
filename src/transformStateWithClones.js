c6bfdedee763bc998f513a527d04575a6c49e606
Andrii Solovei <solovey98.info@gmail.com>
2023-06-21 23:46:15 +0300

    create a tranformState...) function

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const cloneState = { ...state };
+  const arrState = [];
+
+  for (const el of actions) {
+    switch (el.type) {
+      case 'addProperties':
+        const newState = Object.assign(cloneState, el.extraData);
+
+        arrState.push(Object.assign({}, newState));
+        continue;
+      case 'removeProperties':
+        for (const key of el.keysToRemove) {
+          delete cloneState[key];
+        }
+        arrState.push(Object.assign({}, cloneState));
+        continue;
+      case 'clear':
+        for (const key in cloneState) {
+          delete cloneState[key];
+        }
+        arrState.push({});
+        continue;
+    }
+  }
+
+  return arrState;
 }
 
 module.exports = transformStateWithClones;
