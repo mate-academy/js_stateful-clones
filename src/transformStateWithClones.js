5e87531890fd9d7919c2a0d0da9de6a301aec5db
Faridun Mainaev <farid.mainaiev@gmail.com>
2023-06-25 00:01:29 +0300

    first commit

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [state];
+  let currentState = { ...state };
+
+  for (let i = 0; i < actions.length; i++) {
+    const { type, extraData, keysToRemove } = actions[i];
+
+    switch (type) {
+      case 'addProperties':
+        currentState = {
+          ...currentState, ...extraData,
+        };
+        break;
+      case 'removeProperties':
+        keysToRemove.forEach((key) => delete currentState[key]);
+        break;
+      case 'clear':
+        currentState = {};
+        break;
+      default:
+        break;
+    }
+    result.push({ ...currentState });
+  }
+
+  return result.slice(1);
 }
 
 module.exports = transformStateWithClones;
