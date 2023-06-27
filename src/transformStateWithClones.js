c2271ae5b4cde9ba6983f07dc973dec59cdd6b41
Nazar Kyzenko <kyzenko1997@gmail.com>
2023-06-27 21:08:15 +0300

    transforStateWithClonesCommitOne

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,37 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const cloneTransformsState = [];
+  let newStateObject = { ...state };
+
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
+      case 'addProperties':
+        newStateObject = {
+          ...newStateObject,
+          ...extraData,
+        };
+        break;
+
+      case 'removeProperties':
+        for (const key of keysToRemove) {
+          delete newStateObject[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in newStateObject) {
+          delete newStateObject[key];
+        }
+        break;
+    }
+
+    cloneTransformsState.push({ ...newStateObject });
+  }
+
+  return cloneTransformsState;
 }
 
 module.exports = transformStateWithClones;
