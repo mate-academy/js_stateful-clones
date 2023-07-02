d59fdc99e4a82b920602aafd0e06c85e47e2cf41
Viktoria-Moroz <viktoriaandriivna25@gmail.com>
2023-07-02 18:50:12 +0300

    the transformStateClones was coplete

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  const versionsOfState = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const KeyToRemove of action.keysToRemove) {
+          delete stateCopy[KeyToRemove];
+        }
+        break;
+
+      case 'clear':
+        for (const key in stateCopy) {
+          if (key) {
+            delete stateCopy[key];
+          }
+        }
+        break;
+
+      default:
+        throw new Error('Unexpected action type');
+    }
+    versionsOfState.push({ ...stateCopy });
+  }
+
+  return versionsOfState;
 }
 
 module.exports = transformStateWithClones;
