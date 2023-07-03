cf5943da720fc5569c7dee60ec70154212478dc9
Orchibald <orestkozak17@gmail.com>
2023-07-03 17:18:52 +0200

    transform state with clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,39 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    const { type } = action;
+    let nextState;
+
+    switch (type) {
+      case 'addProperties':
+        const { extraData } = action;
+
+        nextState = {
+          ...currentState, ...extraData,
+        };
+        break;
+
+      case 'removeProperties':
+        const { keysToRemove } = action;
+
+        nextState = { ...currentState };
+        keysToRemove.forEach((key) => delete nextState[key]);
+        break;
+
+      case 'clear':
+        nextState = {};
+        break;
+    }
+
+    result.push(nextState);
+    currentState = nextState;
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
