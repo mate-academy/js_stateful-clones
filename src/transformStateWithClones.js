63ebfe9b25cab7b84772c416344c58ccd6e14c4c
Maryna Kyrychynska <m_kirichok@ukr.net>
2023-07-04 17:04:02 +0300

    Transform_State_With_Clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const COPY_STATE = { ...state };
+  const NEW_STATE = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(COPY_STATE, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete COPY_STATE[key];
+        }
+        break;
+
+      case 'clear':
+        for (const value in COPY_STATE) {
+          delete COPY_STATE[value];
+        }
+        break;
+    }
+
+    NEW_STATE.push({ ...COPY_STATE });
+  }
+
+  return NEW_STATE;
 }
 
 module.exports = transformStateWithClones;
