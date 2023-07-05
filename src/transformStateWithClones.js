a5d1b8444ed4b52e3ac07367eb7146d904320c80
Serhii Dranuta <sirojka8@gmail.com>
2023-07-05 21:27:44 +0300

    Add stateWithClones task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateClonesArray = [];
+  const stateCopyObject = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopyObject, action.extraData);
+        stateClonesArray.push({ ...stateCopyObject });
+        break;
+      case 'removeProperties':
+        action.keysToRemove.forEach((key) => delete stateCopyObject[key]);
+        stateClonesArray.push({ ...stateCopyObject });
+        break;
+      case 'clear':
+        Object.keys(stateCopyObject).forEach(
+          (key) => delete stateCopyObject[key]
+        );
+        stateClonesArray.push({ ...stateCopyObject });
+        break;
+      default:
+        return 'Error: invalid value received';
+    }
+  }
+
+  return stateClonesArray;
 }
 
 module.exports = transformStateWithClones;
