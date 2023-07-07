772039021ed5ab5f090d9516c131e2eabb606b4c
Viktoriia Melnyk <melnychka1993@gmail.com>
2023-07-07 12:28:17 +0100

    work is done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateClone = { ...state };
+  const actionsArray = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        for (const key in action.extraData) {
+          stateClone[key] = action.extraData[key];
+        }
+        break;
+
+      case 'removeProperties':
+        for (const word of action.keysToRemove) {
+          delete stateClone[word];
+        }
+        break;
+
+      case 'clear':
+        for (const key in stateClone) {
+          delete stateClone[key];
+        };
+        break;
+    }
+    actionsArray.push({ ...stateClone });
+  }
+
+  return actionsArray;
 }
 
 module.exports = transformStateWithClones;
