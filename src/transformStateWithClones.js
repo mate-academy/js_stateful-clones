e0a559a564fb3afc4b1205b67d09b16159f2488c
4xmplme <andreykav2002@gmail.com>
2023-07-11 22:38:16 +0300

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,35 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let nextState = { ...state };
+  const stateVersions = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        nextState = {
+          ...nextState,
+          ...action.extraData,
+        };
+        break;
+
+      case 'removeProperties':
+        action.keysToRemove.forEach(key => delete nextState[key]);
+        break;
+
+      case 'clear':
+        nextState = {};
+        break;
+
+      default:
+        // console.error('Unexpected action type!');
+        break;
+    }
+
+    stateVersions.push({ ...nextState });
+  }
+
+  return stateVersions;
 }
 
 module.exports = transformStateWithClones;
