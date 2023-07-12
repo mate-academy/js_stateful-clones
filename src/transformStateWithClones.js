8b89c252143ccf05c154bc9090d8e003fe1426d6
Horizon-git <apbreloaded947@gmail.com>
2023-07-12 03:26:06 +0300

    complete task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,33 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const stateAction = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        for (const key in action.extraData) {
+          stateAction[key] = action.extraData[key];
+        }
+        break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete stateAction[key];
+        }
+        break;
+      case 'clear':
+        for (const key in stateAction) {
+          delete stateAction[key];
+        }
+        break;
+    }
+    result.push({ ...stateAction });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
