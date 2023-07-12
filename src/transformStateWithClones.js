1f8afbe48aa96707b46d19f08934b1a5413a0150
Karolina <karolinabednarek888@gmail.com>
2023-07-12 20:37:17 +0200

    Fix

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,36 +7,26 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const stateCopy = { ...state };
-  const emptyArray = [];
+  let stateCopy = { ...state };
 
-  for (const action of actions) {
+  return actions.map((action) => {
     switch (action.type) {
       case 'addProperties':
-        Object.assign(stateCopy, action.extraData);
-        emptyArray.push(Object.assign({}, stateCopy));
-        break;
-
+        return {
+          ...Object.assign(stateCopy, action.extraData),
+        };
       case 'removeProperties':
-        for (const key of action.keysToRemove) {
-          delete stateCopy[key];
-        }
-        emptyArray.push(Object.assign({}, stateCopy));
-        break;
+        action.keysToRemove.forEach((key) => delete stateCopy[key]);
 
+        return { ...stateCopy };
       case 'clear':
-        for (const stateKey in stateCopy) {
-          delete stateCopy[stateKey];
-        }
-        emptyArray.push(Object.assign({}, stateCopy));
-        break;
+        stateCopy = {};
 
+        return {};
       default:
         throw new Error('Whoops, something bad happened');
     }
-  }
-
-  return emptyArray;
+  });
 }
 
 module.exports = transformStateWithClones;
