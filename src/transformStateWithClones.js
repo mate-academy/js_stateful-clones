b1ed05556f4908d20f6a13e9e94a566e0b34c1c9
Kacper <leman_kacper@o2.pl>
2023-07-13 14:51:34 +0200

    Soultion

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,27 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let newState = { ...state };
+
+  actions.forEach((action) => {
+    let clonedState = { ...newState };
+
+    if (action.type === 'addProperties') {
+      Object.assign(clonedState, action.extraData);
+    } else if (action.type === 'removeProperties') {
+      action.keysToRemove.forEach((key) => {
+        delete clonedState[key];
+      });
+    } else if (action.type === 'clear') {
+      clonedState = {};
+    }
+
+    result.push(clonedState);
+    newState = clonedState;
+  });
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
