fe3d7b95a5199ecf32fc7554c6cf1a18e15fb565
Olha Rypich <olha.rypich@gmail.com>
2023-07-15 16:08:09 +0300

    stateful-clones_solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateClone = { ...state };
+  const resultArray = [];
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(stateClone, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete stateClone[key];
+      }
+    }
+
+    if (action.type === 'clear') {
+      for (const key in stateClone) {
+        delete stateClone[key];
+      }
+    }
+
+    resultArray.push({ ...stateClone });
+  }
+
+  return resultArray;
 }
 
 module.exports = transformStateWithClones;
