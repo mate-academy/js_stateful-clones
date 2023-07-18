ce55172f70223bc4e14bbd35655eebfb1c375035
Anna Shevtsova <aniashevtsova@gmail.com>
2023-07-18 21:12:44 +0200

    completed

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,40 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newState = { ...state };
+  const stateCopies = [];
+
+  for (const action of actions) {
+    const actionType = action.type;
+
+    switch (actionType) {
+      case 'addProperties':
+        Object.assign(newState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        const keysToRemove = action.keysToRemove;
+
+        for (const key of keysToRemove) {
+          if (newState.hasOwnProperty(key)) {
+            delete newState[key];
+          }
+        }
+        break;
+
+      case 'clear':
+        for (const key in newState) {
+          delete newState[key];
+        }
+        break;
+
+      default:
+        continue;
+    }
+    stateCopies.push({ ...newState });
+  }
+
+  return stateCopies;
 }
 
 module.exports = transformStateWithClones;
