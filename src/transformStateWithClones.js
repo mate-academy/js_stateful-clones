052304e605b28840a90c882dc5fbaac7e5ead8bc
FurArt <iiionerart@gmail.com>
2023-07-20 08:26:48 +0300

    added solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,63 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCloneArray = [];
+  let indexLastActions = 0;
+
+  actions.forEach((action) => {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
+      case 'clear':
+        stateCloneArray.push(Object.assign({}));
+        break;
+
+      case 'addProperties':
+
+        if (!stateCloneArray.length) {
+          stateCloneArray.push(Object.assign({}, state, extraData));
+          break;
+        }
+
+        indexLastActions = stateCloneArray.length - 1;
+
+        stateCloneArray.push(
+          Object.assign({}, stateCloneArray[indexLastActions], extraData)
+        );
+
+        break;
+
+      default:
+        if (!stateCloneArray.length) {
+          stateCloneArray.push(Object.assign({}, state));
+
+          Object.values(keysToRemove).forEach((removeProperties) => {
+            if (removeProperties in stateCloneArray[0]) {
+              delete stateCloneArray[0][removeProperties];
+            }
+          });
+          break;
+        }
+
+        indexLastActions = stateCloneArray.length - 1;
+
+        stateCloneArray.push(
+          Object.assign({}, stateCloneArray[indexLastActions])
+        );
+
+        indexLastActions += 1;
+
+        Object.values(keysToRemove).forEach((removeProperties) => {
+          if (removeProperties in stateCloneArray[indexLastActions]) {
+            delete stateCloneArray[indexLastActions][removeProperties];
+          }
+        });
+
+        break;
+    }
+  });
+
+  return stateCloneArray;
 }
 
 module.exports = transformStateWithClones;
