d6418c553a42d78c7b58bded7b7238b22095f2e3
Olena Matsyshyna <helenmarth@Olenas-MacBook-Air.local>
2023-07-20 14:52:23 +0300

    finished task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,37 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateClone = { ...state };
+  const result = [];
+
+  function pushToResult() {
+    result.push({ ...stateClone });
+  }
+
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
+      case 'addProperties':
+        (Object.assign(stateClone, extraData));
+        pushToResult();
+        break;
+      case 'removeProperties':
+        for (const key of keysToRemove) {
+          delete stateClone[key];
+        }
+        pushToResult();
+        break;
+      case 'clear':
+        for (const key in stateClone) {
+          delete stateClone[key];
+        }
+        pushToResult();
+        break;
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
