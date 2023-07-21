9439810fb04877bcc58ef02977bf742b971d44ba
Olena Matsyshyna <helenmarth@Olenas-MacBook-Air.local>
2023-07-21 17:32:41 +0300

    fixed after review

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -10,31 +10,25 @@ function transformStateWithClones(state, actions) {
   const stateClone = { ...state };
   const result = [];
 
-  function pushToResult() {
-    result.push({ ...stateClone });
-  }
-
   for (const action of actions) {
     const { type, extraData, keysToRemove } = action;
 
     switch (type) {
       case 'addProperties':
         (Object.assign(stateClone, extraData));
-        pushToResult();
         break;
       case 'removeProperties':
         for (const key of keysToRemove) {
           delete stateClone[key];
         }
-        pushToResult();
         break;
       case 'clear':
         for (const key in stateClone) {
           delete stateClone[key];
         }
-        pushToResult();
         break;
     }
+    result.push({ ...stateClone });
   }
 
   return result;
