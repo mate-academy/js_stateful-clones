b1fc1ba2f78934b71df1af8e2a6de3e8e1294ae0
vadymboichenkoAcupower <vadym.boichenko@acupower.co.uk>
2023-07-23 00:38:36 +0300

    made solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,44 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const ResultObject = [];
+
+  for (const operation of actions) {
+    const CurrentObject = {};
+
+    if (ResultObject.length !== 0) {
+      Object.assign(CurrentObject, ResultObject[ResultObject.length - 1]);
+    }
+
+    if (ResultObject.length === 0) {
+      Object.assign(CurrentObject, state, operation.extraData);
+    }
+
+    if (operation.type === 'addProperties') {
+      if (ResultObject.length !== 0) {
+        Object.assign(CurrentObject, operation.extraData);
+      }
+      ResultObject.push(CurrentObject);
+    }
+
+    if (operation.type === 'clear') {
+      for (const key in CurrentObject) {
+        delete CurrentObject[key];
+      }
+      ResultObject.push(CurrentObject);
+    }
+
+    if (operation.type === 'removeProperties') {
+      const keysToRemove = operation.keysToRemove;
+
+      for (const key of keysToRemove) {
+        delete CurrentObject[key];
+      }
+      ResultObject.push(CurrentObject);
+    }
+  }
+
+  return ResultObject;
 }
 
 module.exports = transformStateWithClones;
