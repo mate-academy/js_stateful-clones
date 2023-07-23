fa901742242ca50d246b3785e33ed17b9f592512
Vladyslav Kolisnyk <vadyslavkolisnyk20@gmail.com>
2023-07-23 18:42:23 +0300

    add task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,33 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  const stateArr = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const prop of action.keysToRemove) {
+          delete stateCopy[prop];
+        }
+        break;
+
+      case 'clear':
+        for (const prop in stateCopy) {
+          delete stateCopy[prop];
+        }
+        break;
+    }
+    stateArr.push({ ...stateCopy });
+  }
+
+  return stateArr;
 }
 
 module.exports = transformStateWithClones;
