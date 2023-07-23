00d7e29bf374a240cdc808fec1b6a1b1249b8c2b
Anton Kuts <anton.a.kuts@gmail.com>
2023-07-23 15:07:10 +0300

    the task is completed, the tests are completed successfully

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  const newStatesArr = [];
+
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
+      case 'addProperties':
+        Object.assign(stateCopy, extraData);
+        break;
+      case 'clear':
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
+        break;
+      case 'removeProperties':
+        keysToRemove.forEach(propKey => {
+          if (stateCopy.hasOwnProperty(propKey)) {
+            delete stateCopy[propKey];
+          }
+        });
+        break;
+      default:
+        throw new Error('Sorry, none of the cases worked out');
+    }
+
+    newStatesArr.push({ ...stateCopy });
+  }
+
+  return newStatesArr;
 }
 
 module.exports = transformStateWithClones;
