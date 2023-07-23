f402f5b3016ef42f58c64a0eeac815f8016b87a2
Andrzej Pec <pimatstudio@gmail.com>
2023-07-23 16:57:10 +0200

    soluzione al problema della copia

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const states = [];
+
+  actions.forEach((action) => {
+    const stateCopy = Object.assign({}, state);
+
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        break;
+
+      case 'removeProperties':
+        action.keysToRemove.forEach((key) => delete stateCopy[key]);
+        break;
+
+      case 'clear':
+        Object.keys(stateCopy).forEach((key) => delete stateCopy[key]);
+        break;
+
+      default:
+        window.alert('Unknown action type: ' + action.type);
+        break;
+    }
+
+    states.push(stateCopy);
+  });
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
