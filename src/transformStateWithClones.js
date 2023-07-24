45bb80480f4c1ca1fe413380303fea6778b3cc8d
Vladyslav Korobka <vladseipa@gmail.com>
2023-07-24 19:09:17 +0300

    task has been done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,52 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const ActionTypes = {};
+  const objectCollect = [];
+
+  actions.map(action => {
+    ActionTypes[action.type] = action.type;
+  });
+
+  for (const action of actions) {
+    const stateCopy = {};
+
+    if (!objectCollect.length) {
+      Object.assign(stateCopy, state);
+    }
+
+    if (objectCollect.length !== 0) {
+      Object.assign(stateCopy, objectCollect[objectCollect.length - 1]);
+    }
+
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
+      case ActionTypes.addProperties:
+        Object.assign(stateCopy, extraData);
+
+        break;
+
+      case ActionTypes.removeProperties:
+        for (const key of keysToRemove) {
+          delete stateCopy[key];
+        };
+
+        break;
+
+      case ActionTypes.clear:
+        Object.keys(stateCopy).forEach(key => delete stateCopy[key]);
+
+        break;
+
+      default:
+        stateCopy.error = 'Action didn\'t find';
+    }
+
+    objectCollect.push(stateCopy);
+  }
+
+  return objectCollect;
 }
 
 module.exports = transformStateWithClones;
