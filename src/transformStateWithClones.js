9e36f149b4c4252bfe1e10380929bc5e68f7d4ab
piotr <piotr_as@yahoo.com>
2023-07-24 15:47:48 +0200

    solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,76 @@
  *
  * @return {Object[]}
  */
+// function transformStateWithClones(state, actions) {
+//   const result = [];
+//   let currentState = Object.assign({}, state);
+
+//   actions.forEach((action) => {
+//     const newState = Object.assign({}, currentState);
+
+//     switch (action.type) {
+//       case 'addProperties':
+//         if (action.extraData && typeof action.extraData === 'object') {
+//           Object.assign(newState, action.extraData);
+//         }
+//         break;
+//       case 'removeProperties':
+//         if (action.keysToRemove && Array.isArray(action.keysToRemove)) {
+//           action.keysToRemove.forEach((key) => {
+//             if (newState.hasOwnProperty(key)) {
+//               delete newState[key];
+//             }
+//           });
+//         }
+//         break;
+//       case 'clear':
+//         newState = {};
+//         break;
+//       default:
+//         break;
+//     }
+
+//     result.push(newState);
+//     currentState = newState;
+//   });
+
+//   return result;
+// }
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let currentState = Object.assign({}, state);
+
+  actions.forEach((action) => {
+    let newState = Object.assign({}, currentState);
+
+    switch (action.type) {
+      case 'addProperties':
+        if (action.extraData && typeof action.extraData === 'object') {
+          Object.assign(newState, action.extraData);
+        }
+        break;
+      case 'removeProperties':
+        if (action.keysToRemove && Array.isArray(action.keysToRemove)) {
+          action.keysToRemove.forEach((key) => {
+            if (newState.hasOwnProperty(key)) {
+              delete newState[key];
+            }
+          });
+        }
+        break;
+      case 'clear':
+        newState = {};
+        break;
+      default:
+        break;
+    }
+
+    result.push(Object.assign({}, newState));
+    currentState = newState;
+  });
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
