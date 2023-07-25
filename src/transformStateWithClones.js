5b337f192d0b76a41c5a0d1e96f14127f6b75051
Hubert Klamann <hubert.klamann1@gmail.com>
2023-07-25 14:31:51 +0200

    task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arr = [];
+  let newState = JSON.parse(JSON.stringify(state));
+
+  for (const obj of actions) {
+    switch (obj.type) {
+      case 'addProperties':
+        Object.assign(newState, obj.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of obj.keysToRemove) {
+          delete newState[key];
+        }
+        break;
+
+      case 'clear':
+        newState = {};
+        break;
+    }
+    arr.push(JSON.parse(JSON.stringify(newState)));
+  }
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
