38a2f4a420eb9f8be330713c4eae9d769670feb0
Matylda Lewandowska <matylda.lewandowska111@gmail.com>
2023-07-25 21:11:51 +0200

    Task Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,50 @@
  *
  * @return {Object[]}
  */
+
+// function deepClone(obj) {
+//   const clone = {};
+
+//   for (const key in obj) {
+//     if (typeof obj[key] === 'object' && obj[key] !== null) {
+//       clone[key] = deepClone(obj[key]);
+//     } else {
+//       clone[key] = obj[key];
+//     }
+//   }
+
+//   return clone;
+// }
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const actionsOfState = [];
+  const stateClone = Object.assign({}, state);
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateClone, action.extraData);
+        break;
+
+      case 'removeProperties':
+        action.keysToRemove.forEach(element => delete stateClone[element]);
+        break;
+
+      case 'clear':
+        for (const key in stateClone) {
+          delete stateClone[key];
+        }
+        break;
+
+      default:
+        Error('sth gone wrong :(');
+        break;
+    }
+
+    actionsOfState.push(Object.assign({}, stateClone));
+  }
+
+  return actionsOfState;
 }
 
 module.exports = transformStateWithClones;
