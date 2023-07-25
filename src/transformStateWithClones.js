789913a65c621d573e5213d3dfc057fa01e9e86b
arturwisnia <wisnia3008@interia.pl>
2023-07-25 22:08:45 +0200

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let clonedState = { ...state };
+
+  for (const action of actions) {
+    const { type } = action;
+
+    if (type === 'addProperties') {
+      const { extraData } = action;
+
+      clonedState = {
+        ...clonedState,
+        ...extraData,
+      };
+    } else if (type === 'removeProperties') {
+      const { keysToRemove } = action;
+
+      for (const key of keysToRemove) {
+        delete clonedState[key];
+      }
+    } else if (type === 'clear') {
+      clonedState = {};
+    }
+
+    result.push({ ...clonedState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
