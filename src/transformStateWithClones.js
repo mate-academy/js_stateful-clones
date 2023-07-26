0fac5d9cf66fd9d1fc2e9a7ddc14fbf25940c2dc
Sviatoslav Vasyletskyi <ditchqiwi@gmail.com>
2023-07-26 12:24:40 +0200

    solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,32 @@
  *
  * @return {Object[]}
  */
+function deepClone(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let currentState = deepClone(state);
+
+  actions.forEach((action) => {
+    if (action.type === 'addProperties') {
+      // eslint-disable-next-line object-curly-newline
+      currentState = { ...currentState, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      action.keysToRemove.forEach((key) => {
+        if (currentState.hasOwnProperty(key)) {
+          delete currentState[key];
+        }
+      });
+    } else if (action.type === 'clear') {
+      currentState = {};
+    }
+
+    result.push(deepClone(currentState));
+  });
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
