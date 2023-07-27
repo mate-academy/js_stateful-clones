eb62ef203cac91ff09f627326809393cc5a052a5
Hubert Wiktor <hubertwiktor6@gmail.com>
2023-07-27 11:24:30 +0200

    fix

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,22 +8,17 @@
  */
 function transformStateWithClones(state, actions) {
   const newState = { ...state };
-  let pushObj;
   const result = [];
 
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties':
         Object.assign(newState, action.extraData);
-        pushObj = { ...newState };
-        result.push(pushObj);
         break;
       case 'removeProperties':
         for (const key of action.keysToRemove) {
           delete newState[key];
         }
-        pushObj = { ...newState };
-        result.push(pushObj);
         break;
       case 'clear':
         for (const key in newState) {
@@ -31,12 +26,11 @@ function transformStateWithClones(state, actions) {
             delete newState[key];
           }
         }
-        pushObj = { ...newState };
-        result.push(pushObj);
         break;
       default:
         throw new Error('Something went wrong !');
     }
+    result.push({ ...newState });
   }
 
   return result;
