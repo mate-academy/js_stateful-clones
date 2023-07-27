ca10fc77f7848ce36c7e5fa9b145d66482d266f8
Patryk Filipiak <patryk.fy@o2.pl>
2023-07-27 09:49:53 +0200

    Updated solution for spread operator

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,28 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const clone = Object.assign({}, state);
+  const stateClone = {
+    ...state,
+  };
   const actionLog = [];
 
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties':
-        Object.assign(clone, action.extraData);
+        Object.assign(stateClone, action.extraData);
         break;
 
       case 'removeProperties':
-        for (const key of action.keysToRemove) {
-          delete clone[key];
-        }
+        action.keysToRemove.forEach(key => delete stateClone[key]);
         break;
 
       case 'clear':
-        for (const key in clone) {
-          delete clone[key];
-        }
+        Object.keys(stateClone).forEach(key => delete stateClone[key]);
         break;
     }
-    actionLog.push(Object.assign({}, clone));
+
+    actionLog.push({
+      ...stateClone,
+    });
   }
 
   return actionLog;
