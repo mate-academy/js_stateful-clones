a05ecbc43194b113e731ca29b2d1d9be0f43adf1
Piotr Dzialo <piotr.sluzbowy@wp.pl>
2023-07-27 04:03:56 +0200

    adding a solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  const stateResults = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete stateCopy[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
+        break;
+
+      default:
+        throw Error('Unknown action');
+    }
+    stateResults.push(Object.assign({}, stateCopy));
+  }
+
+  return stateResults;
 }
 
 module.exports = transformStateWithClones;
