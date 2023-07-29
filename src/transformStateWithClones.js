692f6b998884140a06971e5eee73203bc0330660
stunnerboyone <stunnerboy691@gmail.com>
2023-07-29 12:14:55 +0300

    Solution: Stateful clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,36 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const clonedState = [];
+  const currentStateData = { ...state };
+  const addDataToArray = () => {
+    clonedState.push({ ...currentStateData });
+  };
+
+  for (const action in actions) {
+    switch (actions[action].type) {
+      case 'addProperties':
+        Object.assign(currentStateData, { ...actions[action].extraData });
+        addDataToArray();
+        break;
+      case 'removeProperties':
+        for (const keyToRemove in actions[action].keysToRemove) {
+          delete currentStateData[actions[action].keysToRemove[keyToRemove]];
+        }
+        addDataToArray();
+        break;
+      case 'clear':
+        for (const key in currentStateData) {
+          delete currentStateData[key];
+        }
+        addDataToArray();
+        break;
+      default:
+        throw new Error('Array have not correct action value');
+    }
+  }
+
+  return clonedState;
 }
 
 module.exports = transformStateWithClones;
