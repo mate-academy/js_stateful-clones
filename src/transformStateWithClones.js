d78ebbedd29e6eceb8566f0abbe9176d8a443258
Oleg Tarasov <tarasov.oleg.9779@gmail.com>
2023-07-29 14:36:26 +0300

    stateful clones v1

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let myState = { ...state };
+  const result = [];
+
+  for (const obj of actions) {
+    if (obj.type === 'addProperties') {
+      myState = {
+        ...myState,
+        ...obj.extraData,
+      };
+      result.push({ ...myState });
+    }
+
+    if (obj.type === 'removeProperties') {
+      for (const toDelete of obj.keysToRemove) {
+        delete myState[toDelete];
+      }
+      result.push({ ...myState });
+    }
+
+    if (obj.type === 'clear') {
+      myState = {}; // Очищаємо myState
+      result.push({ ...myState });
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
