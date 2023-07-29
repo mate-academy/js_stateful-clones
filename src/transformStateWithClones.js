17ada0b4f892236a2005cd7f429b4cf12e3bf7b6
arturwisnia <wisnia3008@interia.pl>
2023-07-29 18:33:18 +0200

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,6 +6,7 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
   const result = [];
   let clonedState = { ...state };
@@ -13,21 +14,30 @@ function transformStateWithClones(state, actions) {
   for (const action of actions) {
     const { type } = action;
 
-    if (type === 'addProperties') {
-      const { extraData } = action;
+    switch (type) {
+      case 'addProperties': {
+        const { extraData } = action;
 
-      clonedState = {
-        ...clonedState,
-        ...extraData,
-      };
-    } else if (type === 'removeProperties') {
-      const { keysToRemove } = action;
-
-      for (const key of keysToRemove) {
-        delete clonedState[key];
+        clonedState = {
+          ...clonedState,
+          ...extraData,
+        };
+        break;
       }
-    } else if (type === 'clear') {
-      clonedState = {};
+
+      case 'removeProperties': {
+        const { keysToRemove } = action;
+
+        for (const key of keysToRemove) {
+          delete clonedState[key];
+        }
+        break;
+      }
+      case 'clear':
+        clonedState = {};
+        break;
+      default:
+        throw new Error(`Unknown action type: ${type}`);
     }
 
     result.push({ ...clonedState });
