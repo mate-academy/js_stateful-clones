c582a10889837bcee9422403ce753f4b9d1dd3a8
Max <gcrazy007@gmail.com>
2023-07-30 19:02:54 +0300

    4

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -13,7 +13,7 @@ function transformStateWithClones(state, actions) {
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties':
-        currentState = Object.assign(currentState, action.extraData);
+        Object.assign(currentState, action.extraData);
         break;
 
       case 'clear':
