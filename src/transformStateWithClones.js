793228a974cc0a0d9436e142a4f09517d30f81ce
maksym-mishchanchuk <mishchanchuk.maksym@gmail.com>
2023-07-31 23:40:44 +0300

    initial commit

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,49 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const copyState = { ...state };
+  const allStateData = [];
+
+  for (const action of actions) {
+    const typeAction = action.type;
+
+    switch (typeAction) {
+      case 'addProperties':
+        add(copyState, action.extraData);
+        allStateData.push({ ...copyState });
+        break;
+
+      case 'removeProperties':
+        remove(copyState, action.keysToRemove);
+        allStateData.push({ ...copyState });
+        break;
+
+      case 'clear':
+        clear(copyState);
+        allStateData.push({ ...copyState });
+        break;
+    }
+  }
+
+  return allStateData;
 }
+
+const add = (state, action) => {
+  for (const key in action) {
+    state[key] = action[key];
+  }
+};
+
+const remove = (state, action) => {
+  for (const key of action) {
+    delete state[key];
+  }
+};
+
+const clear = (state) => {
+  for (const key in state) {
+    delete state[key];
+  }
+};
 
 module.exports = transformStateWithClones;
