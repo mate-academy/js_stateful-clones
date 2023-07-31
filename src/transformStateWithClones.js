30bd0ee2a69aa31ea1f3081af8be46fa70c2619a
Denys Poltorak <itwfrontend@gmail.com>
2023-07-31 16:43:52 +0300

    feat: add task solution (Mon, Jul 31, 2023 04:43:51 PM)

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,82 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  // check arguments type
+  if (!isObject(state) && !isObject(actions)) {
+    throw new Error('Invalid arguments');
+  }
+
+  const cloneState = { ...state };
+  const clonedStates = [];
+
+  for (const action of actions) {
+    // get type of action
+    const { type } = action;
+
+    // choice correct action
+    switch (type) {
+      case 'addProperties':
+        const { extraData } = action;
+
+        // get object with adding new properties
+        const addedProperties = addPropertiesToState(cloneState, extraData);
+
+        // add generated object to cloned array
+        clonedStates.push(addedProperties);
+        break;
+
+      case 'removeProperties':
+        const { keysToRemove } = action;
+
+        // get object with removing properties with special keys
+        const removedProperties
+          = removePropertiesToState(cloneState, keysToRemove);
+
+        // add generated object to cloned array
+        clonedStates.push(removedProperties);
+        break;
+
+      case 'clear':
+        // get object with cleared properties
+        const clearedProperties = clearPropertiesToState(cloneState);
+
+        // add generated object to cloned array
+        clonedStates.push(clearedProperties);
+        break;
+    }
+  }
+
+  return clonedStates;
+}
+
+function isObject(obj) {
+  return typeof obj === 'object' && obj !== null;
+}
+
+function addPropertiesToState(state, extraData) {
+  for (const key in extraData) {
+    state[key] = extraData[key];
+  }
+
+  return { ...state };
+}
+
+function removePropertiesToState(state, keysToRemove) {
+  for (const removeKey of keysToRemove) {
+    delete state[removeKey];
+  }
+
+  return { ...state };
+}
+
+function clearPropertiesToState(state) {
+  for (const key in state) {
+    delete state[key];
+  }
+
+  return { ...state };
 }
 
 module.exports = transformStateWithClones;
