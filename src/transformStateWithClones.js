fa0ab3333bcceb1498c4983b7709a21474682414
Taras Voievoda <voevoda.taras@gmail.com>
2023-07-31 15:53:49 +0300

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,28 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const operationsResult = [];
+  const stateCopy = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        break;
+      case 'removeProperties':
+        action.keysToRemove.map(key => delete stateCopy[key]);
+        break;
+      case 'clear':
+        Object.keys(stateCopy).map(key => delete stateCopy[key]);
+        break;
+      default:
+        break;
+    }
+
+    operationsResult.push({ ...stateCopy });
+  };
+
+  return operationsResult;
 }
 
 module.exports = transformStateWithClones;
