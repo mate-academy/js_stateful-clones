255a1007593a2708f4589f55162ec94afd683d61
MateuszSlezal <M4teks93@gmail.com>
2023-07-31 10:17:46 +0200

    changes

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,36 +6,33 @@
  *
  * @return {Object[]}
  */
+const ADD_PROPERTIES = 'addProperties';
+const REMOVE_PROPERTIES = 'removeProperties';
+const CLEAR_ALL = 'clear';
 
 function transformStateWithClones(state, actions) {
   const finalState = { ...state };
   const stateHistory = [];
 
   for (const order of actions) {
-    const ADD_PROPERTIES = order.type === 'addProperties';
-    const REMOVE_PROPERTIES = order.type === 'removeProperties';
-    const CLEAR_ALL = order.type === 'clear';
+    switch (order.type) {
+      case ADD_PROPERTIES:
+        Object.assign(finalState, order.extraData);
+        break;
 
-    if (ADD_PROPERTIES) {
-      Object.assign(finalState, order.extraData);
-      stateHistory.push(Object.assign({}, finalState));
+      case REMOVE_PROPERTIES:
+        for (const remove of order.keysToRemove) {
+          delete finalState[remove];
+        }
+        break;
+
+      case CLEAR_ALL:
+        for (const key in finalState) {
+          delete finalState[key];
+        }
+        break;
     }
-
-    if (REMOVE_PROPERTIES) {
-      for (const remove of order.keysToRemove) {
-        delete finalState[remove];
-      }
-
-      stateHistory.push(Object.assign({}, finalState));
-    }
-
-    if (CLEAR_ALL) {
-      for (const key in finalState) {
-        delete finalState[key];
-      }
-
-      stateHistory.push(Object.assign({}, finalState));
-    }
+    stateHistory.push(Object.assign({}, finalState));
   }
 
   return stateHistory;
