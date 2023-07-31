e60b5a351681cbd22474a725d27bbd5aff208390
Oleksandr Voloshyn <voloshyn244@gmail.com>
2023-08-01 00:54:44 +0200

    transformStateClones v1

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,26 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
-}
+  const result = [];
+  const copyState = { ...state };
 
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(copyState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const removeKey of action.keysToRemove) {
+          delete copyState[removeKey];
+        }
+        break;
+      case 'clear':
+        Object.keys(copyState).forEach(key => delete copyState[key]);
+    }
+    result.push({ ...copyState });
+  }
+
+  return result;
+}
 module.exports = transformStateWithClones;
