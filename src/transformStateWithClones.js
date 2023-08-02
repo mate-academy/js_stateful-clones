41bd08d7c1dd06f4a99f1e42eb6a245e2ab40530
Andrii Kuz <andrykuz4556@gmail.com>
2023-08-03 00:07:26 +0300

    Changes after review

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,31 +8,31 @@
  */
 function transformStateWithClones(state, actions) {
   const stateVersions = [];
+  const copyState = { ...state };
 
   for (const action of actions) {
-    const lastVersion = stateVersions.length - 1;
-    const copyState = !stateVersions.length
-      ? { ...state }
-      : { ...stateVersions[lastVersion] };
-
     switch (action.type) {
       case 'addProperties':
         Object.assign(copyState, action.extraData);
         break;
+
       case 'clear':
         for (const key of Object.keys(copyState)) {
           delete copyState[key];
         }
         break;
+
       case 'removeProperties':
         for (const key of action.keysToRemove) {
           delete copyState[key];
         }
         break;
+
       default:
-        break;
+        throw new Error('Something went wrong!');
     }
-    stateVersions.push(copyState);
+
+    stateVersions.push({ ...copyState });
   };
 
   return stateVersions;
