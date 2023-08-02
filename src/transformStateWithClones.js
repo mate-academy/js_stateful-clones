eecac20777b487afe6d63304c197d6cc0cf91b56
Nazar Votkanych <nazarvotkanych@gmail.com>
2023-08-02 23:22:36 +0300

    add unfinished task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newArr = [];
+
+  for (const action of actions) {
+    let stateCopy = { ...state };
+
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const prop of action.keysToRemove) {
+          delete stateCopy[prop];
+        }
+        break;
+
+      case 'clear':
+        stateCopy = {};
+        break;
+    }
+    newArr.push(stateCopy);
+  }
+
+  return newArr;
 }
 
 module.exports = transformStateWithClones;
