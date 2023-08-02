e7940033304d77d2108ae5eb70bbdd669ca97be5
KlymSudakov <sudakov.klym@gmail.com>
2023-08-02 14:22:59 +0300

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newState = JSON.parse(JSON.stringify(state));
+  const newStateArr = [];
+
+  actions.forEach((action) => {
+    switch (action.type) {
+      case 'clear':
+        Object.keys(newState).forEach((element) => delete newState[element]);
+        break;
+
+      case 'addProperties':
+        Object.assign(newState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        action.keysToRemove.forEach((element) => delete newState[element]);
+        break;
+
+      default:
+        throw new Error('Unexpected error');
+    }
+    newStateArr.push({ ...newState });
+  });
+
+  return newStateArr;
 }
 
 module.exports = transformStateWithClones;
