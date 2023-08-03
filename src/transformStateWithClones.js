c694a6ac36f38ea72b9d9cdde18511b5a6ce48e4
Denys Poltorak <itwfrontend@gmail.com>
2023-08-03 13:36:25 +0300

    refactor: fix some problem

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -13,37 +13,36 @@ function transformStateWithClones(state, actions) {
     throw new Error('Invalid arguments');
   }
 
-  const cloneState = { ...state };
+  let cloneState = { ...state };
   const clonedStates = [];
 
   for (const action of actions) {
     // get type of action
     const { type } = action;
-    let changedObject = null;
 
     // choice correct action
     switch (type) {
       case 'addProperties':
         const { extraData } = action;
 
-        changedObject = addPropertiesToState(cloneState, extraData);
+        addPropertiesToState(cloneState, extraData);
         break;
 
       case 'removeProperties':
         const { keysToRemove } = action;
 
-        changedObject = removePropertiesToState(cloneState, keysToRemove);
+        removePropertiesToState(cloneState, keysToRemove);
         break;
 
       case 'clear':
-        changedObject = clearPropertiesToState(cloneState);
+        cloneState = {};
         break;
 
       default:
         throw new Error('Smth gone wrong');
     }
 
-    clonedStates.push(changedObject);
+    clonedStates.push({ ...cloneState });
   }
 
   return clonedStates;
@@ -57,24 +56,12 @@ function addPropertiesToState(state, extraData) {
   for (const key in extraData) {
     state[key] = extraData[key];
   }
-
-  return { ...state };
 }
 
 function removePropertiesToState(state, keysToRemove) {
   for (const removeKey of keysToRemove) {
     delete state[removeKey];
   }
-
-  return { ...state };
-}
-
-function clearPropertiesToState(state) {
-  for (const key in state) {
-    delete state[key];
-  }
-
-  return { ...state };
 }
 
 module.exports = transformStateWithClones;
