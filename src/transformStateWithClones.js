5ad2b9749fd6abdffd46bb0db331ecbf52652569
Vasyl Kurtiak <pocophonef1start@gmail.com>
2023-08-03 09:23:22 +0300

    solution_task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,40 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const actionHandlers = {
+    addProperties: (action, currentState) => {
+      const { extraData } = action;
+
+      return {
+        ...currentState, ...extraData,
+      };
+    },
+    removeProperties: (action, currentState) => {
+      const { keysToRemove } = action;
+      const newState = { ...currentState };
+
+      for (const key of keysToRemove) {
+        delete newState[key];
+      }
+
+      return newState;
+    },
+    clear: () => ({}),
+  };
+
+  const clonesArray = [];
+
+  for (const action of actions) {
+    const handler = actionHandlers[action.type];
+
+    if (handler) {
+      const newState = handler(action, state);
+
+      clonesArray.push(JSON.parse(JSON.stringify(newState)));
+    }
+  }
+
+  return clonesArray;
 }
 
 module.exports = transformStateWithClones;
