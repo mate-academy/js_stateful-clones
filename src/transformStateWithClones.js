3a3762c956974d219d9c1420cb7c8fd0b5e74b39
Kriss Kozak <misskrissretro@gmail.com>
2023-08-09 18:26:16 +0300

    сloning and transforming state

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -10,25 +10,30 @@ function transformStateWithClones(state, actions) {
   const clones = [Object.assign({}, state)];
 
   actions.forEach((action) => {
-    const { extraData } = action;
+    const { extraData, type, keysToRemove } = action;
 
-    if (extraData) {
-      clones.push(Object.assign({}, clones[clones.length - 1], extraData));
-    } else if (action.type === 'removeProperties') {
-      const { keysToRemove } = action;
-      const currentState = clones[clones.length - 1];
+    switch (type) {
+      case 'addProperties':
+        clones.push(Object.assign({}, clones[clones.length - 1], extraData));
+        break;
+      case 'removeProperties':
+        const currentState = clones[clones.length - 1];
 
-      clones.push(
-        Object.keys(currentState).reduce((newState, key) => {
-          if (!keysToRemove.includes(key)) {
-            newState[key] = currentState[key];
-          }
+        clones.push(
+          Object.keys(currentState).reduce((newState, key) => {
+            if (!keysToRemove.includes(key)) {
+              newState[key] = currentState[key];
+            }
 
-          return newState;
-        }, {})
-      );
-    } else if (action.type === 'clear') {
-      clones.push({});
+            return newState;
+          }, {})
+        );
+        break;
+      case 'clear':
+        clones.push({});
+        break;
+      default:
+        break;
     }
   });
 
