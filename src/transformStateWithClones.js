624ab9ace2fb6f938723d9b4f8664c83eedcdf90
dima <dima98doroshenko@gmail.com>
2023-08-11 15:12:30 +0300

    js_stateful-clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,37 @@
  *
  * @return {Object[]}
  */
-function transformStateWithClones(state, actions) {
-  // write code here
-}
 
+function transformStateWithClones(state, actions) {
+  const copyState = { ...state };
+  const resultTransform = [];
+
+  for (const ob in actions) {
+    const action = actions[ob];
+
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(copyState, action.extraData);
+        break;
+
+      case 'clear':
+        for (const key in copyState) {
+          delete copyState[key];
+        };
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete copyState[key];
+        }
+
+        break;
+      default:
+        return {};
+    }
+    resultTransform.push({ ...copyState });
+  }
+
+  return resultTransform;
+}
 module.exports = transformStateWithClones;
