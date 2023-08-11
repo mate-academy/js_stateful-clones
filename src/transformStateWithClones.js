38201c495eb277b232bdff91487ea33adda1ea24
Ruslan Hrynchuk <r.momos@gmail.com>
2023-08-11 14:40:17 +0300

    Statful_clones. Tests passed

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,40 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const allActionsArray = [];
+  const stateCopy = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case ('addProperties'):
+        Object.assign(stateCopy, action.extraData);
+
+        break;
+
+      case ('removeProperties'):
+        for (const keyToRemove of action.keysToRemove) {
+          if (stateCopy.hasOwnProperty(keyToRemove)) {
+            delete stateCopy[keyToRemove];
+          }
+        }
+
+        break;
+
+      case ('clear'):
+        for (const stateCopyKey in stateCopy) {
+          delete stateCopy[stateCopyKey];
+        }
+
+        break;
+
+      default:
+        return 'ERROR';
+    }
+
+    allActionsArray.push({ ...stateCopy });
+  }
+
+  return allActionsArray;
 }
 
 module.exports = transformStateWithClones;
