f014ef6b257a3927449e35b877fb212300868d40
Denys Khvashchenko <superdened@gmail.com>
2023-08-20 14:53:49 +0300

    fix repeat push

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -16,24 +16,22 @@ function transformStateWithClones(state, actions) {
         copyState = {
           ...copyState, ...action.extraData,
         };
-        result.push({ ...copyState });
         break;
 
       case 'removeProperties':
         for (const key of action.keysToRemove) {
           delete copyState[key];
         }
-        result.push({ ...copyState });
         break;
 
       case 'clear':
         copyState = {};
-        result.push({ ...copyState });
         break;
 
       default:
         break;
     }
+    result.push({ ...copyState });
   }
 
   return result;
