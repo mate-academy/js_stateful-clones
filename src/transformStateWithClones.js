2e6df4f2fc5c4463622e0c171b6c0af0256f94fb
Markiian Haletskyi <haletskiy.markiuan.school91@gmail.com>
2023-08-21 15:41:16 +0200

    add tast solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    let newState = { ...currentState };
+
+    if (action.type === 'addProperties') {
+      newState = Object.assign(newState, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      const keysToRemove = action.keysToRemove || [];
+
+      for (const key of keysToRemove) {
+        delete newState[key];
+      }
+    }
+
+    if (action.type === 'clear') {
+      newState = {};
+    }
+
+    result.push(newState);
+    currentState = newState;
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
