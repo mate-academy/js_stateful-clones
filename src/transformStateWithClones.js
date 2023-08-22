870d24ec099e27b94cf1f66a45f0e44b130a2871
Volodymyr Tantsiura <rimidbit@gmail.com>
2023-08-23 00:13:40 +0300

    finish

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateFinish = [];
+  let currentState = { ...state };
+
+  for (const obj of actions) {
+    const { type, extraData, keysToRemove } = obj;
+
+    switch (type) {
+      case 'addProperties':
+        currentState = {
+          ...currentState, ...extraData,
+        };
+        stateFinish.push({ ...currentState });
+        break;
+
+      case 'removeProperties':
+        keysToRemove.forEach(key => delete currentState[key]);
+        stateFinish.push({ ...currentState });
+        break;
+
+      case 'clear':
+        currentState = {};
+        stateFinish.push({ ...currentState });
+        break;
+    }
+  }
+
+  return stateFinish;
 }
 
 module.exports = transformStateWithClones;
