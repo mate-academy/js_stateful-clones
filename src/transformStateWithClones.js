bdbeab8f90310bebea7bfe64301a7ede2f5c706e
Denys Vasylenkoe <kapodenim@gmail.com>
2023-08-28 18:42:00 +0300

    v2

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,59 +6,41 @@
  *
  * @return {Object[]}
  */
-function transform(state, action) {
-  const cloneState = {};
-
-  for (const key in state) {
-    cloneState[key] = state[key];
-  }
-
-  switch (action.type) {
-    case 'addProperties':
-      // add all extraData props to the state
-      Object.assign(cloneState, action.extraData);
-      break;
-
-    case 'removeProperties': {
-      // remove all keysToRemove from the state
-      for (const key of action.keysToRemove) {
-        delete cloneState[key];
-      }
-
-      break;
-    }
-
-    case 'clear': {
-      // remove all state keys from it
-      for (const key of Object.keys(cloneState)) {
-        delete cloneState[key];
-      }
-
-      break;
-    }
-
-    default: {
-      return {};
-    }
-  }
-
-  return cloneState;
-}
-
 function transformStateWithClones(state, actions) {
   // write code here
-  const actionArray = [];
+  const statesArray = [];
+  const cloneState = { ...state };
 
   for (const action of actions) {
-    // apply every action to the state
-    if (actionArray.length > 0) {
-      actionArray.push(transform(actionArray[actionArray.length - 1], action));
-    } else {
-      actionArray.push(transform(state, action));
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(cloneState, action.extraData);
+        break;
+
+      case 'removeProperties': {
+        for (const key of action.keysToRemove) {
+          delete cloneState[key];
+        }
+
+        break;
+      }
+
+      case 'clear': {
+        for (const key of Object.keys(cloneState)) {
+          delete cloneState[key];
+        }
+
+        break;
+      }
+
+      default: {
+        return {};
+      }
     }
+    statesArray.push({ ...cloneState });
   }
 
-  return actionArray;
+  return statesArray;
 }
 
 module.exports = transformStateWithClones;
