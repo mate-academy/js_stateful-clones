55d64afac15098369c3d982a646ccb6fd32b4410
Bohdan Svystun <bogdansvystun02@gmail.com>
2023-08-31 15:27:14 +0300

    First commit [Svystun Bohdan]

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let currentState = { ...state }; // Clone the initial state
+
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    // Clone the current state for the next iteration
+    let nextState = { ...currentState };
+
+    if (type === 'addProperties' && extraData) {
+      nextState = {
+        ...nextState, ...extraData,
+      };
+    } else if (type === 'removeProperties' && keysToRemove) {
+      for (const key of keysToRemove) {
+        delete nextState[key];
+      }
+    } else if (type === 'clear') {
+      nextState = {};
+    }
+
+    result.push(nextState);
+    currentState = nextState; // Update the current state for the next iteration
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
