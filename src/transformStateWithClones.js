93181d9b89d9f77fc72b2229bf756ebd808c6b4a
Olena Nimakova <nimakova.lena@gmail.com>
2023-09-02 19:38:44 +0300

    transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,32 @@
  *
  * @return {Object[]}
  */
-function transformStateWithClones(state, actions) {
-  // write code here
-}
 
+function transformStateWithClones(state, actions) {
+  let resultObject = { ...state };
+  const resultArray = [];
+
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'addProperties') {
+      resultArray.push(Object.assign({}, resultObject, actions[i].extraData));
+      Object.assign(resultObject, actions[i].extraData);
+    }
+
+    if (actions[i].type === 'removeProperties') {
+      for (const key of actions[i].keysToRemove) {
+        if (resultObject.hasOwnProperty(key)) {
+          delete resultObject[key];
+        }
+      }
+      resultArray.push({ ...resultObject });
+    }
+
+    if (actions[i].type === 'clear') {
+      resultArray.push({});
+      resultObject = {};
+    }
+  }
+
+  return resultArray;
+}
 module.exports = transformStateWithClones;
