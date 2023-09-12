9c553db0de177816240a62897eb6d5e2fb6ec1bf
Olena <e.gorbushko@ukr.net>
2023-09-12 15:05:06 +0300

    add StateClone

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
-}
+  const clone = [];
+  const stateClone = { ...state };
 
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    if (type === 'clear') {
+      for (const key in stateClone) {
+        delete stateClone[key];
+      }
+    }
+
+    if (type === 'addProperties') {
+      for (const data in extraData) {
+        stateClone[data] = extraData[data];
+      }
+    } else if (type === 'removeProperties') {
+      for (const item in keysToRemove) {
+        delete stateClone[keysToRemove[item]];
+      }
+    }
+    clone.push(Object.assign({}, stateClone));
+  }
+
+  return clone;
+}
 module.exports = transformStateWithClones;
