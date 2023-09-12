c3cdc6eba3d1bac2a7a09b2bba5e7998910baf10
Bohdan Antipenko <dr.antipenko2016@gmail.com>
2023-09-12 09:37:01 +0300

    better solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,49 +8,27 @@
  */
 function transformStateWithClones(state, actions) {
   const clones = [];
-  const stateClone = { ...state };
-  // i like my way
-  // for (let i = 0; i < actions.length; i++) {
-  //   if (actions[i].type === 'addProperties') {
-  //     Object.assign(stateClone, actions[i].extraData);
-  //     clones[i] = Object.assign({}, stateClone);
-  //   } else if (actions[i].type === 'removeProperties') {
-  //     for (let j = 0; j < actions[i].keysToRemove.length; j++) {
-  //       const keyToRemove = actions[i].keysToRemove[j];
+  let stateClone = { ...state };
 
-  //       delete stateClone[keyToRemove];
-  //     }
-  //     clones[i] = Object.assign({}, stateClone);
-  //   } else if (actions[i].type === 'clear') {
-  //     for (const k in stateClone) {
-  //       delete stateClone[k];
-  //     }
-  //     clones[i] = Object.assign({}, stateClone);
-  //   };
-  // }
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties':
         Object.assign(stateClone, action.extraData);
-        clones.push(Object.assign({}, stateClone));
         break;
 
       case 'removeProperties': {
         for (const key of action.keysToRemove) {
           delete stateClone[key];
         }
-        clones.push(Object.assign({}, stateClone));
         break;
       }
 
       case 'clear': {
-        for (const key of Object.keys(stateClone)) {
-          delete stateClone[key];
-        }
-        clones.push(Object.assign({}, stateClone));
+        stateClone = {};
         break;
       }
     }
+    clones.push(Object.assign({}, stateClone));
   }
 
   return clones;
