a81bb25748d65ada5c22a9e13c1d0ed43faac06b
Marta <martusia216@gmail.com>
2023-09-13 20:48:31 +0300

    fix clear mistake

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,7 +8,7 @@
  */
 function transformStateWithClones(state, actions) {
   const result = [];
-  const stateCopy = { ...state };
+  let stateCopy = { ...state };
 
   for (const action of actions) {
     switch (action.type) {
@@ -23,9 +23,7 @@ function transformStateWithClones(state, actions) {
         break;
 
       case 'clear':
-        for (const key in stateCopy) {
-          delete stateCopy[key];
-        }
+        stateCopy = {};
         break;
 
       default: break;
