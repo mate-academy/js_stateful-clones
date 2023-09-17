7ae617e4fe7e5ac147a72e47b79460d6f4c99642
Vasil Pankulich <pankulich654@gmail.com>
2023-09-17 19:18:19 +0300

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,34 @@
 'use strict';
 
-/**
- * @param {Object} state
- * @param {Object[]} actions
- *
- * @return {Object[]}
- */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newState = { ...state };
+  const masiv = [];
+
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'addProperties') {
+      for (const add in actions[i].extraData) {
+        newState[add] = actions[i].extraData[add];
+      }
+      masiv.push({ ...newState });
+    }
+
+    if (actions[i].type === 'removeProperties') {
+      for (let j = 0;
+        j < actions[i].keysToRemove.length; j++) {
+        delete newState[actions[i].keysToRemove[j]];
+      }
+      masiv.push({ ...newState });
+    }
+
+    if (actions[i].type === 'clear') {
+      for (const element in newState) {
+        delete newState[element];
+      }
+      masiv.push({ ...newState });
+    }
+  }
+
+  return masiv;
 }
 
 module.exports = transformStateWithClones;
