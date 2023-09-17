2b3c1d759fa013d4e097e87a273d8f638badf8a4
koros <korosov.j@gmail.com>
2023-09-17 16:50:17 +0300

    added task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,5 +1,29 @@
 'use strict';
 
+const procces = (state, action) => {
+  const newState = { ...state };
+
+  switch (action.type) {
+    case 'addProperties':
+      Object.assign(newState, action.extraData);
+      break;
+    case 'removeProperties':
+      for (const prop of action.keysToRemove) {
+        delete newState[prop];
+      }
+      break;
+    case 'clear':
+      for (const key in state) {
+        delete newState[key];
+      }
+      break;
+    default:
+      throw new Error('Switch/Case error');
+  }
+
+  return newState;
+};
+
 /**
  * @param {Object} state
  * @param {Object[]} actions
@@ -7,7 +31,15 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+
+  for (const action of actions) {
+    const currentState = stateHistory[stateHistory.length - 1] || state;
+
+    stateHistory.push(procces(currentState, action));
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
