d745a35a1a44acb647a2d137494991ba4b62ad3d
Denys Stronin <den.stronin@gmail.com>
2023-09-18 16:02:22 +0300

    StateWithClone - first commit

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const resultArr = [];
+  let stateCopy = { ...state };
+
+  const ADD_PROP = 'addProperties';
+  const REMOVE_PROP = 'removeProperties';
+  const CLEAR_PROP = 'clear';
+
+  for (const action of actions) {
+    switch (action.type) {
+      case ADD_PROP:
+        Object.assign(stateCopy, action.extraData);
+        break;
+
+      case REMOVE_PROP:
+        for (const key of action.keysToRemove) {
+          if (stateCopy.hasOwnProperty(key)) {
+            delete stateCopy[key];
+          }
+        }
+        break;
+
+      case CLEAR_PROP:
+        stateCopy = {};
+        break;
+    }
+
+    resultArr.push({ ...stateCopy });
+  }
+
+  return resultArr;
 }
 
 module.exports = transformStateWithClones;
