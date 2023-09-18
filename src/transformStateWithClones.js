e26ea69d694bff3b27d38207e55b1db039f3168f
Illia Drach <illyadrach@gmail.com>
2023-09-18 17:05:06 +0300

    implement task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  const changesArray = [];
+
+  for (let i = 0; i < actions.length; i++) {
+    switch (actions[i].type) {
+      case 'addProperties':
+        for (const key in actions[i].extraData) {
+          stateCopy[key] = actions[i].extraData[key];
+        };
+        break;
+
+      case 'removeProperties':
+        for (let j = 0; j < actions[i].keysToRemove.length; j++) {
+          delete stateCopy[actions[i].keysToRemove[j]];
+        };
+        break;
+
+      case 'clear':
+        for (const lock in stateCopy) {
+          delete stateCopy[lock];
+        };
+        break;
+      default:
+        throw new Error('invalid action type');
+    }
+
+    changesArray.push({ ...stateCopy });
+  }
+
+  return changesArray;
 }
 
 module.exports = transformStateWithClones;
