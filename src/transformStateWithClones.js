5583ab5b5b53491c4f2907c1233127f1de2fec9d
Serhii <sergeymih098123@gmail.com>
2023-09-18 17:16:48 +0300

    add error handling

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -33,7 +33,7 @@ function transformStateWithClones(state, actions) {
         break;
 
       default:
-        break;
+        throw new Error('wrong action type');
     }
     currentState = stateCopy || currentState;
     logs.push(stateCopy);
