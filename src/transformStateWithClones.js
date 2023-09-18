1e90fc2c4597de2531aa3190a70ea0eec7558689
getsi <getsize1144@gmail.com>
2023-09-18 22:10:40 +0300

    Completed the task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newState = { ...state };
+  const newArray = [];
+
+  for (let i = 0; i < actions.length; i++) {
+    switch (actions[i].type) {
+      case 'addProperties':
+        for (const key in actions[i].extraData) {
+          newState[key] = actions[i].extraData[key];
+        };
+        break;
+
+      case 'removeProperties':
+        for (let y = 0; y < actions[i].keysToRemove.length; y++) {
+          delete newState[actions[i].keysToRemove[y]];
+        };
+        break;
+
+      case 'clear':
+        for (const key in newState) {
+          delete newState[key];
+        }
+        break;
+    }
+    newArray.push({ ...newState });
+  }
+
+  return newArray;
 }
 
 module.exports = transformStateWithClones;
