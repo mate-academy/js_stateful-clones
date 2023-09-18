acd7212b4d6a83b948926f75bf54be95e0343a93
Helen Tsyhypalo <lena.tsigipalo@gmail.com>
2023-09-18 14:08:19 +0200

    add transformState solving

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,47 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const states = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    if (states.length > 0) {
+      currentState = { ...states[states.length - 1] };
+    }
+
+    switch (action.type) {
+      case 'addProperties':
+        const data = action.extraData;
+
+        for (const property in data) {
+          currentState[property] = data[property];
+        }
+        break;
+
+      case 'removeProperties':
+        const removes = action.keysToRemove;
+
+        for (const key of removes) {
+          if (key in currentState) {
+            delete currentState[key];
+          }
+        }
+        break;
+
+      case 'clear':
+        for (const entry in currentState) {
+          delete currentState[entry];
+        }
+        break;
+
+      default:
+        throw new Error('Non-existing action');
+    }
+
+    states.push(currentState);
+  }
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
