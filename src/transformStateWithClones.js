fefc70f1a65195a06b7fdc1d651ceb999a4a83cc
Nazar Hnatyuk <108331415+Stim1@users.noreply.github.com>
2023-09-19 15:08:25 +0300

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const clonedStates = [{ ...state }];
+
+  for (const action of actions) {
+    const newState = { ...clonedStates[clonedStates.length - 1] };
+
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(newState, action.extraData);
+        break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete newState[key];
+        }
+        break;
+      case 'clear':
+        for (const key in newState) {
+          if (key in newState) {
+            delete newState[key];
+          }
+        }
+        break;
+    }
+    clonedStates.push(newState);
+  }
+
+  return clonedStates.slice(1);
 }
 
 module.exports = transformStateWithClones;
