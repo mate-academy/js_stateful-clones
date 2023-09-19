6a1e0e7d3b1e92cd5956816f6eb1458971948a99
Mari <kap.mebel.a@gmail.com>
2023-09-19 12:17:44 +0200

    task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const clone = { ...state };
+  const res = [];
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(clone, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      const remove = action.keysToRemove;
+
+      for (let i = 0; i < remove.length; i++) {
+        delete clone[remove[i]];
+      }
+    }
+
+    if (action.type === 'clear') {
+      for (const st in clone) {
+        delete clone[st];
+      }
+    }
+
+    res.push({ ...clone });
+  }
+
+  return res;
 }
 
 module.exports = transformStateWithClones;
