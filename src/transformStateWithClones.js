4ff7dcb3261cded1ae4f6fadb72064354a62cae0
Danylo Morhun <ayogoharo@gmail.com>
2023-09-19 19:56:26 +0300

    transform with clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,26 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  const log = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'clear':
+        Object.keys(stateCopy).forEach(key => delete stateCopy[key]);
+        break;
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        break;
+      case 'removeProperties':
+        action.keysToRemove.forEach(key => delete stateCopy[key]);
+        break;
+    }
+
+    log.push({ ...stateCopy });
+  }
+
+  return log;
 }
 
 module.exports = transformStateWithClones;
