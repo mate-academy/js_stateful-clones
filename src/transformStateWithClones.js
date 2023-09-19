4fdc07a724843ff8876ad6ec8d8835e439af000a
Leonid <aleksandrovi4.l.p@gmail.com>
2023-09-19 21:41:55 +0300

    Pijavskiy transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,36 @@
  *
  * @return {Object[]}
  */
+
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arrayResult = [];
+  const stateCopy = Object.assign({}, state);
+  for (const action of  actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+
+        break;
+
+      case 'removeProperty':
+       for (const prop of action.keysToRemove) {
+        delete stateCopy[prop];
+        break;
+       }
+
+       case 'clear':
+        for (key in stateCopy) {
+          delete stateCopy[key];
+        }
+
+
+    }
+
+    arrayResult.push({...stateCopy});
+  }
+  return arrayResult;
 }
+
 
 module.exports = transformStateWithClones;
