78f052f417ad6ffd1a19c3f1173c7a77892111d5
Evhen <phesar.channel@gmail.com>
2023-09-19 17:24:03 +0300

    added function which returns copy of state file with changes

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,35 @@
 'use strict';
 
 /**
- * @param {Object} state
+ * @param {Object} copy
  * @param {Object[]} actions
  *
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const res = [];
+  let copy = Object.assign({}, state);
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(copy, action.extraData);
+        break;
+      case 'removeProperties':
+        for (const keyToRemove of action.keysToRemove) {
+          delete copy[keyToRemove];
+        }
+        break;
+      case 'clear':
+        copy = {};
+        break;
+      default:
+        break;
+    }
+    res.push({ ...copy });
+  }
+
+  return res;
 }
 
 module.exports = transformStateWithClones;
