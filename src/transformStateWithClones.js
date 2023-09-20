6ad2d053aafdad0c82c4fe42f48dfe3cab1a4dc9
ElenaGrasovskaya <elenagrasovskaya@gmail.com>
2023-09-20 14:22:53 +0300

    Resolving git problem with not submitting the latest changes

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,23 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
+
   const statesAfterActionsArray = [];
-  let stateCopy = Object.assign({}, state);
+  const stateCopy = Object.assign({}, state);
+  const ADD = 'addProperties';
+  const REMOVE = 'removeProperties';
+  const CLEAR = 'clear';
 
   for (const action of actions) {
     const { type, extraData, keysToRemove } = action;
 
     switch (type) {
-      case 'addProperties':
+      case ADD:
         Object.assign(stateCopy, extraData);
         break;
-      case 'removeProperties':
+      case REMOVE:
         for (const keyToRemove of keysToRemove) {
           delete stateCopy[keyToRemove];
         }
         break;
-      case 'clear':
-        stateCopy = {};
+      case CLEAR:
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
         break;
       default:
         throw new Error('Invalid action');
