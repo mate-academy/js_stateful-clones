05cff050444522048234ceab366532fb87f9198d
Oleksandr Kosenko <d1izel@ukr.net>
2023-09-23 12:26:40 +0300

    add function transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  const result = [ ];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties' :
+        Object.assign(stateCopy, action.extraData);
+        result.push({ ...stateCopy });
+        break;
+
+      case 'removeProperties' :
+        for (const item of action.keysToRemove) {
+          delete stateCopy[item];
+        }
+        result.push({ ...stateCopy });
+        break;
+
+      case 'clear' :
+        for (const key of Object.keys(stateCopy)) {
+          delete stateCopy[key];
+        }
+        result.push({ ...stateCopy });
+        break;
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
