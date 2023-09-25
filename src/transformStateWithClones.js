b4c23a42dcbb124c5fdeac018903660ab0a2decf
Робенко Олександр Олександрович <Robenko-O@yugcontract.ua>
2023-09-25 17:58:30 +0300

    sulution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const stateCopy = { ...state };
+
+  for (let i = 0; i < actions.length; i++) {
+    switch (actions[i].type) {
+      case 'addProperties': {
+        Object.assign(stateCopy, actions[i].extraData);
+        result.push({ ...stateCopy });
+        break;
+      }
+
+      case 'removeProperties': {
+        for (const key of actions[i].keysToRemove) {
+          delete stateCopy[key];
+        }
+        result.push({ ...stateCopy });
+        break;
+      }
+
+      case 'clear': {
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
+        result.push({ ...stateCopy });
+        break;
+      }
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
