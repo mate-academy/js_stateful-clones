c2a5f61832fd0a06b40947c09b8302624495efb5
Andrii Voitovych <andrew.voitovych.v@gmail.com>
2023-10-01 18:50:40 +0300

    change solution with switch

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,42 +7,27 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const stateModified = [];
-  const stateObject = { ...state };
+  const stateResult = [];
+  let stateObject = { ...state };
 
-  for (const obj of actions) {
-    for (const key in obj) {
-      if (obj[key] === 'addProperties') {
-        Object.assign(stateObject, obj.extraData);
-
-        const addedObject = { ...stateObject };
-
-        stateModified.push(addedObject);
-      }
-
-      if (obj[key] === 'removeProperties') {
-        obj.keysToRemove.forEach(e => delete stateObject[e]);
-
-        const removedObject = { ...stateObject };
-
-        stateModified.push(removedObject);
-      }
-
-      if (obj[key] === 'clear') {
-        for (const variableKey in stateObject) {
-          if (stateObject.hasOwnProperty(variableKey)) {
-            delete stateObject[variableKey];
-          }
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateObject, action.extraData);
+        break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete stateObject[key];
         }
-
-        const clearedObject = { ...stateObject };
-
-        stateModified.push(clearedObject);
-      }
+        break;
+      case 'clear':
+        stateObject = {};
     }
+
+    stateResult.push({ ...stateObject });
   }
 
-  return stateModified;
+  return stateResult;
 }
 
 module.exports = transformStateWithClones;
