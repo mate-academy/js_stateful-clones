02f6a75cd7999ebdfc1f5d0b11d902cb282ea4df
Denis Shyshenko <denisbmw2.84@gmail.com>
2023-10-02 11:43:35 +0300

    stateful_clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,33 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const ADD_PROPERTIES = 'addProperties';
+  const REMOVE_PROPERTIES = 'removeProperties';
+  const CLEAR = 'clear';
+  const newState = { ...state };
+  const arrState = [];
+
+  for (const types of actions) {
+    if (types.type === ADD_PROPERTIES) {
+      Object.assign(newState, types.extraData);
+    }
+
+    if (types.type === REMOVE_PROPERTIES) {
+      for (const key of types.keysToRemove) {
+        delete newState[key];
+      }
+    }
+
+    if (types.type === CLEAR) {
+      for (const keys in newState) {
+        delete newState[keys];
+      }
+    }
+
+    arrState.push({ ...newState });
+  }
+
+  return arrState;
 }
 
 module.exports = transformStateWithClones;
