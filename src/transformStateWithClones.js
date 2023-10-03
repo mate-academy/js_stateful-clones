6b77ee3abfdf9989420bd1a44afc52b62ab5b69b
Kseniia Chepur <kseniia.chepur@gmail.com>
2023-10-03 22:37:21 +0300

    fix code as per the comments on github

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,39 +8,32 @@
  */
 function transformStateWithClones(state, actions) {
   const statesArray = [];
+  const stateCopy = { ...state };
 
-  for (let i = 0; i < actions.length; i++) {
-    switch (actions[i].type) {
+  for (const action of actions) {
+    switch (action.type) {
       case 'addProperties': {
-        const currentState = i ? { ...statesArray[i - 1] } : { ...state };
-
-        Object.assign(currentState, actions[i].extraData);
-        statesArray.push(currentState);
+        Object.assign(stateCopy, action.extraData);
         break;
       }
 
       case 'removeProperties': {
-        const currentState = i ? { ...statesArray[i - 1] } : { ...state };
-
-        for (const key of actions[i].keysToRemove) {
-          delete currentState[key];
+        for (const key of action.keysToRemove) {
+          delete stateCopy[key];
         }
-        statesArray.push(currentState);
         break;
       }
 
       case 'clear': {
-        const currentState = i ? { ...statesArray[i - 1] } : { ...state };
-
-        for (const key of Object.keys(currentState)) {
-          delete currentState[key];
+        for (const key of Object.keys(stateCopy)) {
+          delete stateCopy[key];
         }
-        statesArray.push(currentState);
         break;
       }
 
       default: throw new Error('Invalid action type');
     }
+    statesArray.push({ ...stateCopy });
   }
 
   return statesArray;
