94490fd44318d9c4565859dd8d43b642beee48f3
ErnestMk1 <ernmyr434@gmail.com>
2023-10-03 22:36:52 +0300

    implemented task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateClone = { ...state };
+  const stateVersions = [];
+
+  for (const object of actions) {
+    if (object.type === 'addProperties') {
+      Object.assign(stateClone, object.extraData);
+
+      stateVersions.push({ ...stateClone });
+    } else if (object.type === 'removeProperties') {
+      for (const toRemove of object.keysToRemove) {
+        delete stateClone[toRemove];
+      }
+
+      stateVersions.push({ ...stateClone });
+    } else if (object.type === 'clear') {
+      for (const key in stateClone) {
+        delete stateClone[key];
+      }
+
+      stateVersions.push({ ...stateClone });
+    }
+  }
+
+  return stateVersions;
 }
 
 module.exports = transformStateWithClones;
