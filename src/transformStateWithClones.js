e17d7a232ece2b675cf6af3a5c1356056b26c1ef
DianaFakhim <dianafahimn@gmail.com>
2023-10-04 10:59:49 -0400

    use switch case

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -12,28 +12,28 @@ function transformStateWithClones(state, actions) {
   const arrayOfOriginals = [];
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      for (const [key, value] of Object.entries(action.extraData)) {
-        stateCopy[key] = value;
-      }
-      arrayOfOriginals.push(Object.assign({}, stateCopy));
-    }
+    switch (action.type) {
+      case 'addProperties':
+        for (const [key, value] of Object.entries(action.extraData)) {
+          stateCopy[key] = value;
+        };
+        break;
 
-    if (action.type === 'removeProperties') {
-      for (const key in action.keysToRemove) {
-        const actionKey = action.keysToRemove[key];
+      case 'removeProperties':
+        for (const key in action.keysToRemove) {
+          const actionKey = action.keysToRemove[key];
 
-        delete stateCopy[actionKey];
-      }
-      arrayOfOriginals.push(Object.assign({}, stateCopy));
-    }
+          delete stateCopy[actionKey];
+        };
+        break;
 
-    if (action.type === 'clear') {
-      for (const key in stateCopy) {
-        delete stateCopy[key];
-      }
-      arrayOfOriginals.push(Object.assign({}, stateCopy));
+      case 'clear':
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        };
+        break;
     }
+    arrayOfOriginals.push(Object.assign({}, stateCopy));
   }
 
   return arrayOfOriginals;
