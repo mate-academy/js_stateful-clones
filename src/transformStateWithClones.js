ccd13dd43a1669976bd5ed140a10ff8e886be689
newqou <137334905+newqou@users.noreply.github.com>
2023-10-05 21:06:31 +0300

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      currentState = {
+        ...currentState, ...action.extraData,
+      };
+    } else if (action.type === 'removeProperties') {
+      const keysToRemove = action.keysToRemove || [];
+
+      for (const key of keysToRemove) {
+        if (currentState.hasOwnProperty(key)) {
+          delete currentState[key];
+        }
+      }
+    } else if (action.type === 'clear') {
+      currentState = {};
+    }
+    result.push({ ...currentState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
