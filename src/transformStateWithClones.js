8272d9f04413ca5855f72cd8736983959fed27e1
LabPetro <lamppetro30@outlook.com>
2023-10-06 11:58:42 +0300

    2 out of 12 failing

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const allStates = [];
+
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+    const clone = { ...state };
+
+    switch (type) {
+      case 'addProperties':
+        Object.assign(clone, extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of keysToRemove) {
+          delete clone[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in clone) {
+          delete clone[key];
+        }
+        break;
+
+      default:
+        return 'Error : Type Unknown';
+    }
+    allStates.push(clone);
+  }
+
+  return allStates;
 }
 
 module.exports = transformStateWithClones;
