f5a65ef5ebb2e68a2cc98ab641f01b8972ccbcbe
Viktor Bykh <bykhviktor21@gmail.com>
2023-10-06 11:54:59 +0300

    first version of hw js_stateful-clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  let currentStateCopy = {};
+  const result = [];
+
+  for (const action of actions) {
+    for (const key in action) {
+      switch (action[key]) {
+        case 'addProperties':
+          Object.assign(stateCopy, action.extraData);
+          break;
+        case 'removeProperties':
+          for (const keyToRemove of action.keysToRemove) {
+            delete stateCopy[keyToRemove];
+          }
+          break;
+        case 'clear':
+          for (const use in stateCopy) {
+            delete stateCopy[use];
+          }
+          break;
+        default:
+      }
+    }
+    currentStateCopy = { ...stateCopy };
+    result.push(currentStateCopy);
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
