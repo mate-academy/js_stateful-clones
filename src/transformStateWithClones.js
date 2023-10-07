a8541a2038bcba636f14d480e924700badf06562
Tatiana <tanya_potapenko@bigmir.net>
2023-10-07 15:08:36 +0200

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    if (type === 'addProperties') {
+      currentState = { ...currentState, ...extraData };
+    } else
+    if (type === 'removeProperties') {
+      currentState = Object.keys(currentState).reduce((newState, key) => {
+        if (!keysToRemove.includes(key)) {
+          newState[key] = currentState[key];
+        }
+
+        return newState;
+      }, {});
+    } else
+    if (type === 'clear') {
+      currentState = {};
+    }
+
+    result.push({ ...currentState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
