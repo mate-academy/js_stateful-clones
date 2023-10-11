4a543ff9810b66c4da749c2ff1a94340dc63a605
Viktor Bykh <bykhviktor21@gmail.com>
2023-10-11 09:32:35 +0300

    updated version after mentor review

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,24 +11,22 @@ function transformStateWithClones(state, actions) {
   let currentStateCopy = {};
   const result = [];
 
-  for (const action of actions) {
-    for (const key in action) {
-      switch (action[key]) {
-        case 'addProperties':
-          Object.assign(stateCopy, action.extraData);
-          break;
-        case 'removeProperties':
-          for (const keyToRemove of action.keysToRemove) {
-            delete stateCopy[keyToRemove];
-          }
-          break;
-        case 'clear':
-          for (const use in stateCopy) {
-            delete stateCopy[use];
-          }
-          break;
-        default:
-      }
+  for (let i = 0; i < actions.length; i++) {
+    switch (actions[i].type) {
+      case 'addProperties':
+        Object.assign(stateCopy, actions[i].extraData);
+        break;
+      case 'removeProperties':
+        for (const keyToRemove of actions[i].keysToRemove) {
+          delete stateCopy[keyToRemove];
+        }
+        break;
+      case 'clear':
+        for (const use in stateCopy) {
+          delete stateCopy[use];
+        }
+        break;
+      default:
     }
     currentStateCopy = { ...stateCopy };
     result.push(currentStateCopy);
