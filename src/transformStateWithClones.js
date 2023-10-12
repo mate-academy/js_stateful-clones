749ab6f2128ff0f2f180957ac822110c66346f94
Volodymyr Liubyn <vovalu88@gmail.com>
2023-10-12 21:51:27 +0300

    transformStateWithClones #1

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,40 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  const stateArray = [];
+
+  for (const action of actions) {
+    transform(stateCopy, action);
+
+    const varState = { ...stateCopy };
+
+    stateArray.push(varState);
+  }
+
+  return stateArray;
+}
+
+function transform(state, action) {
+  switch (action.type) {
+    case 'addProperties':
+      Object.assign(state, action.extraData);
+      break;
+
+    case 'removeProperties': {
+      for (const key of action.keysToRemove) {
+        delete state[key];
+      }
+      break;
+    }
+
+    case 'clear': {
+      for (const key of Object.keys(state)) {
+        delete state[key];
+      }
+      break;
+    }
+  }
 }
 
 module.exports = transformStateWithClones;
