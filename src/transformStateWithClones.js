fb7f82cd2eec143f4304534f1e0f3680ff9b6f33
Serhii Kot <serhiikot.personal@outlook.com>
2023-10-13 12:27:41 +0300

    initial commit

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let stateCopy = { ...state };
+  let tempObj = {};
+  let stateArray = [];
+
+  for (let i = 0; i < actions.length; i++) {
+    switch (actions[i].type) {
+      case 'addProperties':
+        stateCopy = {
+          ...stateCopy,
+          ...actions[i].extraData,
+        };
+        tempObj = { ...stateCopy };
+        stateArray = [...stateArray, tempObj];
+        break;
+
+      case 'removeProperties':
+        for (const item of actions[i].keysToRemove) {
+          delete stateCopy[item];
+        }
+        stateArray = [...stateArray, stateCopy];
+        break;
+
+      case 'clear':
+        stateCopy = {};
+        stateArray = [...stateArray, stateCopy];
+        break;
+    }
+  }
+
+  return stateArray;
 }
 
 module.exports = transformStateWithClones;
