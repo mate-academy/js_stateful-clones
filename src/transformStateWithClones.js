f983f4f88a2844612869de1c3a5822389348b84a
Dmytro Moshkivskyi <dmosh@ukr.net>
2023-10-14 01:19:16 +0300

    js_stateful-clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,43 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newArrayObject = [Object.assign({}, state)]; // масив об'єктів
+
+  for (let i = 0; i < actions.length; i++) {
+    if (i > 0) {
+      newArrayObject[i] = { ...newArrayObject[i - 1] };
+    }
+
+    switch (actions[i].type) {
+      case 'addProperties': {
+        for (const key1 in actions[i].extraData) {
+          newArrayObject[i][`${key1}`] = actions[i].extraData[key1];
+        }
+
+        break;
+      }
+
+      case 'removeProperties': {
+        for (const key2 in actions[i].keysToRemove) {
+          delete newArrayObject[i][`${actions[i].keysToRemove[key2]}`];
+        }
+
+        break;
+      }
+
+      case 'clear': {
+        for (const key3 in newArrayObject[i]) {
+          delete newArrayObject[i][`${key3}`];
+        }
+
+        break;
+      }
+    }
+  }
+
+  return newArrayObject;
 }
 
 module.exports = transformStateWithClones;
