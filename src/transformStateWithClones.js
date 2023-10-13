d35ba7d00a198b84d5342922cce5b04382fff04f
Olesia Semen <semenolesa01@gmail.com>
2023-10-13 14:39:44 +0300

    Testing

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /**
  * @param {Object} state
  * @param {Object[]} actions
@@ -7,7 +6,25 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let currentState = { ...state };
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      const updatedState = { ...currentState, ...action.extraData };
+      result.push(updatedState);
+    } else if (action.type === 'removeProperties') {
+      const keysToRemove = action.keysToRemove || [];
+      const updatedState = { ...currentState };
+      for (const key of keysToRemove) {
+          delete updatedState[key];
+          
+      }
+      result.push(updatedState);
+    } else if (action.type === 'clear') {
+      result.push({});
+    }
+  }
+  return result;
 }
 
 module.exports = transformStateWithClones;
