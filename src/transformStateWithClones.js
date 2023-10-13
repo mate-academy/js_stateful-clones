8ad890ea064c4b2dd3668dac8ddf44aaa7ab4a67
Kostya Niekrasov <kostyanekrasov1501@gmail.com>
2023-10-13 20:20:17 +0300

    transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,41 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const newState = { ...state };
+
+  function transform(object, action) {
+    switch (action.type) {
+      case 'addProperties':
+        if (action.extraData) {
+          Object.assign(object, action.extraData);
+        }
+        break;
+
+      case 'removeProperties':
+        if (action.keysToRemove) {
+          for (const key of action.keysToRemove) {
+            if (key in object) {
+              delete object[key];
+            }
+          }
+        }
+        break;
+
+      case 'clear':
+        for (const key in object) {
+          delete object[key];
+        }
+        break;
+    }
+  }
+
+  for (const action of actions) {
+    transform(newState, action);
+    result.push({ ...newState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
