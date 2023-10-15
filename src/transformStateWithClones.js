efbea269efd91e363fa2db5f4ebb3e9b04490472
NataSerg <na.s@ukr.net>
2023-10-15 09:27:24 +0300

    simplify by creating stateCopy and pushing at the end of iteration

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -10,50 +10,30 @@ function transformStateWithClones(state, actions) {
   const ADD_PROPERTIES_TYPE = 'addProperties';
   const REMOVE_PROPERTIES_TYPE = 'removeProperties';
   const CLEAR_TYPE = 'clear';
-  const PROPERTIES_TO_ADD_KEY = 'extraData';
-  const PROPERTIES_TO_REMOVE_KEY = 'keysToRemove';
 
   const clonesArray = [];
+  let stateCopy = { ...state };
 
   for (const action of actions) {
     switch (action.type) {
       case ADD_PROPERTIES_TYPE:
-        if (clonesArray.length === 0) {
-          clonesArray.push(Object.assign({ ...state },
-            action[PROPERTIES_TO_ADD_KEY]));
-        } else {
-          const previousObject = clonesArray[clonesArray.length - 1];
-
-          clonesArray.push(Object
-            .assign({ ...previousObject }, action[PROPERTIES_TO_ADD_KEY]));
-        }
+        stateCopy = {
+          ...stateCopy,
+          ...action.extraData,
+        };
         break;
 
       case REMOVE_PROPERTIES_TYPE:
-        if (clonesArray.length > 0) {
-          const previousObjectCopy
-            = { ...clonesArray[clonesArray.length - 1] };
-
-          for (const key of action[PROPERTIES_TO_REMOVE_KEY]) {
-            delete previousObjectCopy[key];
-          }
-
-          clonesArray.push(previousObjectCopy);
-        } else {
-          const initialObjectCopy = { ...state };
-
-          for (const key of action[PROPERTIES_TO_REMOVE_KEY]) {
-            delete initialObjectCopy[key];
-          }
-
-          clonesArray.push(initialObjectCopy);
+        for (const key of action.keysToRemove) {
+          delete stateCopy[key];
         }
         break;
 
       case CLEAR_TYPE:
-        clonesArray.push({});
+        stateCopy = {};
         break;
     }
+    clonesArray.push({ ...stateCopy });
   }
 
   return clonesArray;
