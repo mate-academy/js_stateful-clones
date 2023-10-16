6041c1fe2d90c083c6cc9398bf1a02d31feb477d
Sakhno Andrii <andreysakhno1986@gmail.com>
2023-10-16 20:39:32 +0300

    Implemented transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const currentState = { ...state };
+
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
+      case 'addProperties':
+        Object.assign(currentState, extraData);
+        break;
+
+      case 'removeProperties':
+        keysToRemove.forEach((key) => delete currentState[key]);
+        break;
+
+      default:
+        Object.keys(currentState).forEach((key) => delete currentState[key]);
+    }
+
+    result.push({ ...currentState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
