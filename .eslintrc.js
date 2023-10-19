81a389ac32c34e9b105070073b9167555cccf82d
Serhii Mykolaienko <mikolaenko.sa@gmail.com>
2023-10-19 16:31:27 +0300

    add solution

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,13 @@
 module.exports = {
-  extends: '@mate-academy/eslint-config',
-  env: {
-    jest: true
-  },
-  rules: {
-    'no-proto': 0
-  },
-  plugins: ['jest']
+	extends: '@mate-academy/eslint-config',
+	env: {
+		jest: true,
+	},
+	rules: {
+		'no-proto': 0,
+		indent: [0, 'tab'],
+		'no-tabs': 0,
+		'no-console': 'off',
+	},
+	plugins: ['jest'],
 };
--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+	const stateClone = { ...state };
+	const resultArray = [];
+
+	for (const action of actions) {
+		if (action.type === 'addProperties') {
+			for (const key in action.extraData) {
+				stateClone[key] = action.extraData[key];
+			}
+		}
+
+		if (action.type === 'removeProperties') {
+			for (const keyToRemove of action.keysToRemove) {
+				if (stateClone[keyToRemove]) {
+					delete stateClone[keyToRemove];
+				}
+			}
+		}
+
+		if (action.type === 'clear') {
+			for (const keyState in stateClone) {
+				delete stateClone[keyState];
+			}
+		}
+		resultArray.push({ ...stateClone });
+	}
+
+	return resultArray;
 }
 
 module.exports = transformStateWithClones;
