f28507b8ba6928f50315ce2e1a61fa3a6183392a
Serhii Mykolaienko <mikolaenko.sa@gmail.com>
2023-10-21 12:45:37 +0300

    fixed indents

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,35 +7,35 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-	const stateClone = { ...state };
-	const resultArray = [];
+  const stateClone = { ...state };
+  const resultArray = [];
 
-	for (const action of actions) {
-		switch (action.type) {
-			case 'addProperties':
-				Object.assign(stateClone, action.extraData);
-				break;
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateClone, action.extraData);
+        break;
 
-			case 'removeProperties':
-				for (const keyToRemove of action.keysToRemove) {
-					delete stateClone[keyToRemove];
-				}
-				break;
+      case 'removeProperties':
+        for (const keyToRemove of action.keysToRemove) {
+          delete stateClone[keyToRemove];
+        }
+        break;
 
-			case 'clear':
-				for (const keyState in stateClone) {
-					delete stateClone[keyState];
-				}
-				break;
+      case 'clear':
+        for (const keyState in stateClone) {
+          delete stateClone[keyState];
+        }
+        break;
 
-			default:
-				return stateClone;
-		}
+      default:
+        return stateClone;
+    }
 
-		resultArray.push({ ...stateClone });
-	}
+    resultArray.push({ ...stateClone });
+  }
 
-	return resultArray;
+  return resultArray;
 }
 
 module.exports = transformStateWithClones;
