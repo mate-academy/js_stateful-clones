29caff498ffdccc6e83ab66a9bfab228e435ea34
Krot Nikita <krotnikitaasd@gmail.com>
2023-10-22 17:39:27 +0300

    transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,39 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const instuction = Object.assign(actions);
+  const loopResult = { ...state };
+
+  for (let i = 0; i < instuction.length; i++) {
+    switch (instuction[i].type) {
+      case 'addProperties':
+        for (const key in instuction[i].extraData) {
+          loopResult[key] = instuction[i].extraData[key];
+        }
+        result.push({ ...loopResult });
+        break;
+
+      case 'removeProperties':
+        for (const key of instuction[i].keysToRemove) {
+          delete loopResult[key];
+        }
+        result.push({ ...loopResult });
+        break;
+
+      case 'clear':
+        for (const key in loopResult) {
+          delete loopResult[key];
+        }
+        result.push({ ...loopResult });
+        break;
+
+      default:
+        return 'Sorry, but you entered values that are not as expected';
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
