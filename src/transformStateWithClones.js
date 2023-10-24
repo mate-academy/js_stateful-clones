45d3b960022100b6e2b76e7bf63714843f1c4bad
Дядя Саня <alvlru1971@gmail.com>
2023-10-24 19:47:09 +0300

    mistakes

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const massiv = [];
+  const copy = { ...state };
+
+  for (const action of actions) {
+    transform(copy, action);
+  }
+
+  function transform(copy, action) {
+    switch (action.type) {
+      case 'addProperties':
+        const result = Object.assign(copy, action.extraData);
+
+        massiv.push(result);
+
+      case 'removeProperties': {
+        for (const key of action.keysToRemove) {
+          delete copy[key];
+          massiv.push(copy);
+        }
+      }
+
+      case 'clear': {
+        for (const key of Object.keys(state)) {
+          delete copy[key];
+        }
+      }
+    }
+  }
+
+  return massiv;
 }
 
 module.exports = transformStateWithClones;
