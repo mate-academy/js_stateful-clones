168a4cccd26e02badaa4c827ef75e86143d34164
Barbara Krysiak <b.krysiakk@gmail.com>
2023-10-25 10:20:10 +0200

    task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,34 +7,27 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let currentState = { ...state };
   const result = [];
-  const cloneState = { ...state };
 
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties':
-        Object.assign(cloneState, action.extraData);
+        Object.assign(currentState, action.extraData);
         break;
-
       case 'removeProperties':
-        action.keysToRemove.forEach(item => delete cloneState[item]);
-        break;
-
-      case 'clear':
-        for (const key in cloneState) {
-          delete cloneState[key];
+        for (const key of action.keysToRemove) {
+          delete currentState[key];
         }
         break;
-
-      default:
+      case 'clear':
+        currentState = {};
         break;
     }
 
-    result.push({ ...cloneState });
+    result.push({ ...currentState });
   }
 
   return result;
 }
-
 module.exports = transformStateWithClones;
