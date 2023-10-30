537fcd42cfa1888499ae5e7c096d313f8c557d60
Dmytro Krasnoholovenko <errisi.v@gmail.com>
2023-10-30 16:09:29 +0200

    result

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [Object.assign({}, state)];
+
+  for (const action of actions) {
+    const stateCopy = Object.assign({}, stateHistory[stateHistory.length - 1]);
+
+    if (action.type === 'addProperties') {
+      for (const key in action.extraData) {
+        stateCopy[key] = action.extraData[key];
+      }
+    } else if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete stateCopy[key];
+      }
+    } else if (action.type === 'clear') {
+      for (const key in stateCopy) {
+        delete stateCopy[key];
+      }
+    }
+
+    stateHistory.push(stateCopy);
+  }
+
+  return stateHistory.slice(1);
 }
 
 module.exports = transformStateWithClones;
