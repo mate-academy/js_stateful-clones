a01c9f207b828778321fb38a0a381088af730598
Malyholovka Ostap <maligolovkaostap@gmail.com>
2023-10-30 19:57:30 +0200

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,39 @@
 'use strict';
 
 /**
- * @param {Object} state
+ * @param {Object} stateCopy
  * @param {Object[]} actions
  *
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const STATE_COPYS = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    let stateCopy = { ...currentState };
+
+    if (action.type === 'addProperties') {
+      Object.assign(stateCopy, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const keyRemove of action.keysToRemove) {
+        if (stateCopy[keyRemove] !== undefined) {
+          delete stateCopy[keyRemove];
+        }
+      }
+    }
+
+    if (action.type === 'clear') {
+      stateCopy = {};
+    }
+
+    STATE_COPYS.push(stateCopy);
+    currentState = stateCopy;
+  }
+
+  return STATE_COPYS;
 }
 
 module.exports = transformStateWithClones;
