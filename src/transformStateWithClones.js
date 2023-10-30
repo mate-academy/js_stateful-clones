b88d97d59c1607eb39bf26026ce375dd8ca504f3
Fluebubble <Trots.anatoly@gmail.com>
2023-10-30 20:48:47 +0200

    solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,45 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const result = [];
+  let prevState = { ...state };
+
+  for (const action of actions) {
+    let stepResult = { ...prevState };
+
+    if (action.type === 'addProperties') {
+      stepResult = {
+        ...stepResult,
+        ...action.extraData,
+      };
+
+      result.push(stepResult);
+
+      prevState = { ...stepResult };
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        if (stepResult[key]) {
+          delete stepResult[key];
+        }
+      }
+
+      result.push(stepResult);
+
+      prevState = { ...stepResult };
+    }
+
+    if (action.type === 'clear') {
+      stepResult = {};
+
+      result.push(stepResult);
+
+      prevState = { ...stepResult };
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
