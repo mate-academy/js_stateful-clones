29ac647ba559cd27f81426fdc2cf86c1d2b066df
kate-prakofyeva <catherine.smiff@gmail.com>
2023-10-30 21:37:24 +0100

    feat: add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,41 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const STATE_COPY = {
+    ...state,
+  };
+
+  const result = [STATE_COPY];
+
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+    const currentStep = {
+      ...result[result.length - 1],
+    };
+
+    switch (type) {
+      case 'addProperties':
+        for (const [key, value] of Object.entries(extraData)) {
+          currentStep[key] = value;
+        }
+        result.push(currentStep);
+        break;
+      case 'removeProperties':
+        for (const key of keysToRemove) {
+          delete currentStep[key];
+        }
+        result.push(currentStep);
+        break;
+      case 'clear':
+        result.push({});
+        break;
+      default:
+        throw new Error('Unknown action type');
+    }
+  }
+  result.shift();
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
