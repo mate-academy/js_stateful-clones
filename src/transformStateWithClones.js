a5b538bae18293a12b3ca4a025794e18b1e2c523
boytrend0108 <boytrend@gmail.com>
2023-10-30 18:59:19 +0300

    complete task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,45 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newClone = { ...state };
+  const stateClones = [];
+
+  actions.forEach(action => {
+    stateClones.push(createClone(newClone, action));
+  });
+
+  return stateClones;
+}
+
+function createClone(newClone, action) {
+  switch (action.type) {
+    case 'addProperties':
+      for (const key in action.extraData) {
+        newClone[key] = action.extraData[key];
+      };
+
+      return { ...newClone };
+
+    case 'removeProperties':
+      for (const el in action.keysToRemove) {
+        if (action.keysToRemove[el] in newClone) {
+          delete newClone[action.keysToRemove[el]];
+        }
+      };
+
+      return Object.keys(newClone).length === 0 ? {} : { ...newClone };
+
+    case 'clear':
+      for (const key in newClone) {
+        delete newClone[key];
+      }
+
+      return {};
+
+    default: throw new Error('Enter correct value');
+  }
 }
 
 module.exports = transformStateWithClones;
