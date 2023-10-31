0d12acb9e8c7fbe141a446357f3ba8be3d991527
Oleksii Mantserov <a.v.mantserov@gmail.com>
2023-10-31 16:12:54 +0200

    transformSWC done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,47 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+
+  const copy = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      adding(copy, action);
+    }
+
+    if (action.type === 'removeProperties') {
+      removing(copy, action);
+    }
+
+    if (action.type === 'clear') {
+      clearing(copy);
+    }
+
+    result.push({ ...copy });
+  }
+
+  return result;
+}
+
+function adding(state, action) {
+  for (const key in action.extraData) {
+    state[key] = action.extraData[key];
+  }
+}
+
+function removing(state, action) {
+  for (const key of action.keysToRemove) {
+    delete state[key];
+  }
+}
+
+function clearing(state) {
+  for (const key in state) {
+    delete state[key];
+  }
 }
 
 module.exports = transformStateWithClones;
