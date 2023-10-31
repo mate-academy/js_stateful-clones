95f66a3f39db808f9a3cbe163ef54f5d57a19c6b
Maria Berezhna <marua545@gmail.com>
2023-10-31 10:17:30 +0200

    stateful dlines

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateClones = [Object.assign({}, state)];
+
+  for (const action of actions) {
+    let newState = Object.assign({}, stateClones[stateClones.length - 1]);
+
+    switch (action.type) {
+      case 'addProperties':
+        newState = Object.assign(newState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const keyToRemove of action.keysToRemove) {
+          delete newState[keyToRemove];
+        }
+        break;
+
+      case 'clear':
+        newState = {};
+        break;
+    }
+
+    stateClones.push(newState);
+  }
+  stateClones.shift();
+
+  return stateClones;
 }
 
 module.exports = transformStateWithClones;
