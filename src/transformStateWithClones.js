316a6fc2a35b7048182cd80b1dfbe699d5809d71
Monika Osowiecka <mosowiecka@users.noreply.github.com>
2023-10-31 14:46:02 +0100

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,53 @@
  *
  * @return {Object[]}
  */
+function deepCopy(obj) {
+  if (obj === null) {
+    return null;
+  }
+
+  if (typeof obj !== 'object') {
+    return obj;
+  }
+
+  const copy = {};
+
+  for (const key in obj) {
+    copy[key] = deepCopy(obj[key]);
+  }
+
+  return copy;
+}
+
 function transformStateWithClones(state, actions) {
   // write code here
+  const result = [];
+  const state2 = deepCopy(state);
+
+  for (const a of actions) {
+    if (a.type === 'addProperties') {
+      for (const d in a.extraData) {
+        state2[d] = a.extraData[d];
+      }
+      result.push(deepCopy(state2));
+    }
+
+    if (a.type === 'removeProperties') {
+      for (const d of a.keysToRemove) {
+        delete state2[d];
+      }
+      result.push(deepCopy(state2));
+    }
+
+    if (a.type === 'clear') {
+      for (const d in state2) {
+        delete state2[d];
+      }
+      result.push(deepCopy(state2));
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
