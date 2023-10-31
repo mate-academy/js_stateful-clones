8aa52f5dafde7c18abe37425a31a1e4162ddfa4a
Egor <yehorheneralov@gmail.com>
2023-10-31 17:27:26 +0200

    solution in switch case

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,39 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    let newState = { ...currentState };
+
+    switch (action.type) {
+      case 'addProperties':
+        const { extraData } = action;
+
+        for (const key in extraData) {
+          newState[key] = extraData[key];
+        }
+        break;
+
+      case 'removeProperties':
+        const { keysToRemove } = action;
+
+        for (const key of keysToRemove) {
+          delete newState[key];
+        }
+
+        break;
+
+      case 'clear':
+        newState = {};
+        break;
+    }
+    result.push(newState);
+    currentState = newState;
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
