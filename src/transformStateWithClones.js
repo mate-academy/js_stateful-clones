7a29fd114860cb97df832c6b2604f46145441b2a
Svitlana <svitlana.ponomarchuk@gmail.com>
2023-10-31 15:47:00 +0200

    added task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,34 @@
  *
  * @return {Object[]}
  */
+function transform(state, action) {
+  if (action.type === 'addProperties') {
+    Object.assign(state, action.extraData);
+  }
+
+  if (action.type === 'removeProperties') {
+    for (const key of action.keysToRemove) {
+      delete state[key];
+    }
+  }
+
+  if (action.type === 'clear') {
+    for (const key of Object.keys(state)) {
+      delete state[key];
+    }
+  }
+}
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const obj = { ...state };
+  const result = [];
+
+  for (const action of actions) {
+    transform(obj, action);
+    result.push(Object.assign({}, obj));
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
