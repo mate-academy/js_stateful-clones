59e513c4834072ab8a46baa26c7dc3a332d44c84
olegnovikov10 <relaxinfh071@gmail.com>
2023-10-31 23:43:39 +0200

    done task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,39 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const res = [];
+  let newState = {
+    ...state,
+  };
+  const addProp = 'addProperties';
+  const removeProp = 'removeProperties';
+  const clear = 'clear';
+
+  for (const item of actions) {
+    if (item.type === addProp) {
+      newState = {
+        ...newState, ...item.extraData,
+      };
+    }
+
+    if (item.type === removeProp) {
+      for (const keys of item.keysToRemove) {
+        delete newState[keys];
+      }
+    }
+
+    if (item.type === clear) {
+      for (const keys in newState) {
+        delete newState[keys];
+      }
+    }
+
+    res.push({
+      ...newState,
+    });
+  }
+
+  return res;
 }
 
 module.exports = transformStateWithClones;
