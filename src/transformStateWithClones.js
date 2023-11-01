553ab5c2c9c6f7ef90c70410c6ecff63e44f8496
Oksana Vasinkevych <kicjunka@gmail.com>
2023-11-01 18:21:32 +0100

    work

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = Object.assign({}, state);
+
+  for (let i = 0; i < actions.length; i++) {
+    const newState = Object.assign({}, result[result.length - 1]);
+
+    if (actions[i].type === 'addProperties') {
+      Object.assign(newState, actions[i].extraData);
+    }
+
+    if (actions[i].type === 'removeProperties') {
+      for (const keyRemove of actions[i].keysToRemove) {
+        delete newState[keyRemove];
+      }
+    }
+
+    if (actions[i].type === 'clear') {
+      for (const key in newState) {
+        delete newState[key];
+      }
+    }
+    result.push(newState);
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
