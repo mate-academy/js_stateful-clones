6d84e7a1dfe93072107bdbc4322cfa60296fedce
Mykhailo Hubko <mixelios@gmail.com>
2023-11-01 17:19:52 +0100

    stateful clone task complete

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,35 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const copyOfState = Object.assign({}, state);
+
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'removeProperties') {
+      for (const key in copyOfState) {
+        for (const key1 in actions[i].keysToRemove) {
+          if (key === actions[i].keysToRemove[key1]) {
+            delete copyOfState[key];
+          }
+        }
+      }
+      result.push(Object.assign({}, copyOfState));
+    }
+
+    if (actions[i].type === 'addProperties') {
+      Object.assign(copyOfState, actions[i].extraData);
+      result.push(Object.assign({}, copyOfState));
+    }
+
+    if (actions[i].type === 'clear') {
+      for (const key in copyOfState) {
+        delete copyOfState[key];
+      }
+      result.push(Object.assign({}, copyOfState));
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
