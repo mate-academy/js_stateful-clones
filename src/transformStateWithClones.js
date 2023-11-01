996bd45f0e0509095f08747708a75c0a0f36ac29
Monika Osowiecka <mosowiecka@users.noreply.github.com>
2023-11-01 18:07:43 +0100

    Added switch and changed result.push

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,50 +6,31 @@
  *
  * @return {Object[]}
  */
-function deepCopy(obj) {
-  if (obj === null) {
-    return null;
-  }
-
-  if (typeof obj !== 'object') {
-    return obj;
-  }
-
-  const copy = {};
-
-  for (const key in obj) {
-    copy[key] = deepCopy(obj[key]);
-  }
-
-  return copy;
-}
-
 function transformStateWithClones(state, actions) {
   // write code here
   const result = [];
-  const state2 = deepCopy(state);
+  const stateCopy = { ...state };
 
   for (const a of actions) {
-    if (a.type === 'addProperties') {
-      for (const d in a.extraData) {
-        state2[d] = a.extraData[d];
-      }
-      result.push(deepCopy(state2));
-    }
+    switch (a.type) {
+      case 'addProperties':
+        for (const d in a.extraData) {
+          stateCopy[d] = a.extraData[d];
+        }
+        break;
 
-    if (a.type === 'removeProperties') {
-      for (const d of a.keysToRemove) {
-        delete state2[d];
-      }
-      result.push(deepCopy(state2));
-    }
+      case 'removeProperties':
+        for (const d of a.keysToRemove) {
+          delete stateCopy[d];
+        }
+        break;
 
-    if (a.type === 'clear') {
-      for (const d in state2) {
-        delete state2[d];
-      }
-      result.push(deepCopy(state2));
+      case 'clear':
+        for (const d in stateCopy) {
+          delete stateCopy[d];
+        }
     }
+    result.push({ ...stateCopy });
   }
 
   return result;
