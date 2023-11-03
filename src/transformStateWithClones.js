07411f30111c05341b49e0b5457c37feec36138a
VolodimirKurhan <aeroafuss@mail.com>
2023-11-03 18:13:20 +0200

    Sol

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,37 @@
 'use strict';
 
-/**
- * @param {Object} state
- * @param {Object[]} actions
- *
- * @return {Object[]}
- */
-function transformStateWithClones(state, actions) {
-  // write code here
-}
+// /**
+//  * @param {Object} state
+//  * @param {Object[]} actions
+//  *
+//  * @return {Object[]}
+//  */
 
+function transformStateWithClones(state, actions) {
+  const newState = [];
+  let cloneState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(cloneState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete cloneState[key];
+        }
+
+        break;
+
+      case 'clear':
+        cloneState = {};
+        break;
+    }
+
+    newState.push({ ...cloneState });
+  }
+
+  return newState;
+}
 module.exports = transformStateWithClones;
