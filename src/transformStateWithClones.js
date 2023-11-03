0958125ca4df600bdd00024b53b290a6765e05f8
Intellium <lomaleks@gmail.com>
2023-11-03 18:40:52 +0200

    Initial Commit

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,38 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const obj = { ...state };
+  const result = [];
+
+  for (const action of actions) {
+    // якщо key.type === 'addProperties'
+    if (action.type === 'addProperties') {
+      // додаємо name: 'Jim' до state
+      Object.assign(obj, action.extraData);
+
+      result.push({ ...obj });
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        // видаляє на кожній ітерації циклу key зі state, якщо такий ключ є
+        delete obj[key];
+      }
+
+      result.push({ ...obj });
+    }
+
+    if (action.type === 'clear') {
+      // потрібно циклом пройтись по state
+      for (const key in obj) {
+        delete obj[key];
+      }
+
+      result.push({});
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
