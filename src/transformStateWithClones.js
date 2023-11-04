ea7352697d8b439e2dd14a97a93b823896379d1b
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2023-11-04 02:14:40 +0200

    feat [js-adv]: implement object`s state transformer

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const ADD_ACTION = 'addProperties';
+const REMOVE_ACTION = 'removeProperties';
+const CLEAR_ACTION = 'clear';
+
 /**
  * @param {Object} state
  * @param {Object[]} actions
@@ -7,7 +11,27 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let temp = { ...state };
+  const states = [];
+
+  actions.forEach(action => {
+    if (action.type === ADD_ACTION) {
+      temp = Object.assign({}, temp, action.extraData);
+    }
+
+    if (action.type === REMOVE_ACTION) {
+      temp = Object.fromEntries(Object.entries(temp)
+        .filter(entry => !action.keysToRemove.includes(entry[0])));
+    }
+
+    if (action.type === CLEAR_ACTION) {
+      temp = {};
+    }
+
+    states.push(temp);
+  });
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
