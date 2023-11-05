1d466c1e9836e40f49d582a1eb987410da54be4f
dima91020 <vereskodima76@gmail.com>
2023-11-05 22:44:17 +0200

    first commit transform state with clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const clonesArray = [];
+  let cloneState = { ...state };
+
+  actions.forEach(action => {
+    let currentClone = { ...cloneState };
+
+    if (action.type === 'addProperties') {
+      const { extraData } = action;
+
+      Object.assign(currentClone, extraData);
+    } else if (action.type === 'removeProperties') {
+      const { keysToRemove } = action;
+
+      keysToRemove.forEach(key => {
+        delete currentClone[key];
+      });
+    } else if (action.type === 'clear') {
+      for (const key in currentClone) {
+        delete currentClone[key];
+      }
+      currentClone = {};
+    }
+
+    clonesArray.push(currentClone);
+    cloneState = { ...currentClone };
+  });
+
+  return clonesArray;
 }
 
 module.exports = transformStateWithClones;
