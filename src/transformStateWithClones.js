3ebcd669fee1c6cc91dc9071a33fc2d52213e5bc
Mykola Kharin <kharinapc@gmail.com>
2023-11-06 23:58:05 +0000

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const transformState = [];
+  const stateCopy = { ...state };
+
+  for (const act of actions) {
+    if (act.type === 'addProperties') {
+      Object.assign(stateCopy, act.extraData);
+    }
+
+    if (act.type === 'removeProperties') {
+      for (const key of act.keysToRemove) {
+        delete stateCopy[key];
+      }
+    }
+
+    if (act.type === 'clear') {
+      for (const key in stateCopy) {
+        delete stateCopy[key];
+      }
+    }
+    transformState.push({ ...stateCopy });
+  }
+
+  return transformState;
 }
 
 module.exports = transformStateWithClones;
