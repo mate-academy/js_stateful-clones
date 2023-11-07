4d0f171e36b1ccfa599672a770d218a5d28444af
Nazar Dudnyk <dudnyk_ak17@nuwm.edu.ua>
2023-11-07 12:26:28 +0200

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -9,24 +9,28 @@
 function transformStateWithClones(state, actions) {
   const copyState = { ...state };
   const result = [];
+  const AD_PROP = 'addProperties';
+  const REM_PROP = 'removeProperties';
 
   for (const action of actions) {
-    switch (action.type) {
-      case 'addProperties':
-        for (const key in action.extraData) {
-          copyState[key] = action.extraData[key];
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
+      case AD_PROP:
+        for (const data in extraData) {
+          copyState[data] = extraData[data];
         }
         break;
 
-      case 'removeProperties':
-        for (const key of action.keysToRemove) {
+      case REM_PROP:
+        for (const key of keysToRemove) {
           delete copyState[key];
         }
         break;
 
       case 'clear':
-        for (const key in copyState) {
-          delete copyState[key];
+        for (const property in copyState) {
+          delete copyState[property];
         }
         break;
     }
