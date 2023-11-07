5435480ad952ab64480a25e8e8e9898e3546b626
Mykola Kharin <kharinapc@gmail.com>
2023-11-07 16:39:43 +0000

    add improvements

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -10,14 +10,14 @@ function transformStateWithClones(state, actions) {
   const transformState = [];
   let stateCopy = { ...state };
 
-  for (const act of actions) {
-    switch (act.type) {
+  for (const action of actions) {
+    switch (action.type) {
       case 'addProperties':
-        Object.assign(stateCopy, act.extraData);
+        Object.assign(stateCopy, action.extraData);
         break;
 
       case 'removeProperties':
-        for (const key of act.keysToRemove) {
+        for (const key of action.keysToRemove) {
           delete stateCopy[key];
         }
         break;
@@ -31,31 +31,5 @@ function transformStateWithClones(state, actions) {
 
   return transformState;
 }
-
-// function transformStateWithClones(state, actions) {
-//   const transformState = [];
-//   const stateCopy = { ...state };
-
-//   for (const act of actions) {
-//     if (act.type === 'addProperties') {
-//       Object.assign(stateCopy, act.extraData);
-//     }
-
-//     if (act.type === 'removeProperties') {
-//       for (const key of act.keysToRemove) {
-//         delete stateCopy[key];
-//       }
-//     }
-
-//     if (act.type === 'clear') {
-//       for (const key in stateCopy) {
-//         delete stateCopy[key];
-//       }
-//     }
-//     transformState.push({ ...stateCopy });
-//   }
-
-//   return transformState;
-// }
 
 module.exports = transformStateWithClones;
