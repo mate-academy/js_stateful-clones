92191c671462a18b98983677ac3b48be9f0a3699
Anastasia Len <anastasiia.len11@gmail.com>
2023-11-11 12:41:54 -0500

    add implementation

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const STATE_HISTORY = [];
+
+  let currentState = { ...state };
+
+  actions.forEach((element) => {
+    switch (element.type) {
+      case 'addProperties':
+        currentState = {
+          ...currentState, ...element.extraData,
+        };
+        break;
+      case 'removeProperties':
+        element.keysToRemove.forEach(key => delete currentState[key]);
+        break;
+      case 'clear':
+        currentState = {};
+        break;
+      default:
+        // eslint-disable-next-line no-console
+        console.error('ERROR');
+    }
+
+    STATE_HISTORY.push({ ...currentState });
+  });
+
+  return STATE_HISTORY;
 }
 
 module.exports = transformStateWithClones;
