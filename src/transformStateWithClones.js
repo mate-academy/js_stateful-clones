eb9070d40e3da3d2696e781b326587f94086c157
Paweł Nowicki <pawel.nowicki87@tlen.pl>
2023-11-12 18:26:13 +0100

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
-}
+  const result = [];
+  let currentState = { ...state };
 
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      currentState = {
+        ...currentState, ...action.extraData,
+      };
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const keyToRemove of action.keysToRemove) {
+        if (currentState.hasOwnProperty(keyToRemove)) {
+          delete currentState[keyToRemove];
+        }
+      }
+    }
+
+    if (action.type === 'clear') {
+      currentState = {};
+    }
+
+    result.push({ ...currentState });
+  }
+
+  return result;
+}
 module.exports = transformStateWithClones;
