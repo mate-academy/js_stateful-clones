d2b7a4bbb7415beba6e83855bb3aa262fb9172bc
Viktor Kyrychok <viktor.kirichok@gmail.com>
2023-11-13 18:08:14 +0200

    task finished

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,39 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let actionToSave = {};
+  const stateActionsArray = [];
+
+  if (Object.keys(state).length !== 0) {
+    Object.assign(actionToSave, state);
+  }
+
+  for (const action of actions) {
+    for (const value of Object.values(action)) {
+      if (value === 'addProperties') {
+        Object.assign(actionToSave, action.extraData);
+        stateActionsArray.push(Object.assign({}, actionToSave));
+      }
+
+      if (value === 'removeProperties') {
+        for (const key of action.keysToRemove) {
+          delete actionToSave[key];
+        }
+        stateActionsArray.push(Object.assign({}, actionToSave));
+      }
+
+      if (value === 'clear') {
+        stateActionsArray.push({});
+        actionToSave = {};
+      }
+    }
+  }
+
+  if (stateActionsArray.length === 0) {
+    stateActionsArray.push({});
+  }
+
+  return stateActionsArray;
 }
 
 module.exports = transformStateWithClones;
