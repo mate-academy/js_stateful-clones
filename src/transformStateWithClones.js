7be43839861a203403322e3ddb9274a2bcc12076
VladSlob <141657806+VladSlob@users.noreply.github.com>
2023-11-15 08:03:54 +0200

    transformStateWithClones.js

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,28 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const states = [];
+  let stateCopy = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        stateCopy = { ...stateCopy, ...action.extraData };
+        break;
+      case 'removeProperties':
+        stateCopy = { ...stateCopy };
+        action.keysToRemove.forEach(key => delete stateCopy[key]);
+        break;
+      case 'clear':
+        stateCopy = {};
+        break;
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
+    }
+    states.push(stateCopy);
+  }
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
