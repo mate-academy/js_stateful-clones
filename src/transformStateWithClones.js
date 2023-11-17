f8b34ab7e45a60a7a27c699e8f993b3b837174f0
Vitaliy Labunskyi <vitold85@gmail.com>
2023-11-17 16:17:02 +0200

    start

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,7 +8,7 @@
  */
 function transformStateWithClones(state, actions) {
   const result = [];
-  const stateCopy = { ...state };
+  let stateCopy = { ...state };
 
   for (const action of actions) {
     switch (action.type) {
@@ -16,26 +16,18 @@ function transformStateWithClones(state, actions) {
         Object.assign(stateCopy, action.extraData);
         break;
 
-      case 'removeProperties': {
+      case 'removeProperties':
         for (const key of action.keysToRemove) {
-          if (key in stateCopy) {
-            delete stateCopy[key];
-          }
-        }
-
-        break;
-      }
-
-      case 'clear': {
-        for (const key of Object.keys(state)) {
           delete stateCopy[key];
         }
-
         break;
-      }
+
+      case 'clear':
+        stateCopy = {};
+        break;
     }
 
-    result.push(stateCopy);
+    result.push({ ...stateCopy });
   }
 
   return result;
