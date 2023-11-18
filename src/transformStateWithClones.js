70f4824aff736d0a6a1fe46fc61be7711352d09d
Oleksii Karpov <alneon1@gmail.com>
2023-11-18 22:55:21 +0200

    some improvments added

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,34 +1,26 @@
 'use strict';
 
-/**
- * @param {Object} state
- * @param {Object[]} actions
- *
- * @return {Object[]}
- */
 function transformStateWithClones(state, actions) {
   const newActions = [];
+  let nextState = { ...state };
 
   for (let i = 0; i < actions.length; i++) {
-    const nextState = {};
+    switch (actions[i].type) {
+      case 'addProperties':
+        Object.assign(nextState, actions[i].extraData);
+        break;
 
-    if (i > 0) {
-      Object.assign(nextState, newActions[i - 1]);
-    } else {
-      Object.assign(nextState, state);
+      case 'removeProperties':
+        for (let j = 0; j < actions[i].keysToRemove.length; j++) {
+          delete nextState[actions[i].keysToRemove[j]];
+        }
+        break;
+
+      default:
+        nextState = {};
     }
 
-    if (actions[i].type === 'addProperties') {
-      Object.assign(nextState, actions[i].extraData);
-    } else if (actions[i].type === 'removeProperties') {
-      for (let j = 0; j < actions[i].keysToRemove.length; j++) {
-        delete nextState[actions[i].keysToRemove[j]];
-      }
-    } else {
-      Object.keys(nextState).forEach(key => delete nextState[key]);
-    }
-
-    newActions[i] = nextState;
+    newActions.push({ ...nextState });
   }
 
   return newActions;
