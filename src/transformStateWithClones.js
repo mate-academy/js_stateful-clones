657411882fdf4c3507baf6c8f1295263df0aecb1
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-11-23 21:02:16 +0200

    Revert "newState"
    This reverts commit 4a1b86afdab79dde0428f3dbbbdfcf8ccfae8e18.

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,33 +7,7 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  let clone = { ...state };
-  const arr = [];
-
-  for (const action of actions) {
-    switch (action.type) {
-      case 'addProperties':
-        Object.assign(clone, action.extraData);
-        break;
-
-      case 'removeProperties':
-        for (const key of action.keysToRemove) {
-          delete clone[key];
-        }
-        break;
-
-      case 'clear':
-        clone = {};
-        break;
-
-      default:
-        break;
-    }
-
-    arr.push({ ...clone });
-  }
-
-  return arr;
+  // write code here
 }
 
 module.exports = transformStateWithClones;
