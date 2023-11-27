688d2734ee810ebcd4e36b3d09c28e2fccc62dfa
Gennadiy Boreyko <gennadiyboreyko0@gmail.com>
2023-11-27 11:40:19 +0200

    replacement of the multiple if with the switch/case

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -14,24 +14,26 @@ function transformStateWithClones(state, actions) {
   for (let k = 0; k < actions.length; k++) {
     const action = actions[k];
 
-    if (action.type === 'addProperties') {
-      const addKeys = action.extraData;
+    switch (action.type) {
+      case 'addProperties':
+        const addKeys = action.extraData;
 
-      Object.assign(copyState, addKeys);
-    }
+        Object.assign(copyState, addKeys);
+        break;
 
-    if (action.type === 'removeProperties') {
-      const removeKeys = action.keysToRemove;
+      case 'removeProperties':
+        const removeKeys = action.keysToRemove;
 
-      for (let i = 0; i < removeKeys.length; i++) {
-        delete copyState[removeKeys[i]];
-      }
-    }
+        for (let i = 0; i < removeKeys.length; i++) {
+          delete copyState[removeKeys[i]];
+        }
+        break;
 
-    if (action.type === 'clear') {
-      for (const key in copyState) {
-        delete copyState[key];
-      }
+      case 'clear':
+        for (const key in copyState) {
+          delete copyState[key];
+        }
+        break;
     }
 
     arrayStates[k] = { ...copyState };
