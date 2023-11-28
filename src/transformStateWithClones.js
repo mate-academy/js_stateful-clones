dc85f76616c1a2cdb7474ba01c055b07b1cdc296
Oleksandra Kuziura <radioday2014@gmail.com>
2023-11-28 20:14:59 +0200

    do not work with multiple types

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
-}
+  const resultingArray = [];
+  const clone = Object.assign({}, state);
 
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(clone, action.extraData);
+
+      resultingArray.push(clone);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of Object.entries(action.keysToRemove)) {
+        delete clone[key[1]];
+      }
+
+      resultingArray.push(clone);
+    }
+
+    if (action.type === 'clear') {
+      for (const key in state) {
+        delete clone[key];
+      }
+
+      resultingArray.push(clone);
+    }
+  }
+
+  return resultingArray;
+}
 module.exports = transformStateWithClones;
