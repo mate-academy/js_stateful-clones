82715be857c7a0260e84ff007c00ff5438737343
Sergay Trytiak <trytik12@gmail.com>
2023-12-04 21:33:25 +0200

    add task solution 2

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,33 +6,65 @@
  *
  * @return {Object[]}
  */
+// function transformStateWithClones(state, actions) {
+//   const stateCopy = { ...state };
+
+//   const actionsResult = [];
+
+//   for (const ch of actions) {
+//     if (ch.type === 'addProperties') {
+//       Object.assign(stateCopy, ch.extraData);
+//       actionsResult.push(Object.assign({}, stateCopy));
+//     };
+
+//     if (ch.type === 'removeProperties') {
+//       for (const cn of ch.keysToRemove) {
+//         delete (stateCopy[cn]);
+//       };
+//       actionsResult.push(Object.assign({}, stateCopy));
+//     }
+
+//     if (ch.type === 'clear') {
+//       for (const cm in stateCopy) {
+//         delete (stateCopy[cm]);
+//       };
+//       actionsResult.push(Object.assign({}, stateCopy));
+//     };
+//   };
+
+//   return actionsResult;
+// }
+
 function transformStateWithClones(state, actions) {
-  const stateCopy = { ...state };
+  let stateCopy = { ...state };
 
   const actionsResult = [];
 
-  for (const ch of actions) {
-    if (ch.type === 'addProperties') {
-      Object.assign(stateCopy, ch.extraData);
-      actionsResult.push(Object.assign({}, stateCopy));
-    };
+  const add = 'addProperties';
+  const remove = 'removeProperties';
+  const clear = 'clear';
 
-    if (ch.type === 'removeProperties') {
-      for (const cn of ch.keysToRemove) {
-        delete (stateCopy[cn]);
-      };
-      actionsResult.push(Object.assign({}, stateCopy));
+  for (const action of actions) {
+    switch (action.type) {
+      case add:
+        Object.assign(stateCopy, action.extraData);
+        break;
+
+      case remove:
+        for (const cn of action.keysToRemove) {
+          delete stateCopy[cn];
+        };
+        break;
+
+      case clear:
+        stateCopy = {};
+        break;
     }
 
-    if (ch.type === 'clear') {
-      for (const cm in stateCopy) {
-        delete (stateCopy[cm]);
-      };
-      actionsResult.push(Object.assign({}, stateCopy));
-    };
+    actionsResult.push({ ...stateCopy });
   }
 
   return actionsResult;
-}
+};
 
 module.exports = transformStateWithClones;
