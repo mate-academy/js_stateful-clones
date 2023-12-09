fc3c8e9692f2351ad791162228cecb739f3d2c7d
Stanislav Kukharuk <kukharuk.stan1slav@gmail.com>
2023-12-09 15:13:38 +0000

    result

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,38 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  const result = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        // add all extraData props to the state
+        Object.assign(stateCopy, action.extraData);
+        break;
+
+      case 'removeProperties': {
+        // remove all keysToRemove from the state
+        for (const key of action.keysToRemove) {
+          delete stateCopy[key];
+        }
+
+        break;
+      }
+
+      case 'clear': {
+        // remove all state keys from it
+        for (const key of Object.keys(stateCopy)) {
+          delete stateCopy[key];
+        }
+
+        break;
+      }
+    }
+    result.push({ ...stateCopy });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
