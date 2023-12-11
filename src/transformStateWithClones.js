2f918c013091e4d8446af4f515109960d63fbf8b
Dmytro Manankin <dmitry.manankin@gmail.com>
2023-12-11 22:19:51 +0200

    task done, all test successful

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,40 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const stateCopy = { ...state };
+  const result = [];
+
+  for (const item of actions) {
+    switch (item.type) {
+      case 'addProperties':
+        for (const key in item.extraData) {
+          stateCopy[key] = item.extraData[key];
+        };
+        // пушимо через деструктурізацію, зберігаючи модифікований об'єкт
+        result.push({ ...stateCopy });
+        break;
+
+      case 'removeProperties':
+        for (const key of item.keysToRemove) {
+          delete stateCopy[key];
+        };
+        // пушимо через деструктурізацію, зберігаючи модифікований об'єкт
+        result.push({ ...stateCopy });
+        break;
+
+      case 'clear':
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
+        // пушимо через деструктурізацію
+        result.push({ ...stateCopy });
+        break;
+
+      default: break;
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
