c7920b2ebe360429a1e4edb4d15d625a3ffe1ba8
Yaroslav Samoilenko <yasik2255@live.com>
2023-12-12 13:17:07 +0200

    add solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,57 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const resultedArray = [];
+  let currentObj = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        resultedArray.push(addProperties(currentObj, action.extraData));
+        currentObj = { ...resultedArray[resultedArray.length - 1] };
+        break;
+      case 'removeProperties':
+        resultedArray.push(removeProperties(currentObj, action.keysToRemove));
+        currentObj = { ...resultedArray[resultedArray.length - 1] };
+        break;
+      case 'clear':
+        resultedArray.push(clearObj(currentObj));
+        currentObj = { ...resultedArray[resultedArray.length - 1] };
+        break;
+    }
+  }
+
+  return resultedArray;
+}
+
+function addProperties(obj, extraData) {
+  const newObj = { ...obj };
+
+  for (const key in extraData) {
+    newObj[key] = extraData[key];
+  }
+
+  return newObj;
+}
+
+function removeProperties(obj, keyList = []) {
+  const newObj = { ...obj };
+
+  for (const key of keyList) {
+    delete newObj[key];
+  }
+
+  return newObj;
+}
+
+function clearObj(obj) {
+  const newObj = { ...obj };
+
+  for (const key in obj) {
+    delete newObj[key];
+  }
+
+  return newObj;
 }
 
 module.exports = transformStateWithClones;
