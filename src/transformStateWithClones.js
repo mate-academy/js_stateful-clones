712c2fa3e3fa5bf8c3f71a019a12f460c8cc8618
Ruslan Voshchylo <ruslan.voshchylo2@gmail.com>
2023-12-12 22:01:09 +0200

    add tusk solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const newState = Object.assign({}, state);
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(newState, action.extraData);
+        result.push({ ...newState });
+        break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete newState[key];
+        }
+        result.push({ ...newState });
+        break;
+      case 'clear':
+        for (const keys in newState) {
+          delete newState[keys];
+        }
+        result.push({ ...newState });
+        break;
+      default:
+        break;
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
