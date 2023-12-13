49aebf8cc0010a961b0e3158d4f405f2354779e1
White EdenI <danil.gayvoronsky2201@gmail.com>
2023-12-14 01:44:29 +0200

    Finally Done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,56 @@
 'use strict';
 
 /**
- * @param {Object} state
- * @param {Object[]} actions
- *
- * @return {Object[]}
+ * Deep clone function to create a copy of an object.
+ * @param {Object} obj - The object to be cloned.
+ * @returns {Object} - The deep clone of the object.
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const resultArray = [simpleClone(state)];
+
+  for (const action of actions) {
+    let currentStateCopy = simpleClone(resultArray[resultArray.length - 1]);
+
+    switch (action.type) {
+      case 'addProperties':
+        currentStateCopy = {
+          ...currentStateCopy, ...action.extraData,
+        };
+        break;
+      case 'removeProperties':
+        action.keysToRemove.forEach(key => delete currentStateCopy[key]);
+        break;
+      case 'clear':
+        currentStateCopy = {};
+        break;
+      default:
+        break;
+    }
+
+    resultArray.push(currentStateCopy);
+  }
+
+  return resultArray.slice(1);
+}
+
+function simpleClone(obj) { // Странный костыль. Очень долго мучался
+  if (obj === null || typeof obj !== 'object') {
+    return obj;
+  }
+
+  if (Array.isArray(obj)) {
+    return obj.map(item => simpleClone(item));
+  }
+
+  const objCopy = {};
+
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      objCopy[key] = simpleClone(obj[key]);
+    }
+  }
+
+  return objCopy;
 }
 
 module.exports = transformStateWithClones;
