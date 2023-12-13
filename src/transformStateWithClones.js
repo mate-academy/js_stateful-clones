24e1c6821e5391626ac7f35cfebbac3c5cbceea1
Vadym Sainiuk <vadik.sajnuykk@gmail.com>
2023-12-13 19:01:32 +0200

    added default in switch

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -27,6 +27,9 @@ function transformStateWithClones(state, actions) {
           delete initialObject[key];
         }
         break;
+
+      default:
+        break;
     }
 
     result.push({ ...initialObject });
