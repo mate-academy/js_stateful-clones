6d564c37e50882b3417a4754da73c1f469aa39d5
Maksym Kalashnyk <maxplay123890@gmail.com>
2023-12-14 04:42:15 +0200

    add js object task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const initialStateClone = { ...state };
+
+  const clonedStates = [];
+
+  for (const action of actions) {
+    const currentStateClone = clonedStates.length > 0
+      ? { ...clonedStates[clonedStates.length - 1] }
+      : { ...initialStateClone };
+
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(currentStateClone, action.extraData);
+        break;
+      case 'removeProperties':
+        action.keysToRemove.forEach(key => delete currentStateClone[key]);
+        break;
+      case 'clear':
+        Object.keys(currentStateClone).forEach(key =>
+          delete currentStateClone[key]);
+        break;
+      default:
+    }
+
+    clonedStates.push(currentStateClone);
+  }
+
+  return clonedStates;
 }
 
 module.exports = transformStateWithClones;
