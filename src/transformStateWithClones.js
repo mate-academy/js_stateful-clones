16b5151b5a1736978b906e6d82be40cf82fda2c0
Nikita Ganzha <geekneek28@gmail.com>
2023-12-15 10:44:06 +0200

    create function transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,38 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  const resultArray = [];
+
+  for (let i = 0; i < actions.length; i++) {
+    switch (actions[i].type) {
+      case 'addProperties':
+        for (const key in actions[i].extraData) {
+          stateCopy[key] = actions[i].extraData[key];
+        }
+        resultArray.push({ ...stateCopy });
+        break;
+
+      case 'removeProperties':
+        for (let j = 0; j < actions[i].keysToRemove.length; j++) {
+          delete stateCopy[actions[i].keysToRemove[j]];
+        }
+        resultArray.push({ ...stateCopy });
+        break;
+
+      case 'clear':
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
+        resultArray.push({ ...stateCopy });
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  return resultArray;
 }
 
 module.exports = transformStateWithClones;
