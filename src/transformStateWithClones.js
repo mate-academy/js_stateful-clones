19694ff35774275685ab852ebe633a0469224401
Pavlo Chudin <maks.chudin567@gmail.com>
2023-12-18 19:02:37 +0200

    working function

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arr = [];
+  const copyState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(copyState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const remove of action.keysToRemove) {
+          delete copyState[remove];
+        }
+        break;
+
+      case 'clear':
+        Object.keys(copyState).forEach(key => delete copyState[key]);
+        break;
+    }
+
+    arr.push({ ...copyState });
+  }
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
