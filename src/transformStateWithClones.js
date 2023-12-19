d66c5f94b4e5cb4252dc7f595bf787b41f7f35b3
Tetyana Chuhai <chugaytatyana@gmail.&git config --global user.email chugaytatyana@gmail.>
2023-12-19 21:19:04 +0200

    make solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,40 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let stateClone = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        addProperties(stateClone, action.extraData);
+        break;
+
+      case 'removeProperties':
+        removeProperties(stateClone, action.keysToRemove);
+        break;
+
+      case 'clear':
+        stateClone = {};
+        break;
+
+      default:
+        break;
+    }
+    result.push({ ...stateClone });
+  }
+
+  return result;
+}
+
+function addProperties(stateClone, extraData) {
+  Object.assign(stateClone, extraData);
+}
+
+function removeProperties(stateClone, keysToRemove) {
+  for (const key of keysToRemove) {
+    delete stateClone[key];
+  }
 }
 
 module.exports = transformStateWithClones;
