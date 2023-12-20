16e4e39111743f112b5a16d8050a278d68a7ec28
LadyLiza <elizabet132010@gmail.com>
2023-12-20 14:43:08 +0100

    addressed comments

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -10,30 +10,30 @@ function transformStateWithClones(state, actions) {
   const result = [];
   let currentState = { ...state };
 
-  for (let i = 0; i < actions.length; i++) {
-    const action = actions[i];
+  for (const action of actions) {
     let newState = { ...currentState };
 
-    if (action.type === 'addProperties') {
-      for (const key in action.extraData) {
-        newState[key] = action.extraData[key];
-      }
-    }
+    switch (action.type) {
+      case 'addProperties':
+        for (const key in action.extraData) {
+          newState[key] = action.extraData[key];
+        }
+        break;
 
-    if (action.type === 'removeProperties') {
-      const keysToRemove = action.keysToRemove;
+      case 'removeProperties':
+        const keysToRemove = action.keysToRemove;
 
-      for (let j = 0; j < keysToRemove.length; j++) {
-        const key = keysToRemove[j];
-
-        if (!(newState[key] === undefined)) {
+        for (const key of keysToRemove) {
           delete newState[key];
         }
-      }
-    }
+        break;
 
-    if (action.type === 'clear') {
-      newState = {};
+      case 'clear':
+        newState = {};
+        break;
+
+      default:
+        break;
     }
 
     result.push(newState);
