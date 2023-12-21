a6532796f040453fe9683836d57aae9153e0841b
Hlib Kaponkin <glebkapenkin@gmail.com>
2023-12-21 19:12:35 +0200

    fixed

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,33 +6,6 @@
  *
  * @return {Object[]}
  */
-// function transformStateWithClones(state, actions) {
-//   const array = [];
-//   const cloneState = Object.assign({}, state);
-
-//   for (const action of actions) {
-//     if (action.type === 'addProperties') {
-//       Object.assign(cloneState, action.extraData);
-//     }
-
-//     if (action.type === 'removeProperties') {
-//       for (const key of action.keysToRemove) {
-//         delete cloneState[key];
-//       }
-//     }
-
-//     if (action.type === 'clear') {
-//       for (const key in cloneState) {
-//         delete cloneState[key];
-//       }
-//     }
-//     array.push(Object.assign({}, cloneState));
-//   }
-
-//   return array;
-// }
-
-module.exports = transformStateWithClones;
 
 function transformStateWithClones(state, actions) {
   const array = [];
