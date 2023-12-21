3e4084230d0564a9fbf7ade2dcad8420001125c4
Hlib Kaponkin <glebkapenkin@gmail.com>
2023-12-21 19:10:36 +0200

    fixed

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,25 +6,52 @@
  *
  * @return {Object[]}
  */
+// function transformStateWithClones(state, actions) {
+//   const array = [];
+//   const cloneState = Object.assign({}, state);
+
+//   for (const action of actions) {
+//     if (action.type === 'addProperties') {
+//       Object.assign(cloneState, action.extraData);
+//     }
+
+//     if (action.type === 'removeProperties') {
+//       for (const key of action.keysToRemove) {
+//         delete cloneState[key];
+//       }
+//     }
+
+//     if (action.type === 'clear') {
+//       for (const key in cloneState) {
+//         delete cloneState[key];
+//       }
+//     }
+//     array.push(Object.assign({}, cloneState));
+//   }
+
+//   return array;
+// }
+
+module.exports = transformStateWithClones;
+
 function transformStateWithClones(state, actions) {
   const array = [];
   const cloneState = Object.assign({}, state);
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(cloneState, action.extraData);
-    }
-
-    if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(cloneState, action.extraData);
+        break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete cloneState[key];
+        };
+        break;
+      case 'clear': for (const key in cloneState) {
         delete cloneState[key];
       }
-    }
-
-    if (action.type === 'clear') {
-      for (const key in cloneState) {
-        delete cloneState[key];
-      }
+        break;
     }
     array.push(Object.assign({}, cloneState));
   }
