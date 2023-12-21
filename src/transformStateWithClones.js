1ad4964ccbf59845d4a95a35347453e1f0898696
Zadrayca <coxatiy@gmail.com>
2023-12-21 19:04:01 +0200

    changes_1

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -9,38 +9,32 @@
 function transformStateWithClones(state, actions) {
   // write code here
 
-  const arrResolt = [];
-
-  let counter = 0;
+  const arrResult = [];
+  const stateCopy = ({ ...state });
 
   for (const action of actions) {
-    const { type, extraData = undefined, keysToRemove = undefined } = action;
-
-    if (arrResolt.length === 0) {
-      arrResolt.push({ ...state });
-    } else {
-      arrResolt.push({ ...arrResolt[counter - 1] });
-    }
+    const { type, extraData, keysToRemove } = action;
 
     switch (type) {
       case 'addProperties':
-        Object.assign(arrResolt[counter], extraData);
+        Object.assign(stateCopy, extraData);
         break;
       case 'removeProperties':
         for (const key of keysToRemove) {
-          delete arrResolt[counter][key];
+          delete stateCopy[key];
         }
         break;
       case 'clear':
-        for (const key in arrResolt[counter]) {
-          delete arrResolt[counter][key];
+        for (const key of Object.keys(stateCopy)) {
+          delete stateCopy[key];
         }
         break;
     }
-    counter += 1;
+
+    arrResult.push({ ...stateCopy });
   }
 
-  return arrResolt;
+  return arrResult;
 }
 
 module.exports = transformStateWithClones;
