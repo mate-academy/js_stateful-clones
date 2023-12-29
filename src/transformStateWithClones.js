48db2c12ad4869efea141fbd363a3734061e3c54
dmitruz <dmitruz970@gmail.com>
2023-12-29 20:45:29 +0200

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,40 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = JSON.parse(JSON.stringify(state));
+  const result = [stateCopy];
+
+  for (const action of actions) {
+    // Creating new copy for each iteration
+    const newState = JSON.parse(JSON.stringify(result[result.length - 1]));
+
+    switch (action.type) {
+      case 'addProperties': {
+        const { extraData } = action;
+
+        Object.assign(newState, extraData);
+        break;
+      }
+
+      case 'removeProperties': {
+        const { keysToRemove } = action;
+
+        keysToRemove.forEach(key => delete newState[key]);
+        break;
+      }
+      case 'clear':
+        // next create new epty state
+        Object.keys(newState).forEach(key => delete newState[key]);
+        break;
+      default:
+        // handle an error
+        throw new Error(`Invalid action type: ${action.type}`);
+    }
+    result.push(newState);
+  }
+  result.shift();
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
