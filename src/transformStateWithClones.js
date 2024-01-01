b93fb25648a955296b94f03c4a256cc4df890799
Oleh Duma <hakerprof@gmail.com>
2024-01-01 20:44:35 +0200

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,41 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newState = { ...state };
+  const resultArr = [];
+
+  for (let i = 0; i < actions.length; i++) {
+    switch (actions[i].type) {
+      case 'addProperties': {
+        Object.assign(newState, actions[i].extraData);
+        break;
+      }
+
+      case 'removeProperties': {
+        for (let j = 0; j < actions[i].keysToRemove.length; j++) {
+          if (newState[actions[i].keysToRemove[j]]) {
+            delete newState[actions[i].keysToRemove[j]];
+          }
+        }
+        break;
+      }
+
+      case 'clear': {
+        for (const key of Object.keys(newState)) {
+          delete newState[key];
+        }
+        break;
+      }
+
+      default: {
+        throw new Error(`Unhandled action type: ${actions[i].type}`);
+      }
+    }
+
+    resultArr.push({ ...newState }); // Pushing a clone of the modified state
+  }
+
+  return resultArr;
 }
 
 module.exports = transformStateWithClones;
