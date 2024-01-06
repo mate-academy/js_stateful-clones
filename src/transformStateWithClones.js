44856b12c814a34547bccee42d367bca05a66ff3
Vitalii Kredinets <kredinec.v@gmail.com>
2024-01-06 21:08:18 +0200

    solve task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,35 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      const newState = result.length
+        ? { ...result[result.length - 1] } : { ...state };
+
+      for (const key in action.extraData) {
+        newState[key] = action.extraData[key];
+      }
+      result.push(newState);
+    }
+
+    if (action.type === 'removeProperties') {
+      const newState = result.length
+        ? { ...result[result.length - 1] } : { ...state };
+
+      for (const key of action.keysToRemove) {
+        delete newState[key];
+      }
+      result.push(newState);
+    }
+
+    if (action.type === 'clear') {
+      result.push({});
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
