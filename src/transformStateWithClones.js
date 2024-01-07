710aa259270f9f6cb78a09010a4f8976cd5b6ea1
tarashulak <ajax14imagine@gmail.com>
2024-01-07 21:43:35 +0000

    task done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,40 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const resultArray = [];
+  const obj = cloneObj(state);
+
+  for (const item in actions) {
+    switch (actions[item].type) {
+      case 'addProperties':
+        for (const key in actions[item].extraData) {
+          obj[key] = actions[item].extraData[key];
+        }
+        resultArray.push({ ...obj });
+        break;
+
+      case 'removeProperties':
+        for (const key in actions[item].keysToRemove) {
+          delete obj[actions[item].keysToRemove[key]];
+        }
+
+        resultArray.push({ ...obj });
+        break;
+
+      case 'clear':
+        for (const key in obj) {
+          delete obj[key];
+        }
+        resultArray.push({ ...obj });
+        break;
+    }
+  }
+
+  return resultArray;
+}
+
+function cloneObj(obj) {
+  return { ...obj };
 }
 
 module.exports = transformStateWithClones;
