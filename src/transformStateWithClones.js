9cbbcc3c5453d109e2e0c283cc586f107ce7b1aa
beliyandr <beliyandr@gmail.com>
2024-01-08 17:05:52 +0200

    1st mod

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,52 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+
+  function doAction(type, action) {
+    switch (type) {
+      case 'addProperties':
+        console.log('addProperties');
+        addProperties(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        console.log('removeProperties');
+        break;
+
+      case 'clear':
+        console.log('clear');
+        break;
+    }
+
+    return;
+  }
+
+  function addProperties(state, data) {
+    Object.assign(state, data);
+    return;
+  }
+
+
+
+  for (const action of actions) {
+    doAction(action.type, action);
+    console.log(state);
+  }
+
 }
+
+const state1 = {
+  foo: 'bar', bar: 'foo',
+};
+const type = [
+  {
+    type: 'addProperties',
+    extraData: {
+      foo: 'new', hello: 'world',
+    },
+  },
+];
+
+transformStateWithClones(state1, type);
 
 module.exports = transformStateWithClones;
