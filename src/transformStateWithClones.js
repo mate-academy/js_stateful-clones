1be6db9455a295081b9546c96970233111471ec7
kkrasnoshchok <krasnoshchok4work@gmail.com>
2024-01-10 18:09:05 +0100

    feat: provides with solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -3,11 +3,36 @@
 /**
  * @param {Object} state
  * @param {Object[]} actions
- *
- * @return {Object[]}
+ * @returns {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateClones = [];
+  let stateCopy = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        stateClones.push({ ...stateCopy });
+        break;
+      case 'removeProperties':
+        const filteredEntries = Object.entries(stateCopy).filter(
+          ([key]) => !action.keysToRemove.includes(key)
+        );
+
+        stateCopy = Object.fromEntries(filteredEntries);
+        stateClones.push({ ...stateCopy });
+        break;
+      case 'clear':
+        stateCopy = {};
+        stateClones.push({ ...stateCopy });
+        break;
+      default:
+        break;
+    }
+  }
+
+  return stateClones;
 }
 
 module.exports = transformStateWithClones;
