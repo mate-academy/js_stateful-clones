7450cc3e0f118a92c9d012e5edb3948a67c73698
avakiel <79792776+avakiel@users.noreply.github.com>
2024-01-13 15:16:37 +0200

    some fixes

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+/* eslint-disable no-lone-blocks */
 'use strict';
 
 /**
@@ -7,22 +9,57 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const result = [];
-  let stateCopy = { ...state };
+  const ADD_PROPERTIES = 'addProperties';
+  const CLEAR = 'clear';
+  const REMOVE_PROPERTIES = 'removeProperties';
 
-  for (let i = 0; i < actions.length; i++) {
-    if (actions[i].type === 'clear') {
-      stateCopy = {};
-      result.push({});
-    } else if (actions[i].type === 'addProperties') {
-      stateCopy = {
-        ...stateCopy,
-        ...actions[i].extraData,
-      };
-      result.push({ ...stateCopy });
-    } else if (actions[i].type === 'removeProperties') {
-      actions[i].keysToRemove.forEach(e => delete stateCopy[e]);
-      result.push({ ...stateCopy });
+  const result = [];
+  const stateClone = { ...state };
+
+  for (const actionObject of actions) {
+    switch (actionObject.type) {
+      case ADD_PROPERTIES:
+        {
+          const entries = Object.entries(actionObject.extraData);
+
+          for (const entrie of entries) {
+            Object.defineProperty(stateClone, entrie[0], {
+              value: entrie[1],
+              writable: true,
+              enumerable: true,
+              configurable: true,
+            });
+          }
+          result.push({ ...stateClone });
+        }
+        break;
+
+      case CLEAR:
+        {
+          const keysOfStateClone = Object.keys(stateClone);
+
+          for (const key of keysOfStateClone) {
+            if (Object.hasOwn(stateClone, key)) {
+              delete stateClone[key];
+            }
+          }
+          result.push({ ...stateClone });
+        }
+        break;
+
+      case REMOVE_PROPERTIES:
+        {
+          for (const keyToRemove of actionObject.keysToRemove) {
+            if (Object.hasOwn(stateClone, keyToRemove)) {
+              delete stateClone[keyToRemove];
+            }
+          }
+          result.push({ ...stateClone });
+        }
+        break;
+
+      default:
+        result.push({ ...stateClone });
     }
   }
 
