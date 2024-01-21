1e25334ccc5beca0609c0fa4ed21c66c6f19db50
Ivan <lavrinenkoivan51@gmail.com>
2024-01-21 22:32:22 +0100

    created transformStateWithClones function

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,46 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const states = [state];
+
+  function addProperties(st, extraData) {
+    return {
+      ...st,
+      ...extraData,
+    };
+  }
+
+  function removeProperties(st, keysToRemove) {
+    const newState = { ...st }; // Change from state to st
+
+    keysToRemove.forEach(key => delete newState[key]);
+
+    return newState;
+  }
+
+  actions.forEach(action => {
+    let clonedState = states[states.length - 1];
+
+    switch (action.type) {
+      case 'addProperties':
+        clonedState = addProperties(clonedState, action.extraData);
+        break;
+      case 'removeProperties':
+        clonedState = removeProperties(clonedState, action.keysToRemove);
+        break;
+      case 'clear':
+        clonedState = {};
+        break;
+      default:
+        break;
+    }
+
+    states.push(clonedState);
+  });
+
+  states.shift();
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
