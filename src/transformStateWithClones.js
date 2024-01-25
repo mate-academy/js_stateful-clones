dac8a35d25b2ab9bc430a772e3c92eb3664934e7
Oleksandr <alex.volkovskyi@gmail.com>
2024-01-25 10:15:34 +0200

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,44 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        if (stateHistory.length === 0) {
+          stateHistory.push(Object.assign({}, state, action.extraData));
+        } else {
+          stateHistory.push(Object.assign(
+            {}, stateHistory[stateHistory.length - 1], action.extraData)
+          );
+        }
+        break;
+
+      case 'removeProperties':
+        if (stateHistory.length === 0) {
+          stateHistory.push({ ...state });
+        } else {
+          stateHistory.push({ ...stateHistory[stateHistory.length - 1] });
+        }
+
+        for (const key of action.keysToRemove) {
+          if (stateHistory[stateHistory.length - 1].hasOwnProperty(key)) {
+            delete stateHistory[stateHistory.length - 1][key];
+          }
+        }
+        break;
+
+      case 'clear':
+        stateHistory.push({});
+        break;
+
+      default:
+        throw new Error('Type not supported');
+    }
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
