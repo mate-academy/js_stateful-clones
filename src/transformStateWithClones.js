8ea089e0f0a7a3da82bb7be980b5505d225c6672
dmkopylets <dm.kopylets@gmail.com>
2024-01-27 17:05:41 +0200

    task accomplished

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,41 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+
+  for (const action of actions) {
+    let newState;
+
+    if (action.type === 'clear') {
+      newState = {};
+    } else {
+      const lastState = stateHistory[stateHistory.length - 1];
+
+      newState = JSON.parse(JSON.stringify(lastState || state));
+    }
+
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(newState, action.extraData);
+        break;
+      case 'removeProperties':
+        action.keysToRemove.forEach(key => {
+          if (newState.hasOwnProperty(key)) {
+            delete newState[key];
+          }
+        });
+        break;
+      case 'clear':
+        for (const key in newState) {
+          delete newState[key];
+        }
+        break;
+    }
+
+    stateHistory.push(newState);
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
