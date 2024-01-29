3a47a28f73bac8038a2195328b393a64c75e0525
dmytrochernega <dimadneprfdb@gmail.coma>
2024-01-29 19:31:00 +0200

    Rewritten to switch/case

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -10,28 +10,46 @@ function transformState(state, actions) {
   let stateClone = { ...state };
   const result = [];
 
-  for (const a of actions) {
-    if (a.type === 'addProperties') {
-      for (const key in a.extraData) {
-        stateClone[key] = a.extraData[key];
-      }
-      result.push({ ...stateClone });
-    }
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateClone, action.extraData);
+        break;
 
-    if (a.type === 'removeProperties') {
-      for (const b of a.keysToRemove) {
-        delete stateClone[b];
-      }
-      result.push({ ...stateClone });
-    }
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete stateClone[key];
+        }
+        break;
 
-    if (a.type === 'clear') {
-      stateClone = {};
-      result.push({ ...stateClone });
+      case 'clear':
+        stateClone = {};
     }
+    result.push({ ...stateClone });
   }
 
   return result;
 }
 
 module.exports = transformState;
+
+// for (const a of actions) {
+//   if (a.type === 'addProperties') {
+//     Object.assign(stateClone, a.extraData);
+//     result.push({ ...stateClone });
+//   }
+
+//   if (a.type === 'removeProperties') {
+//     for (const b of a.keysToRemove) {
+//       delete stateClone[b];
+//     }
+//     result.push({ ...stateClone });
+//   }
+
+//   if (a.type === 'clear') {
+//     stateClone = {};
+//     result.push({ ...stateClone });
+//   }
+// }
+
+// return result;
