fdbe595c38cc0dfd40f501eb4241c1005b1e56cc
dmytrochernega <dimadneprfdb@gmail.coma>
2024-01-30 18:01:34 +0200

    delete comments

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -32,24 +32,3 @@ function transformState(state, actions) {
 }
 
 module.exports = transformState;
-
-// for (const a of actions) {
-//   if (a.type === 'addProperties') {
-//     Object.assign(stateClone, a.extraData);
-//     result.push({ ...stateClone });
-//   }
-
-//   if (a.type === 'removeProperties') {
-//     for (const b of a.keysToRemove) {
-//       delete stateClone[b];
-//     }
-//     result.push({ ...stateClone });
-//   }
-
-//   if (a.type === 'clear') {
-//     stateClone = {};
-//     result.push({ ...stateClone });
-//   }
-// }
-
-// return result;
