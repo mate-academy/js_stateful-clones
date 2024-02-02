749f8ebc1eec1d224c7fd5715331654719a40ad9
zozulia.kirill <kirillzozulya95@gmail.com>
2024-02-02 23:54:29 +0200

    resolve task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,35 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const ADD_PROPERTY_TYPE = 'addProperties';
+  const REMOVE_PROPERTY_TYPE = 'removeProperties';
+  const CLEAR_TYPE = 'clear';
+
+  const statesHistory = [];
+  let newState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case CLEAR_TYPE:
+        newState = {};
+        statesHistory.push({ ...newState });
+        break;
+
+      case ADD_PROPERTY_TYPE:
+        Object.assign(newState, action.extraData);
+        statesHistory.push({ ...newState });
+        break;
+
+      case REMOVE_PROPERTY_TYPE:
+        for (const key of action.keysToRemove) {
+          delete newState[key];
+        }
+        statesHistory.push({ ...newState });
+        break;
+    }
+  }
+
+  return statesHistory;
 }
 
 module.exports = transformStateWithClones;
