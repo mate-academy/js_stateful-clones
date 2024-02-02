cdd18521de8197627496c96d466866e8c08f2396
VerJane23 <verjane23@gmail.com>
2024-02-02 12:35:40 +0200

    transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,28 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const resultArr = [];
+  let cloneState = { ...state };
+
+  actions.forEach(action => {
+    if (action.type === 'addProperties') {
+      cloneState = {
+        ...cloneState, ...action.extraData,
+      };
+    }
+
+    if (action.type === 'clear') {
+      cloneState = {};
+    }
+
+    if (action.type === 'removeProperties') {
+      action.keysToRemove.forEach(key => delete cloneState[key]);
+    }
+
+    resultArr.push({ ...cloneState });
+  });
+
+  return resultArr;
 }
 
 module.exports = transformStateWithClones;
