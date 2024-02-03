32bd08d57978836f453645d7ccf3305d48d0550a
ivan.kolbukh <ivan.kolbukh@vitechteam.com>
2024-02-04 00:36:01 +0200

    add solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let currentState = { ...state };
+  const result = [];
+
+  actions.forEach(action => {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(currentState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        action.keysToRemove.forEach(key => {
+          delete currentState[key];
+        });
+        break;
+
+      case 'clear':
+        currentState = {};
+        break;
+
+      default:
+        break;
+    }
+
+    result.push({ ...currentState });
+  });
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
