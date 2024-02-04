c2f8b8e41b892562e491653189399617f989c9fc
Denys209818 <deniskravcuk63@gmail.com>
2024-02-05 00:46:00 +0200

    Load full realization

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,46 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const ADD_PROPERTY_KEY = 'addProperties';
+  const REMOVE_PROPERTY_KEY = 'removeProperties';
+  const CLEAR = 'clear';
+
+  const states = [];
+  let stateItem = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case ADD_PROPERTY_KEY: {
+        for (const key in action.extraData) {
+          stateItem[key] = action.extraData[key];
+        }
+
+        break;
+      }
+
+      case REMOVE_PROPERTY_KEY: {
+        for (const item of action.keysToRemove) {
+          delete stateItem[item];
+        }
+
+        break;
+      }
+
+      case CLEAR: {
+        for (const key in stateItem) {
+          delete stateItem[key];
+        }
+
+        break;
+      }
+    }
+
+    states.push(stateItem);
+
+    stateItem = { ...stateItem };
+  }
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
