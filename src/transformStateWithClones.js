9d675eaf87ab679111c92a7f523b30d8ec32bbe8
DESKTOP-EHDJ2FA\yarik <yarik.zubyk@gmail.com>
2024-02-06 13:59:21 +0200

    created funciton for multiply actions with object

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,37 @@
  *
  * @return {Object[]}
  */
+
+const loopRemove = (obj, state) => {
+  for (const key of obj) {
+    delete state[key];
+  }
+};
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const prevState = { ...state };
+
+  for (const val of actions) {
+    switch (val.type) {
+      case 'addProperties':
+        Object.assign(prevState, val.extraData);
+        result.push({ ...prevState });
+        break;
+      case 'removeProperties':
+        loopRemove(val.keysToRemove, prevState);
+        result.push({ ...prevState });
+        break;
+      case 'clear':
+        loopRemove(Object.keys(prevState), prevState);
+        result.push({ ...prevState });
+        break;
+      default:
+        break;
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
