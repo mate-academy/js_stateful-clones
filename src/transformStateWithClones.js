105ac813fbff0af32bca02046c055019645de898
Roman Romanchuk <roma.roman4uk.48@gmail.com>
2024-02-08 20:57:16 +0100

    completed the task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,39 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
-}
+  const stateHistory = [];
+  let currentState = { ...state };
 
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
+      case 'clear':
+        currentState = {};
+        break;
+
+      case 'addProperties':
+        currentState = {
+          ...currentState, ...extraData,
+        };
+        break;
+
+      case 'removeProperties':
+        const keyToRemove = keysToRemove;
+
+        for (const key of keyToRemove) {
+          delete currentState[key];
+        }
+        break;
+
+      default:
+
+        break;
+    }
+
+    stateHistory.push({ ...currentState });
+  }
+
+  return stateHistory;
+}
 module.exports = transformStateWithClones;
