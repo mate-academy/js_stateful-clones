ef5df9c0a7080ef2fcbfdd7d3fc8989af5d0184d
Misha Remeslo <misha.remeslol@gmail.com>
2024-02-11 11:04:37 +0200

    stateful clones task done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,74 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let newState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'clear':
+        newState = {};
+        break;
+
+      case 'addProperties':
+        const extraData = action.extraData;
+
+        newState = {
+          ...newState, ...extraData,
+        };
+        break;
+
+      case 'removeProperties':
+        newState = { ...newState };
+
+        const keysToRemove = action.keysToRemove;
+
+        for (const key of keysToRemove) {
+          if (newState.hasOwnProperty(key)) {
+            delete newState[key];
+          }
+        }
+        break;
+
+      default:
+        break;
+    }
+    result.push({ ...newState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
+
+// const result = [];
+// let newState = { ...state };
+
+// for (const action of actions) {
+//   if (action.type === 'addProperties') {
+//     const extraData = action.extraData;
+
+//     for (const key in extraData) {
+//       newState[key] = extraData[key];
+//     }
+//   }
+// }
+
+// for (const action of actions) {
+//   if (action.type === 'removeProperties') {
+//     const keysToRemove = action.keysToRemove;
+
+//     for (const key of keysToRemove) {
+//       delete newState[key];
+//     }
+//   }
+// }
+
+// for (const action of actions) {
+//   if (action.type === 'clear') {
+//     newState = {};
+//   }
+// }
+// result.push(newState);
+
+// return result;
