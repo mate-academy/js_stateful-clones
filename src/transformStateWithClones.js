770ab46a7026e268831ee6a25da2a66adf92b701
Maksym Prokopovych <prokopovych2006@gmail.com>
2024-02-11 12:28:22 +0200

    result

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,24 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const finalHistoryArray = [];
+  let modifiedObject = { ...state };
+
+  actions.forEach((action) => {
+    if (action.type === 'clear') {
+      modifiedObject = {};
+
+      finalHistoryArray.push({ ...modifiedObject });
+    } else if (action.type === 'addProperties') {
+      Object.assign(modifiedObject, action.extraData);
+      finalHistoryArray.push({ ...modifiedObject });
+    } else if (action.type === 'removeProperties') {
+      action.keysToRemove.forEach((key) => delete modifiedObject[key]);
+      finalHistoryArray.push({ ...modifiedObject });
+    }
+  });
+
+  return finalHistoryArray;
 }
 
 module.exports = transformStateWithClones;
