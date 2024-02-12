13ec10948ac041745a82f16cc53a184f13e03131
Vitalii <NuclearSunStar@gmail.com>
2024-02-12 21:16:15 +0200

    I will read checklist before doing task, I promise

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,29 +8,32 @@
  */
 function transformStateWithClones(state, actions) {
   const clonedState = { ...state };
-  const finalArr = [];
+  const finalState = [];
+  const deleteKey = (key) => delete clonedState[key];
 
   for (const obj of actions) {
-    if (obj.type === 'addProperties') {
-      Object.assign(clonedState, obj.extraData);
+    switch (obj.type) {
+      case 'addProperties':
+        Object.assign(clonedState, obj.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of obj.keysToRemove) {
+          deleteKey(key);
+        };
+        break;
+
+      case 'clear':
+        for (const key of Object.keys(clonedState)) {
+          deleteKey(key);
+        };
+        break;
     };
 
-    if (obj.type === 'removeProperties') {
-      for (const key of obj.keysToRemove) {
-        delete clonedState[key];
-      };
-    };
-
-    if (obj.type === 'clear') {
-      for (const key of Object.keys(clonedState)) {
-        delete clonedState[key];
-      };
-    };
-
-    finalArr.push({ ...clonedState });
+    finalState.push({ ...clonedState });
   };
 
-  return finalArr;
+  return finalState;
 };
 
 module.exports = transformStateWithClones;
