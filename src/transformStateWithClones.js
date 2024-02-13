42443360de49fe8070d88493c9669adfa041c513
Anton <one.month.for23@gmail.com>
2024-02-13 15:39:05 +0200

    add

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+  const stateClone = { ...state };
+
+  for (const { type, extraData, keysToRemove } of actions) {
+    switch (type) {
+      case 'addProperties':
+        Object.assign(stateClone, extraData);
+        break;
+
+      case `removeProperties`:
+        for (const propToBeRemoved of keysToRemove) {
+          delete stateClone[propToBeRemoved];
+        };
+        break;
+
+      case `clear`:
+        for (const elementOfState in stateClone) {
+          delete stateClone[elementOfState];
+        }
+        break;
+
+      default:
+        throw new Error('Data is not valid');
+    }
+    stateHistory.push(Object.assign({}, stateClone));
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
