535aa2acc15ef87577d4fdc8c292abfb693a9dcb
Nikita Donskoy <donskoy.nikita13@gmail.com>
2024-02-13 22:06:18 +0200

    add solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,5 +1,20 @@
 'use strict';
 
+function addProperties(target, source) {
+  return Object.assign(target, source);
+}
+
+function removeProperties(obj, properties) {
+  return Object.keys(obj)
+    .reduce((result, key) => {
+      if (!properties.includes(key)) {
+        result[key] = obj[key];
+      }
+
+      return result;
+    }, {});
+}
+
 /**
  * @param {Object} state
  * @param {Object[]} actions
@@ -7,7 +22,25 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const history = [];
+  let object = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        object = addProperties(object, action.extraData);
+        break;
+      case 'removeProperties':
+        object = removeProperties(object, action.keysToRemove);
+        break;
+      case 'clear':
+        object = {};
+        break;
+    }
+    history.push({ ...object });
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
