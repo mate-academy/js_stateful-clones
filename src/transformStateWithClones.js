6a24251e824b75adeb86744b516c362a2dd82cbf
Denis Kotov <dionisiy2904@gmail.com>
2024-02-13 22:30:54 +0200

    Clone state

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const STATE_HISTORY = [];
+
+  let stateClone = { ...state };
+
+  for (const action of actions) {
+    const VALUES = Object.values(action);
+
+    if (VALUES[0] === 'addProperties') {
+      Object.assign(stateClone, VALUES[1]);
+    }
+
+    if (VALUES[0] === 'removeProperties') {
+      for (const key of VALUES[1]) {
+        delete stateClone[key];
+      }
+    }
+
+    if (VALUES[0] === 'clear') {
+      stateClone = {};
+    }
+
+    STATE_HISTORY.push({ ...stateClone });
+  }
+
+  return STATE_HISTORY;
 }
 
 module.exports = transformStateWithClones;
