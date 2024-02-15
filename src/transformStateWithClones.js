f039d9170141a72acf237a6b556983293359b4c2
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-02-15 13:30:14 +0100

    task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,40 @@
  *
  * @return {Object[]}
  */
+
+
 function transformStateWithClones(state, actions) {
-  console.log('test');
-}
+  let newState = state;
+
+   for (let object in actions) {
+     if (actions[object].type === 'addProperties') {
+
+       newState = Object.assign({}, newState, actions[object].extraData)
+
+       }
+
+     if (actions[object].type === 'removeProperties') {
+
+
+       for (const key in actions[object].keysToRemove) {
+         delete newState[actions[object].keysToRemove[key]];
+       }
+
+     }
+
+     if (actions[object].type === 'clear') {
+       for (const key in newState) {
+         delete newState[key];
+       }
+     }
+
+   }
+
+   return newState;
+
+ }
+
+
+
 
 module.exports = transformStateWithClones;
