97f18242dd5963724d3c6fe0faf485cb41e83dcf
Ruslan Ogerenko <ruslanogerenko@gmail.com>
2024-02-16 11:53:51 +0200

    improve the structure of code

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,22 +11,20 @@ function transformStateWithClones(state, actions) {
   const historyOfState = [];
 
   for (const action of actions) {
-    for (const key in action) {
-      if (action[key] === 'addProperties') {
+    switch (action.type) {
+      case 'addProperties':
         addProperties(stateClone, action.extraData);
-        historyOfState.push({ ...stateClone });
-      }
+        break;
 
-      if (action[key] === 'removeProperties') {
+      case 'removeProperties':
         removeProperties(stateClone, action.keysToRemove);
-        historyOfState.push({ ...stateClone });
-      }
+        break;
 
-      if (action[key] === 'clear') {
+      case 'clear':
         clearProperties(stateClone);
-        historyOfState.push({ ...stateClone });
-      }
+        break;
     }
+    historyOfState.push({ ...stateClone });
   }
 
   return historyOfState;
