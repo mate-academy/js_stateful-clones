944ae3c6092885009ea7c878fcad317db0d7e365
Taras Matarzhuk <tarasmat@icloud.com>
2024-02-16 11:24:43 +0100

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,31 @@
 'use strict';
 
-/**
- * @param {Object} state
- * @param {Object[]} actions
- *
- * @return {Object[]}
- */
-function transformStateWithClones(state, actions) {
-  // write code here
+function transformStateWithClones(initialState, actions) {
+  const resultStates = [];
+  let currentState = { ...initialState };
+
+  actions.forEach(action => {
+    switch (action.type) {
+      case 'clear':
+        currentState = {};
+        break;
+      case 'addProperties':
+        currentState = {
+          ...currentState,
+          ...action.extraData,
+        };
+        break;
+      case 'removeProperties':
+        currentState = { ...currentState };
+        action.keysToRemove.forEach(key => delete currentState[key]);
+        break;
+      default:
+        break;
+    }
+    resultStates.push({ ...currentState });
+  });
+
+  return resultStates;
 }
 
 module.exports = transformStateWithClones;
