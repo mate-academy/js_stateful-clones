b11d613d3ede1de02d49218a71481a3d3310480c
Oksana Kril <oksanakril@Oksanas-MacBook-Air.local>
2024-02-18 17:57:33 -0600

    Task is done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,44 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const newState = { ...state };
+  const stateArr = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        addProperties(newState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        removeProperties(newState, action.keysToRemove);
+        break;
+
+      case 'clear':
+        clearProperties(newState);
+        break;
+    }
+
+    stateArr.push({ ...newState });
+  }
+
+  return stateArr;
+}
+
+function addProperties(state, extraData) {
+  Object.assign(state, extraData);
+}
+
+function removeProperties(state, keysToRemove) {
+  for (const key of keysToRemove) {
+    delete state[key];
+  }
+}
+
+function clearProperties(state) {
+  for (const key in state) {
+    delete state[key];
+  }
 }
 
 module.exports = transformStateWithClones;
