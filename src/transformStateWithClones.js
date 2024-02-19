d0582d0f41492f69e704524ca45bd6e298c40c59
Wojciech <weswojciech@gmail.com>
2024-02-19 19:11:15 +0100

    add switch instead of if

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,24 +11,30 @@ function transformStateWithClones(state, actions) {
   const history = [];
   const stateClone = Object.assign({}, state);
 
-  function iterate(arr) {
+  function deleteKeysFromObj(arr) {
     for (const key of arr) {
       delete stateClone[key];
     }
   }
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(stateClone, action.extraData);
-      history.push({ ...stateClone });
-    } else if (action.type === 'removeProperties') {
-      iterate(action.keysToRemove);
-      history.push({ ...stateClone });
-    } else if (action.type === 'clear') {
-      const keys = Object.keys(stateClone);
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateClone, action.extraData);
+        history.push({ ...stateClone });
+        break;
+      case 'removeProperties':
+        deleteKeysFromObj(action.keysToRemove);
+        history.push({ ...stateClone });
+        break;
+      case 'clear':
+        const keys = Object.keys(stateClone);
 
-      iterate(keys);
-      history.push({ ...stateClone });
+        deleteKeysFromObj(keys);
+        history.push({ ...stateClone });
+        break;
+      default:
+        throw Error('Error');
     }
   }
 
