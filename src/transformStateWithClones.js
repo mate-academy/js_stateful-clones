21c000632d17141798452e7c99dd5de7ee83bc20
Oki <oksana.nastenko89@gmail.com>
2024-02-20 11:32:50 +0100

    replace with switch statement

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -14,16 +14,22 @@ function transformStateWithClones(state, actions) {
   for (const action of actions) {
     currentState = { ...currentState };
 
-    if (action.type === 'addProperties') {
-      for (const key in action.extraData) {
-        currentState[key] = action.extraData[key];
-      }
-    } else if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete currentState[key];
-      }
-    } else if (action.type === 'clear') {
-      currentState = {};
+    switch (action.type) {
+      case 'addProperties':
+        for (const key in action.extraData) {
+          currentState[key] = action.extraData[key];
+        }
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete currentState[key];
+        }
+        break;
+
+      case 'clear':
+        currentState = {};
+        break;
     }
 
     stateHistory.push(currentState);
