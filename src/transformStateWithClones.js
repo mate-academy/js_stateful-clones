7d6ee58bc5b3bdb06711fa287bfcfeccdea12f44
MaximAV <atanasovmaksim56@gmail.com>
2024-02-20 22:41:50 +0200

    added solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,74 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+  let stateClone = { ...state };
+
+  for (const action of actions) {
+    for (const par in action) {
+      switch (action[`${par}`]) {
+        case 'addProperties':
+          for (const data in action.extraData) {
+            stateClone[`${data}`] = action.extraData[`${data}`];
+          }
+          stateHistory.push({ ...stateClone });
+          break;
+
+        case 'removeProperties':
+          for (const data in action.keysToRemove) {
+            delete stateClone[action.keysToRemove[`${data}`]];
+          }
+          stateHistory.push({ ...stateClone });
+          break;
+
+        case 'clear':
+          stateClone = {};
+          stateHistory.push({ ...stateClone });
+          break;
+
+        default:
+          break;
+      }
+
+      // if (action[`${par}`] === 'addProperties') {
+      //   for (const data in action.extraData) {
+      //     stateClone[`${data}`] = action.extraData[`${data}`];
+      //     stateHistory.push(stateClone);
+      //   }
+      // }
+
+      // if (action[`${par}`] === 'removeProperties') {
+      //   for (const data in action.keysToRemove) {
+      //     delete stateClone[action.keysToRemove[`${data}`]];
+      //     stateHistory.push(stateClone);
+      //   }
+      // }
+
+      // if (action[`${par}`] === 'clear') {
+      //   stateClone = {};
+      //   stateHistory.push(stateClone);
+      // }
+    }
+  }
+
+  return stateHistory;
 }
+
+transformStateWithClones({
+  foo: 'bar', bar: 'foo',
+}, [
+  {
+    type: 'addProperties',
+    extraData: {
+      name: 'Jim', hello: 'world',
+    },
+  },
+  {
+    type: 'removeProperties', keysToRemove: ['bar', 'hello'],
+  },
+  {
+    type: 'addProperties', extraData: { another: 'one' },
+  },
+]);
 
 module.exports = transformStateWithClones;
