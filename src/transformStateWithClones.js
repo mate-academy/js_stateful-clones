eb025f22c4204e855b224deb6a1ce69b1428b235
MaximAV <atanasovmaksim56@gmail.com>
2024-02-21 17:30:12 +0200

    fixes

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,31 +11,28 @@ function transformStateWithClones(state, actions) {
   let stateClone = { ...state };
 
   for (const action of actions) {
-    for (const par in action) {
-      switch (action[`${par}`]) {
-        case 'addProperties':
-          for (const data in action.extraData) {
-            stateClone[`${data}`] = action.extraData[`${data}`];
-          }
-          stateHistory.push({ ...stateClone });
-          break;
+    switch (action.type) {
+      case 'addProperties':
+        for (const data in action.extraData) {
+          stateClone[data] = action.extraData[data];
+        }
+        break;
 
-        case 'removeProperties':
-          for (const data in action.keysToRemove) {
-            delete stateClone[action.keysToRemove[`${data}`]];
-          }
-          stateHistory.push({ ...stateClone });
-          break;
+      case 'removeProperties':
+        for (const data in action.keysToRemove) {
+          delete stateClone[action.keysToRemove[data]];
+        }
+        break;
 
-        case 'clear':
-          stateClone = {};
-          stateHistory.push({ ...stateClone });
-          break;
+      case 'clear':
+        stateClone = {};
+        break;
 
-        default:
-          break;
-      }
+      default:
+        break;
     }
+
+    stateHistory.push({ ...stateClone });
   }
 
   return stateHistory;
