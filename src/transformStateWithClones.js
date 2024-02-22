82d481809d2f1d99b97622501f1ad62e1c242c2d
Bartlomiej Karbownik <bartlomiej.karbownik56@gmail.com>
2024-02-22 14:44:53 +0100

    Task solution proposal

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,41 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+  let presentState = { ...state };
+
+  if (actions.length === 0) {
+    return stateHistory;
+  }
+
+  for (const action of actions) {
+    const secondState = { ...presentState };
+
+    switch (action.type) {
+      case 'clear':
+        for (const key in secondState) {
+          delete secondState[key];
+        }
+        break;
+      case 'addProperties':
+        Object.assign(secondState, action.extraData);
+        break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          if (secondState.hasOwnProperty(key)) {
+            delete secondState[key];
+          }
+        }
+        break;
+      default:
+        break;
+    }
+
+    stateHistory.push(secondState);
+    presentState = { ...secondState };
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
