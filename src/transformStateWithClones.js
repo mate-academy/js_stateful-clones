f9bba6f37fba1d59052caf0a1ce1735aa9f2863d
GetApple <getapple@MacBook-Pro.local>
2024-02-23 17:00:29 +0200

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const historyState = [];
+  let currentState = { ...state };
+
+  actions.forEach(action => {
+    if (action.type === 'addProperties') {
+      Object.assign(currentState, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        if (key in currentState) {
+          delete currentState[key];
+        }
+      }
+    }
+
+    if (action.type === 'clear') {
+      currentState = {};
+    }
+
+    historyState.push({ ...currentState });
+  });
+
+  return historyState;
 }
 
 module.exports = transformStateWithClones;
