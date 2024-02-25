dd2cd8168b1ad5a2211db3121cc2d51294e18d68
Tymofii Fastovetskyi <timoxa.sykunov2017@gmail.com>
2024-02-25 13:52:39 +0200

    have a some stuck

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,56 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let stateHistory = [];
+  let cloneState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(cloneState, action.extraData);
+        stateHistory.push({ ...cloneState });
+        break;
+      case 'removeProperties':
+        action.keysToRemove.forEach(key => delete cloneState[key]);
+        stateHistory.push({ ...cloneState });
+        break;
+      case 'clear':
+        cloneState = {};
+        stateHistory.push(cloneState);
+        break;
+    }
+  }
+  console.log(stateHistory);
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
+
+// const stateHistory = [];
+// let stateCopy = {};
+
+// Object.assign(stateCopy, ...);
+// stateHistory.push(stateCopy);
+
+// for (const action of actions) {
+//   switch (action.type) {
+//     case 'addProperties':
+//       const stateCopy = {};
+
+//       Object.assign(stateCopy, action.extraData);
+//       stateHistory.push(stateCopy);
+//       break;
+//     case 'removeProperties':
+//       action.keysToRemove.forEach(key => delete stateCopy[key]);
+//       stateHistory.push(stateCopy);
+//       break;
+//     case 'clear':
+//       for (const key in stateCopy) {
+//         delete stateCopy[key];
+//         stateHistory.push(stateCopy);
+//       }
+//       break;
+//   }
+// }
+
+// return stateHistory;
