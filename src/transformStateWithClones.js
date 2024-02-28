70039da6768879bbe1a7438063fbcc85d6301e06
Mariia <mulitskaya@gmail.com>
2024-02-27 21:47:55 -0500

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,50 +7,38 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const COPY__STATE = { ...state };
-  const ADD_PROP = {};
-  const DEL_PROP = {};
-  const result = [];
-  const DEL_FROM_ADD_CLEAR = {};
+  const newArr = [{ ...state }];
 
-  for (let i = 0; i < actions.length; i++) {
-    for (const key of Object.keys(actions[i])) {
-      let copyDelProp = { ...DEL_PROP };
+  actions.forEach((value, index) => {
+    const newState = { ...newArr[index] };
 
-      if (actions[i][key] === 'clear') {
-        copyDelProp = {};
-        result.push(copyDelProp);
-      }
+    switch (value.type) {
+      case 'addProperties':
+        for (const key in value.extraData) {
+          newState[key] = value.extraData[key];
+        }
+        newArr.push(newState);
+        break;
 
-      const COPPY_ADD = { ...ADD_PROP };
+      case 'removeProperties':
+        if (value.keysToRemove) {
+          value.keysToRemove.forEach((key) => {
+            delete newState[key];
+          });
+        }
+        newArr.push(newState);
+        break;
 
-      if (actions[i][key] === 'removeProperties' && result.length === 0) {
-        actions[i].keysToRemove.forEach(prop => delete COPY__STATE[prop]);
-        result.push(COPY__STATE);
-      } else if (actions[i][key] === 'removeProperties'
-      && result.length > 0 && result.length < 5) {
-        actions[i].keysToRemove.forEach(prop => delete COPPY_ADD[prop]);
-        Object.assign(DEL_PROP, COPPY_ADD);
-        result.push(COPPY_ADD);
-      } else if (actions[i][key] === 'removeProperties' && result.length >= 5) {
-        actions[i].keysToRemove.forEach(prop =>
-          delete DEL_FROM_ADD_CLEAR[prop]);
-        result.push(DEL_FROM_ADD_CLEAR);
-      }
-
-      if (actions[i][key] === 'addProperties' && result.length === 0) {
-        result.push(Object.assign(ADD_PROP, COPY__STATE, actions[i].extraData));
-      } else if (actions[i][key] === 'addProperties'
-      && result.length > 0 && result.length < 5) {
-        result.push(Object.assign(copyDelProp, actions[i].extraData));
-      } else if (actions[i][key] === 'addProperties' && result.length > 5) {
-        result.push(Object.assign(copyDelProp, actions[i].extraData));
-        Object.assign(DEL_FROM_ADD_CLEAR, actions[i].extraData);
-      }
+      case 'clear':
+        for (const key in newState) {
+          delete newState[key];
+        }
+        newArr.push(newState);
+        break;
     }
-  }
+  });
 
-  return result;
+  return newArr.slice(1);
 }
 
 module.exports = transformStateWithClones;
