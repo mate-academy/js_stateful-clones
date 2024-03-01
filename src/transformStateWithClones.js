5c44d924d53801a9583b880f6bddd32014cd4ff3
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2024-03-01 14:41:40 +0200

    added needed implementation

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const resultArray = [];
+  const result = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      for (const data in action.extraData) {
+        result[data] = action.extraData[data];
+      }
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete result[key];
+      }
+    }
+
+    if (action.type === 'clear') {
+      Object.keys(result).forEach((key) => delete result[key]);
+    }
+
+    resultArray.push({ ...result });
+  }
+
+  return resultArray;
 }
 
 module.exports = transformStateWithClones;
