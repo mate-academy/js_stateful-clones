8cba4721d525b7f3de98c95c6210246b43ec4a16
Daniil Fadeev <daniilfadeev2018@gmail.com>
2024-03-04 11:38:28 +0200

    fe2-changed-some-code

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,51 +8,35 @@
  */
 function transformStateWithClones(state, actions) {
   const arr = [];
-  const clone = { ...state };
+  let clone = { ...state };
 
   for (let i = 0; i < actions.length; i++) {
     const actionObj = actions[i];
 
     switch (actionObj.type) {
       case 'clear':
-        for (const clearKey in clone) {
-          if (Object.prototype.hasOwnProperty.call(clone, clearKey)) {
-            delete clone[clearKey];
-          }
-        }
-
-        arr.push({ ...clone });
+        clone = {};
 
         break;
 
       case 'addProperties':
         const data = actionObj.extraData;
 
-        for (const dataKey in data) {
-          const keyName = dataKey;
-          const keyValue = data[dataKey];
-
-          clone[keyName] = keyValue;
-        }
-
-        arr.push({ ...clone });
+        clone = { ...clone, ...data };
 
         break;
 
       case 'removeProperties':
         const remove = actionObj.keysToRemove;
 
-        for (const stateKey in clone) {
-          for (let j = 0; j < remove.length; j++) {
-            if (stateKey === remove[j]) {
-              delete clone[remove[j]];
-            }
-          }
+        for (const cloneKey in remove) {
+          delete clone[cloneKey];
         }
 
-        arr.push({ ...clone });
         break;
     }
+
+    arr.push({ ...clone });
   }
 
   return arr;
