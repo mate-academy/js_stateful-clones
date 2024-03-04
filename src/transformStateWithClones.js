19b5c630095239743fcd98d219cd4db2679f8ef2
Daniil Fadeev <daniilfadeev2018@gmail.com>
2024-03-04 10:40:45 +0200

    fe1-resolved-task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,55 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arr = [];
+  const clone = { ...state };
+
+  for (let i = 0; i < actions.length; i++) {
+    const actionObj = actions[i];
+
+    switch (actionObj.type) {
+      case 'clear':
+        for (const clearKey in clone) {
+          if (Object.prototype.hasOwnProperty.call(clone, clearKey)) {
+            delete clone[clearKey];
+          }
+        }
+
+        arr.push({ ...clone });
+
+        break;
+
+      case 'addProperties':
+        const data = actionObj.extraData;
+
+        for (const dataKey in data) {
+          const keyName = dataKey;
+          const keyValue = data[dataKey];
+
+          clone[keyName] = keyValue;
+        }
+
+        arr.push({ ...clone });
+
+        break;
+
+      case 'removeProperties':
+        const remove = actionObj.keysToRemove;
+
+        for (const stateKey in clone) {
+          for (let j = 0; j < remove.length; j++) {
+            if (stateKey === remove[j]) {
+              delete clone[remove[j]];
+            }
+          }
+        }
+
+        arr.push({ ...clone });
+        break;
+    }
+  }
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
