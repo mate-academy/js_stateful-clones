8ff1b18e21c015a098701eaa7e72a562955e29cb
metrakon <metrakon@proton.me>
2024-03-09 15:34:50 +0200

    add solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,32 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  let currentState = [{ ...state }];
+  const stateHistory = [];
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      currentState[0] = { ...currentState[0], ...action.extraData };
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete currentState[0][key];
+      }
+    }
+
+    if (action.type === 'clear') {
+      currentState = [{}];
+    }
+
+    const newState = { ...currentState[0] };
+
+    stateHistory.push(newState);
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
