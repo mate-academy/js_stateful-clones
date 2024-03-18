3d28ed9a21384e50672efb08149d2ce6b28d3aaa
MatheusStefan <roccomatheus0@gmail.com>
2024-03-18 16:02:07 -0300

    task solution using switch

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,33 @@
  *
  * @return {Object[]}
  */
+/**
+ * @param {Object} state
+ * @param {Object[]} actions
+ */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const history = [];
+
+  actions.forEach((action, index) => {
+    let newState = index === 0 ? { ...state } : { ...history[index - 1] };
+
+    switch (action.type) {
+      case 'clear':
+        newState = {};
+        break;
+      case 'addProperties':
+        Object.assign(newState, action.extraData);
+        break;
+      case 'removeProperties':
+        if (Array.isArray(action.keysToRemove)) {
+          action.keysToRemove.forEach((key) => delete newState[key]);
+        }
+        break;
+    }
+    history.push({ ...newState });
+  });
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
