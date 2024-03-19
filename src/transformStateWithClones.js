3641a17d027e370ab9e9a0c32b29b9491ac043f1
MatheusStefan <roccomatheus0@gmail.com>
2024-03-19 11:13:28 -0300

    new task solution using reduce method + add error handling

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,28 +11,40 @@
  * @param {Object[]} actions
  */
 function transformStateWithClones(state, actions) {
-  const history = [];
-
-  actions.forEach((action, index) => {
+  return actions.reduce((history, action, index) => {
     let newState = index === 0 ? { ...state } : { ...history[index - 1] };
 
-    switch (action.type) {
-      case 'clear':
-        newState = {};
-        break;
-      case 'addProperties':
-        Object.assign(newState, action.extraData);
-        break;
-      case 'removeProperties':
-        if (Array.isArray(action.keysToRemove)) {
-          action.keysToRemove.forEach((key) => delete newState[key]);
-        }
-        break;
-    }
-    history.push({ ...newState });
-  });
+    if (action.type && action.type !== '') {
+      switch (action.type) {
+        case 'clear':
+          newState = {};
+          break;
+        case 'addProperties':
+          if (!action.extraData) {
+            throw new Error('Missing extraData type in addProperties action.');
+          }
+          Object.assign(newState, action.extraData);
+          break;
+        case 'removeProperties':
+          if (!action.keysToRemove) {
+            throw new Error(
+              'Missing keysToRemove type in removeProperties action.',
+            );
+          }
 
-  return history;
+          if (Array.isArray(action.keysToRemove)) {
+            action.keysToRemove.forEach((key) => delete newState[key]);
+          }
+          break;
+        default:
+          throw new Error(`Unsupported action type: ${action.type}`);
+      }
+    } else {
+      throw new Error('Action type must be provided and non-empty!');
+    }
+
+    return [...history, { ...newState }];
+  }, []);
 }
 
 module.exports = transformStateWithClones;
