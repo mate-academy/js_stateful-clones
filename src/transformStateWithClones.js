cfdd2abe064e191b6e8b3d7a788449c087cab918
Yurii Filiushkin <yurii.filiushkin@gmail.com>
2024-03-26 15:59:03 +0200

    Sonic

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,28 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let newState = { ...state };
+  const result = [];
+
+  for (const comand of actions) {
+    if (comand.type === 'addProperties') {
+      Object.assign(newState, comand.extraData);
+    }
+
+    if (comand.type === 'removeProperties') {
+      for (const remove of comand.keysToRemove) {
+        delete newState[remove];
+      }
+    }
+
+    if (comand.type === 'clear') {
+      newState = {};
+    }
+
+    result.push({ ...newState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
