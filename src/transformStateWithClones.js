9bbfa029391e7b6f505b1331f11b086b72156c88
Levik-24 <lfetman@gmail.com>
2024-03-26 13:12:33 +0200

    finished task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,42 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const newState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(newState, action.extraData);
+        result.push({ ...newState });
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          if (newState.hasOwnProperty(key)) {
+            delete newState[key];
+          }
+        }
+        result.push({ ...newState });
+        break;
+
+      case 'clear':
+        for (const key in newState) {
+          if (newState.hasOwnProperty(key)) {
+            delete newState[key];
+          }
+        }
+        result.push({ ...newState });
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
