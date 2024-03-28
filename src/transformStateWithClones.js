0c16ef72a94509566880c13edb94dfb05369f4d9
my-git-username <118355685+my-git-username@users.noreply.github.com>
2024-03-28 17:24:13 +0200

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const transformResult = [];
+  const stateClone = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'clear') {
+      for (const key in stateClone) {
+        delete stateClone[key];
+      }
+    }
+
+    if (action.type === 'addProperties') {
+      Object.assign(stateClone, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const rKey of action.keysToRemove) {
+        delete stateClone[rKey];
+      }
+    }
+
+    transformResult.push({ ...stateClone });
+  }
+
+  return transformResult;
 }
 
 module.exports = transformStateWithClones;
