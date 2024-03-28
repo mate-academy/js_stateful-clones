fa72db88c05b0c1e197f9568ee7cfbba33223c41
Anton Molchanov <antonmolchanov18@gmail.com>
2024-03-28 20:48:51 +0200

    code optimization

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -10,31 +10,25 @@ function transformStateWithClones(state, actions) {
   const res = [];
   let obj = Object.assign({}, state);
 
-  function pushClonedObject() {
-    res.push({ ...obj });
-  }
-
   for (const action of actions) {
     const { type, extraData, keysToRemove } = action;
 
     switch (type) {
       case 'addProperties':
         obj = { ...obj, ...extraData };
-        pushClonedObject();
         break;
 
       case 'removeProperties':
         for (const removeElement of keysToRemove) {
           delete obj[removeElement];
         }
-        pushClonedObject();
         break;
 
       case 'clear':
         obj = {};
-        pushClonedObject();
         break;
     }
+    res.push({ ...obj });
   }
 
   return res;
