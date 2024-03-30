cd5c40d6ec0d200aaeca42fd54fd540128250a35
Vladislav <7evenLays@gmail.com>
2024-03-30 15:57:31 +0200

    deleted repeats

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -14,22 +14,20 @@ function transformStateWithClones(state, actions) {
     switch (type) {
       case 'addProperties':
         addProperties(newState, extraData);
-        stateHistory.push({ ...newState });
         break;
 
       case 'removeProperties':
         removeProperties(newState, keysToRemove);
-        stateHistory.push({ ...newState });
         break;
 
       case 'clear':
         clearProperties(newState);
-        stateHistory.push({ ...newState });
         break;
 
       default:
         break;
     }
+    stateHistory.push({ ...newState });
   }
 
   return stateHistory;
