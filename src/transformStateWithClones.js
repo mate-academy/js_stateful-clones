df7d9d0bbaa6e8f2100b1a4ff28d08e8615f419b
ThyLightt <izengard96@gmail.com>
2024-04-10 15:45:52 +0300

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -27,6 +27,8 @@ function transformStateWithClones(state, actions) {
       case 'clear':
         nextState = {};
         break;
+      default:
+        break;
     }
 
     currentStateClone = nextState;
