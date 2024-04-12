8cdc34db6bb1069eb29d27bc150e662b39da3b7d
Mariami Borul <mhurhienidze@gmail.com>
2024-04-12 15:59:52 +0300

    added second variant (switch case) task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,30 +7,74 @@
  * @return {Object[]}
  */
 
+// first variant
+
+// function transformStateWithClones(state, actions) {
+//   const stateHistory = [];
+//   const stateCopy = { ...state };
+
+//   for (const action of actions) {
+//     if (action.type === 'clear') {
+//       for (const key in stateCopy) {
+//         delete stateCopy[key];
+//       }
+//     }
+
+//     if (action.type === 'addProperties') {
+//       Object.assign(stateCopy, action.extraData);
+//     }
+
+//     if (action.type === 'removeProperties') {
+//       for (const key of action.keysToRemove) {
+//         delete stateCopy[key];
+//       }
+//     }
+//     stateHistory.push({ ...stateCopy });
+//   }
+
+//   return stateHistory;
+// }
+
+// second variant
+
 function transformStateWithClones(state, actions) {
   const stateHistory = [];
   const stateCopy = { ...state };
 
   for (const action of actions) {
-    if (action.type === 'clear') {
-      for (const key in stateCopy) {
-        delete stateCopy[key];
-      }
-    }
+    switch (action.type) {
+      case 'clear':
+        clear(stateCopy);
+        break;
 
-    if (action.type === 'addProperties') {
-      Object.assign(stateCopy, action.extraData);
-    }
+      case 'addProperties':
+        addProperties(stateCopy, action.extraData);
+        break;
 
-    if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete stateCopy[key];
-      }
+      case 'removeProperties':
+        removeProperties(stateCopy, action.keysToRemove);
+        break;
     }
     stateHistory.push({ ...stateCopy });
   }
 
   return stateHistory;
+}
+
+function clear(stateCopy) {
+  for (const key in stateCopy) {
+    delete stateCopy[key];
+  }
+}
+
+function addProperties(stateCopy, extraData) {
+  Object.assign(stateCopy, extraData);
+}
+
+function removeProperties(stateCopy, keysToRemove) {
+  for (const key of keysToRemove) {
+    delete stateCopy[key];
+  }
 }
 
 module.exports = transformStateWithClones;
