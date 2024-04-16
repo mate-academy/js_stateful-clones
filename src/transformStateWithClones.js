1b88886ee18195be4e07795e4cf954115374245c
Anna <aniasuper1996@gmail.com>
2024-04-16 18:17:17 +0300

    stateful clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const cloneState = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(cloneState, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete cloneState[key];
+      }
+    }
+
+    if (action.type === 'clear') {
+      for (const key in cloneState) {
+        delete cloneState[key];
+      }
+    }
+
+    result.push({ ...cloneState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
