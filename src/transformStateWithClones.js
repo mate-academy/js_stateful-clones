c1b00f5299d9faa40cc275d6d7c95b28debf29d4
Alexandr Shatskyy <autome@ukr.net>
2024-04-25 10:38:36 +0200

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -10,35 +10,29 @@
 function transformStateWithClones(state, actions) {
   const rez = [];
 
+  rez.push(Object.assign({}, state));
+
   for (let i = 0; i < actions.length; i++) {
-    if (actions[i].type === 'clear') {
-      rez.push({});
-    }
+    switch (true) {
+      case actions[i].type === 'clear':
+        rez.push({});
 
-    if (actions[i].type === 'addProperties' && rez.length > 0) {
-      rez.push(Object.assign({}, rez[rez.length - 1], actions[i].extraData));
-    }
+        break;
+      case actions[i].type === 'addProperties':
+        rez.push(Object.assign({}, rez[rez.length - 1], actions[i].extraData));
 
-    if (actions[i].type === 'addProperties' && rez.length === 0) {
-      rez.push(Object.assign({}, state, actions[i].extraData));
-    }
+        break;
+      case actions[i].type === 'removeProperties':
+        rez.push(Object.assign({}, rez[rez.length - 1]));
 
-    if (actions[i].type === 'removeProperties' && rez.length > 0) {
-      rez.push(Object.assign({}, rez[rez.length - 1]));
+        for (let e = 0; e < actions[i].keysToRemove.length; e++) {
+          delete rez[rez.length - 1][actions[i].keysToRemove[e]];
+        }
 
-      for (let e = 0; e < actions[i].keysToRemove.length; e++) {
-        delete rez[rez.length - 1][actions[i].keysToRemove[e]];
-      }
-    }
-
-    if (actions[i].type === 'removeProperties' && rez.length === 0) {
-      rez.push(Object.assign({}, state));
-
-      for (let e = 0; e < actions[i].keysToRemove.length; e++) {
-        delete rez[rez.length - 1][actions[i].keysToRemove[e]];
-      }
+        break;
     }
   }
+  rez.splice(0, 1);
 
   return rez;
 }
