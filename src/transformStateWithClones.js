5b418f584fed4618cd38ef12905fa83b1d5e8552
yanatoto <yanas@bluewin.ch>
2024-04-26 16:36:18 +0200

    start task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,23 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      result.push(Object.assign(state, action.extraData));
+    } else if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        result.push(delete state[key]);
+      }
+    } else if (action.type === 'clear') {
+      for (const key in state) {
+        result.push(delete state[key]);
+      }
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
