53fc0b035d05a3cf887cf9cf4189db417adf8c07
Arthur <avers.target@gmail.com>
2024-04-26 22:43:01 +0300

    part1

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,22 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
-}
+  let clonedState = { ...state };
+  const stateHistory = [];
 
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(clonedState, action.extraData);
+    } else if (action.type === 'removeProperties') {
+      for (const key in action.extraData) {
+        delete clonedState[key];
+      }
+    } else if (action.type === 'clear') {
+      clonedState = {};
+    }
+    stateHistory.push(clonedState);
+  }
+
+  return stateHistory;
+}
 module.exports = transformStateWithClones;
