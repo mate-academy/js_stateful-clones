b3dc4d4e1542a48e9086c9cf2b4f2b957e19a21c
mate-student <166617557+mate-student@users.noreply.github.com>
2024-04-29 13:58:34 +0300

    Update transformStateWithClones.js

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,36 @@
 'use strict';
 
 /**
- * @param {Object} state
+ * @param {Object} result
  * @param {Object[]} actions
  *
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const solution = [];
+  let result = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(result, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete result[key];
+        }
+        break;
+
+      case 'clear':
+        result = {};
+        break;
+    }
+
+    solution.push({ ...result });
+  }
+
+  return solution;
 }
 
 module.exports = transformStateWithClones;
