0dec14bb4d97c3ca493d409aa96ac8f01b22c206
Mikhailo Koshilka <mkosilka@gmail.com>
2024-04-29 21:18:32 +0100

    fix code

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,39 +11,32 @@ function transformStateWithClones(state, actions) {
   const remove = 'removeProperties';
   const clear = 'clear';
   const result = [];
-  let currentState = Object.assign({}, state);
+  let currentState = { ...state };
 
   for (const target of actions) {
     const { type, extraData, keysToRemove } = target;
-    let current;
 
     switch (type) {
       case add:
-        current = Object.assign({}, currentState, extraData);
-        result.push(current);
-        currentState = current;
+        currentState = { ...currentState, ...extraData };
         break;
 
       case remove:
-        current = Object.assign({}, currentState);
-
         for (const key of keysToRemove) {
-          delete current[key];
+          delete currentState[key];
         }
-
-        result.push(current);
-        currentState = current;
         break;
 
       case clear:
-        current = {};
-        result.push(current);
-        currentState = current;
+        currentState = {};
         break;
     }
+
+    result.push({ ...currentState });
   }
 
   return result;
 }
+
 
 module.exports = transformStateWithClones;
