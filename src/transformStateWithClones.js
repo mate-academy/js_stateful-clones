c292ff362ea4700f4cdce38497087d81d4a45063
Oleksii Kozlov <oleksiikozlov@gmail.com>
2024-05-01 19:48:51 +0300

    Stateful Clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,27 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const res = [];
+  let newObj = Object.assign({}, state);
+
+  for (const obj of actions) {
+    if (obj.type === 'addProperties') {
+      newObj = Object.assign(newObj, obj.extraData);
+    }
+
+    if (obj.type === 'removeProperties') {
+      for (const keyToRemove of obj.keysToRemove) {
+        delete newObj[keyToRemove];
+      }
+    }
+
+    if (obj.type === 'clear') {
+      newObj = {};
+    }
+    res.push({ ...newObj });
+  }
+
+  return res;
 }
 
 module.exports = transformStateWithClones;
