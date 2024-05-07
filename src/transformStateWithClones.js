b542bb19ba42bef89cd2f752d9305a4823b99a21
Turani Yevhen <t.zsenya@gmail.com>
2024-05-07 17:05:43 +0200

    improvment

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,21 +11,23 @@ function transformStateWithClones(state, actions) {
   const stateCopy = [];
 
   for (const key in actions) {
-    if (actions[key].type === 'addProperties') {
-      Object.assign(objectCopy, actions[key].extraData);
-      stateCopy.push({ ...objectCopy });
-    }
-
-    if (actions[key].type === 'removeProperties') {
-      for (const rem in actions[key].keysToRemove) {
-        delete objectCopy[actions[key].keysToRemove[rem]];
-      }
-      stateCopy.push({ ...objectCopy });
-    }
-
-    if (actions[key].type === 'clear') {
-      objectCopy = {};
-      stateCopy.push({ ...{} });
+    switch (actions[key].type) {
+      case 'addProperties':
+        Object.assign(objectCopy, actions[key].extraData);
+        stateCopy.push({ ...objectCopy });
+        break;
+      case 'removeProperties':
+        for (const keyToRemove in actions[key].keysToRemove) {
+          delete objectCopy[actions[key].keysToRemove[keyToRemove]];
+        }
+        stateCopy.push({ ...objectCopy });
+        break;
+      case 'clear':
+        objectCopy = {};
+        stateCopy.push({});
+        break;
+      default:
+        break;
     }
   }
 
