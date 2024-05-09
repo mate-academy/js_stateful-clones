b1c3771e692213c4ea3ab7747c505bb3ea6870fa
Svitlana-Vitryk <vitriksvitlana@gmail.com>
2024-05-09 21:29:24 +0300

    transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,27 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const states = [];
+  let currentState = { ...state };
+
+  actions.forEach((action) => {
+    if (action.type === 'clear') {
+      currentState = {};
+    } else if (action.type === 'addProperties') {
+      currentState = { ...currentState, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      const keysToRemove = new Set(action.keysToRemove);
+
+      currentState = Object.fromEntries(
+        Object.entries(currentState).filter(
+          ([key, _]) => !keysToRemove.has(key),
+        ),
+      );
+    }
+    states.push({ ...currentState });
+  });
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
