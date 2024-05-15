4cb457545018a022f3ccc4047d25309a13a8fcd9
Alexejklimov <alexej.kl@gmail.com>
2024-05-15 22:55:51 +0200

    rerewriting the code

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,15 +11,17 @@ function transformStateWithClones(state, actions) {
   const result = [];
 
   for (const action of actions) {
-    switch (action.type) {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
       case 'addProperties':
-        for (const key in action.extraData) {
-          newState[key] = action.extraData[key];
+        for (const key in extraData) {
+          newState[key] = extraData[key];
         }
         break;
 
       case 'removeProperties':
-        for (const key of action.keysToRemove) {
+        for (const key of keysToRemove) {
           delete newState[key];
         }
         break;
