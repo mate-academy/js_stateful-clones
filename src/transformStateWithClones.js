42df46255e82928c502a2d7a2331dbe6ff6661c1
MariCodec <masha1994la@gmail.com>
2024-05-18 11:05:46 +0300

    corrected some comments

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,31 +8,24 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
-  const currentState = { ...state };
+  let currentState = { ...state };
   const stateHistory = [];
 
-  for (const obj of actions) {
-    if (obj.type === 'addProperties') {
-      for (const date in obj.extraData) {
-        currentState[date] = obj.extraData[date];
-      }
+  for (const property of actions) {
+    if (property.type === 'addProperties') {
+      Object.assign(currentState, property.extraData);
     }
 
-    if (obj.type === 'removeProperties') {
-      for (const date in obj.keysToRemove) {
-        for (const key in currentState) {
-          if (key === obj.keysToRemove[date]) {
-            delete currentState[key];
-          }
-        }
-      }
-    }
-
-    if (obj.type === 'clear') {
-      for (const key in currentState) {
+    if (property.type === 'removeProperties') {
+      property.keysToRemove.map((key) => {
         delete currentState[key];
-      }
+      });
     }
+
+    if (property.type === 'clear') {
+      currentState = {};
+    }
+
     stateHistory.push({ ...currentState });
   }
 
