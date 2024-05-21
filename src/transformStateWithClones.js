78ba7b1cc4a553f359ac828a70a795566b49affb
Vitalii <shut.vitaliy.ol@gmail.com>
2024-05-21 14:25:00 +0300

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,25 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newArray = [];
+
+  let object = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      object = { ...object, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete object[key];
+      }
+    } else if (action.type === 'clear') {
+      object = {};
+    }
+
+    newArray.push({ ...object });
+  }
+
+  return newArray;
 }
 
 module.exports = transformStateWithClones;
