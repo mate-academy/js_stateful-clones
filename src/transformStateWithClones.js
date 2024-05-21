29c93bfdc0846d7aba5b48272537abcb4b543ab0
DSB-18 <boboshko.dmitriy@gmail.com>
2024-05-21 23:34:51 +0300

    Update transformStateWithClones.js
    Default action added

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -25,6 +25,9 @@ function transformStateWithClones(state, actions) {
       case 'removeProperties':
         action.keysToRemove.forEach((key) => delete nextState[key]);
         break;
+
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
     }
 
     stateHistory.push(nextState);
