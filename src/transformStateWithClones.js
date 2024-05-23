8babcfa800c2140add513bbc4fc56f1771566de4
Yaroslav Leshchenko <yaroslavleshenkoforwork@gmail.com>
2024-05-23 17:53:24 +0300

    solved by if else

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const states = [];
+
+  const clonState = { ...state };
+
+  for (const action of actions) {
+    // console.log(action);
+
+    if (action.type === 'addProperties') {
+      Object.assign(clonState, action.extraData);
+      states.push({ ...clonState });
+    } else if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete clonState[key];
+      }
+      states.push({ ...clonState });
+    } else if (action.type === 'clear') {
+      for (const key in clonState) {
+        if (clonState[key]) {
+          delete clonState[key];
+        }
+      }
+      states.push({ ...clonState });
+    }
+  }
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
