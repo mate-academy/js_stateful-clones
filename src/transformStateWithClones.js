74b18efeede42fecbd99e721f7844e653526b45c
Artem Androsenko <tyoma.and@gmail.com>
2024-05-25 12:34:40 +0300

    Fix and improve code

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,48 +8,54 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
-  const stateCopy = { ...state };
   const stateHistory = [];
 
   for (const action of actions) {
+    const lastState =
+      stateHistory.length > 0 ? stateHistory[stateHistory.length - 1] : state;
+    let newState = {};
+
     switch (action.type) {
       case 'addProperties':
-        stateHistory.push(addProperties(stateCopy, action.extraData));
+        newState = addProperties(lastState, action.extraData);
         break;
       case 'removeProperties':
-        stateHistory.push(removeProperties(stateCopy, action.keysToRemove));
+        newState = removeProperties(lastState, action.keysToRemove);
         break;
       case 'clear':
-        stateHistory.push(clearState(stateCopy));
+        newState = clearState();
         break;
+      default:
+        throw new Error(`Sorry but try a different type of actions.`);
     }
+    stateHistory.push(newState);
   }
 
   return stateHistory;
 }
 
-function addProperties(stateCopy, properties) {
+function addProperties(state, properties) {
+  const stateCopy = { ...state };
+
   for (const prop in properties) {
     stateCopy[prop] = properties[prop];
   }
 
-  return { ...stateCopy };
+  return stateCopy;
 }
 
-function removeProperties(stateCopy, properties) {
+function removeProperties(state, properties) {
+  const stateCopy = { ...state };
+
   for (const prop of properties) {
     delete stateCopy[prop];
   }
 
-  return { ...stateCopy };
+  return stateCopy;
 }
 
-function clearState(stateCopy) {
-  for (const state in stateCopy) {
-    delete stateCopy[state];
-  }
-
-  return { ...stateCopy };
+function clearState() {
+  return {};
 }
 
 module.exports = transformStateWithClones;
