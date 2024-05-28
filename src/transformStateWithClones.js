ae29bfdcd04526d7890e7354a6549f80b2473aea
Nazarii Siryi <aakkrryyttoo99@gmail.com>
2024-05-28 15:56:14 +0300

    add solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/* eslint-disable */
 /**
  * @param {Object} state
  * @param {Object[]} actions
@@ -7,7 +7,27 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let newState = { ...state };
+  const historyChanges = [];
+
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'addProperties') {
+      newState = {
+        ...newState,
+        ...actions[i].extraData,
+      };
+    } else if (actions[i].type === 'removeProperties') {
+      for (const fieldName of actions[i].keysToRemove) {
+        delete newState[fieldName];
+      }
+    } else if (actions[i].type === 'clear') {
+      newState = {};
+    }
+    historyChanges.push({ ...newState });
+  }
+
+  return historyChanges;
 }
 
 module.exports = transformStateWithClones;
+/* eslint-enable */
