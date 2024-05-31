642b3646717b4d73a16efd2d88128b6614554950
Volodymyr Isaiev <isaiev.v.o@gmail.com>
2024-06-01 00:26:36 +0300

    Add state history tracker

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const STATE_HISTORY = [];
+  const CLONED_STATE = { ...state };
+
+  for (const ACTION of actions) {
+    switch (ACTION.type) {
+      case 'addProperties':
+        for (const KEY in ACTION.extraData) {
+          CLONED_STATE[KEY] = ACTION.extraData[KEY];
+        }
+
+        STATE_HISTORY.push({ ...CLONED_STATE });
+        break;
+      case 'removeProperties':
+        for (const KEY of ACTION.keysToRemove) {
+          delete CLONED_STATE[KEY];
+        }
+
+        STATE_HISTORY.push({ ...CLONED_STATE });
+        break;
+      case 'clear':
+        for (const KEY in CLONED_STATE) {
+          delete CLONED_STATE[KEY];
+        }
+
+        STATE_HISTORY.push({ ...CLONED_STATE });
+        break;
+    }
+  }
+
+  return STATE_HISTORY;
 }
 
 module.exports = transformStateWithClones;
