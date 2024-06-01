1b3d790f04f1108982f8972838fa0faf158a1102
Deniel Frolov <fvgdanil@gmail.com>
2024-06-01 20:22:45 +0300

    add transform function ver.1

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,27 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const finalObject = [];
+  let modObject = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(modObject, action.extraData);
+      finalObject.push({ ...modObject });
+    }
+
+    if (action.type === 'removeProperties') {
+      action.keysToRemove.forEach((key) => delete modObject[key]);
+      finalObject.push({ ...modObject });
+    }
+
+    if (action.type === 'clear') {
+      modObject = {};
+      finalObject.push({ ...modObject });
+    }
+  }
+
+  return finalObject;
 }
 
 module.exports = transformStateWithClones;
