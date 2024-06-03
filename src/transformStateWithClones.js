33266552f8df3e49d2ceab71941c089cc7450c79
Nazarii Siryi <aakkrryyttoo99@gmail.com>
2024-06-03 18:41:39 +0300

    fix problems#2

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -10,16 +10,16 @@ function transformStateWithClones(state, actions) {
   let newState = { ...state };
   const historyChanges = [];
 
-  for (let i = 0; i < actions.length; i++) {
-    switch (actions[i].type) {
+  for (const action of actions) {
+    switch (action.type) {
       case 'addProperties':
         newState = {
           ...newState,
-          ...actions[i].extraData,
+          ...action.extraData,
         };
         break;
       case 'removeProperties':
-        for (const fieldName of actions[i].keysToRemove) {
+        for (const fieldName of action.keysToRemove) {
           delete newState[fieldName];
         }
         break;
