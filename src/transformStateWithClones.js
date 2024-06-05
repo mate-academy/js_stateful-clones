27c2acd8986d48196920fab3a04d75a4dac1cf16
Anastasiia Tatarchenko <neveykina.nastya@gmail.com>
2024-06-05 16:33:28 +0200

    remade the work

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,47 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const statesStack = [];
+  const result = [];
+  const stateCopy = { ...state };
 
   for (const action of actions) {
-    const currentState = statesStack.length
-      ? statesStack[statesStack.length - 1]
-      : state;
-
     switch (action.type) {
       case 'addProperties':
-        statesStack.push(addProperties(currentState, action.extraData));
+        for (const key in action.extraData) {
+          stateCopy[key] = action.extraData[key];
+        }
         break;
-
       case 'removeProperties':
-        statesStack.push(removeProperties(currentState, action.keysToRemove));
+        for (const key of action.keysToRemove) {
+          delete stateCopy[key];
+        }
         break;
-
       case 'clear':
-        statesStack.push(clearProperties(currentState));
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
         break;
+      default:
+        throw new Error('Unknown action type: ' + action.type);
     }
+    result.push({ ...stateCopy });
   }
 
-  function addProperties(lastElementStack, extraData) {
-    return Object.assign({}, lastElementStack, extraData);
-  }
-
-  function removeProperties(lastElementStack, keysToRemove) {
-    const newObject = { ...lastElementStack };
-
-    for (const key of keysToRemove) {
-      delete newObject[key];
-    }
-
-    return newObject;
-  }
-
-  function clearProperties(lastElementStack) {
-    return {};
-  }
-
-  return statesStack;
+  return result;
 }
 
 module.exports = transformStateWithClones;
