b3777251ab81077dcd92b9a931ca32018c51a228
Arthur Tolkachov <babanjerelo@gmail.com>
2024-06-06 20:10:35 +0300

    [Feature] added transform state function

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,50 @@
  *
  * @return {Object[]}
  */
+
+const ACTION_TYPES = {
+  add: 'addProperties',
+  remove: 'removeProperties',
+  clear: 'clear',
+};
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const history = [];
+
+  for (const { type, extraData, keysToRemove } of actions) {
+    switch (type) {
+      case ACTION_TYPES.add: {
+        const prevStateCopy = { ...(history.at(-1) ?? state) };
+
+        history.push({ ...prevStateCopy, ...extraData });
+
+        continue;
+      }
+
+      case ACTION_TYPES.remove: {
+        const prevStateCopy = { ...(history.at(-1) ?? state) };
+
+        for (const key of keysToRemove) {
+          delete prevStateCopy[key];
+        }
+
+        history.push(prevStateCopy);
+        continue;
+      }
+
+      case ACTION_TYPES.clear: {
+        history.push({});
+
+        continue;
+      }
+
+      default: {
+        throw new Error('Unexpected action type!');
+      }
+    }
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
