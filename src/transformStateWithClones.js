16818728421f5cb3a07ff2fd81850af6275ab482
Kateryna <moskkat40@gmail.com>
2024-06-07 18:10:55 +0200

    add task js_stateful-clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 'use strict';
 
 /**
@@ -7,7 +8,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
-}
+  const result = [];
 
+  let newObject = { ...state };
+
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    if (type === 'addProperties') {
+      newObject = { ...newObject, ...extraData };
+    }
+
+    if (type === 'removeProperties') {
+      for (const item of keysToRemove) {
+        delete newObject[item];
+      }
+    }
+
+    if (type === 'clear') {
+      for (const key in newObject) {
+        delete newObject[key];
+      }
+    }
+
+    result.push(newObject);
+  }
+
+  return result;
+}
 module.exports = transformStateWithClones;
