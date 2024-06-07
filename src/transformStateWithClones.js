1c8e35ffe9a967b82b42ecddf99f0d46b28a7fa5
MelnykVlad <21vladmel15@gmail.com>
2024-06-07 13:02:45 +0300

    transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,25 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+  let currentCopy = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'clear') {
+      currentCopy = {};
+    } else if (action.type === 'addProperties') {
+      currentCopy = { ...currentCopy, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      currentCopy = { ...currentCopy };
+
+      for (const key of action.keysToRemove) {
+        delete currentCopy[key];
+      }
+    }
+    stateHistory.push(currentCopy);
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
