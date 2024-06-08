18f318165f0b727c3524db6059f390f86f5a4de1
Viktor Shchehelskyi <victorshegelsky@gmail.com>
2024-06-08 23:44:32 +0300

    finish

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,39 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+  const currentState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(currentState, action.extraData);
+        stateHistory.push({ ...currentState });
+        break;
+
+      case 'removeProperties':
+        for (const ch of action.keysToRemove) {
+          if (currentState.hasOwnProperty(ch)) {
+            delete currentState[ch];
+          }
+        }
+        stateHistory.push({ ...currentState });
+        break;
+
+      case 'clear':
+        for (const j in currentState) {
+          if (currentState.hasOwnProperty(j)) {
+            delete currentState[j];
+          }
+        }
+        stateHistory.push({ ...currentState });
+        break;
+    }
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
