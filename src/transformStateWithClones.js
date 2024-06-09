65a8f1c8e617bf0f47cdb912010a04e74623725f
Vadim <volandemortuwka@gmail.com>
2024-06-09 12:05:01 +0300

    save

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+  const preperadeState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(preperadeState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete preperadeState[key];
+        }
+
+        break;
+
+      case 'clear':
+        for (const key in preperadeState) {
+          delete preperadeState[key];
+        }
+        break;
+    }
+
+    stateHistory.push(Object.assign({}, preperadeState));
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
