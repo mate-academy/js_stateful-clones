7bf4707a258658bc68798af146f033e70d552b48
Stas <staskapica93@gmail.com>
2024-06-11 18:34:21 +0200

    v 1.1

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,42 +7,37 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  let addProperties = {};
-  let removeProperties = {};
-  let cleanProperties = {};
-  let addObject = state;
+  let currentState = state;
 
   const array = [];
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      addProperties = { ...addObject, ...action.extraData };
-      array.push(addProperties);
-      addObject = addProperties;
-      continue;
-    }
+    switch (action.type) {
+      case 'addProperties': {
+        const addProperties = { ...currentState, ...action.extraData };
 
-    if (action.type === 'removeProperties') {
-      removeProperties = { ...addObject };
-
-      for (const i of action.keysToRemove) {
-        delete removeProperties[i];
+        currentState = addProperties;
+        break;
       }
-      array.push(removeProperties);
-      addObject = removeProperties;
-      continue;
-    }
 
-    if (action.type === 'clear') {
-      cleanProperties = { ...state };
+      case 'removeProperties': {
+        const actionProperty = action.keysToRemove;
+        const removeProperties = { ...currentState };
 
-      for (const keys in cleanProperties) {
-        delete cleanProperties[keys];
+        for (const i of actionProperty) {
+          delete removeProperties[i];
+        }
+        currentState = removeProperties;
+        break;
       }
-      array.push(cleanProperties);
-      addObject = cleanProperties;
-      continue;
+
+      case 'clear': {
+        currentState = {};
+        break;
+      }
     }
+
+    array.push(currentState);
   }
 
   return array;
