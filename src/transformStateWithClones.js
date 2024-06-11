20c6abc791eba149ba3eb249af0d9450f27361ad
Stas <staskapica93@gmail.com>
2024-06-11 18:04:28 +0200

    v 1.0

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,45 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let addProperties = {};
+  let removeProperties = {};
+  let cleanProperties = {};
+  let addObject = state;
+
+  const array = [];
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      addProperties = { ...addObject, ...action.extraData };
+      array.push(addProperties);
+      addObject = addProperties;
+      continue;
+    }
+
+    if (action.type === 'removeProperties') {
+      removeProperties = { ...addObject };
+
+      for (const i of action.keysToRemove) {
+        delete removeProperties[i];
+      }
+      array.push(removeProperties);
+      addObject = removeProperties;
+      continue;
+    }
+
+    if (action.type === 'clear') {
+      cleanProperties = { ...state };
+
+      for (const keys in cleanProperties) {
+        delete cleanProperties[keys];
+      }
+      array.push(cleanProperties);
+      addObject = cleanProperties;
+      continue;
+    }
+  }
+
+  return array;
 }
 
 module.exports = transformStateWithClones;
