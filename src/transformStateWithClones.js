5c25aa4e31bcca62218028e98c23be950925ca26
Aleksandr Shramchenko <mostwar1988@gmail.com>
2024-06-13 10:10:52 +0300

    clone

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,49 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let anotherState = {};
+  let massState = [];
+
+  if (state) {
+    anotherState = { ...state };
+  }
+
+  for (const action in actions) {
+    const {
+      type: anotherType,
+      extraData: anotherExtraData = {},
+      keysToRemove: anotherKeysToRemove = [],
+    } = actions[action];
+
+    if (anotherType === 'addProperties') {
+      Object.assign(anotherState, anotherExtraData);
+      massState.push(anotherState);
+    }
+    // return anotherState;
+
+    if (anotherType === 'removeProperties') {
+      for (const del in anotherKeysToRemove) {
+        if (anotherState[anotherKeysToRemove[del]]) {
+          delete anotherState[anotherKeysToRemove[del]];
+        }
+      }
+      massState.push(anotherState);
+    }
+
+    // return anotherState;
+
+    if (anotherType === 'clear') {
+      if (anotherState) {
+        for (const delItem in anotherState) {
+          delete anotherState[delItem];
+        }
+      }
+      anotherState = {};
+      massState.push(anotherState);
+    }
+  }
+
+  return massState;
 }
 
 module.exports = transformStateWithClones;
