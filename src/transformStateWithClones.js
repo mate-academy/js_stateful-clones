34b949460d59bd01dd5fc0ed411a50c3775b6254
luter <newgglutt@gmail.com>
2024-06-16 16:28:56 +0300

    add new reviews

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const states = [];
+  let currentState = { ...state };
+
+  actions.forEach((action) => {
+    switch (action.type) {
+      case 'clear':
+        currentState = {};
+        break;
+      case 'addProperties':
+        currentState = { ...currentState, ...action.extraData };
+        break;
+      case 'removeProperties':
+        if (Array.isArray(action.keysToRemove)) {
+          action.keysToRemove.forEach((key) => {
+            delete currentState[key];
+          });
+        }
+        break;
+      default:
+        break;
+    }
+    states.push({ ...currentState });
+  });
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
