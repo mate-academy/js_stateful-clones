08c4ae0252951649d9108f519ceca2c408c68c4c
Olia Shymanska <oliaa.shymanska@gmail.com>
2024-06-19 01:30:37 +0300

    creat function transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,50 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const transformState = [];
+  const cloneState = { ...state };
+
+  for (const action of actions) {
+    let newObj = {};
+
+    switch (action.type) {
+      case 'clear':
+        newObj = { ...clear(cloneState) };
+        break;
+
+      case 'addProperties':
+        newObj = { ...addProperties(cloneState, action.extraData) };
+        break;
+
+      case 'removeProperties':
+        newObj = { ...removeProperties(cloneState, action.keysToRemove) };
+        break;
+    }
+
+    transformState.push(newObj);
+  }
+
+  return transformState;
+}
+
+function addProperties(cloneState, extraData) {
+  return Object.assign(cloneState, extraData);
+}
+
+function removeProperties(cloneState, keysToRemove) {
+  for (const key of keysToRemove) {
+    delete cloneState[key];
+  }
+
+  return cloneState;
+}
+
+function clear(cloneState) {
+  for (const key in cloneState) {
+    delete cloneState[key];
+  }
+
+  return cloneState;
 }
 
 module.exports = transformStateWithClones;
