89c7baa45dfb7958300c7f2868c35583b5e36782
Bohdan Smachylo <smachyloooo@gmail.com>
2024-06-19 23:11:40 +0200

    change as ai recomend

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -13,16 +13,20 @@ function transformStateWithClones(state, actions) {
   for (const action of actions) {
     currentState = { ...currentState };
 
-    if (action.type === 'addProperties') {
-      for (const key in action.extraData) {
-        currentState[key] = action.extraData[key];
-      }
-    } else if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete currentState[key];
-      }
-    } else if (action.type === 'clear') {
-      currentState = {};
+    switch (action.type) {
+      case 'addProperties':
+        for (const key in action.extraData) {
+          currentState[key] = action.extraData[key];
+        }
+        break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete currentState[key];
+        }
+        break;
+      default:
+        currentState = {};
+        break;
     }
 
     history.push({ ...currentState });
