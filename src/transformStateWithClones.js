0766f900dd0ad15962191742b283161519521476
Kirill Gasan <dp160190gkj@gmail.com>
2024-06-20 17:17:03 +0300

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,27 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const historyArray = [];
+  let changedObject = { ...state };
+
+  actions.forEach((action) => {
+    if (action.type === 'clear') {
+      changedObject = {};
+      historyArray.push({ ...changedObject });
+    }
+
+    if (action.type === 'addProperties') {
+      Object.assign(changedObject, action.extraData);
+      historyArray.push({ ...changedObject });
+    }
+
+    if (action.type === 'removeProperties') {
+      action.keysToRemove.forEach((key) => delete changedObject[key]);
+      historyArray.push({ ...changedObject });
+    }
+  });
+
+  return historyArray;
 }
 
 module.exports = transformStateWithClones;
