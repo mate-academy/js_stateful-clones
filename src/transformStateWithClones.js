09119b06131a6fd4842177a140731ee5f86750e4
Maksym Hryvko <grivkomaksim70@gmail.com>
2024-06-23 19:32:11 +0300

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,3 +1,11 @@
+'use strict';
+
+/**
+ * @param {Object} state
+ * @param {Object[]} actions
+ *
+ * @return {Object[]}
+ */
 function transformStateWithClones(state, actions) {
   const result = [];
   let currentState = { ...state };
