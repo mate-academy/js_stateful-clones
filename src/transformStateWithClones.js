72c635b9df3e794fed36e190657c59e935d8177e
Andrii Zakharenko <andriizakharenko@gmail.com>
2024-07-03 01:07:08 +0300

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,34 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const currentState = { ...state };
+  const newState = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(currentState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const remove of action.keysToRemove) {
+          delete currentState[remove];
+        }
+        break;
+
+      case 'clear':
+        for (const key in currentState) {
+          delete currentState[key];
+        }
+        break;
+
+      default:
+        throw new Error(`Unhandled action type: ${action.type}`);
+    }
+    newState.push({ ...currentState });
+  }
+
+  return newState;
 }
 
 module.exports = transformStateWithClones;
