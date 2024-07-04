fd7a2e44e2af59950012ffa2e592527220966d94
Maksym <max.borovique@gmail.com>
2024-07-04 15:07:42 +0300

    omg

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
-}
+  const states = [];
+  let currentState = { ...state };
 
+  for (const action of actions) {
+    let newState = { ...currentState };
+
+    switch (action.type) {
+      case 'clear':
+        newState = {};
+        break;
+      case 'addProperties':
+        Object.assign(newState, action.extraData);
+        break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete newState[key];
+        }
+        break;
+      default:
+        break;
+    }
+
+    states.push(newState);
+    currentState = newState;
+  }
+
+  return states;
+}
 module.exports = transformStateWithClones;
