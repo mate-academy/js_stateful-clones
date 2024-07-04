fda6ad73d1cca8417e4ac125994d3f43225e69ab
Andrii Zakharenko <andriizakharenko@gmail.com>
2024-07-04 09:40:18 +0300

    changed variables name, updated an empty object for clear case

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,34 +8,32 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
-  const currentState = { ...state };
-  const newState = [];
+  let previousState = { ...state };
+  const history = [];
 
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties':
-        Object.assign(currentState, action.extraData);
+        Object.assign(previousState, action.extraData);
         break;
 
       case 'removeProperties':
         for (const remove of action.keysToRemove) {
-          delete currentState[remove];
+          delete previousState[remove];
         }
         break;
 
       case 'clear':
-        for (const key in currentState) {
-          delete currentState[key];
-        }
+        previousState = {};
         break;
 
       default:
         throw new Error(`Unhandled action type: ${action.type}`);
     }
-    newState.push({ ...currentState });
+    history.push({ ...previousState });
   }
 
-  return newState;
+  return history;
 }
 
 module.exports = transformStateWithClones;
