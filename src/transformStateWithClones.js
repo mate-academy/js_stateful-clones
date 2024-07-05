09b46a3a45f81a9e49cb230d9691a07c7a973859
Artem-Brui <artembryj@gmail.com>
2024-07-05 12:56:17 +0200

    solved all comments

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -9,31 +9,30 @@
 function transformStateWithClones(state, actions) {
   const stateCopyList = [];
 
-  const oneAction = (task) => {
+  for (const oneAction of actions) {
+    const { type, extraData, keysToRemove } = oneAction;
     const stateCopy =
-      stateCopyList[0] !== undefined
-        ? { ...stateCopyList[stateCopyList.length - 1] }
-        : { ...state };
+      stateCopyList[0] === undefined
+        ? { ...state }
+        : { ...stateCopyList[stateCopyList.length - 1] };
 
-    switch (task.type) {
+    switch (type) {
       case 'addProperties':
-        Object.assign(stateCopy, task.extraData);
+        Object.assign(stateCopy, extraData);
         break;
       case 'removeProperties':
-        task.keysToRemove.forEach((key) => delete stateCopy[key]);
+        keysToRemove.forEach((key) => delete stateCopy[key]);
         break;
       case 'clear':
         Object.keys(stateCopy).forEach((key) => delete stateCopy[key]);
         break;
       default:
         Object.keys(stateCopy).forEach((key) => delete stateCopy[key]);
-        Object.assign(stateCopy, { error: `${task.type} is not available.` });
+        Object.assign(stateCopy, { error: `${type} is not available.` });
     }
 
-    return stateCopy;
-  };
-
-  actions.forEach((task) => stateCopyList.push(oneAction(task)));
+    stateCopyList.push(stateCopy);
+  }
 
   return stateCopyList;
 }
