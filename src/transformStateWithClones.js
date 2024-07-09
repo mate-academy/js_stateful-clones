20835a991bc88e0912784621a9fd86aec2cf078b
Sergiy Dolgov <info@devmax.pro>
2024-07-09 11:18:51 -0400

    v 1.0

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newState = { ...state };
+  const history = [];
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      for (const [key, value] of Object.entries(action.extraData)) {
+        newState[key] = value;
+      }
+      history.push({ ...newState });
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete newState[key];
+      }
+      history.push({ ...newState });
+    }
+
+    if (action.type === 'clear') {
+      Object.keys(newState).forEach((key) => {
+        delete newState[key];
+      });
+      history.push({ ...newState });
+    }
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
