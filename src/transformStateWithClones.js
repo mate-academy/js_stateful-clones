ab0808054c10905268f922ca309b5a3382f7569b
Teut`s <teuts@MacBook-Air.localdomain>
2024-07-10 14:46:37 +0300

    taskStatefulClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,26 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let newState = { ...state };
+  const states = [];
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      newState = { ...newState, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      const updatedState = { ...newState };
+
+      for (const keyToRemove of action.keysToRemove) {
+        delete updatedState[keyToRemove];
+      }
+      newState = updatedState;
+    } else if (action.type === 'clear') {
+      newState = {};
+    }
+    states.push({ ...newState });
+  }
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
