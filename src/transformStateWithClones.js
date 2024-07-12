bb795a853dfc1f67f80f752d115709661ebc51d8
Sviatoslav Pokhvalenko <pokhvalenkosvyat@gmail.com>
2024-07-11 22:23:09 -0400

    changed my task according to ai buddy advices

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -9,27 +9,25 @@
 function transformStateWithClones(state, actions) {
   // write code here
   const stateHistory = [];
-  let currentState = { ...state }; // Start with a clone of the initial state
+  let stateCopy = { ...state }; // Start with a clone of the initial state
 
   actions.forEach((action) => {
     switch (action.type) {
       case 'addProperties':
-        currentState = { ...currentState, ...action.extraData };
+        stateCopy = { ...stateCopy, ...action.extraData };
         break;
       case 'removeProperties':
-        currentState = { ...currentState };
-
         action.keysToRemove.forEach((key) => {
-          delete currentState[key];
+          delete stateCopy[key];
         });
         break;
       case 'clear':
-        currentState = {};
+        stateCopy = {};
         break;
       default:
         throw new Error(`Unknown action type: ${action.type}`);
     }
-    stateHistory.push(currentState);
+    stateHistory.push({ ...stateCopy });
   });
 
   return stateHistory;
