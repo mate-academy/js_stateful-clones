320f37e5a281b1988e7ce15afe048ca5e08efc59
Kayky Rodrigues da Silva <kaykyr.dev@gmail.com>
2024-07-12 01:37:18 -0300

    feat: Resolve PR changes

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -12,41 +12,31 @@ function transformStateWithClones(state, actions) {
 
   const addProperties = (extraData) => {
     stateCopy = Object.assign(stateCopy, extraData);
-    stateHistory.push({ ...stateCopy });
   };
 
   const removeProperties = (keysToRemove) => {
-    for (const remove in keysToRemove) {
-      for (const keys in stateCopy) {
-        if (keysToRemove[remove] === keys) {
-          delete stateCopy[keys];
-        }
-      }
+    for (const remove of keysToRemove) {
+      delete stateCopy[remove];
     }
-
-    stateHistory.push({ ...stateCopy });
   };
 
   const clear = () => {
-    for (const keys in stateCopy) {
-      delete stateCopy[keys];
-    }
-
-    stateHistory.push({ ...stateCopy });
+    stateCopy = Object.assign({});
   };
 
   for (const keys in actions) {
-    if (actions[keys].type === 'addProperties') {
-      addProperties(actions[keys].extraData);
+    switch (actions[keys].type) {
+      case 'addProperties':
+        addProperties(actions[keys].extraData);
+        break;
+      case 'removeProperties':
+        removeProperties(actions[keys].keysToRemove);
+        break;
+      case 'clear':
+        clear();
+        break;
     }
-
-    if (actions[keys].type === 'removeProperties') {
-      removeProperties(actions[keys].keysToRemove);
-    }
-
-    if (actions[keys].type === 'clear') {
-      clear(state);
-    }
+    stateHistory.push({ ...stateCopy });
   }
 
   return stateHistory;
