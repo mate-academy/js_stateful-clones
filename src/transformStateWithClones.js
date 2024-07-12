8ddbb589a82662607dcc0b60bbd1a67f02dc06fb
Yevhenii Lutsenko <evgenlutscenko@icloud.com>
2024-07-12 12:18:00 +0200

    some changes

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -13,33 +13,33 @@ function transformStateWithClones(state, actions) {
   for (const action of actions) {
     const { type = null, extraData = {}, keysToRemove = [] } = action;
 
-    let newObj;
-
-    if (states.length === 0) {
-      newObj = { ...state };
-    } else {
-      newObj = { ...states[states.length - 1] };
-    }
+    const obj =
+      states.length === 0 ? { ...state } : { ...states[states.length - 1] };
 
     switch (type) {
       case 'addProperties':
-        newObj = Object.assign(newObj, extraData);
+        const newObj = {
+          ...obj,
+          ...extraData,
+        };
+
+        states.push(newObj);
         break;
       case 'removeProperties':
         keysToRemove.forEach((element) => {
-          if (newObj[element]) {
-            delete newObj[element];
-          }
+          delete obj[element];
         });
+        states.push(obj);
         break;
       case 'clear':
-        Object.keys(newObj).forEach((element) => {
-          delete newObj[element];
+        Object.keys(obj).forEach((element) => {
+          delete obj[element];
         });
+        states.push(obj);
         break;
+      default:
+        throw new Error(`Unknow type: ${type}`);
     }
-
-    states.push(newObj);
   }
 
   return states;
