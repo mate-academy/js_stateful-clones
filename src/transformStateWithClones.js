4a3f1eebb063af7c12f465913f87c70e9940a23a
Andrii IEvych <yevych27@gmail.com>
2024-07-13 12:14:55 +0300

    fix

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,22 +11,23 @@ function transformStateWithClones(state, actions) {
   const newState = { ...state };
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(newState, action.extraData);
-    }
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(newState, action.extraData);
+        break;
 
-    if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete newState[key];
-      }
-    }
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete newState[key];
+        }
+        break;
 
-    if (action.type === 'clear') {
-      for (const key in newState) {
-        delete newState[key];
-      }
+      case 'clear':
+        for (const key in newState) {
+          delete newState[key];
+        }
+        break;
     }
-
     stateHistory.push({ ...newState });
   }
 
