88adf10e84d3fc5f556d2ac806f8de59a2f50db3
Jonathan da Silva Hora <jonathan.s.hora@gmail.com>
2024-07-15 19:33:08 -0300

    add solution task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,38 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+  let currentState = state;
+
+  actions.forEach((action) => {
+    let nextState;
+
+    switch (action.type) {
+      case 'clear':
+        nextState = {};
+        break;
+      case 'addProperties':
+        nextState = { ...currentState, ...action.extraData };
+        break;
+      case 'removeProperties':
+        nextState = { ...currentState };
+
+        action.keysToRemove.forEach((key) => {
+          if (nextState.hasOwnProperty(key)) {
+            delete nextState[key];
+          }
+        });
+        break;
+      default:
+        nextState = { ...currentState };
+        break;
+    }
+
+    stateHistory.push(nextState);
+    currentState = nextState; // Update currentState for the next iteration
+  });
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
