f12034b97f405abb6f32c2bcc94a21b5a0943ccc
renata-aline <renatalinec@gmail.com>
2024-07-16 14:47:10 -0300

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,39 +6,74 @@
  *
  * @return {Object[]}
  */
+// function transformStateWithClones(state, actions) {
+//   let stateClone = { ...state };
+//   const list = [];
+
+//   function removeProperties(obj, keysToRemove) {
+//     for (let index = 0; index < keysToRemove.length; index++) {
+//       delete stateClone[keysToRemove[index]];
+//       delete obj[keysToRemove[index]];
+//     }
+
+//     return obj;
+//   }
+
+//   for (let index = 0; index < actions.length; index++) {
+//     const item = actions[index];
+
+//     switch (item.type) {
+//       case 'addProperties':
+//         stateClone = { ...stateClone, ...item.extraData };
+//         list.push({ ...stateClone, ...item.extraData });
+//         break;
+//       case 'removeProperties':
+//         list.push(removeProperties({ ...stateClone }, item.keysToRemove));
+//         break;
+//       case 'clear':
+//         stateClone = {};
+//         list.push({});
+//         break;
+//       default:
+//         break;
+//     }
+//   }
+
+//   return list;
+// }
+
 function transformStateWithClones(state, actions) {
-  let stateClone = { ...state };
-  const list = [];
+  const stateHistory = [];
+  const stateClone = { ...state };
 
-  function removeProperties(obj, keysToRemove) {
-    for (let index = 0; index < keysToRemove.length; index++) {
-      delete stateClone[keysToRemove[index]];
-      delete obj[keysToRemove[index]];
-    }
-
-    return obj;
-  }
-
-  for (let index = 0; index < actions.length; index++) {
-    const item = actions[index];
-
-    switch (item.type) {
+  for (const action of actions) {
+    switch (action.type) {
       case 'addProperties':
-        stateClone = { ...stateClone, ...item.extraData };
-        list.push({ ...stateClone, ...item.extraData });
+        Object.assign(stateClone, action.extraData);
         break;
+
       case 'removeProperties':
-        list.push(removeProperties({ ...stateClone }, item.keysToRemove));
+        for (const key of action.keysToRemove) {
+          delete stateClone[key];
+        }
         break;
+
       case 'clear':
-        stateClone = {};
-        list.push({});
+        for (const key in stateClone) {
+          delete stateClone[key];
+        }
         break;
+
       default:
         break;
     }
+
+    const newStateClone = { ...stateClone };
+
+    stateHistory.push(newStateClone);
   }
 
-  return list;
+  return stateHistory;
 }
+
 module.exports = transformStateWithClones;
