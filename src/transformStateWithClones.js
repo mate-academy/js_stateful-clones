5ccd527ffe3b5622f9f570be257b9114098a2bf1
renata-aline <renatalinec@gmail.com>
2024-07-16 14:50:55 -0300

    made requested chnage

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,41 +6,6 @@
  *
  * @return {Object[]}
  */
-// function transformStateWithClones(state, actions) {
-//   let stateClone = { ...state };
-//   const list = [];
-
-//   function removeProperties(obj, keysToRemove) {
-//     for (let index = 0; index < keysToRemove.length; index++) {
-//       delete stateClone[keysToRemove[index]];
-//       delete obj[keysToRemove[index]];
-//     }
-
-//     return obj;
-//   }
-
-//   for (let index = 0; index < actions.length; index++) {
-//     const item = actions[index];
-
-//     switch (item.type) {
-//       case 'addProperties':
-//         stateClone = { ...stateClone, ...item.extraData };
-//         list.push({ ...stateClone, ...item.extraData });
-//         break;
-//       case 'removeProperties':
-//         list.push(removeProperties({ ...stateClone }, item.keysToRemove));
-//         break;
-//       case 'clear':
-//         stateClone = {};
-//         list.push({});
-//         break;
-//       default:
-//         break;
-//     }
-//   }
-
-//   return list;
-// }
 
 function transformStateWithClones(state, actions) {
   const stateHistory = [];
