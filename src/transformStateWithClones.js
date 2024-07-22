22423c31bf635076630910dcb565729b6ed869e8
Bohdan Hovorukha <hovorukha.bohdan@gmail.com>
2024-07-22 20:39:52 +0300

    Added trasformStateWithClones method

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,47 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+  const newState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'clear':
+        clearState(newState);
+        stateHistory.push({ ...newState });
+        break;
+
+      case 'addProperties':
+        addProperties(newState, action.extraData);
+        stateHistory.push({ ...newState });
+        break;
+
+      case 'removeProperties':
+        removeProperties(newState, action.keysToRemove);
+        stateHistory.push({ ...newState });
+        break;
+    }
+  }
+
+  return stateHistory;
+}
+
+function clearState(state) {
+  for (const property in state) {
+    delete state[property];
+  }
+}
+
+function addProperties(state, extraData) {
+  for (const property in extraData) {
+    state[property] = extraData[property];
+  }
+}
+
+function removeProperties(state, keys) {
+  for (const key of keys) {
+    delete state[key];
+  }
 }
 
 module.exports = transformStateWithClones;
