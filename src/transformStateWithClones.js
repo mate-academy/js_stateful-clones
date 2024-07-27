8c3211d6d234d82c9995a9348b4d240c2cb57cef
Vladyslav Humeniak <vladyslavhumenyak@gmail.com>
2024-07-27 12:36:11 +0300

    problem with 3 tests

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const STATES = [];
+  let copy = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'clear':
+        copy = {};
+        break;
+      case 'addProperties':
+        copy = { ...action.extraData };
+        break;
+      case 'removeProperties':
+        copy = { ...copy };
+
+        for (const key of action.keysToRemove) {
+          delete copy[key];
+        }
+        break;
+      default:
+        break;
+    }
+
+    STATES.push(copy);
+  }
+
+  return STATES;
 }
 
 module.exports = transformStateWithClones;
