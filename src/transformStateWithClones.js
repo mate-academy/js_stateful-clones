c52f4ab6cde5df838cf83de0350faf3964d6b18d
Artur Misko <arthurmisko@gmail.com>
2024-07-28 14:44:47 -0500

    task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const copyState = { ...state };
+  const massiveToReturn = [];
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(copyState, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete copyState[key];
+      }
+    }
+
+    if (action.type === 'clear') {
+      for (const prop in copyState) {
+        delete copyState[prop];
+      }
+    }
+    massiveToReturn.push({ ...copyState });
+  }
+
+  return massiveToReturn;
 }
 
 module.exports = transformStateWithClones;
