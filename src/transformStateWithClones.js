9c8f4aec31863752412c4a6e625fb5bb3a483ca1
vanvalera <115824927+vanvalera@users.noreply.github.com>
2024-07-30 19:51:37 +0300

    completed task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const objArr = [];
+  const newState = { ...state };
+
+  for (const a of actions) {
+    switch (a.type) {
+      case 'addProperties':
+        Object.assign(newState, a.extraData);
+        objArr.push({ ...newState });
+        break;
+
+      case 'removeProperties':
+        for (const k of a.keysToRemove) {
+          delete newState[k];
+        }
+        objArr.push({ ...newState });
+        break;
+
+      case 'clear':
+        for (const key in newState) {
+          delete newState[key];
+        }
+        objArr.push({});
+        break;
+    }
+  }
+
+  return objArr;
 }
 
 module.exports = transformStateWithClones;
