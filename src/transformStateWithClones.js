1cab63f776fb83d1f6de1ebacddf0b4d59ea1e28
Dima Kovalkov <vysserath@gmail.com>
2024-08-03 20:26:11 +0300

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,25 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const resultArray = [];
+  let copyState = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(copyState, action.extraData);
+      resultArray.push(Object.assign({}, copyState));
+    } else if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete copyState[key];
+      }
+      resultArray.push(Object.assign({}, copyState));
+    } else if (action.type === 'clear') {
+      copyState = {};
+      resultArray.push({});
+    }
+  }
+
+  return resultArray;
 }
 
 module.exports = transformStateWithClones;
