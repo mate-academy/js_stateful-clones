40e4975f631aa845ba07d9fdfc1349463d2b5553
Vlad Zahorovskyi <zagorovskii12@gmail.com>
2024-08-04 14:46:28 +0300

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const history = [];
+  const stateCopy = { ...state };
+
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'addProperties') {
+      Object.assign(stateCopy, actions[i].extraData);
+      history.push({ ...stateCopy });
+    }
+
+    if (actions[i].type === 'removeProperties') {
+      for (let j = 0; j < actions[i].keysToRemove.length; j++) {
+        delete stateCopy[actions[i].keysToRemove[j]];
+      }
+      history.push({ ...stateCopy });
+    }
+
+    if (actions[i].type === 'clear') {
+      Object.keys(stateCopy).forEach((key) => delete stateCopy[key]);
+      history.push({ ...stateCopy });
+    }
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
