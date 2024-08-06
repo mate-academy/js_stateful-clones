340e99b524b7831b73ec6a03b24d92c27403bc36
Your Name <you@example.com>
2024-08-06 14:34:04 +0700

    feat: delete stateCloen with Object.fromEntries([]), push empty object to stateHistory

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,6 +6,39 @@
  *
  * @return {Object[]}
  */
+const state = {
+  foo: 'bar',
+  name: 'Jim',
+  another: 'one',
+};
+
+console.log(
+  transformStateWithClones(state, [
+    {
+      type: 'removeProperties',
+      keysToRemove: ['another'],
+    },
+    { type: 'clear' },
+    { type: 'clear' },
+    { type: 'clear' },
+    {
+      type: 'addProperties',
+      extraData: { yet: 'another property' },
+    },
+    { type: 'clear' },
+    {
+      type: 'addProperties',
+      extraData: {
+        foo: 'bar',
+        name: 'Jim',
+      },
+    },
+    {
+      type: 'removeProperties',
+      keysToRemove: ['name', 'hello'],
+    },
+  ]),
+);
 
 function transformStateWithClones(state, actions) {
   let stateClone = { ...state };
@@ -25,11 +58,10 @@ function transformStateWithClones(state, actions) {
         stateHistory.push({ ...stateClone });
         break;
       case 'clear':
-        stateClone = {};
-        stateHistory.push(stateClone);
+        stateClone = Object.fromEntries([]);
+        stateHistory.push({});
         break;
       default:
-        return 1;
     }
   }
   return stateHistory;
