2981501e736d77af612057781ef0c1e63a7ffeb0
Maksym Mohyla <greenparksquad@gmail.com>
2024-08-06 18:00:34 +0300

    transform state with clones solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,42 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = []; // will be our result
+  let currentState = { ...state }; /* create copy of the
+  initial object so later we can push more copies of this object
+   to the stateHistory array at each iteration cycle without modifying
+   initial state object */
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties': {
+        currentState = { ...currentState, ...action.extraData };
+        /* if action array has an object with 'type: addProperties' property,
+        we add properties from extraData object and create another copy of
+        current state */
+        break;
+      }
+
+      case 'removeProperties': {
+        currentState = { ...currentState };
+        action.keysToRemove.forEach((key) => delete currentState[key]);
+        /* additional iteration through keysToRemove object to remove keys
+        from currentState */
+        break;
+      }
+
+      case 'clear': {
+        currentState = {};
+        break;
+      }
+    }
+
+    stateHistory.push({ ...currentState });
+    /* push copy of the currentState at each iteration cycle
+    to the result array */
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
