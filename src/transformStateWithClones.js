89a9ae4fc59f092d26907181573a347e45e6eb8d
Wiktor Pinuszewski <wiktor@pinuszewski.info>
2024-08-06 14:04:09 +0200

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,38 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  function deepClone(obj) {
+    return JSON.parse(JSON.stringify(obj));
+}
+
+let currentState = deepClone(state);
+const stateHistory = [];
+
+for (const action of actions) {
+    switch (action.type) {
+        case 'clear':
+            currentState = {};
+            break;
+        case 'addProperties':
+            currentState = {
+                ...currentState,
+                ...action.extraData,
+            };
+            break;
+        case 'removeProperties':
+            currentState = { ...currentState };
+            for (const key of action.keysToRemove) {
+                delete currentState[key];
+            }
+            break;
+        default:
+            throw new Error(`Unknown action type: ${action.type}`);
+    }
+
+    stateHistory.push(deepClone(currentState));
+}
+
+return stateHistory;
 }
 
 module.exports = transformStateWithClones;
