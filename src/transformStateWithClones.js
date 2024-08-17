46b0034e7b617693f5fe3f94f61ec045850574e5
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2024-08-17 12:56:10 +0300

    switch instead of if

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -10,26 +10,31 @@ function transformStateWithClones(state, actions) {
   const history = [];
   let currentState = Object.assign({}, state);
 
-  for (let i = 0; i < actions.length; i++) {
-    if (actions[i].type === 'clear') {
-      currentState = {};
-      history.push(currentState);
-    }
+  for (const i in actions) {
+    switch (actions[i].type) {
+      case 'clear':
+        currentState = {};
+        history.push(currentState);
+        break;
 
-    if (actions[i].type === 'addProperties') {
-      const add = actions[i].extraData;
+      case 'addProperties':
+        const propertiesToAdd = actions[i].extraData;
 
-      currentState = Object.assign({}, currentState, add);
-      history.push(currentState);
-    }
+        currentState = Object.assign({}, currentState, propertiesToAdd);
+        history.push(currentState);
+        break;
 
-    if (actions[i].type === 'removeProperties') {
-      currentState = Object.assign({}, currentState);
+      case 'removeProperties':
+        currentState = Object.assign({}, currentState);
 
-      actions[i].keysToRemove.forEach((key) => {
-        delete currentState[key];
-      });
-      history.push(currentState);
+        actions[i].keysToRemove.forEach((key) => {
+          delete currentState[key];
+        });
+        history.push(currentState);
+        break;
+
+      default:
+        break;
     }
   }
 
