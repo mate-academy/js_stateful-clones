d4505dd15e13526da76ee7b2363dd5fae87a3023
Inesa Shtepa <khinessa@gmail.com>
2024-08-22 14:49:24 +0300

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -9,34 +9,28 @@
 
 function transformStateWithClones(state, actions) {
   const stateHistory = [];
-  let stateNew = JSON.parse(JSON.stringify(state));
+  let stateNew = Object.assign({ ...state });
 
   for (let key = 0; key < actions.length; ++key) {
-    if (actions[key].type === 'addProperties') {
-      const stateNew1 = Object.assign({}, stateNew, actions[key].extraData);
+    switch (actions[key].type) {
+      case 'addProperties':
+        stateNew = Object.assign({}, stateNew, actions[key].extraData);
+        break;
 
-      stateHistory.push(stateNew1);
-      stateNew = JSON.parse(JSON.stringify(stateNew1));
+      case 'removeProperties':
+        stateNew = Object.assign({}, stateNew);
+
+        for (const i in actions[key].keysToRemove) {
+          delete stateNew[actions[key].keysToRemove[i]];
+        }
+        break;
+
+      case 'clear':
+        stateNew = {};
+        break;
     }
 
-    if (actions[key].type === 'removeProperties') {
-      const stateNew2 = {};
-
-      Object.assign(stateNew2, stateNew);
-
-      for (const i in actions[key].keysToRemove) {
-        delete stateNew2[actions[key].keysToRemove[i]];
-      }
-      stateHistory.push(stateNew2);
-      stateNew = JSON.parse(JSON.stringify(stateNew2));
-    }
-
-    if (actions[key].type === 'clear') {
-      const stateNew3 = {};
-
-      stateHistory.push(stateNew3);
-      stateNew = JSON.parse(JSON.stringify(stateNew3));
-    }
+    stateHistory.push(stateNew);
   }
 
   return stateHistory;
