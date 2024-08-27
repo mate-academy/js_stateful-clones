6da8efb78c63e6428b06d95a5af563d18aea7316
Max Fedorchuk <maxeffect.apollo@gmail.com>
2024-08-28 02:02:14 +0300

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,32 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const copyState = { ...state };
+  const result = [];
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      result.push({ ...Object.assign(copyState, action.extraData) });
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete copyState[key];
+      }
+
+      result.push({ ...copyState });
+    }
+
+    if (action.type === 'clear') {
+      for (const key in copyState) {
+        delete copyState[key];
+      }
+
+      result.push({ ...copyState });
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
