b63fa80f713592477159573fe1e89b12e90c652e
Yurii Kryvorotko <krivorotkokrivorotko2016@gmail.com>
2024-08-27 13:11:47 +0300

    Updated Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,52 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const stateHistory = [{ ...state }];
+  const stateHistory = [];
+  let stateCopy = { ...state };
 
-  if (actions[0].type === 'clear') {
-    stateHistory[0] = {};
-  }
+  for (let i = 0; i < actions.length; i++) {
+    const action = actions[i];
 
-  if (actions[0].type === 'addProperties') {
-    const extraData = actions[0].extraData;
-
-    for (const key in extraData) {
-      stateHistory[0][key] = extraData[key];
-    }
-  }
-
-  if (actions[0].type === 'removeProperties') {
-    const keysToRemove = actions[0].keysToRemove;
-
-    for (let y = 0; y < keysToRemove.length; y++) {
-      delete stateHistory[0][keysToRemove[y]];
-    }
-  }
-
-  for (let i = 1; i < actions.length; i++) {
-    if (actions[i].type === 'clear') {
-      stateHistory[i] = {};
+    switch (action.type) {
+      case 'clear':
+        stateCopy = {};
+        break;
+      case 'addProperties':
+        stateCopy = { ...stateCopy, ...action.extraData };
+        break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete stateCopy[key];
+        }
+        break;
+      default:
+        break;
     }
 
-    if (actions[i].type === 'addProperties') {
-      stateHistory[i] = { ...stateHistory[i - 1] };
-
-      const extraData = actions[i].extraData;
-
-      for (const key in extraData) {
-        stateHistory[i][key] = extraData[key];
-      }
-    }
-
-    if (actions[i].type === 'removeProperties') {
-      stateHistory[i] = { ...stateHistory[i - 1] };
-
-      const keysToRemove = actions[i].keysToRemove;
-
-      for (let y = 0; y < keysToRemove.length; y++) {
-        delete stateHistory[i][keysToRemove[y]];
-      }
-    }
+    stateHistory.push({ ...stateCopy });
   }
 
   return stateHistory;
