a64934b18b758240731863cea70540c4aaad9e78
Dmitry <kononenko.dima2001@gmail.com>
2024-08-28 18:19:07 +0300

    solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,33 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  const history = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        break;
+      case 'removeProperties':
+        for (const removeElement of action.keysToRemove) {
+          delete stateCopy[removeElement];
+        }
+        break;
+      case 'clear':
+        for (const stateCopyKey in stateCopy) {
+          delete stateCopy[stateCopyKey];
+        }
+        break;
+      default:
+        throw new Error('Parameter is not a number!');
+    }
+    history.push({ ...stateCopy });
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
