a61fddf0b1b8be075b774191921ee6070a256ae1
Yevhenii Melnyk <rpm.steel@gmail.com>
2024-08-28 17:23:00 +0300

    done with this task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const history = [];
+  const stateForEachAction = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(stateForEachAction, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete stateForEachAction[key];
+      }
+    }
+
+    if (action.type === 'clear') {
+      for (const key in stateForEachAction) {
+        delete stateForEachAction[key];
+      }
+    }
+
+    history.push({ ...stateForEachAction });
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
