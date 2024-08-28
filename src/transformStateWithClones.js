bf3af66e5adb5f5d56539c1ebb8c1182bd24c462
Vlad Hriaznov <vlad14wery@gmail.com>
2024-08-28 15:44:54 +0300

    fix

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,18 +11,27 @@ function transformStateWithClones(state, actions) {
   let currentState = { ...state };
 
   for (const action of actions) {
-    if (action.type === 'clear') {
-      currentState = {};
-    } else if (action.type === 'addProperties') {
-      currentState = { ...currentState, ...action.extraData };
-    } else if (action.type === 'removeProperties') {
-      currentState = { ...currentState };
+    switch (action.type) {
+      case 'clear':
+        currentState = {};
+        break;
 
-      for (const key of action.keysToRemove) {
-        delete currentState[key];
-      }
+      case 'addProperties':
+        currentState = { ...currentState, ...action.extraData };
+        break;
+
+      case 'removeProperties':
+        currentState = { ...currentState };
+
+        for (const key of action.keysToRemove) {
+          delete currentState[key];
+        }
+        break;
+
+      default:
+        break;
     }
-    stateHistory.push({ ...currentState });
+    stateHistory.push(currentState);
   }
 
   return stateHistory;
