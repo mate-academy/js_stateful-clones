6f3b68e56e893a483624526708767c184657e5d9
Valerii Omelnyk <valeraaomelnik@gmail.com>
2024-08-28 17:02:32 +0300

    fix solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,15 +8,9 @@
  */
 function transformStateWithClones(state, actions) {
   const states = [];
-  let stateCopy = null;
+  let stateCopy = { ...state };
 
   for (const action of actions) {
-    if (states.length === 0) {
-      stateCopy = { ...state };
-    } else {
-      stateCopy = { ...states[states.length - 1] };
-    }
-
     switch (action.type) {
       case 'clear':
         stateCopy = {};
@@ -36,7 +30,7 @@ function transformStateWithClones(state, actions) {
         return 'Error';
     }
 
-    states.push(stateCopy);
+    states.push({ ...stateCopy });
   }
 
   return states;
