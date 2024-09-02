1240aaa08163989f6b9eb4599301e9ceb088bd6b
Yehor Mishchenko <yehormishchenkowork@gmail.com>
2024-09-02 20:39:54 +0200

    Added changes to the name of variable

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -9,31 +9,31 @@
 
 function transformStateWithClones(state, actions) {
   const result = [];
-  let currentState = { ...state };
+  let currentStateCopy = { ...state };
 
   for (const ch of actions) {
-    const clonedState = { ...currentState };
+    const stateCopy = { ...currentStateCopy };
 
     if (ch.type === 'addProperties') {
       for (const [key, value] of Object.entries(ch.extraData)) {
-        clonedState[key] = value;
+        stateCopy[key] = value;
       }
     }
 
     if (ch.type === 'removeProperties') {
       for (const key of ch.keysToRemove) {
-        delete clonedState[key];
+        delete stateCopy[key];
       }
     }
 
     if (ch.type === 'clear') {
-      for (const key in clonedState) {
-        delete clonedState[key];
+      for (const key in stateCopy) {
+        delete stateCopy[key];
       }
     }
 
-    result.push(clonedState);
-    currentState = clonedState;
+    result.push(stateCopy);
+    currentStateCopy = stateCopy;
   }
 
   return result;
