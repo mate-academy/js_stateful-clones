8868fbe8810da40c8f4128c98752da5d2481b00d
Ivan Vaniuk <ivanvanuk77@gmail.com>
2024-09-10 20:00:32 +0300

    Done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,24 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let newState = { ...state };
+  const result = [];
+
+  actions.forEach((action) => {
+    newState = { ...newState };
+
+    if (action.type === 'addProperties') {
+      Object.assign(newState, action.extraData);
+    } else if (action.type === 'removeProperties') {
+      action.keysToRemove.forEach((elem) => delete newState[elem]);
+    } else if (action.type === 'clear') {
+      newState = {};
+    }
+
+    result.push(newState);
+  });
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
