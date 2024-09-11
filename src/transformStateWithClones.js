dfeee972b3ea03e64597d8bef841569a2821c7be
italomagno <italomagno10@gmail.com>
2024-09-11 19:05:22 -0300

    finish transform state with clones task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,35 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newStates = [];
+
+  actions.forEach((action) => {
+    switch (action.type) {
+      case 'addProperties':
+        const StateBfAdd = { ...(newStates[newStates.length - 1] || state) };
+
+        newStates.push({ ...StateBfAdd, ...action.extraData });
+        break;
+      case 'removeProperties':
+        const stateBfRemove = { ...(newStates[newStates.length - 1] || state) };
+
+        action.keysToRemove.forEach((key) => {
+          if (Object.keys(stateBfRemove).includes(key)) {
+            delete stateBfRemove[key];
+          }
+        });
+        newStates.push({ ...stateBfRemove });
+        break;
+      case 'clear':
+        newStates.push({});
+        break;
+      default:
+        newStates.push({ ...state });
+        break;
+    }
+  });
+
+  return newStates;
 }
 
 module.exports = transformStateWithClones;
