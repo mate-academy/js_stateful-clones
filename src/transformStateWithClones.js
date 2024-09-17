59609d0dcc68978ba96c02c405597a02fae01e9e
EqVert <EqVert@gmail.com>
2024-09-17 03:39:07 +0300

    sol

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,47 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newState = { ...state };
+  const transformLogState = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        addProperties(newState, action.extraData);
+        transformLogState.push({ ...newState });
+        break;
+
+      case 'removeProperties':
+        removeProperties(newState, action.keysToRemove);
+        transformLogState.push({ ...newState });
+        break;
+
+      case 'clear':
+        clearProperties(newState);
+        transformLogState.push({ ...newState });
+        break;
+    }
+  }
+
+  return transformLogState;
+}
+
+function addProperties(newState, extraData) {
+  Object.assign(newState, extraData);
+}
+
+function removeProperties(newState, keysToRemove) {
+  for (const key of keysToRemove) {
+    delete newState[key];
+  }
+}
+
+function clearProperties(newState) {
+  for (const key in newState) {
+    delete newState[key];
+  }
 }
 
 module.exports = transformStateWithClones;
