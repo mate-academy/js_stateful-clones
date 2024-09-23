854099be75af5dadbf13f21559d7df866b4606b4
Ivan <ivan.kartavy@icloud.com>
2024-09-23 21:08:25 +0300

    task done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,48 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const results = [];
+  const stateClone = {};
+
+  Object.assign(stateClone, state);
+
+  for (const action of actions) {
+    const { type } = action;
+
+    switch (type) {
+      case 'addProperties':
+        {
+          const { extraData } = action;
+
+          Object.assign(stateClone, extraData);
+
+          results.push({ ...stateClone });
+        }
+        break;
+
+      case 'removeProperties':
+        {
+          const { keysToRemove } = action;
+
+          for (const key of keysToRemove) {
+            delete stateClone[key];
+          }
+
+          results.push({ ...stateClone });
+        }
+        break;
+
+      case 'clear':
+        for (const key of Object.keys(stateClone)) {
+          delete stateClone[key];
+        }
+
+        results.push({ ...stateClone });
+        break;
+    }
+  }
+
+  return results;
 }
 
 module.exports = transformStateWithClones;
