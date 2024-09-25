8847bb67f29ded610a3360fd41a5deeaab7be2ba
ab3MN <ab3mn3@gmail.com>
2024-09-25 17:51:22 +0300

    feat:implement solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,43 @@
  *
  * @return {Object[]}
  */
+
+const addProperties = (state, properties, res) => {
+  Object.assign(state, properties);
+  res.push({ ...state });
+};
+
+const removeProperty = (state, keysToRemove, res = []) => {
+  keysToRemove.forEach((key) => {
+    delete state[key];
+  });
+  res.push({ ...state });
+};
+
+const clearState = (state, res) => {
+  Object.keys(state).forEach((key) => delete state[key]);
+
+  res.push({ ...state });
+};
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newState = { ...state };
+  const res = [];
+
+  actions.forEach((action) => {
+    switch (action.type) {
+      case 'addProperties':
+        return addProperties(newState, action.extraData, res);
+
+      case 'removeProperties':
+        return removeProperty(newState, action.keysToRemove, res);
+
+      default:
+        return clearState(newState, res);
+    }
+  });
+
+  return res;
 }
 
 module.exports = transformStateWithClones;
