a38e74703583e9e9a7313706deee085a5d114d6a
aproragadozo <aproragadozo@gmail.com>
2024-10-01 10:10:56 +0200

    Revert "Update transformStateWithClones.js"
    This reverts commit fe34c275b1dae2d32c31cd300f95984d616d5d7c.

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,32 +7,7 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const stateHistory = [];
-  actions.forEach((action) => {
-    switch (action.type) {
-      case 'addProperties':
-        const addState = {...state};
-        Object.keys(action.extraData).forEach(key => {
-          addState[key] = action.extraData[key];
-        });
-        stateHistory.push(addState);
-        break;
-
-      case 'removeProperties':
-        const removeState = {...state};
-        for (const key of action.keysToRemove) {
-          delete removeState[key];
-        };
-        stateHistory.push(removeState);
-        break;
-
-      case 'clear':
-        const clearState = {...state};
-        Object.keys(state).forEach(key => delete state[key]);
-        stateHistory.push(clearState);
-    };
-  });
-  return stateHistory;
+  // write code here
 }
 
 module.exports = transformStateWithClones;
