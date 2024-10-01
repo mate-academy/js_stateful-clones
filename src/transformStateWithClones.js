fe34c275b1dae2d32c31cd300f95984d616d5d7c
Balázs Bujna <aproragadozo@users.noreply.github.com>
2024-10-01 09:55:57 +0200

    Update transformStateWithClones.js

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+  actions.forEach((action) => {
+    switch (action.type) {
+      case 'addProperties':
+        const addState = {...state};
+        Object.keys(action.extraData).forEach(key => {
+          addState[key] = action.extraData[key];
+        });
+        stateHistory.push(addState);
+        break;
+
+      case 'removeProperties':
+        const removeState = {...state};
+        for (const key of action.keysToRemove) {
+          delete removeState[key];
+        };
+        stateHistory.push(removeState);
+        break;
+
+      case 'clear':
+        const clearState = {...state};
+        Object.keys(state).forEach(key => delete state[key]);
+        stateHistory.push(clearState);
+    };
+  });
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
