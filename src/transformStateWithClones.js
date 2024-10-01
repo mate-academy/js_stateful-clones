c9c56860520abe48b463abb0632e87e2afde6bbe
Vladyslav <unadvisedmail@gmail.com>
2024-10-01 04:33:46 +0200

    solved

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,66 @@
  *
  * @return {Object[]}
  */
-function transformStateWithClones(state, actions) {
-  // write code here
+
+const state = {
+  foo: 'bar',
+  name: 'Jim',
+  another: 'one',
+};
+
+function transformStateWithClones(currentState, actions) {
+  const history = [];
+  let clonedState = Object.assign({}, currentState);
+
+  actions.forEach((obj) => {
+    if (obj.type === 'clear') {
+      clonedState = {};
+      history.push({});
+    }
+
+    if (obj.type === 'addProperties') {
+      clonedState = { ...clonedState, ...obj.extraData };
+      history.push({ ...clonedState });
+    }
+
+    if (obj.type === 'removeProperties') {
+      for (const key of obj.keysToRemove) {
+        delete clonedState[key];
+      }
+
+      history.push({ ...clonedState });
+    }
+  });
+
+  return history;
 }
+
+transformStateWithClones(state, [
+  {
+    type: 'removeProperties',
+    keysToRemove: ['another'],
+  },
+  { type: 'clear' },
+  { type: 'clear' },
+  { type: 'clear' },
+  {
+    type: 'addProperties',
+    extraData: { yet: 'another property' },
+  },
+  { type: 'clear' },
+  {
+    type: 'addProperties',
+    extraData: {
+      foo: 'bar',
+      name: 'Jim',
+    },
+  },
+  {
+    type: 'removeProperties',
+    keysToRemove: ['name', 'hello'],
+  },
+]);
+
+// console.log(cloned);
 
 module.exports = transformStateWithClones;
