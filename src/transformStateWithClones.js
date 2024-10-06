590f0a759607bff01a4bb4921367324b035cb6d4
kaplunova <melamori1605@gmail.com>
2024-10-06 19:08:43 +0300

    Edited function

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const resultArray = [];
+
+  for (let i = 0; i < actions.length; i++) {
+    const data = actions[i];
+
+    if (data.type === 'addProperties') {
+      Object.assign(state, data.extraData);
+    }
+
+    if (data.type === 'removeProperties') {
+      for (let j = 0; j < data.keysToRemove.length; j++) {
+        delete state[data.keysToRemove[j]];
+      }
+    }
+
+    if (data.type === 'clear') {
+      Object.keys(state).forEach((key) => {
+        delete state[key];
+      });
+    }
+    resultArray.push({ ...state });
+  }
+
+  return resultArray;
 }
 
 module.exports = transformStateWithClones;
