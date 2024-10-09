9d6ca4ac0556618b80f531033b1ef99f449064a6
lucas mendes <luckajw@gmail.com>
2024-10-09 00:40:44 -0300

    implementando a função stateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,32 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+
+  let copy = { ...state };
+  const story = [];
+
+  actions.forEach((action) => {
+    switch (action.type) {
+      case 'clear':
+        copy = {};
+        break;
+
+      case 'addProperties':
+        copy = { ...copy, ...action.extraData };
+        break;
+      case 'removeProperties':
+        actions.keysToRemove.forEach((key) => {
+          delete copy[key];
+        });
+        break;
+
+      default:
+        break;
+    }
+    story.push({ ...copy });
+  });
+
+  return story;
 }
 
 module.exports = transformStateWithClones;
