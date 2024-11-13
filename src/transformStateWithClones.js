fcc700bb826a79872b4f1978129bf4d1ec69a4a6
Yehor <y.krapivka18@gmail.com>
2024-11-13 13:20:42 -0500

    result

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,63 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arrayResults = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        if (arrayResults.length === 0) {
+          arrayResults.push(Object.assign({}, state, action.extraData));
+        } else {
+          arrayResults.push(
+            Object.assign(
+              {},
+              arrayResults[arrayResults.length - 1],
+              action.extraData,
+            ),
+          );
+        }
+        break;
+
+      case 'removeProperties':
+        if (arrayResults.length === 0) {
+          const newState = { ...state };
+
+          for (const key of action.keysToRemove) {
+            delete newState[key];
+          }
+          arrayResults.push(newState);
+        } else {
+          const newState = { ...arrayResults[arrayResults.length - 1] };
+
+          for (const key of action.keysToRemove) {
+            delete newState[key];
+          }
+          arrayResults.push(newState);
+        }
+        break;
+
+      case 'clear':
+        if (arrayResults.length === 0) {
+          const newStates = { ...state };
+
+          for (const keys in newStates) {
+            delete newStates[keys];
+          }
+          arrayResults.push(newStates);
+        } else {
+          const newStates = { ...arrayResults[arrayResults.length - 1] };
+
+          for (const keys in newStates) {
+            delete newStates[keys];
+          }
+          arrayResults.push(newStates);
+        }
+        break;
+    }
+  }
+
+  return arrayResults;
 }
 
 module.exports = transformStateWithClones;
