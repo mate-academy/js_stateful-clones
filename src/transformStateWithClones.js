b4902674cf68b69a190cc3e57186e3506526c098
Blervin <126615593+Blervin1@users.noreply.github.com>
2024-11-21 17:07:27 +0100

    Update transformStateWithClones.js

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,42 @@
  *
  * @return {Object[]}
  */
-function transformStateWithClones(state, actions) {
-  // write code here
+function transformStateWithClones(state, transforms) {
+  const object = Object.assign({}, state);
+  const arr = [];
+  function addProperties(copy, additionalProps) {
+    for (const key in additionalProps) {
+      copy[key] = additionalProps[key];
+    }
+    arr.push(Object.assign({}, copy));
+  }
+  function removeProperties(copy, additionalProps) {
+    additionalProps.forEach(function(key) {
+      delete copy[key];
+    });
+    arr.push(Object.assign({}, copy));
+  }
+  function clearProperties(copy) {
+    for (const key in object) {
+      delete copy[key];
+    }
+    arr.push(Object.assign({}, copy));
+  }
+  transforms.forEach(function(item) {
+    const { operation, properties } = item;
+    switch (operation) {
+      case 'addProperties':
+        addProperties(object, properties);
+        break;
+      case 'removeProperties':
+        removeProperties(object, properties);
+        break;
+      case 'clear':
+        clearProperties(object);
+        break;
+    }
+  });
+  return arr;
 }
 
 module.exports = transformStateWithClones;
