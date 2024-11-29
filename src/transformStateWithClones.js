8f519098c8a3715fa31994ffb69df604523bd348
Tania Kolesnik <kolesnik@gmail.com>
2024-11-29 12:43:56 +0000

    add solutuon

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const changesArray = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'clear') {
+      currentState = {};
+    }
+
+    if (action.type === 'addProperties') {
+      currentState = { ...currentState, ...action.extraData };
+    }
+
+    if (action.type === 'removeProperties') {
+      const { keysToRemove } = action;
+
+      currentState = { ...currentState };
+
+      for (const key of keysToRemove) {
+        delete currentState[key];
+      }
+    }
+
+    changesArray.push({ ...currentState });
+  }
+
+  return changesArray;
 }
 
 module.exports = transformStateWithClones;
