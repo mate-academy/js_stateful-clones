aa10133f64aa875d7f90bcbba675990fe27a2d45
Denys Piddubnyi <iforgotmypincode@gmail.com>
2024-12-09 21:40:37 +0200

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,24 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
-}
+  let copyState = { ...state };
+  const arrayStates = [];
 
+  for (const action of actions) {
+    if (action.type === 'clear') {
+      copyState = {};
+    } else if (action.type === 'addProperties') {
+      copyState = { ...copyState, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      copyState = { ...copyState };
+
+      for (const key of action.keysToRemove) {
+        delete copyState[key];
+      }
+    }
+    arrayStates.push(copyState);
+  }
+
+  return arrayStates;
+}
 module.exports = transformStateWithClones;
