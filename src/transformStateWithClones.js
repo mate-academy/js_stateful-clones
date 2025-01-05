9d1e3e1387d1d11e060f19245d77760c88f6061d
g.palyukh <g.palyukh@gmail.com>
2025-01-05 20:25:05 +0100

    Implement task Cloning and transforming state

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let stateCopy = { ...state };
+  const statesHistory = [];
+
+  for (const action of actions) {
+    const { type, extraData = {}, keysToRemove = [] } = action;
+
+    switch (type) {
+      case 'addProperties':
+        stateCopy = { ...stateCopy, ...extraData };
+        break;
+
+      case 'removeProperties':
+        for (const key of keysToRemove) {
+          if (key in stateCopy) {
+            delete stateCopy[key];
+          }
+        }
+        break;
+
+      case 'clear':
+        stateCopy = {};
+        break;
+
+      default:
+        continue;
+    }
+    statesHistory.push(structuredClone(stateCopy));
+  }
+
+  return statesHistory;
 }
 
 module.exports = transformStateWithClones;
