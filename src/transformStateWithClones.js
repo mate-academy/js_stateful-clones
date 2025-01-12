4154860ba1f790e55013aa5a6ed0983d83e18db9
Ivan <tushchukivan@gmail.com>
2025-01-12 18:24:59 +0200

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,37 @@
-'use strict';
-
-/**
+/*
  * @param {Object} state
  * @param {Object[]} actions
  *
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let currentState = { ...state }; // Створюємо копію початкового state
+  const states = []; // Масив для збереження результатів
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      // Додаємо властивості
+      currentState = { ...currentState, ...action.extraData };
+    }
+
+    if (action.type === 'removeProperties') {
+      // Видаляємо вказані ключі
+      currentState = { ...currentState };
+
+      action.keysToRemove.forEach((key) => {
+        delete currentState[key];
+      });
+    }
+
+    if (action.type === 'clear') {
+      // Створюємо новий порожній об'єкт
+      currentState = {};
+    }
+
+    states.push(currentState); // Додаємо поточний стан до результатів
+  }
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
