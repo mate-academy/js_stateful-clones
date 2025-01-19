8c0b8c64bbce752430f54a3cdda4bbf427d02616
Oleh Tymoshchuk <pryge321@gmail.com>
2025-01-19 19:48:46 +0200

    Executed code

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,41 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+  let currentState = { ...state };
+
+  actions.forEach((action) => {
+    let newState;
+
+    switch (action.type) {
+      case 'clear':
+        newState = {};
+
+        break;
+      case 'addProperties':
+        newState = { ...currentState, ...action.extraData };
+
+        break;
+
+      case 'removeProperties':
+        newState = { ...currentState };
+
+        action.keysToRemove.forEach((key) => {
+          delete newState[key];
+        });
+        break;
+
+      default:
+        newState = { ...currentState };
+        break;
+    }
+
+    stateHistory.push(newState);
+
+    currentState = newState;
+  });
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
