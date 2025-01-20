7ef85d258ac93edc395c2cf7a7550b792507c97b
marcosvinalves <marcos_araujo7@Hotmail.com>
2025-01-20 10:52:11 -0300

    solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,38 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+  let currentState = { ...state }; // Clone the initial state
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'clear':
+        currentState = {}; // Clear the state
+        break;
+
+      case 'addProperties':
+        currentState = {
+          ...currentState,
+          ...action.extraData,
+        }; // Add properties
+        break;
+
+      case 'removeProperties':
+        currentState = { ...currentState }; // Clone current state
+
+        for (const key of action.keysToRemove) {
+          delete currentState[key]; // Remove specified keys
+        }
+        break;
+
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
+    }
+
+    stateHistory.push(currentState); // Push the updated state to history
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
