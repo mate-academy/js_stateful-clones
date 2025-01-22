f49f20f634f88e569b6f5bd1e7386169a1b34b80
AndreaBoiko <andriy.boyko35@gmail.com>
2025-01-22 21:30:12 +0100

    finish

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    const newObj = { ...currentState };
+
+    if (action.type === 'addProperties') {
+      for (const value in action.extraData) {
+        newObj[value] = action.extraData[value];
+      }
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const value of action.keysToRemove) {
+        delete newObj[value];
+      }
+    }
+
+    if (action.type === 'clear') {
+      for (const value in newObj) {
+        delete newObj[value];
+      }
+    }
+    result.push(newObj);
+    currentState = newObj;
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
