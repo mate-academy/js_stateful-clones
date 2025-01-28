0ad50f4709852ee2ae820603180ba108ae1b0bb0
DmytroMacro <macro.expert.ua@gmail.com>
2025-01-28 16:34:58 +0200

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,37 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = []; // Початкова порожня історія
+
+  actions.forEach((action) => {
+    let currentState =
+      stateHistory.length === 0
+        ? { ...state }
+        : { ...stateHistory[stateHistory.length - 1] };
+
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(currentState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        action.keysToRemove.forEach((key) => {
+          delete currentState[key];
+        });
+        break;
+
+      case 'clear':
+        currentState = {};
+        break;
+
+      default:
+        break;
+    }
+
+    stateHistory.push(currentState);
+  });
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
