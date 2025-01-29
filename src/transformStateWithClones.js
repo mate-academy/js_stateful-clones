5973211907497d17ce40173f95d3943a2ee058e0
Vitor MF <vitorfreire419@gmail.com>
2025-01-29 15:16:03 -0300

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,30 @@
 'use strict';
 
-/**
- * @param {Object} state
- * @param {Object[]} actions
- *
- * @return {Object[]}
- */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let currentState = { ...state };
+  const history = [];
+
+  actions.forEach((action) => {
+    if (action.type === 'clear') {
+      currentState = {};
+    }
+
+    if (action.type === 'addProperties') {
+      currentState = { ...currentState, ...action.extraData };
+    }
+
+    if (action.type === 'removeProperties') {
+      currentState = { ...currentState };
+
+      action.keysToRemove.forEach((key) => {
+        delete currentState[key];
+      });
+    }
+
+    history.push(currentState);
+  });
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
