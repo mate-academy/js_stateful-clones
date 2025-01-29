2dd240c494ff8a8371785c2ff98e844e64793d8c
Ivan Ryabukha <ryabukhaivan98@gmail.com>
2025-01-29 16:03:37 +0200

    'Solution'

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -9,46 +9,46 @@
 function transformStateWithClones(state, actions) {
   const actionsArray = [];
 
-  const tempState = {};
+  const stateCopy = {};
 
   for (const { type, extraData, keysToRemove } of actions) {
     if (actionsArray.length === 0) {
-      Object.assign(tempState, state);
+      Object.assign(stateCopy, state);
     } else {
       clearProperties();
-      Object.assign(tempState, actionsArray[actionsArray.length - 1]);
+      Object.assign(stateCopy, actionsArray[actionsArray.length - 1]);
     }
 
-    if (type === 'addProperties') {
-      addProperties(extraData);
+    switch (type) {
+      case 'addProperties':
+        addProperties(extraData);
+        break;
+      case 'removeProperties':
+        removeProperties(keysToRemove);
+        break;
+      case 'clear':
+        clearProperties();
+        break;
+      default:
     }
-
-    if (type === 'removeProperties') {
-      removeProperties(keysToRemove);
-    }
-
-    if (type === 'clear') {
-      clearProperties();
-    }
-
-    actionsArray.push({ ...tempState });
+    actionsArray.push({ ...stateCopy });
   }
 
   function addProperties(addProp) {
     for (const key in addProp) {
-      tempState[key] = addProp[key];
+      stateCopy[key] = addProp[key];
     }
   }
 
   function removeProperties(removePorp) {
     for (const key of removePorp) {
-      delete tempState[key];
+      delete stateCopy[key];
     }
   }
 
   function clearProperties() {
-    for (const key in tempState) {
-      delete tempState[key];
+    for (const key in stateCopy) {
+      delete stateCopy[key];
     }
   }
 
