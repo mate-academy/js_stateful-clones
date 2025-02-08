22ad41d43e844e44850aab9dd740298b49981832
David Moskalenko <dvdmsk21@gmail.com>
2025-02-08 14:24:17 +0200

    Add initial HTML file and implement state transformation logic

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,57 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const states = [{ ...state }];
+
+  let countOperations = 0;
+
+  for (const { type, ...keys } of actions) {
+    let newState = { ...states[states.length - 1] };
+
+    switch (type) {
+      case 'addProperties':
+        newState = Object.assign(newState, keys.extraData);
+
+        if (countOperations === 0) {
+          states[states.length - 1] = newState;
+        } else {
+          states.push(newState);
+        }
+
+        countOperations++;
+        break;
+      case 'removeProperties':
+        for (const key of keys.keysToRemove) {
+          delete newState[key];
+        }
+
+        if (countOperations === 0) {
+          states[states.length - 1] = newState;
+        } else {
+          states.push(newState);
+        }
+
+        countOperations++;
+        break;
+      case 'clear':
+        for (const key in newState) {
+          delete newState[key];
+        }
+
+        if (countOperations === 0) {
+          states[states.length - 1] = newState;
+        } else {
+          states.push(newState);
+        }
+
+        countOperations++;
+        break;
+    }
+  }
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
