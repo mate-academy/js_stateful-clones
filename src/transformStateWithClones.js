5375a11f83dc23fa7576aa7ec491d2ee1928aff6
Roman Ohirok <matsuhita.2307@gmail.com>
2025-02-10 15:39:35 +0200

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,42 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  let stateClon = { ...state };
+  const STATE_STEPS_ARR = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case `addProperties`: {
+        for (const key in action.extraData) {
+          stateClon[key] = action.extraData[key];
+        }
+
+        STATE_STEPS_ARR.push({ ...stateClon });
+        break;
+      }
+
+      case `removeProperties`: {
+        for (const key of action.keysToRemove) {
+          delete stateClon[key];
+        }
+
+        STATE_STEPS_ARR.push({ ...stateClon });
+        break;
+      }
+
+      case `clear`: {
+        stateClon = {};
+        STATE_STEPS_ARR.push({ ...stateClon });
+        break;
+      }
+
+      default: {
+        break;
+      }
+    }
+  }
+
+  return STATE_STEPS_ARR;
 }
 
 module.exports = transformStateWithClones;
