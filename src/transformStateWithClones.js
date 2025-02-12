43ea4cdbe6a39cceff65379ec67773eba5a3074c
Hleb Bondar <hlebbondar2004@gmail.com>
2025-02-12 16:25:30 +0200

    add task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let currentState = { ...state };
+  const history = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        currentState = { ...currentState, ...action.extraData };
+        break;
+
+      case 'removeProperties': {
+        const stateCopy = {};
+
+        for (const key in currentState) {
+          if (!action.keysToRemove.includes(key)) {
+            stateCopy[key] = currentState[key];
+          }
+        }
+        currentState = stateCopy;
+        break;
+      }
+
+      case 'clear':
+        currentState = {};
+        break;
+    }
+
+    history.push(currentState);
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
