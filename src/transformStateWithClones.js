131144a51c1366188653bda946bc7e72ca5dd79d
Nikola57 <nikola.sribniy@gmail.com>
2025-02-12 19:56:12 +0100

    first commit

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  // Клонуємо початковий стан
+  let currentState = JSON.parse(JSON.stringify(state));
+  const history = [];
+
+  for (let i = 0; i < actions.length; i++) {
+    const action = actions[i];
+
+    // Очищаємо стан
+    if (action.type === 'clear') {
+      currentState = {};
+    } else if (action.type === 'addProperties') {
+      for (const key in action.extraData) {
+        currentState[key] = action.extraData[key];
+      }
+    } else if (action.type === 'removeProperties') {
+      for (let j = 0; j < action.keysToRemove.length; j++) {
+        // Видаляємо зазначені ключі
+        delete currentState[action.keysToRemove[j]];
+      }
+    }
+
+    // Додаємо копію поточного стану в історію
+    history.push({ ...currentState });
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
