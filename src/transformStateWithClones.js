5b927d82c8483185d49b74805e297a5e8a2a2ed2
Misha shimchishin git config --global user,email mykhailo.shimchishin@icloud,com <you@example.com>
2025-02-14 23:50:14 +0200

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let currentState = { ...state };
+  const states = [];
+
+  for (const { type, extraData, keysToRemove } of actions) {
+    switch (type) {
+      case 'addProperties':
+        currentState = { ...currentState, ...extraData };
+        break;
+
+      case 'removeProperties':
+        for (const key of keysToRemove) {
+          delete currentState[key];
+        }
+        break;
+
+      case 'clear':
+        currentState = {};
+        break;
+
+      default:
+        break;
+    }
+    states.push({ ...currentState });
+  }
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
