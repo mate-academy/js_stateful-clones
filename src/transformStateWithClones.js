3b2816b5c7b47cf963f7b6755fc3bf6de1a91f12
Alona Mutruk <alonamutruk@gmail.com>
2025-02-19 20:54:05 +0200

    Changes in js_stateful-clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const deepClone = (obj) => {
+    return JSON.parse(JSON.stringify(obj)); // Простий спосіб глибокого копіювання
+  };
+
+  let currentState = deepClone(state); // Створюємо глибоку копію початкового стану
+  const stateHistory = [currentState]; // Містить всі стани після кожної дії
+
+  actions.forEach(action => {
+    switch (action.type) {
+      case 'clear':
+        currentState = {}; // Створюємо порожній об'єкт
+        break;
+      case 'addProperties':
+        currentState = { ...currentState, ...action.extraData }; // Додаємо нові властивості
+        break;
+      case 'removeProperties':
+        const newState = { ...currentState };
+        action.keysToRemove.forEach(key => {
+          delete newState[key]; // Видаляємо зазначені властивості
+        });
+        currentState = newState;
+        break;
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
+    }
+
+    stateHistory.push(deepClone(currentState)); // Додаємо новий стан в історію
+  });
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
--- src/transformStateWithClones.test.js
+++ src/transformStateWithClones.test.js
@@ -1,279 +1,53 @@
 'use strict';
 
-const transformStateWithClones = require('./transformStateWithClones');
+const transformStateWithClones = require('../src/transformStateWithClones');
 
-test('Should create a new object with a single added property', () => {
-  const state = {};
 
-  expect(transformStateWithClones(state, [
-    {
-      type: 'addProperties', extraData: { name: 'Jim' },
-    },
-  ]))
-    .toEqual([
-      { name: 'Jim' },
-    ]);
+describe('transformStateWithClones', () => {
+  test('should correctly handle clear action', () => {
+    const state = { a: 1, b: 2 };
+    const actions = [{ type: 'clear' }];
+    const result = transformStateWithClones(state, actions);
 
-  expect(state)
-    .toEqual({});
+    expect(result.length).toBe(2); // Має бути два стани: початковий і після очищення
+    expect(result[1]).toEqual({}); // Після clear стан має бути порожнім об'єктом
+  });
+
+  test('should correctly handle addProperties action', () => {
+    const state = { a: 1 };
+    const actions = [{ type: 'addProperties', extraData: { b: 2 } }];
+    const result = transformStateWithClones(state, actions);
+
+    expect(result.length).toBe(2); // Має бути два стани
+    expect(result[1]).toEqual({ a: 1, b: 2 }); // Створюється нова властивість b
+  });
+
+  test('should correctly handle removeProperties action', () => {
+    const state = { a: 1, b: 2, c: 3 };
+    const actions = [{ type: 'removeProperties', keysToRemove: ['b'] }];
+    const result = transformStateWithClones(state, actions);
+
+    expect(result.length).toBe(2); // Має бути два стани
+    expect(result[1]).toEqual({ a: 1, c: 3 }); // Властивість b повинна бути видалена
+  });
+
+  test('should handle multiple actions correctly', () => {
+    const state = { a: 1 };
+    const actions = [
+      { type: 'addProperties', extraData: { b: 2 } },
+      { type: 'removeProperties', keysToRemove: ['a'] },
+    ];
+    const result = transformStateWithClones(state, actions);
+
+    expect(result.length).toBe(3); // Має бути три стани
+    expect(result[2]).toEqual({ b: 2 }); // Властивість a повинна бути видалена
+  });
+
+  test('should throw error for unknown action type', () => {
+    const state = { a: 1 };
+    const actions = [{ type: 'unknownAction' }];
+
+    expect(() => transformStateWithClones(state, actions)).toThrow('Unknown action type: unknownAction');
+  });
 });
 
-test('Should create a new object with multiple added properties', () => {
-  const state = {};
-
-  expect(transformStateWithClones(state, [
-    {
-      type: 'addProperties',
-      extraData: {
-        name: 'Jim', hello: 'world',
-      },
-    },
-  ]))
-    .toEqual([
-      {
-        name: 'Jim', hello: 'world',
-      },
-    ]);
-
-  expect(state)
-    .toEqual({});
-});
-
-test('Should combine old properties with added ones', () => {
-  const state = {
-    foo: 'bar', bar: 'foo',
-  };
-
-  expect(transformStateWithClones(state, [
-    {
-      type: 'addProperties',
-      extraData: {
-        name: 'Jim', hello: 'world',
-      },
-    },
-  ]))
-    .toEqual([
-      {
-        foo: 'bar', bar: 'foo', name: 'Jim', hello: 'world',
-      },
-    ]);
-
-  expect(state)
-    .toEqual({
-      foo: 'bar', bar: 'foo',
-    });
-});
-
-test('Should use the latest value when adding an existent property', () => {
-  const state = {
-    foo: 'bar', bar: 'foo',
-  };
-
-  expect(transformStateWithClones(state, [
-    {
-      type: 'addProperties',
-      extraData: {
-        foo: 'new', hello: 'world',
-      },
-    },
-  ]))
-    .toEqual([
-      {
-        foo: 'new', bar: 'foo', hello: 'world',
-      },
-    ]);
-
-  expect(state)
-    .toEqual({
-      foo: 'bar', bar: 'foo',
-    });
-});
-
-test('Should create an empty object when removing the last property', () => {
-  const state = { foo: 'bar' };
-
-  expect(transformStateWithClones(state, [
-    {
-      type: 'removeProperties', keysToRemove: ['foo'],
-    },
-  ]))
-    .toEqual([{}]);
-
-  expect(state)
-    .toEqual({ foo: 'bar' });
-});
-
-test('Should create an object without removed properties', () => {
-  const state = {
-    foo: 'bar', bar: 'foo', name: 'Jim', hello: 'world',
-  };
-
-  expect(transformStateWithClones(state, [
-    {
-      type: 'removeProperties', keysToRemove: ['hello', 'foo', 'name'],
-    },
-  ]))
-    .toEqual([
-      { bar: 'foo' },
-    ]);
-
-  expect(state)
-    .toEqual({
-      foo: 'bar', bar: 'foo', name: 'Jim', hello: 'world',
-    });
-});
-
-test('Should create the same state when removing no properties', () => {
-  const state = {
-    foo: 'bar', bar: 'foo', name: 'Jim', hello: 'world',
-  };
-
-  expect(transformStateWithClones(state, [
-    {
-      type: 'removeProperties', keysToRemove: [],
-    },
-  ]))
-    .toEqual([
-      {
-        foo: 'bar', bar: 'foo', name: 'Jim', hello: 'world',
-      },
-    ]);
-
-  expect(state)
-    .toEqual({
-      foo: 'bar', bar: 'foo', name: 'Jim', hello: 'world',
-    });
-});
-
-test('Should not fail when removing not existing property', () => {
-  const state = {
-    foo: 'bar', bar: 'foo', name: 'Jim', hello: 'world',
-  };
-
-  expect(transformStateWithClones(state, [
-    {
-      type: 'removeProperties', keysToRemove: ['test', 'bar'],
-    },
-  ]))
-    .toEqual([
-      {
-        foo: 'bar', name: 'Jim', hello: 'world',
-      },
-    ]);
-
-  expect(state)
-    .toEqual({
-      foo: 'bar', bar: 'foo', name: 'Jim', hello: 'world',
-    });
-});
-
-test('Should create an empty object after clear', () => {
-  const state = {
-    foo: 'bar', name: 'Jim', another: 'one',
-  };
-
-  expect(transformStateWithClones(state, [
-    { type: 'clear' },
-  ]))
-    .toEqual([{}]);
-
-  expect(state)
-    .toEqual({
-      foo: 'bar', name: 'Jim', another: 'one',
-    });
-});
-
-test('Should not fails when calling clear for an empty state', () => {
-  const state = {};
-
-  transformStateWithClones(state, [
-    { type: 'clear' },
-  ]);
-
-  expect(state)
-    .toEqual({});
-});
-
-test('Should handle multiple types', () => {
-  const state = {
-    foo: 'bar', bar: 'foo',
-  };
-
-  expect(transformStateWithClones(state, [
-    {
-      type: 'addProperties',
-      extraData: {
-        name: 'Jim', hello: 'world',
-      },
-    },
-    {
-      type: 'removeProperties', keysToRemove: ['bar', 'hello'],
-    },
-    {
-      type: 'addProperties', extraData: { another: 'one' },
-    },
-  ]))
-    .toEqual([
-      {
-        foo: 'bar', bar: 'foo', name: 'Jim', hello: 'world',
-      },
-      {
-        foo: 'bar', name: 'Jim',
-      },
-      {
-        foo: 'bar', name: 'Jim', another: 'one',
-      },
-    ]);
-
-  expect(state)
-    .toEqual({
-      foo: 'bar', bar: 'foo',
-    });
-});
-
-test('Should handle a long list of types', () => {
-  const state = {
-    foo: 'bar', name: 'Jim', another: 'one',
-  };
-
-  expect(transformStateWithClones(state, [
-    {
-      type: 'removeProperties', keysToRemove: ['another'],
-    },
-    { type: 'clear' },
-    { type: 'clear' },
-    { type: 'clear' },
-    {
-      type: 'addProperties', extraData: { yet: 'another property' },
-    },
-    { type: 'clear' },
-    {
-      type: 'addProperties',
-      extraData: {
-        foo: 'bar', name: 'Jim',
-      },
-    },
-    {
-      type: 'removeProperties', keysToRemove: ['name', 'hello'],
-    },
-  ]))
-    .toEqual([
-      {
-        foo: 'bar', name: 'Jim',
-      },
-      {},
-      {},
-      {},
-      { yet: 'another property' },
-      {},
-      {
-        foo: 'bar', name: 'Jim',
-      },
-      { foo: 'bar' },
-    ]);
-
-  expect(state)
-    .toEqual({
-      foo: 'bar', name: 'Jim', another: 'one',
-    });
-});
