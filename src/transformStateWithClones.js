a1ad98b34ab178c1fc13d96a7ae5b446a11e9d3a
Maksym Kostiv <rafik9202@gmail.com>
2025-02-26 09:19:43 +0200

    Solutinon

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,26 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let prevObj = { ...state };
+  const result = [];
+
+  actions.forEach((action) => {
+    if (action.type === 'addProperties') {
+      prevObj = { ...prevObj, ...action.extraData };
+    } else if (action.type === 'clear') {
+      prevObj = {};
+    } else if (action.type === 'removeProperties') {
+      prevObj = { ...prevObj };
+
+      action.keysToRemove.forEach((key) => {
+        delete prevObj[key];
+      });
+    }
+
+    result.push({ ...prevObj });
+  });
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
