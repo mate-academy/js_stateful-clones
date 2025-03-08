9d075df01bd9dd52cc5e5f5d64f3da9e83f1ffcc
Alessandro <Alessandrogabriel15160@gmail.com>
2025-03-08 15:53:15 -0300

    create function

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,39 @@
 'use strict';
 
-/**
- * @param {Object} state
- * @param {Object[]} actions
- *
- * @return {Object[]}
- */
-function transformStateWithClones(state, actions) {
-  // write code here
+function transformStateWithClones(stateInput, actionsInput) {
+  const stateHistory = [];
+  let updatedState = { ...stateInput };
+
+  for (const action of actionsInput) {
+    if (action.type === 'addProperties') {
+      updatedState = { ...updatedState, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      action.keysToRemove.forEach((key) => {
+        if (key in updatedState) {
+          delete updatedState[key];
+        }
+      });
+    } else if (action.type === 'clear') {
+      updatedState = {};
+    }
+
+    stateHistory.push({ ...updatedState });
+  }
+
+  return stateHistory;
 }
 
-module.exports = transformStateWithClones;
+// Exemplo de uso:
+const state = {
+  foo: 'bar',
+  name: 'Alice',
+  country: 'USA',
+};
+
+const actions = [
+  { type: 'addProperties', extraData: { age: 25, city: 'NY' } },
+  { type: 'removeProperties', keysToRemove: ['foo', 'city'] },
+  { type: 'clear' },
+];
+
+transformStateWithClones(state, actions);
