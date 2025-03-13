f90836b8e8f9be67aa1119f1d4cc9e9f78f8ca50
Alex Kryzhanovskyi <kryzhanoskyi.oleksandr@gmail.com>
2025-03-13 16:05:47 +0200

    transformStatesTask

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let duplicateState = { ...state };
+  const changesToState = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        duplicateState = { ...duplicateState, ...action.extraData };
+        break;
+
+      case 'clear':
+        duplicateState = {};
+        break;
+
+      case 'removeProperties':
+        duplicateState = { ...duplicateState };
+
+        for (const key of action.keysToRemove) {
+          delete duplicateState[key];
+        }
+        break;
+    }
+    changesToState.push({ ...duplicateState });
+  }
+
+  return changesToState;
 }
 
 module.exports = transformStateWithClones;
