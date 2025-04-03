9aedd5bf82e17518afa4f150ce2fce04d8332995
dima.yeromin <dima.yeromin@gmail.com>
2025-04-03 22:36:50 +0200

    re-write overcomlicated code from scratch

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,47 +1,37 @@
-'use strict';
-
+/**
+ * Transform state with clones
+ * @param {Object} state - Initial state object
+ * @param {Array} actions - Array of actions to apply
+ * @return {Array} - Array of states after each action
+ */
 function transformStateWithClones(state, actions) {
-  if (!Array.isArray(actions)) {
-    return [];
+  const stateHistory = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'clear':
+        currentState = {};
+        break;
+      case 'addProperties':
+        currentState = {
+          ...currentState,
+          ...action.extraData,
+        };
+        break;
+      case 'removeProperties':
+        currentState = { ...currentState };
+
+        for (const key of action.keysToRemove) {
+          delete currentState[key];
+        }
+        break;
+    }
+
+    stateHistory.push(currentState);
   }
 
-  return actions.reduce((results, action) => {
-    // Create a fresh clone of the current state
-    const currentState = { ...(results[results.length - 1] || state) };
-
-    // Apply the action
-    const newState = (() => {
-      switch (action.type) {
-        case 'addProperties':
-          return {
-            ...currentState,
-            ...(action.extraData || {}),
-          };
-
-        case 'removeProperties':
-          if (!action.keysToRemove?.length) {
-            return currentState;
-          }
-
-          const stateAfterRemoval = { ...currentState };
-
-          action.keysToRemove.forEach((key) => {
-            delete stateAfterRemoval[key];
-          });
-
-          return stateAfterRemoval;
-
-        case 'clear':
-          return {};
-
-        default:
-          return currentState;
-      }
-    })();
-
-    // Add the new state to results
-    return [...results, newState];
-  }, []);
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
