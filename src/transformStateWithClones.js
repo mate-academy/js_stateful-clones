c814e0e7b1cf0585f6d94372cc93c4802d111b24
dima.yeromin <dima.yeromin@gmail.com>
2025-04-03 11:00:19 +0200

    Refactor transformStateWithClones function to use 'actions' parameter instead of 'transforms'

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,48 +1,33 @@
 'use strict';
 
-/**
- * @param {Object} state
- * @param {Object[]} actions
- *
- * @return {Object[]}
- */
-function transformStateWithClones(state, transforms) {
-  // Initialize the result array to store all states
+function transformStateWithClones(state, actions) {
   const results = [];
-
-  // Clone the initial state
   let currentState = { ...state };
 
-  // Process each transform operation
-  transforms.forEach((transform) => {
-    // Create a new clone for each operation
+  actions.forEach((action) => {
     currentState = { ...currentState };
 
-    switch (transform.type) {
+    switch (action.type) {
       case 'addProperties':
-        // Add or update properties from extraData
         currentState = {
           ...currentState,
-          ...transform.extraData,
+          ...action.extraData,
         };
         break;
 
       case 'removeProperties':
-        // Remove specified properties
-        if (transform.keysToRemove && transform.keysToRemove.length > 0) {
-          transform.keysToRemove.forEach((key) => {
+        if (action.keysToRemove && action.keysToRemove.length > 0) {
+          action.keysToRemove.forEach((key) => {
             delete currentState[key];
           });
         }
         break;
 
       case 'clear':
-        // Reset to empty object
         currentState = {};
         break;
     }
 
-    // Add the new state to results
     results.push(currentState);
   });
 
