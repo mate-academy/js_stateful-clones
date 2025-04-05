f3491e259c4c4a0b058f0b60844cbc6829c907bf
Viktor <Sofiyanviktor@gamil.com>
2025-04-05 10:29:16 +0300

    Виконання завдання номер 2

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,28 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const results = [];
+
+  // Починаємо з копії початкового стану
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      currentState = { ...currentState, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      currentState = { ...currentState };
+
+      for (const key of action.keysToRemove) {
+        delete currentState[key];
+      }
+    } else if (action.type === 'clear') {
+      currentState = {};
+    }
+
+    results.push(currentState);
+  }
+
+  return results;
 }
 
 module.exports = transformStateWithClones;
