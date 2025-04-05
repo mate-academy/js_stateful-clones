7dfb7d65998a5bf000c29cc6c3ef765db44d0f90
Viktor <Sofiyanviktor@gamil.com>
2025-04-05 10:39:32 +0300

    Виправив зауваження ШІ

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,34 +1,27 @@
-'use strict';
-
-/**
- * @param {Object} state
- * @param {Object[]} actions
- *
- * @return {Object[]}
- */
-function transformStateWithClones(state, actions) {
+// eslint-disable-next-line no-unused-vars
+function ransformStateWithClones(state, actions) {
   const results = [];
-
-  // Починаємо з копії початкового стану
-  let currentState = { ...state };
+  let stateCopy = { ...state };
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      currentState = { ...currentState, ...action.extraData };
-    } else if (action.type === 'removeProperties') {
-      currentState = { ...currentState };
+    switch (action.type) {
+      case 'addProperties':
+        stateCopy = { ...stateCopy, ...action.extraData };
+        break;
+      case 'removeProperties':
+        stateCopy = { ...stateCopy };
 
-      for (const key of action.keysToRemove) {
-        delete currentState[key];
-      }
-    } else if (action.type === 'clear') {
-      currentState = {};
+        for (const key of action.keysToRemove) {
+          delete stateCopy[key];
+        }
+        break;
+      case 'clear':
+        stateCopy = {};
+        break;
     }
 
-    results.push(currentState);
+    results.push({ ...stateCopy }); // Важливо: пушимо копію!
   }
 
   return results;
 }
-
-module.exports = transformStateWithClones;
