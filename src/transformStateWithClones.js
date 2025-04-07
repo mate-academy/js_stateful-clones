651b8adb2b5ed113148cf7ab80d7b5bd14150381
lewis <61358721+ALTER-2357@users.noreply.github.com>
2025-04-07 14:10:24 +0100

    Revert "wip"

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,35 +7,7 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-    const stateHistory = [];
-    let currentState = { ...state };
-
-    for (const action of actions) {
-        const newState = { ...currentState };
-
-        switch (action.type) {
-            case 'clear':
-                currentState = {};
-                break;
-            case 'addProperties':
-                Object.assign(newState, action.extraData);
-                currentState = newState;
-                break;
-            case 'removeProperties':
-                action.keysToRemove.forEach(key => {
-                    delete newState[key];
-                });
-                currentState = newState;
-                break;
-            default:
-                break;
-        }
-
-        stateHistory.push(currentState);
-    }
-
-    return stateHistory;
+  // write code here
 }
-
 
 module.exports = transformStateWithClones;
