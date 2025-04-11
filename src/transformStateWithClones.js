6466caa30dd2f1469bbe62127324c181fd017ca5
Maksym Dukhan <maxduh747@icloud.com>
2025-04-11 12:48:41 +0300

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -34,7 +34,7 @@ function transformStateWithClones(state, actions) {
         nextState = { ...currentState };
     }
 
-    result.push({ ...nextState });
+    result.push(nextState);
     currentState = nextState;
   }
 
