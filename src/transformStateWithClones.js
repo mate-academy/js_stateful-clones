481486c1b22d56886924708bae806fddd04eea39
tobias <tobias.harasim@seznam.cz>
2025-04-22 10:31:30 +0200

    develop

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let stateClone = { ...state };
+  const stateHistory = [];
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      const extraData = action.extraData;
+
+      stateClone = { ...stateClone, ...extraData };
+    }
+
+    if (action.type === 'removeProperties') {
+      const propertiesToRemove = action.keysToRemove;
+
+      for (const property of propertiesToRemove) {
+        delete stateClone[property];
+      }
+    }
+
+    if (action.type === 'clear') {
+      stateClone = {};
+    }
+
+    stateHistory.push({ ...stateClone });
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
