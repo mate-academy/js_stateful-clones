07b9f5e904b27f46d3f91855bba6b5bd539271ce
Mykhailo Savchenko <savchenko.mihail02@gmail.com>
2025-04-24 15:49:14 +0300

    New solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -32,7 +32,7 @@ function transformStateWithClones(state, actions) {
         throw new Error(`Unknown action type: '${act.type}'`);
     }
 
-    allStates.push(copyState);
+    allStates.push({ ...copyState });
   }
 
   return allStates;
