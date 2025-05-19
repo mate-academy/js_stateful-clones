df46a5a96d00cd9e5b0c4638048d0d8b01ac6bd8
Your Name <lipe291@gmail.com>
2025-05-19 13:01:44 -0300

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,37 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    let nextState = { ...currentState };
+
+    switch (action.type) {
+      case 'clear':
+        nextState = {};
+        break;
+      case 'addProperties':
+        if (action.extraData) {
+          nextState = { ...nextState, ...action.extraData };
+        }
+        break;
+      case 'removeProperties':
+        if (action.keysToRemove) {
+          const newState = { ...nextState };
+
+          for (const key of action.keysToRemove) {
+            delete newState[key];
+          }
+          nextState = newState;
+        }
+        break;
+      default:
+        break;
+    }
+    currentState = nextState;
+  }
+
+  return [currentState];
 }
 
 module.exports = transformStateWithClones;
