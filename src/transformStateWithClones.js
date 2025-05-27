84a184717ee4b88dfa0f847359abacd025289895
Joe Bob <cmac2617@yahoo.com>
2025-05-27 14:41:24 -0400

    Simplified. Only one push operation after if-else block.

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -13,18 +13,16 @@ function transformStateWithClones(state, actions) {
   for (let i = 0; i < actions.length; i++) {
     if (actions[i].type === 'addProperties') {
       Object.assign(newState, actions[i].extraData);
-      stateHistory.push({ ...newState });
     } else if (actions[i].type === 'removeProperties') {
       for (let x = 0; x < actions[i].keysToRemove.length; x++) {
         delete newState[actions[i].keysToRemove[x]];
       }
-      stateHistory.push({ ...newState });
     } else {
       for (const key in newState) {
         delete newState[key];
       }
-      stateHistory.push({ ...newState });
     }
+    stateHistory.push({ ...newState });
   }
 
   return stateHistory;
