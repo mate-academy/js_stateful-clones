0950120ac05caf96cc9707bd465cc1dc27e131db
Mykyta Churylov <mykyta.churylov@student.tuke.sk>
2025-06-02 18:34:48 +0200

    update

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,21 +7,24 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  let massiv = [];
+  const massiv = [];
   let curState = { ...state };
+
   for (const key of actions) {
     if (key.type === 'addProperties') {
-      curState = {...curState, ...key.extraData}
+      curState = { ...curState, ...key.extraData };
     } else if (key.type === 'removeProperties') {
-      curState = {...curState};
+      curState = { ...curState };
+
       for (const key2 of key.keysToRemove) {
         delete curState[key2];
       }
     } else if (key.type === 'clear') {
       curState = {};
     }
-    massiv.push(curState);
+    massiv.push({ ...curState });
   }
+
   return massiv;
 }
 
