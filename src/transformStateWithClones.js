5609cdc89559a6101fdc7053093650eaf42593bb
Mateusz Seler <mateusz.seler@gmail.com>
2025-06-09 16:53:43 +0200

    implementation

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,41 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateArray = [];
+  let index = 0;
+
+  for (const action of actions) {
+    const newState = {};
+
+    if (index === 0) {
+      Object.assign(newState, state);
+    } else {
+      Object.assign(newState, stateArray[index - 1]);
+    }
+
+    if (action.type === 'addProperties') {
+      Object.assign(newState, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const propertie of action.keysToRemove) {
+        delete newState[propertie];
+      }
+    }
+
+    if (action.type === 'clear') {
+      if (Object.keys(newState).length > 0) {
+        for (const key in newState) {
+          delete newState[key];
+        }
+      }
+    }
+
+    index++;
+    stateArray.push(newState);
+  }
+
+  return stateArray;
 }
 
 module.exports = transformStateWithClones;
