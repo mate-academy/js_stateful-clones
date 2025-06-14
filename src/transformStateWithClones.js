fff96760093879d472278a14baaf6d45e16257f0
keygen_exe <matkovskiyf3vlad@gmail.com>
2025-06-15 01:15:28 +0300

    solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,40 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = structuredClone(state);
+
+  actions.forEach((el) => {
+    switch (el.type) {
+      case 'addProperties':
+        addProperties(el.extraData);
+        break;
+      case 'removeProperties':
+        removeProperties(el.keysToRemove);
+        break;
+      default:
+        clear();
+    }
+  });
+
+  const addProperties = (obj) => {
+    Object.keys(obj).forEach((key) => {
+      stateCopy[key] = obj[key];
+    });
+
+    return stateCopy;
+  };
+
+  const removeProperties = (obj) => {
+    Object.keys(obj).forEach((key) => {
+      delete stateCopy[key];
+    });
+
+    return stateCopy;
+  };
+
+  const clear = () => {
+    return {};
+  };
 }
 
 module.exports = transformStateWithClones;
