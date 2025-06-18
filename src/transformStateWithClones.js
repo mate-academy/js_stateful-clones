9174b5538cc329b6d83a31c0610b9548eb3578ed
Pavlo Zhabort <zhabortp@gmail.com>
2025-06-18 15:33:33 +0300

    finished task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,24 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+
+let currentState = { ...state }; 
+  const history = [];
+
+  for (const action of actions) {
+    if (action.type === 'clear') {
+      currentState = {};
+    } else if (action.type === 'addProperties') {
+      currentState = { ...currentState, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      currentState = { ...currentState }; 
+      for (const key of action.keysToRemove) {
+        delete currentState[key];
+      }
+    }
+
+    history.push(currentState);
+  }
 }
 
 module.exports = transformStateWithClones;
