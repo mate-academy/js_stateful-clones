6f5b83f4466f2ad79985170686d241f89b94a2bf
michelam <michelammilian@outlook.com>
2025-06-20 13:56:25 -0300

    Ajustes de lint requisitados para pull request

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -12,52 +12,48 @@ function transformStateWithClones(state, actions) {
   const chainActions = [];
   let keys = [];
   let values = [];
-  
-  for(const action of actions) {
-    
+
+  for (const action of actions) {
     let objectAction = {};
 
-    switch(action.type) {
-      
-          case 'addProperties':
-              
-              chainActions.length === 0 ? objectAction = {...state} : objectAction = {...chainActions[chainActions.length-1]}
-              
-              keys = Object.keys(action.extraData);
-              values = Object.values(action.extraData);
-              
-              for(let i = 0; i < keys.length; i++) {
-                
-                objectAction[keys[i]] = values[i]
-                
-              }
+    switch (action.type) {
+      case 'addProperties':
+        (() => {
+          return chainActions.length === 0
+            ? (objectAction = { ...state })
+            : (objectAction = { ...chainActions[chainActions.length - 1] });
+        })();
 
-              break;
-        
-        case 'removeProperties':
+        keys = Object.keys(action.extraData);
+        values = Object.values(action.extraData);
 
-              chainActions.length === 0 ? objectAction = {...state} : objectAction = {...chainActions[chainActions.length-1]};              
-              keys = [...action.keysToRemove];
-              
-              for(let i = 0; i < keys.length; i++) {
-                
-                delete objectAction[keys[i]];
-                
-              }
-              
-              break;
-              
-        case 'clear':
-              objectAction = {};
-              break;
-              
-      
+        for (let i = 0; i < keys.length; i++) {
+          objectAction[keys[i]] = values[i];
+        }
+
+        break;
+
+      case 'removeProperties':
+        (() => {
+          return chainActions.length === 0
+            ? (objectAction = { ...state })
+            : (objectAction = { ...chainActions[chainActions.length - 1] });
+        })();
+
+        for (let i = 0; i < keys.length; i++) {
+          delete objectAction[keys[i]];
+        }
+
+        break;
+
+      case 'clear':
+        objectAction = {};
+        break;
     }
 
     chainActions.push(objectAction);
-    
   }
-  
+
   return chainActions;
 }
 
