e7d2d31837c4104359de49a383ba0499a34f41ed
michelam-pseudo <124099775+michelam-pseudo@users.noreply.github.com>
2025-06-20 14:01:08 -0300

    Update transformStateWithClones.js

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -12,53 +12,48 @@ function transformStateWithClones(state, actions) {
   const chainActions = [];
   let keys = [];
   let values = [];
-  
-  for(const action of actions) {
-    
-    switch(action.type) {
-      
-          case 'addProperties':
-              
-              let objectActionAdd = {};
-              chainActions.length === 0 ? objectActionAdd = {...state} : objectActionAdd = {...chainActions[chainActions.length-1]}
-              
-              keys = Object.keys(action.extraData);
-              values = Object.values(action.extraData);
-              
-              for(let i = 0; i < keys.length; i++) {
-                
-                objectActionAdd[keys[i]] = values[i]
-                
-              }
-              
-              chainActions.push(objectActionAdd);
-              break;
-        
-        case 'removeProperties':
 
-              let objectActionRemove = {};
-              chainActions.length === 0 ? objectActionRemove = {...state} : objectActionRemove = {...chainActions[chainActions.length-1]};              
-              keys = [...action.keysToRemove];
-              
-              for(let i = 0; i < keys.length; i++) {
-                
-                delete objectActionRemove[keys[i]];
-                
-              }
-              
-              chainActions.push(objectActionRemove);
-              break;
-              
-        case 'clear':
-              
-              chainActions.push({});
-              break;
-              
-      
+  for (const action of actions) {
+    let objectAction = {};
+
+    switch (action.type) {
+      case 'addProperties':
+        (() => {
+          return chainActions.length === 0
+            ? (objectAction = { ...state })
+            : (objectAction = { ...chainActions[chainActions.length - 1] });
+        })();
+
+        keys = Object.keys(action.extraData);
+        values = Object.values(action.extraData);
+
+        for (let i = 0; i < keys.length; i++) {
+          objectAction[keys[i]] = values[i];
+        }
+
+        break;
+
+      case 'removeProperties':
+        (() => {
+          return chainActions.length === 0
+            ? (objectAction = { ...state })
+            : (objectAction = { ...chainActions[chainActions.length - 1] });
+        })();
+
+        for (let i = 0; i < keys.length; i++) {
+          delete objectAction[keys[i]];
+        }
+
+        break;
+
+      case 'clear':
+        objectAction = {};
+        break;
     }
-    
+
+    chainActions.push(objectAction);
   }
-  
+
   return chainActions;
 }
 
