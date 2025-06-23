a3c673c75736393af53a738c07eeef392ea428b8
Manuel Lezameta <manuelezameta@gmail.com>
2025-06-23 17:28:52 -0500

    Added logic for transformStateWithClones function

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,26 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateClone = Object.assign({}, state);
+  const stateCloneArr = [];
+
+  actions.forEach((action) => {
+    if (action.type === 'addProperties') {
+      Object.assign(stateClone, action.extraData);
+    } else if (action.type === 'removeProperties') {
+      const keys = action.keysToRemove;
+
+      keys.forEach((k) => {
+        delete stateClone[k];
+      });
+    } else if (action.type === 'clear') {
+      Object.keys(stateClone).forEach((key) => delete stateClone[key]);
+    }
+    stateCloneArr.push(Object.assign({}, stateClone));
+  });
+
+  // Push the final state clone after all actions
+  return stateCloneArr;
 }
 
 module.exports = transformStateWithClones;
