027eaefbb37961d04761046db757facb084bcd5c
anastasia22422 <anastasia.skripka.sao@gmail.com>
2025-06-30 21:25:33 +0200

    Update transformStateWithClones.test.js

--- src/transformStateWithClones.test.js
+++ src/transformStateWithClones.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const transformStateWithClones = require('./transformStateWithClones.js');
+const transformStateWithClones = require('./transformStateWithClones');
 
 test('Should create a new object with a single added property', () => {
   const state = {};
