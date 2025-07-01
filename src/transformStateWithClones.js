d9693c6432b6f8a2255243c924b274aa7f4b9d3b
anastasia22422 <anastasia.skripka.sao@gmail.com>
2025-07-01 22:31:06 +0200

    Add files via upload

---
+++ src/transformStateWithClones.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @param {Object} state
+ * @param {Object[]} actions
+ *
+ * @return {Object[]}
+ */
+function transformStateWithClones(state, actions) {
+  const stateHistory = [];
+  let stateCopy = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        stateCopy = { ...stateCopy, ...action.extraData };
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete stateCopy[key];
+        }
+        break;
+
+      case 'clear':
+        stateCopy = {};
+        break;
+
+      default:
+        throw new Error('Смерть');
+    }
+
+    stateHistory.push({ ...stateCopy });
+  }
+
+  return stateHistory;
+}
+
+module.exports = transformStateWithClones;
