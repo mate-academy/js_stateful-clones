b935f861c182876c2b8e3525b9ef6f73ad99731a
Mykyta Musaiev <n.musaiev@gmail.com>
2025-07-08 16:44:16 +0300

    task done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const statesHistory = [];
+
+  const currentState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'clear':
+        Object.keys(currentState).forEach((key) => delete currentState[key]);
+        break;
+      case 'addProperties':
+        Object.assign(currentState, action.extraData);
+        break;
+      case 'removeProperties':
+        action.keysToRemove.forEach((key) => delete currentState[key]);
+        break;
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
+    }
+
+    statesHistory.push({ ...currentState });
+  }
+
+  return statesHistory;
 }
 
 module.exports = transformStateWithClones;
