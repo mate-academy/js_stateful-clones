e441e71d1f408ab12535a15ece36658907c40221
Eliza Dubiel <dubieleliza89@gmail.com>
2025-07-09 18:01:49 +0200

    feat: implement transformStateWithClones function

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,29 @@
-'use strict';
-
-/**
- * @param {Object} state
- * @param {Object[]} actions
- *
- * @return {Object[]}
- */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let currentState = { ...state };
+  const history = [];
+
+  for (const action of actions) {
+    let newState;
+
+    if (action.type === 'clear') {
+      newState = {};
+    } else {
+      newState = { ...currentState };
+    }
+
+    if (action.type === 'addProperties') {
+      Object.assign(newState, action.extraData);
+    } else if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete newState[key];
+      }
+    }
+
+    currentState = newState;
+    history.push(newState);
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
