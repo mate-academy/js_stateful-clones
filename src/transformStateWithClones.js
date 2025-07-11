e9b4f03e2e6dfc340771ebd185d7d71d099ec0d1
Oleksandr Mykyteichuk <psihanul@ukr.net>
2025-07-11 12:53:58 +0300

    fix problem

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -32,7 +32,7 @@ function transformStateWithClones(state, actions) {
         throw new Error(`Unknown action type: ${action.type}`);
     }
 
-    stateHistory.push(curState);
+    stateHistory.push({ ...curState });
   }
 
   return stateHistory;
