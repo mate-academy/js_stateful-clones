351d5b87300b49d46d8ba0c7a8e633ccdf9d8d10
DmitriyALEX <doiroffical@gmail.com>
2025-07-13 15:49:33 +0300

    changed logic to switch/case

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -12,21 +12,40 @@ function transformStateWithClones(state, actions) {
   let stateCopy = Object.assign({}, state);
 
   actions.forEach((action) => {
-    if (action.type === 'addProperties') {
-      stateCopy = { ...stateCopy, ...action.extraData };
-      recievedStates.push({ ...stateCopy });
-    } else if (action.type === 'removeProperties') {
-      const newState = { ...stateCopy };
+    switch (action.type) {
+      case 'addProperties':
+        stateCopy = { ...stateCopy, ...action.extraData };
+        recievedStates.push({ ...stateCopy });
+        break;
+      case 'removeProperties':
+        const newState = { ...stateCopy };
 
-      action.keysToRemove.forEach((removedKey) => {
-        delete newState[removedKey];
-      });
-      stateCopy = newState;
-      recievedStates.push({ ...stateCopy });
-    } else if (action.type === 'clear') {
-      stateCopy = {};
-      recievedStates.push({ ...stateCopy });
+        action.keysToRemove.forEach((removedKey) => {
+          delete newState[removedKey];
+        });
+        stateCopy = newState;
+        recievedStates.push({ ...stateCopy });
+        break;
+      case 'clear':
+        stateCopy = {};
+        recievedStates.push({ ...stateCopy });
     }
+
+    // if (action.type === 'addProperties') {
+    //   stateCopy = { ...stateCopy, ...action.extraData };
+    //   recievedStates.push({ ...stateCopy });
+    // } else if (action.type === 'removeProperties') {
+    //   const newState = { ...stateCopy };
+
+    //   action.keysToRemove.forEach((removedKey) => {
+    //     delete newState[removedKey];
+    //   });
+    //   stateCopy = newState;
+    //   recievedStates.push({ ...stateCopy });
+    // } else if (action.type === 'clear') {
+    //   stateCopy = {};
+    //   recievedStates.push({ ...stateCopy });
+    // }
   });
 
   return recievedStates;
