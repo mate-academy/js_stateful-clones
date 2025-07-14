f5df2eab7cf73fd58b8f37846e27e05ce1755c4f
Denys <yolm9yom@gmail.com>
2025-07-14 14:25:30 +0300

    cloningObject

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+  let copyState = { ...state };
+
+  for (let i = 0; i < actions.length; i++) {
+    const action = actions[i];
+
+    if (action.type === 'addProperties') {
+      for (const key in action.extraData) {
+        copyState[key] = action.extraData[key];
+      }
+      stateHistory.push({ ...copyState });
+    } else if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete copyState[key];
+      }
+      stateHistory.push({ ...copyState });
+    } else if (action.type === 'clear') {
+      copyState = {};
+      stateHistory.push({ ...copyState });
+    } else {
+      return 'asd';
+    }
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
