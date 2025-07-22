bc7a15cce0c3ae2a3802878bf3f6f02fce3673c9
Denys Nikiforov <166646786+DenysNikiforov@users.noreply.github.com>
2025-07-22 15:09:25 +0300

    Update transformStateWithClones.js

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,30 @@
  *
  * @return {Object[]}
  */
-function transformStateWithClones(state, actions) {
-  // write code here
+function transformState(state, actions) {
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        if (action.extraData) {
+          Object.assign(state, action.extraData);
+        }
+        break;
+
+      case 'removeProperties':
+        if (Array.isArray(action.keysToRemove)) {
+          for (const key of action.keysToRemove) {
+            delete state[key];
+          }
+        }
+        break;
+
+      case 'clear':
+        for (const key of Object.keys(state)) {
+          delete state[key];
+        }
+        break;
+    }
+  }
 }
 
 module.exports = transformStateWithClones;
