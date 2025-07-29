3443c0688f4c6cba9394cef1dbd998819474e4d6
Victor <viktorstupin9@gmail.comgit config --global user.email viktorstupin9@mail.com>
2025-07-29 12:45:47 +0300

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,25 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let current = { ...state };
+  const result = [];
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      current = { ...current, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      current = { ...current };
+
+      for (const key of action.keysToRemove) {
+        delete current[key];
+      }
+    } else if (action.type === 'clear') {
+      current = {};
+    }
+    result.push({ ...current });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
