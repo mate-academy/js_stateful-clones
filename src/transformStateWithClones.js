1755db0103cff44079618fe2ef210e8bce2990e3
Naomi <naomidie1311@gmail.com>
2025-07-29 19:55:59 +0200

    20250729

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,35 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const results = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    let newState = { ...currentState };
+
+    switch (action.type) {
+      case 'clear':
+        newState = {};
+        break;
+
+      case 'addProperties':
+        newState = { ...newState, ...action.extraData };
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete newState[key];
+        }
+        break;
+
+      default:
+        break;
+    }
+    results.push(newState);
+    currentState = newState;
+  }
+
+  return results;
 }
 
 module.exports = transformStateWithClones;
