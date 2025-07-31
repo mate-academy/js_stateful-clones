d6810486e1eaaae35cdbf34ff087826a97cf6328
FRANCISCOPELEGRINI <franciscoprcunha@gmail.com>
2025-07-30 21:40:03 -0300

    CORREÇÃO AGORA 100%

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,39 +1,29 @@
 'use strict';
 
 function transformStateWithClones(state, actions) {
+  let arr = [];
 
-let arr = []
-let suporte = {}
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'addProperties') {
+      let suporte = Object.assign({}, state, actions[i].extraData); 
+      arr[i] = suporte;
 
-for(let i = 0; i < actions.length; i++){
-    
-  if (actions[i].type === 'addProperties') {
-   Object.assign(suporte,state, actions[i].extraData,)
-   arr[i] = suporte
-  }
-  else if(actions[i].type === 'removeProperties'){
-    i === 0 ? arr[0] = state : arr[i] = arr[i-1]
+    } else if (actions[i].type === 'removeProperties') {
+      let clone = i === 0 ? { ...state } : { ...arr[i - 1] };
 
-   for(let o = 0; o < actions[o].keysToRemove.length; o++){ 
-    
-    const key = Object.keys(arr[i])
-    console.log("quero apagar" + " ['foo', 'bar']")
-    console.log("arr por enquanto")
-    console.log(arr[i])
-  
-    if (actions[o].keysToRemove.includes(arr[o][key[o]])){
-    
-       delete arr[i][key[o]]
-       console.log(arr)    
+      for (let o = 0; o < actions[i].keysToRemove.length; o++) {
+        const key = actions[i].keysToRemove[o];
+        delete clone[key];
+      }
+
+      arr[i] = clone;
+
+    } else if (actions[i].type === "clear") {
+      arr[i] = {};
     }
-   }
   }
-  else if (actions[i].type === "clear") {
-    arr[i] = {}
-    console.log(arr)
-   }
+
+  return arr;
 }
 
-}
-module.exports = transformStateWithClones; 
-
+module.exports = transformStateWithClones;
