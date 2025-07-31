1d476019efca8df983b547d8e08792888f104086
FRANCISCOPELEGRINI <franciscoprcunha@gmail.com>
2025-07-30 21:34:15 -0300

    CORREÇÃO MATE ACADMY STATEWITHCLONES

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,39 @@
 'use strict';
 
-/**
- * @param {Object} state
- * @param {Object[]} actions
- *
- * @return {Object[]}
- */
 function transformStateWithClones(state, actions) {
-  // write code here
+
+let arr = []
+let suporte = {}
+
+for(let i = 0; i < actions.length; i++){
+    
+  if (actions[i].type === 'addProperties') {
+   Object.assign(suporte,state, actions[i].extraData,)
+   arr[i] = suporte
+  }
+  else if(actions[i].type === 'removeProperties'){
+    i === 0 ? arr[0] = state : arr[i] = arr[i-1]
+
+   for(let o = 0; o < actions[o].keysToRemove.length; o++){ 
+    
+    const key = Object.keys(arr[i])
+    console.log("quero apagar" + " ['foo', 'bar']")
+    console.log("arr por enquanto")
+    console.log(arr[i])
+  
+    if (actions[o].keysToRemove.includes(arr[o][key[o]])){
+    
+       delete arr[i][key[o]]
+       console.log(arr)    
+    }
+   }
+  }
+  else if (actions[i].type === "clear") {
+    arr[i] = {}
+    console.log(arr)
+   }
 }
 
-module.exports = transformStateWithClones;
+}
+module.exports = transformStateWithClones; 
+
