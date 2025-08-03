d8f6d9288a5ffb1a91e0ae8baae9f2ea8ced2607
Denys Anokhin <denys.anokhin.dev@gmail.com>
2025-08-04 00:48:42 +0300

    js-stateful-clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const stateClone = { ...state };
+
+  for (let i = 0; i < actions.length; i++) {
+    switch (actions[i].type) {
+      case 'addProperties':
+        Object.assign(stateClone, actions[i].extraData);
+        break;
+      case 'removeProperties':
+        for (let y = 0; y < actions[i].keysToRemove.length; y++) {
+          delete stateClone[actions[i].keysToRemove[y]];
+        }
+        break;
+      default:
+        for (const key in stateClone) {
+          delete stateClone[key];
+        }
+        break;
+    }
+
+    result.push({ ...stateClone });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
