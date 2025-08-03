3688120d81b6a9cef023b7c7f0bd62225f1a18cb
kirky359 <lol783176@gmail.com>
2025-08-03 17:21:49 +0300

    task completed

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,26 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const array = [];
+  let currentState = { ...state };
+
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'clear') {
+      currentState = {};
+    } else if (actions[i].type === 'addProperties') {
+      currentState = { ...currentState, ...actions[i].extraData };
+    } else if (actions[i].type === 'removeProperties') {
+      currentState = { ...currentState };
+
+      for (const key of actions[i].keysToRemove) {
+        delete currentState[key];
+      }
+      currentState = { ...currentState };
+    }
+    array.push(currentState);
+  }
+
+  return array;
 }
 
 module.exports = transformStateWithClones;
