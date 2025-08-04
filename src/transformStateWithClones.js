ad6bd37edf9ddb8501582f4268bf5ebc1a033045
unknown <an221m@iicloud.com>
2025-08-04 12:33:29 +0300

    попередньо працює, але перевірка не пропуска 4/12 :)

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,66 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const transformedState = [];
+
+  for (let i = 0; i < actions.length; i++) {
+    if (i === 0) {
+      transformedState[i] = state;
+    }
+
+    if (i > 0) {
+      transformedState[i] = { ...transformedState[i - 1] };
+    }
+
+    if (actions[i]['type'] === 'clear') {
+      transformedState[i] = {};
+    }
+
+    if (actions[i]['type'] === 'addProperties') {
+      transformedState[i] = Object.assign(
+        {},
+        transformedState[i],
+        actions[i]['extraData'],
+      );
+    }
+
+    if (actions[i]['type'] === 'removeProperties') {
+      for (const key of actions[i]['keysToRemove']) {
+        delete transformedState[i][key];
+      }
+    }
+  }
+
+  return transformedState;
 }
+
+// transformStateWithClones(state, [
+//   {
+//     type: 'addProperties',
+//     extraData: { name: 'Jim', hello: 'world' },
+//   },
+//   {
+//     type: 'removeProperties',
+//     keysToRemove: ['bar', 'hello'],
+//   },
+//   {
+//     type: 'addProperties',
+//     extraData: { another: 'one' },
+//   },
+// ]);
+
+// transformStateWithClones(state, [
+//   {
+//     type: 'addProperties',
+//     extraData: { yet: 'another property' },
+//   },
+//   { type: 'clear' },
+//   {
+//     type: 'addProperties',
+//     extraData: { foo: 'bar', name: 'Jim' },
+//   },
+// ]);
 
 module.exports = transformStateWithClones;
