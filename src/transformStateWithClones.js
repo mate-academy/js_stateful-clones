531a5191740fb28ff65958026b4807d9d295ed22
Demetrio dos Anjos <ddoanjos@gmail.com>
2025-08-12 19:30:59 -0300

    Exercício transformStateWithClones realizado

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,27 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const history = [];
+  let currentState = { ...state }; // clone inicial para não modificar o origin
+
+  for (const action of actions) {
+    if (action.type === 'clear') {
+      currentState = {};
+    } else if (action.type === 'addProperties') {
+      currentState = { ...currentState, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      currentState = { ...currentState };
+
+      for (const key of action.keysToRemove) {
+        if (key in currentState) {
+          delete currentState[key];
+        }
+      }
+    }
+    history.push(currentState);
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
