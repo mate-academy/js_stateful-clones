424ec635dd520524ff8ef958f21507ad9ea90448
DropPredator <ionchiccc@gmail.com>
2025-08-14 11:28:22 +0300

    enjoying complete task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateClone = structuredClone(state);
+  const states = [];
+
+  for (const action of actions) {
+    const { type, extraData = {}, keysToRemove = [] } = action;
+
+    if (type === 'addProperties') {
+      for (const [key, value] of Object.entries(extraData)) {
+        stateClone[key] = value;
+      }
+    } else if (type === 'removeProperties') {
+      for (const key of keysToRemove) {
+        delete stateClone[key];
+      }
+    } else if (type === 'clear') {
+      for (const key of Object.keys(stateClone)) {
+        delete stateClone[key];
+      }
+    }
+
+    states.push(structuredClone(stateClone));
+  }
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
