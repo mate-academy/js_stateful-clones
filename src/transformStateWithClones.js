a8f312d76865b85ebbea2ffc6f63a7b075474362
DropPredator <ionchiccc@gmail.com>
2025-08-14 13:00:05 +0300

    solved

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,44 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  let stateCopy = { ...state };
+  const states = [];
+
+  for (const action of actions) {
+    const { type, extraData = {}, keysToRemove = [] } = action;
+
+    switch (type) {
+      case 'addProperties':
+        for (const [key, value] of Object.entries(extraData)) {
+          stateCopy[key] = value;
+        }
+        break;
+
+      case 'removeProperties':
+        for (const key of keysToRemove) {
+          delete stateCopy[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key of Object.keys(stateCopy)) {
+          delete stateCopy[key];
+        }
+        break;
+
+      default:
+        Error(`${type} is not supported`);
+    }
+
+    states.push(structuredClone(stateCopy));
+    stateCopy = Object.assign({}, states[states.length - 1]);
+
+    console.log(stateCopy);
+  }
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
