6e086b8d6da36b49d79389e9b27ede9f76f8f4d6
Aleks Pashkevych <alekspashkevych@gmail.com>
2025-08-18 10:47:37 +0300

    some changes

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,29 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  let obj = { ...state };
+  const arr = [];
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      obj = { ...obj, ...action.extraData };
+      arr.push({ ...obj });
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete obj[key];
+      }
+      arr.push({ ...obj });
+    }
+
+    if (action.type === 'clear') {
+      obj = {};
+      arr.push({ ...obj });
+    }
+  }
+
+  return obj;
 }
 
 module.exports = transformStateWithClones;
