0a22d76460e742200a23b7b7280cca7be9c85707
Maciej Sikora <maciej.sikora1508@gmail.com>
2025-08-26 13:43:27 +0200

    Task Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,30 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  return actions.reduce((acc, action) => {
+    const prev = acc.length ? acc[acc.length - 1] : state;
+    let next;
+
+    if (action.type === 'clear') {
+      next = {};
+    } else if (action.type === 'addProperties') {
+      next = { ...prev, ...(action.extraData || {}) };
+    } else if (action.type === 'removeProperties') {
+      const ban = new Set(action.keysToRemove || []);
+
+      next = Object.fromEntries(
+        Object.entries(prev).filter(([k]) => !ban.has(k)),
+      );
+    } else {
+      next = { ...prev };
+    }
+
+    acc.push(next);
+
+    return acc;
+  }, []);
 }
 
 module.exports = transformStateWithClones;
