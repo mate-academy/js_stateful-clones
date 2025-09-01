d402d62cc3f6261809dbdea693068a9041997206
Zoryana Fedorovska <zfedorovska@gmail.com>
2025-09-01 06:31:36 +0300

    added code for transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -3,11 +3,28 @@
 /**
  * @param {Object} state
  * @param {Object[]} actions
- *
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let prev = { ...state };
+  const history = [];
+
+  for (const a of actions) {
+    if (a.type === 'clear') {
+      prev = {};
+    } else if (a.type === 'addProperties') {
+      prev = { ...prev, ...(a.extraData || {}) };
+    } else if (a.type === 'removeProperties') {
+      const toRemove = new Set(a.keysToRemove || []);
+
+      prev = Object.fromEntries(
+        Object.entries(prev).filter(([k]) => !toRemove.has(k)),
+      );
+    }
+    history.push(prev);
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
