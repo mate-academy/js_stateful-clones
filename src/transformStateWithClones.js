cd95bbd7fcfb5f989deb8a659693cce687cfaa43
Okhrimenko Maksym <maksohrimenko1@gmail.com>
2025-09-02 18:15:39 +0300

    fix needed

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,28 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    if (type === 'addProperties') {
+      Object.assign(state, extraData);
+      stateHistory.push({ ...state });
+    } else if (type === 'removeProperties') {
+      for (const key of keysToRemove) {
+        delete state[key];
+      }
+      stateHistory.push({ ...state });
+    } else if (type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+      stateHistory.push({ ...state });
+    }
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
