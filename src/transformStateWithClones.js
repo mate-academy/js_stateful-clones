0d96476b17b9c972bf82c4c99ad8cea29d7b3beb
Nazarii Kinashchuk <nkinah93@gmail.com>
2025-09-03 17:56:38 +0300

    Solution01

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,31 +7,35 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // Початковий стан, який буде оновлюватись
   let currentState = { ...state };
   const stateHistory = [];
 
-  // Цикл по діях
   actions.forEach((action) => {
-    // В залежності від типу дії, оновлюємо currentState
-    if (action.type === 'addProperties') {
-      currentState = { ...currentState, ...action.extraData };
-    } else if (action.type === 'removeProperties') {
-      const nextState = { ...currentState };
+    switch (action.type) {
+      case 'addProperties':
+        currentState = { ...currentState, ...action.extraData };
+        break;
 
-      action.keysToRemove.forEach((key) => {
-        delete nextState[key];
-      });
-      currentState = nextState;
-    } else if (action.type === 'clear') {
-      currentState = {};
+      case 'removeProperties':
+        const nextState = { ...currentState };
+
+        action.keysToRemove.forEach((key) => {
+          delete nextState[key];
+        });
+        currentState = nextState;
+        break;
+
+      case 'clear':
+        currentState = {};
+        break;
+
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
     }
 
-    // Додаємо клон поточного стану до історії
     stateHistory.push({ ...currentState });
   });
 
-  // Повертаємо історію станів
   return stateHistory;
 }
 
