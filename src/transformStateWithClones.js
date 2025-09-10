07d59a5d59a22d0d456f47b0580ae597d2845a0c
Marina Kramarchuk <kramarchukmm20182018@gmail.com>
2025-09-10 17:37:32 +0300

    made my hometask

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arrayClones = [];
+
+  let objectClone = Object.assign({}, state);
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      const cloneAddingProperties = Object.assign(
+        {},
+        objectClone,
+        action.extraData,
+      );
+
+      arrayClones.push(cloneAddingProperties);
+      objectClone = cloneAddingProperties;
+    } else if (action.type === 'removeProperties') {
+      const cloneRemovedProperties = Object.assign({}, objectClone);
+
+      for (const item of action.keysToRemove) {
+        delete cloneRemovedProperties[item];
+      }
+
+      arrayClones.push(cloneRemovedProperties);
+      objectClone = cloneRemovedProperties;
+    } else if (action.type === 'clear') {
+      arrayClones.push({});
+      objectClone = {};
+    }
+  }
+
+  return arrayClones;
 }
 
 module.exports = transformStateWithClones;
