0a0acdc3ed72a5ce4172eacb5398854701affd05
Elina <elina@MacBook-Pro-Elina.local>
2025-09-10 19:37:53 +0300

    solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,37 @@
  *
  * @return {Object[]}
  */
-function transformStateWithClones(state, actions) {
-  // write code here
+  function transformStateWithClones(state, actions) {
+  const result = [];
+  let currentState = { ...state }; // clone initial state
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'clear':
+        currentState = {};
+        break;
+
+      case 'addProperties':
+        currentState = { ...currentState, ...action.extraData };
+        break;
+
+      case 'removeProperties':
+        currentState = { ...currentState }; // clone before mutation
+        for (const key of action.keysToRemove) {
+          delete currentState[key];
+        }
+        break;
+
+      default:
+        // ignore unknown action types
+        break;
+    }
+
+    result.push({ ...currentState }); // push a clone of the new state
+  }
+
+  return result;
 }
+
 
 module.exports = transformStateWithClones;
