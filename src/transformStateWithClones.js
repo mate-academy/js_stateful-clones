822fb854cbbb4c8092773056bc31f56275954a29
Daria Serebriakova <[  ]>
2025-09-11 14:36:33 +0300

    add solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,37 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+  let copyState = { ...state };
+
+  for (const action of actions) {
+    let newState;
+
+    switch (action.type) {
+      case 'addProperties':
+        newState = { ...copyState, ...action.extraData };
+        break;
+
+      case 'removeProperties':
+        newState = { ...copyState };
+
+        for (const key of action.keysToRemove) {
+          delete newState[key];
+        }
+        break;
+
+      case 'clear':
+        newState = {};
+        break;
+
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
+    }
+    stateHistory.push(newState);
+    copyState = newState;
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
