a703078d493cfc8f538b081bd85ed19cfa4b2f52
Ievgen Petryk <jakeshad123@gmail.com>
2025-09-11 16:38:03 +0300

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  const statesAfterAction = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete stateCopy[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
+
+        break;
+
+      default:
+        // eslint-disable-next-line no-console
+        console.error(`Unknown action type: ${action.type}`);
+    }
+    statesAfterAction.push({ ...stateCopy });
+  }
+
+  return statesAfterAction;
 }
 
 module.exports = transformStateWithClones;
