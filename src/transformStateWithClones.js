9b3a3ae0b4eeeecf135973ae13a2928ab823a618
art <art@debian.art>
2025-09-11 15:13:55 +0300

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,38 @@
  *
  * @return {Object[]}
  */
+'use strict';
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const states = [];
+
+  for (const action of actions) {
+    let newState;
+
+    if (action.type === 'addProperties') {
+      const lastState = states.length ? states[states.length - 1] : state;
+      newState = { ...lastState };
+      for (const key in action.extraData) {
+        newState[key] = action.extraData[key];
+      }
+
+    } else if (action.type === 'removeProperties') {
+      const lastState = states.length ? states[states.length - 1] : state;
+      newState = { ...lastState };
+      for (const key of action.keysToRemove) {
+        delete newState[key];
+      }
+
+    } else if (action.type === 'clear') {
+      newState = {};
+    }
+
+    states.push(newState);
+  }
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
+
+
