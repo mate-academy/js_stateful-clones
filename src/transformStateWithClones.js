4ec4a7caaddf5f411de9a06c73ea7e353e02280d
Dmytruk Serhii <dmitruksl92@gmail.com>
2025-09-12 17:52:50 +0300

    Fixed remarks

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -9,26 +9,25 @@
 function transformStateWithClones(state, actions) {
   // write code here
   let history = [];
-  let current = { ...state };
+  let stateCopy = { ...state };
 
   for (const action of actions) {
     const type = action.type;
 
-    if (type === 'addProperties') {
-      Object.assign(current, action.extraData);
+    switch(type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+      break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete stateCopy[key];
+        }
+      break;
+      default:
+        stateCopy = {}
     }
 
-    if (type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete current[key];
-      }
-    }
-
-    if (type === 'clear') {
-      current = {}
-    }
-
-    history.push({ ...current})
+    history.push({ ...stateCopy})
   }
 
   return history
