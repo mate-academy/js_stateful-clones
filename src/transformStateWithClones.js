4752ff511ba55d54419bcbd550b4517ddfb55e16
nastiaserkh <nastiaserkhove@gmail.com>
2025-09-12 11:54:26 +0300

    refactor

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,22 +11,27 @@ function transformStateWithClones(state, actions) {
   const newState = { ...state };
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      for (const key in action.extraData) {
-        newState[key] = action.extraData[key];
-      }
-    }
+    switch (action.type) {
+      case 'addProperties':
+        for (const key in action.extraData) {
+          newState[key] = action.extraData[key];
+        }
+        break;
 
-    if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete newState[key];
-      }
-    }
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete newState[key];
+        }
+        break;
 
-    if (action.type === 'clear') {
-      for (const key in newState) {
-        delete newState[key];
-      }
+      case 'clear':
+        for (const key in newState) {
+          delete newState[key];
+        }
+        break;
+
+      default:
+        break;
     }
 
     result.push({ ...newState });
