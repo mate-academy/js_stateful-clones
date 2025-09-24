4aa2e929eb1e73281adb4cede02becc7d62179c2
ivandobychin <ivandobychin123@gmail.com>
2025-09-24 18:05:47 +0300

    push

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  /*if (actions[0].type === 'addProperties') {
+    for (const key in actions[0].extraData) {
+      if (Object.hasOwn(state, key) === true) {
+        continue;
+      }
+      state[key] = actions[0].extraData[key];
+    }
+  } else if (actions[0].type === 'removeProperties') {
+    for (const key in actions[0].keysToRemove) {
+      delete state[key];
+    }
+  } else if (actions[0].type === 'clear') {
+    for (const key in state) {
+      delete state[key];
+    }
+  }*/
+
+  for (const action of actions) {
+    console.log(action.type, action.extraData);
+
+    if (action.type === 'addProperties') {
+      for (const key in action.extraData) {
+        state[key] = action.extraData[key];
+      }
+    }
+  }
+
+  console.log(state);
 }
 
 module.exports = transformStateWithClones;
