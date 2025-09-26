e866e4794610e245c5e69d750987be78d3431d3e
DYM <dmetedji@gmail.com>
2025-09-26 18:31:17 +0000

    add throw

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -32,7 +32,7 @@ function transformStateWithClones(state, actions) {
         break;
 
       default:
-        break;
+        throw new Error(`Unknown action type at step ${step}: ${step.type}`);
     }
 
     results.push({ ...newState });
