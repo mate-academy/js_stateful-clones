ff7b7c53e22b1cc24165983bc6712327fdde960d
вера <v.skorobagatova@ukr.net>
2025-09-30 15:02:02 +0300

    requested changes

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -35,7 +35,7 @@ function transformStateWithClones(state, actions) {
         break;
 
       default:
-        break;
+        throw new Error(`Unknown action type: ${act.type}`);
     }
     stateCopyHistory.push({ ...stateCopy });
   }
