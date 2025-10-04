94b8c6866933e33d64d07dac5ace68fb1224d9b2
Valentyna <pr.valya1989@gmail.com>
2025-10-04 17:32:37 +0300

    Solution number 3

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -34,7 +34,7 @@ function transformStateWithClones(state, actions) {
         break;
 
       default:
-        break;
+        throw new Error(`Unknown action type: ${action.type}`);
     }
 
     currentState = stateCopy;
