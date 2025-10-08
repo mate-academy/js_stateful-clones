381dc30d9adc2c32d2cd5789a7a70415f5471980
JoAnna578 <joannakoloczek1@gmail.com>
2025-10-08 23:22:10 +0200

    Add transformStateWithClones solution
    Implementacja funkcji transformStateWithClones zgodnie z wymaganiami zadania.

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,23 @@
-'use strict';
-
-/**
- * @param {Object} state
- * @param {Object[]} actions
- *
- * @return {Object[]}
- */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let currentState = { ...state };
+  const result = [];
+
+  for (const action of actions) {
+    if (action.type === 'clear') {
+      currentState = {};
+    } else if (action.type === 'addProperties') {
+      currentState = { ...currentState, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      currentState = Object.fromEntries(
+        Object.entries(currentState).filter(([key]) => !action.keysToRemove.includes(key))
+      );
+    }
+
+    result.push({ ...currentState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
+
