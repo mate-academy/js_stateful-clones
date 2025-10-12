81be98a4b40c25660a22f579b54bb9341dafd6fc
talan <talantm89@gmail.com>
2025-10-12 18:24:01 +0300

    Implement transformStateWithClones function with immutable state transformations

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,26 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let currentState = { ...state }; // створюємо клон початкового стану
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      currentState = { ...currentState, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      currentState = { ...currentState };
+
+      for (const key of action.keysToRemove) {
+        delete currentState[key];
+      }
+    } else if (action.type === 'clear') {
+      currentState = {};
+    }
+
+    result.push({ ...currentState }); // додаємо клон поточного стану
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
