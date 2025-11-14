36931f017647aab3f5909856f1bb59efacc219fc
Tymofii <zrozik@MacBook-Air-Tymofii.local>
2025-11-14 14:58:53 +0200

    Writed a `transformStateWithClones`

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,28 @@
  *
  * @return {Object[]}
  */
-function transformStateWithClones(state, actions) {
-  // write code here
-}
+const transformStateWithClones = (state, actions) => {
+  const result = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    let newState = { ...currentState };
+
+    if (action.type === 'clear') {
+      newState = {};
+    } else if (action.type === 'addProperties') {
+      Object.assign(newState, action.extraData);
+    } else if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete newState[key];
+      }
+    }
+
+    result.push(newState);
+    currentState = newState;
+  }
+
+  return result;
+};
 
 module.exports = transformStateWithClones;
