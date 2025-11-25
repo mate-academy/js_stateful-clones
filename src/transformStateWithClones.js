e8ac3291dfa4fa86a70fb37426f509d7ecf6d253
Anna Rodina <tavoshka@gmail.com>
2025-11-25 22:09:14 +0200

    task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const allStates = [];
+  let oneIterationState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        oneIterationState = {
+          ...oneIterationState,
+          ...action.extraData,
+        };
+        break;
+      case 'removeProperties':
+        action.keysToRemove.forEach((key) => {
+          delete oneIterationState[key];
+        });
+        break;
+      case 'clear':
+        Object.keys(oneIterationState).forEach(
+          (key) => delete oneIterationState[key],
+        );
+        break;
+      default:
+        throw new Error(`Unknown action: ${action}`);
+    }
+    allStates.push({ ...oneIterationState });
+  }
+
+  return allStates;
 }
 
 module.exports = transformStateWithClones;
