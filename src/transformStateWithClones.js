e8b11ec7ce57fef6e06344c73c7471db8a421f30
Yurii Buriak <yuriiburiak8@gmail.com>
2025-12-02 10:21:11 +0100

    Implement transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    let newState = { ...currentState };
+
+    if (action.type === 'clear') {
+      newState = {};
+
+    } else if (action.type === 'addProperties') {
+      Object.assign(newState, action.extraData);
+
+    } else if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete newState[key];
+      }
+    }
+
+    result.push(newState);
+    currentState = newState;
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
+
