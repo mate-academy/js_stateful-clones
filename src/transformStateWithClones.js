416d138f7c1614a75afc110d56d22a0339a39d76
Nazar <morhunnaza200516@gmail.com>
2025-12-04 18:50:53 +0200

    complate task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let currentState = { ...state };
+  const history = [];
+
+  for (const i of actions) {
+    const nextCurrentState = { ...currentState };
+
+    if (i.type === 'addProperties') {
+      Object.assign(nextCurrentState, i.extraData);
+    }
+
+    if (i.type === 'removeProperties') {
+      for (const key of i.keysToRemove) {
+        delete nextCurrentState[key];
+      }
+    }
+
+    if (i.type === 'clear') {
+      for (const key in nextCurrentState) {
+        delete nextCurrentState[key];
+      }
+    }
+    history.push(nextCurrentState);
+    currentState = nextCurrentState;
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
