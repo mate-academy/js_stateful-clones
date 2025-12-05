26372f68ed657fc7cd3b15499a4772854ee8d3b7
Mykola Matvieiev <matveev-n-a@ukr.net>
2025-12-05 16:41:17 +0200

    на перевірку

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,41 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+
+  const currentState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'clear':
+        for (const key of Object.keys(currentState)) {
+          delete currentState[key];
+        }
+        result.push({ ...currentState });
+
+        break;
+
+      case 'addProperties':
+        for (const newKey in action.extraData) {
+          currentState[newKey] = action.extraData[newKey];
+        }
+
+        result.push({ ...currentState });
+
+        break;
+
+      case 'removeProperties':
+        for (const keysRemove of action.keysToRemove) {
+          delete currentState[keysRemove];
+        }
+        result.push({ ...currentState });
+        break;
+      default:
+        break;
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
