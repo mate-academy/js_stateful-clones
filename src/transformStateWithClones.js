3091686db47c50a5972f303b4b15ac352caea8ce
SlidingMoon <slidingmoon2811@gmail.com>
2025-12-10 21:33:27 +0200

    Final code

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,41 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const states = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    let nextState = { ...currentState };
+
+    switch (action.type) {
+      case 'clear':
+        nextState = {};
+        break;
+
+      case 'addProperties':
+        nextState = {
+          ...nextState,
+          ...action.extraData,
+        };
+        break;
+
+      case 'removeProperties':
+        nextState = { ...nextState };
+
+        for (const key of action.keysToRemove) {
+          delete nextState[key];
+        }
+        break;
+
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
+    }
+
+    states.push(nextState);
+    currentState = nextState;
+  }
+
+  return states;
 }
 
 module.exports = transformStateWithClones;
