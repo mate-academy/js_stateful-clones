5dd97382dbe7407b074fa1133778423fa582a735
Daniel Nykyforov <nykyforov.dan@gmail.com>
2025-12-11 20:00:22 +0200

    Add solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,40 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = structuredClone(state);
+  const statesLog = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        statesLog.push({ ...stateCopy });
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete stateCopy[key];
+        }
+        statesLog.push({ ...stateCopy });
+        break;
+
+      case 'clear':
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
+        statesLog.push({ ...stateCopy });
+        break;
+
+      default:
+        // eslint-disable-next-line no-console
+        console.error(
+          'Wrong type of action! Write some of this types: ',
+          'addProperties, removeProperties, clear',
+        );
+    }
+  }
+
+  return statesLog;
 }
 
 module.exports = transformStateWithClones;
