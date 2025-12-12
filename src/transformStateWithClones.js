3b711d8716dddaad410e159ecb3c4f04f83c5287
Nastia Mazur <anastasiamazur2006@gmail.com>
2025-12-12 12:02:43 +0200

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,44 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let newState = { ...state };
+
+  const results = [];
+
+  for (const action of actions) {
+    let nextState;
+
+    switch (action.type) {
+      case 'addProperties':
+        nextState = {
+          ...newState,
+          ...(action.extraData || {}),
+        };
+
+        break;
+      case 'removeProperties':
+        nextState = { ...newState };
+
+        if (action.keysToRemove && Array.isArray(action.keysToRemove)) {
+          for (const keydel of action.keysToRemove) {
+            delete nextState[keydel];
+          }
+        }
+        break;
+
+      case 'clear':
+        nextState = {};
+        break;
+
+      default:
+        nextState = { ...newState };
+        break;
+    }
+    results.push(nextState);
+    newState = nextState;
+  }
+
+  return results;
 }
 
 module.exports = transformStateWithClones;
