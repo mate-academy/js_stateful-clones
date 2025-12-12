d4d0531eb6db28e929d5aabd0d71e620de9ac83b
Taras Demianiuk <demtaras1@gamil.com>
2025-12-12 08:50:28 +0200

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,38 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let stateCopy = Object.assign({}, state);
+  const histotyCopy = [];
+
+  for (const action of actions) {
+    const nextCopy = { ...stateCopy };
+
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(nextCopy, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete nextCopy[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in nextCopy) {
+          delete nextCopy[key];
+        }
+        break;
+
+      default:
+        throw new Error('Action is not defined!');
+    }
+
+    histotyCopy.push(nextCopy);
+    stateCopy = { ...nextCopy };
+  }
+
+  return histotyCopy;
 }
 
 module.exports = transformStateWithClones;
