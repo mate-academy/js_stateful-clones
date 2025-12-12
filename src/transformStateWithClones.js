3e597683728bd79abefa853442409f3671dc6924
AdrianaLatorreGit <dri.latorre@gmail.com>
2025-12-12 10:56:49 -0300

    fix: throwing an error type

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -26,7 +26,7 @@ function transformStateWithClones(state, actions) {
         }
         break;
       default:
-        break;
+        throw new Error(`Unknown action type: ${action.type}`);
     }
     stateHistory.push({ ...currentState });
   }
