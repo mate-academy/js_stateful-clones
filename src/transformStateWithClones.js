46554f2fdec0ddf72243178142fc0163ef75db09
DevT <dev.tymofiichuk@gmail.com>
2025-12-15 17:27:40 +0200

    Cloning and transforming state task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        if (action.extraData && typeof action.extraData === 'object') {
+          Object.assign(currentState, action.extraData);
+        }
+        break;
+      case 'removeProperties':
+        if (action.keysToRemove && Array.isArray(action.keysToRemove)) {
+          for (const removeKey of action.keysToRemove) {
+            delete currentState[removeKey];
+          }
+        }
+        break;
+      case 'clear':
+        currentState = {};
+        break;
+      default:
+        throw new Error(`Unknown action type encountered: ${action.type}`);
+    }
+
+    result.push({ ...currentState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
