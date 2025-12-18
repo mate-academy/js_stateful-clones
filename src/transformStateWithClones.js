493e1396107ddb1af2fd5778fda8cb427b7c8255
Matheus Mota <matheusmgf2010@gmail.com>
2025-12-18 12:12:16 -0300

    Correção do exercício

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -17,31 +17,41 @@ const transformStateWithClones = (state, actions) => {
     const action = actions[i];
     const nextState = {};
 
-    if (action.type === 'clear') {
-      // Objeto vazio
-    } else if (action.type === 'addProperties') {
-      for (const key in currentState) {
-        nextState[key] = currentState[key];
-      }
+    switch (action.type) {
+      case 'clear':
+        break;
 
-      for (const key in action.extraData) {
-        nextState[key] = action.extraData[key];
-      }
-    } else if (action.type === 'removeProperties') {
-      for (const key in currentState) {
-        let shouldRemove = false;
-
-        for (let j = 0; j < action.keysToRemove.length; j++) {
-          if (action.keysToRemove[j] === key) {
-            shouldRemove = true;
-            break;
-          }
-        }
-
-        if (!shouldRemove) {
+      case 'addProperties':
+        for (const key in currentState) {
           nextState[key] = currentState[key];
         }
-      }
+
+        for (const key in action.extraData) {
+          nextState[key] = action.extraData[key];
+        }
+        break;
+
+      case 'removeProperties':
+        for (const key in currentState) {
+          let shouldRemove = false;
+
+          for (let j = 0; j < action.keysToRemove.length; j++) {
+            if (action.keysToRemove[j] === key) {
+              shouldRemove = true;
+              break;
+            }
+          }
+
+          if (!shouldRemove) {
+            nextState[key] = currentState[key];
+          }
+        }
+        break;
+
+      default:
+        for (const key in currentState) {
+          nextState[key] = currentState[key];
+        }
     }
 
     currentState = nextState;
