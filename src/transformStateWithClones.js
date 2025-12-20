ffecb8306f70e8dcebd8132bfe33b33304ffcf40
naviailpach <ivan.chaplya13@gmail.com>
2025-12-20 15:07:15 +0200

    changed default

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -29,7 +29,7 @@ function transformStateWithClones(state, actions) {
         break;
 
       default:
-        break;
+        throw new Error(`Unknown action type: ${action.type}`);
     }
 
     result.push(newState);
