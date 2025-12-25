9d2aa62cf989f062cced47878699d377beb99c76
Your Name <cocochoco.story@gmail.com>
2025-12-25 20:57:15 +0200

    solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,26 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let newState = { ...state }; // используем let
+  const results = [];
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      newState = { ...newState, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      newState = { ...newState };
+
+      for (const key of action.keysToRemove) {
+        delete newState[key];
+      }
+    } else if (action.type === 'clear') {
+      newState = {};
+    }
+
+    results.push(newState);
+  }
+
+  return results; // возвращаем массив всех состояний
 }
 
 module.exports = transformStateWithClones;
