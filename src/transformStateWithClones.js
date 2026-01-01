bed0e6b0889c9e2e8cc1d27835df041c13a640d4
Mykola Fatkullin <fatkullinmykola@gmail.com>
2026-01-01 13:35:34 +0200

    created function transform state with clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,43 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const resultedArray = [];
+
+  for (const action of actions) {
+    const type = action.type;
+    const lastChangedObject = resultedArray.at(-1) || state;
+    let resultObject = {};
+
+    switch (type) {
+      case 'addProperties': {
+        Object.assign(resultObject, lastChangedObject, action.extraData);
+        break;
+      }
+
+      case 'removeProperties': {
+        removeProperties(action, lastChangedObject, resultObject);
+        break;
+      }
+      case 'clear':
+        break;
+
+      default:
+        resultObject = undefined;
+        break;
+    }
+
+    resultedArray.push(resultObject);
+  }
+
+  return resultedArray;
 }
 
 module.exports = transformStateWithClones;
+
+function removeProperties(action, clone, resultObject) {
+  Object.assign(resultObject, clone);
+
+  action.keysToRemove.forEach((key) => {
+    delete resultObject[key];
+  });
+}
