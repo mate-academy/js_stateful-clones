6f8dcdb18269b92887c7b568009409c644d8430c
sqw <bogdanfust@gmail.com>
2026-01-06 19:48:01 +0200

    develop

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,27 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
-}
+  const result = [];
 
+  let stateClone = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(stateClone, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete stateClone[key];
+      }
+    }
+
+    if (action.type === 'clear') {
+      stateClone = {};
+    }
+    result.push({ ...stateClone });
+  }
+
+  return result;
+}
 module.exports = transformStateWithClones;
