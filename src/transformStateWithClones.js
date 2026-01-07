e0e4bf260a98696a6b938431f708eb8f0951fe9c
Angela <angelastrechen1@mail.com>
2026-01-07 16:23:58 +0100

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,38 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  // eslint-disable-next-line no-undef
+  let stateCopy = { ...initialState };
+  const history = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'clear':
+        stateCopy = {};
+        break;
+
+      case 'addProperties':
+        stateCopy = { ...stateCopy, ...action.extraData };
+        break;
+
+      case 'removeProperties': {
+        const tempCopy = { ...stateCopy };
+
+        for (const key of action.keysToRemove) {
+          delete tempCopy[key];
+        }
+        stateCopy = tempCopy;
+        break;
+      }
+
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
+    }
+
+    history.push({ ...stateCopy });
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
