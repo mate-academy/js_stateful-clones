547728670e43452c69683225c7d7234b4d1b2454
ParamooreXO <taraskull123@gmail.com>
2026-01-14 15:57:15 +0200

    used switch instead of if component, added default case

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,26 +11,29 @@ function transformStateWithClones(state, actions) {
   const stateClones = [];
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(stateCloneObject, action.extraData);
-      stateClones.push({ ...stateCloneObject });
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCloneObject, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete stateCloneObject[key];
+        }
+
+        break;
+
+      case 'clear':
+        for (const key in stateCloneObject) {
+          delete stateCloneObject[key];
+        }
+        break;
+
+      default:
+        break;
     }
 
-    if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete stateCloneObject[key];
-      }
-
-      stateClones.push({ ...stateCloneObject });
-    }
-
-    if (action.type === 'clear') {
-      for (const key in stateCloneObject) {
-        delete stateCloneObject[key];
-      }
-
-      stateClones.push({ ...stateCloneObject });
-    }
+    stateClones.push({ ...stateCloneObject });
   }
 
   return stateClones;
