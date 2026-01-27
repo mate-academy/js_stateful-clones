806b37b4b387ad8f068fcffc8a2e2ffdbcc8cb2f
Pedro031098 <pedro.paulo027@gmail.com>
2026-01-27 15:24:55 -0300

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -34,6 +34,7 @@ function transformStateWithClones(state, actions) {
         break;
 
       default:
+      // error handling / fallback
         newState = { ...currentState };
         break;
     }
