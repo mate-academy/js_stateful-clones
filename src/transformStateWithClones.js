6f8fcf1362ff745dc7c4ec895d8ca4764bb36d52
XxMariavxX <synevicmaria13@gmail.com>
2026-01-30 20:55:34 +0200

    transformState

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,39 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let copyState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        copyState = {
+          ...copyState,
+          ...action.extraData,
+        };
+        break;
+
+      case 'removeProperties':
+        const newState = { ...copyState };
+
+        for (const key of action.keysToRemove) {
+          delete newState[key];
+        }
+
+        copyState = newState;
+        break;
+
+      case 'clear':
+        copyState = {};
+        break;
+
+      default:
+        throw new Error('Unknown type');
+    }
+    result.push({ ...copyState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
