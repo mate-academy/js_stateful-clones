0e94ae04f25bcd2b734d33ea8dd7fce42209e37d
Tiago <o-seu-email-do-github@exemplo.com>
2026-02-03 09:35:07 +0000

    Task clone stateful

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,28 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const history = [];
+
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    if (type === 'clear') {
+      currentState = {};
+    } else if (type === 'addProperties') {
+      currentState = { ...currentState, ...extraData };
+    } else if (type === 'removeProperties') {
+      currentState = { ...currentState };
+
+      for (const key of keysToRemove) {
+        delete currentState[key];
+      }
+    }
+    history.push(currentState);
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
