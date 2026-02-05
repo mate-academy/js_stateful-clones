62070ccf4cbf221c25c15c7fe4c7811b20fc2dc9
Denys Venzyk <denys.venzyk@gmail.com>
2026-02-05 19:51:29 +0200

    finished, all tests passed

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const results = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      currentState = Object.assign({ ...currentState }, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      currentState = { ...currentState };
+
+      for (const key of action.keysToRemove) {
+        delete currentState[key];
+      }
+    }
+
+    if (action.type === 'clear') {
+      currentState = {};
+    }
+    results.push({ ...currentState });
+  }
+
+  return results;
 }
 
 module.exports = transformStateWithClones;
