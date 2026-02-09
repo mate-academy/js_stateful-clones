82ac6c1e7c2d92bb83d0f0eba07d25537914f46a
botqqku <domsob1@wp.pl>
2026-02-09 16:15:53 +0100

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,42 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let prevState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties': {
+        const newState = { ...prevState, ...action.extraData };
+
+        result.push(newState);
+        prevState = newState;
+        break;
+      }
+
+      case 'removeProperties': {
+        const newState = { ...prevState };
+
+        for (const key of action.keysToRemove) {
+          delete newState[key];
+        }
+
+        result.push(newState);
+        prevState = newState;
+        break;
+      }
+
+      case 'clear': {
+        const newState = {};
+
+        result.push(newState);
+        prevState = newState;
+        break;
+      }
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
