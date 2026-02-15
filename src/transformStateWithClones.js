7bbe94edff0403d6ed296c5de030c576681bf42e
Yurii Leniv <supusman32@gmail.com>
2026-02-15 16:25:37 +0200

    fdsfsdf

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const m = [];
+  let currentstate = { ...state };
+
+  for (const key of actions) {
+    if (key.type === 'addProperties') {
+      currentstate = { ...currentstate, ...key.extraData };
+    }
+
+    if (key.type === 'removeProperties') {
+      const nextState = { ...currentstate };
+
+      for (const t of key.keysToRemove) {
+        delete nextState[t];
+      }
+      currentstate = nextState;
+    }
+
+    if (key.type === 'clear') {
+      currentstate = {};
+    }
+
+    m.push({ ...currentstate });
+  }
+
+  return m;
 }
 
 module.exports = transformStateWithClones;
