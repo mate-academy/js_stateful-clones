c3b38077c375653dfe4c79acff0efd364a6fab74
Yurii Leniv <supusman32@gmail.com>
2026-02-15 16:36:30 +0200

    refdfsdfrew

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,31 +7,39 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const m = [];
-  let currentstate = { ...state };
+  const stateHistory = [];
+  // Використовуємо camelCase для назви змінної
+  let currentState = { ...state };
 
-  for (const key of actions) {
-    if (key.type === 'addProperties') {
-      currentstate = { ...currentstate, ...key.extraData };
+  for (const action of actions) {
+    // Використовуємо switch для обробки типів дій (Checklist Item #2)
+    switch (action.type) {
+      case 'addProperties':
+        currentState = { ...currentState, ...action.extraData };
+        break;
+
+      case 'removeProperties':
+        // Створюємо копію для видалення властивостей
+        const nextState = { ...currentState };
+
+        for (const keyToRemove of action.keysToRemove) {
+          delete nextState[keyToRemove];
+        }
+        currentState = nextState;
+        break;
+
+      case 'clear':
+        currentState = {};
+        break;
+      default:
+        break;
     }
 
-    if (key.type === 'removeProperties') {
-      const nextState = { ...currentstate };
-
-      for (const t of key.keysToRemove) {
-        delete nextState[t];
-      }
-      currentstate = nextState;
-    }
-
-    if (key.type === 'clear') {
-      currentstate = {};
-    }
-
-    m.push({ ...currentstate });
+    // Додаємо клон актуального стану в історію
+    stateHistory.push({ ...currentState });
   }
 
-  return m;
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
