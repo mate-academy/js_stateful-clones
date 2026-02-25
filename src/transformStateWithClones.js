9449e582536b240e2a4cecaca7e922e5673bbcee
yarik <yaroslav.katsap@gmail.com>
2026-02-25 11:40:17 +0200

    Considered review

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -14,24 +14,43 @@ function transformStateWithClones(state, actions) {
   let next = {};
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      next = { ...prevState, ...action.extraData };
-      history.push(next);
-      prevState = next;
-    } else if (action.type === 'removeProperties') {
-      next = Object.fromEntries(
-        Object.entries(prevState).filter(
-          ([k]) => !action.keysToRemove.includes(k),
-        ),
-      );
+    // if (action.type === 'addProperties') {
+    //   next = { ...prevState, ...action.extraData };
+    //   history.push(next);
+    //   prevState = next;
+    // } else if (action.type === 'removeProperties') {
+    //   next = Object.fromEntries(
+    //     Object.entries(prevState).filter(
+    //       ([k]) => !action.keysToRemove.includes(k),
+    //     ),
+    //   );
 
-      history.push(next);
-      prevState = next;
-    } else if (action.type === 'clear') {
-      next = {};
-      history.push(next);
-      prevState = next;
+    //   history.push(next);
+    //   prevState = next;
+    // } else if (action.type === 'clear') {
+    //   next = {};
+    //   history.push(next);
+    //   prevState = next;
+    // }
+    switch (action.type) {
+      case 'addProperties':
+        next = { ...prevState, ...action.extraData };
+        break;
+      case 'removeProperties':
+        next = Object.fromEntries(
+          Object.entries(prevState).filter(
+            ([k]) => !action.keysToRemove.includes(k),
+          ),
+        );
+        break;
+      case 'clear':
+        next = {};
+        break;
+      default:
+        continue;
     }
+    history.push(next);
+    prevState = next;
   }
 
   return history;
