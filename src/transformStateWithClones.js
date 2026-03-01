9c30d292b2e93cc322e568fb058c2a64e22f2ab3
zedg <zedg@gameinspire.dev>
2026-03-01 22:08:28 +0200

    solve the task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,51 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const copy = { ...state };
+  const historyOfStates = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        addProperties(copy, action.extraData);
+        break;
+
+      case 'removeProperties':
+        removeProperties(copy, action.keysToRemove);
+        break;
+
+      case 'clear':
+        clear(copy);
+      
+        break;
+    }
+
+    historyOfStates.push({ ...copy });
+  }
+
+  return historyOfStates;
+}
+
+function addProperties(state, extraData) {
+  Object.assign(state, extraData); 
+}
+
+function removeProperties(state, keysToRemove) {
+  if (!keysToRemove || keysToRemove.length === 0) {
+    return;
+  }
+
+  for (const keyToRemove of keysToRemove) {
+    if (state[keyToRemove]) {
+      delete state[keyToRemove];
+    }
+  }
+}
+
+function clear(state) {
+  for (const key in state) {
+    delete state[key];
+  }
 }
 
 module.exports = transformStateWithClones;
