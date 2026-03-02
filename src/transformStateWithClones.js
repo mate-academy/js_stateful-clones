85fe4713625801478da312e3efb5a6aa5129a5ae
Thiago Moro do Carmo <thiago.moro77@gmail.com>
2026-03-02 10:45:02 -0300

    change if state to switch

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,20 +11,25 @@ function transformStateWithClones(state, actions) {
   const actualState = { ...state };
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(actualState, action.extraData);
-    }
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(actualState, action.extraData);
+        break;
 
-    if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete actualState[key];
-      }
-    }
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete actualState[key];
+        }
+        break;
 
-    if (action.type === 'clear') {
-      for (const key in actualState) {
-        delete actualState[key];
-      }
+      case 'clear':
+        for (const key in actualState) {
+          delete actualState[key];
+        }
+        break;
+
+      default:
+        break;
     }
 
     stateHistory.push({ ...actualState });
