fab2d1cb90d751743ed3f1b0a2b8616ce1e8995e
Thiago Moro do Carmo <thiago.moro77@gmail.com>
2026-03-02 10:31:49 -0300

    add solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+  const actualState = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(actualState, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete actualState[key];
+      }
+    }
+
+    if (action.type === 'clear') {
+      for (const key in actualState) {
+        delete actualState[key];
+      }
+    }
+
+    stateHistory.push({ ...actualState });
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
