b2588a5264745a8025040508912f738515bea51e
Maksim Vashuk <maximka.vashuk@gmail.com>
2026-03-05 19:55:17 +0100

    all code

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    let nextState;
+
+    switch (action.type) {
+      case 'addProperties':
+        nextState = { ...currentState, ...action.extraData };
+        break;
+
+      case 'removeProperties':
+        const updateState = { ...currentState };
+
+        action.keysToRemove.forEach((key) => {
+          delete updateState[key];
+        });
+
+        nextState = updateState;
+        break;
+
+      case 'clear':
+        nextState = {};
+        break;
+    }
+    currentState = nextState;
+    stateHistory.push({ ...currentState });
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
