81f122dc2ec2acc46f108d7c65dae345321bacb9
Dmytro Kasatkin <amid19091995@gmail.com>
2026-03-14 11:46:41 +0200

    udate

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -27,6 +27,9 @@ function transformStateWithClones(state, actions) {
       case 'clear':
         newState = {};
         break;
+
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
     }
 
     stateHistory.push(newState);
