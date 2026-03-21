2ff4390cd088e287fde73d206a411f7e72b948e8
unknown <giudhecca@gmail.com>
2026-03-21 17:58:11 +0100

    2nd version of solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -29,6 +29,9 @@ function transformStateWithClones(state, actions) {
       case 'clear':
         currentState = {};
         break;
+
+      default:
+        break;
     }
     history.push(currentState);
   }
