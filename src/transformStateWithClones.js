9912e6f57a8d899cd1f44174cce5701c9b5578cd
Nazar Bodak <nazar.bodak.qa@gmail.com>
2026-03-21 23:22:39 +0200

    Implement transformStateWithClones function to handle state modifications

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const stateCopy = { ...state };
+
+  for (const object of actions) {
+    if (object.type === 'addProperties') {
+      for (const value in object.extraData) {
+        stateCopy[value] = object.extraData[value];
+      }
+      result.push({ ...stateCopy });
+    } else if (object.type === 'removeProperties') {
+      for (const value of object.keysToRemove) {
+        delete stateCopy[value];
+      }
+      result.push({ ...stateCopy });
+    } else if (object.type === 'clear') {
+      for (const value in stateCopy) {
+        delete stateCopy[value];
+      }
+      result.push({ ...stateCopy });
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
