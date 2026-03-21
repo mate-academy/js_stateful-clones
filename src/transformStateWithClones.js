dcc660b78b0c877bcf396e5562ff81cbffc446bb
yevkrasnenkov <krasnenkov.evgen@gmail.com>
2026-03-21 18:55:27 -0400

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        const { extraData } = action;
+
+        currentState = { ...currentState, ...extraData };
+        result.push(currentState);
+        break;
+      case 'clear':
+        currentState = {};
+        result.push(currentState);
+        break;
+
+      case 'removeProperties':
+        const { keysToRemove } = action;
+
+        currentState = { ...currentState };
+
+        for (const key of keysToRemove) {
+          delete currentState[key];
+        }
+        result.push(currentState);
+        break;
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
