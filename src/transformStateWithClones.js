28b705bc41137f33391309001f102ac722e0d4c4
Nazar Bodak <nazar.bodak.qa@gmail.com>
2026-03-22 23:24:49 +0200

    Refactor transformStateWithClones function to use switch-case for action handling

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,25 +8,29 @@
  */
 function transformStateWithClones(state, actions) {
   const result = [];
-  const stateCopy = { ...state };
+  let stateCopy = { ...state };
 
   for (const object of actions) {
-    if (object.type === 'addProperties') {
-      for (const value in object.extraData) {
-        stateCopy[value] = object.extraData[value];
-      }
-      result.push({ ...stateCopy });
-    } else if (object.type === 'removeProperties') {
-      for (const value of object.keysToRemove) {
-        delete stateCopy[value];
-      }
-      result.push({ ...stateCopy });
-    } else if (object.type === 'clear') {
-      for (const value in stateCopy) {
-        delete stateCopy[value];
-      }
-      result.push({ ...stateCopy });
+    switch (object.type) {
+      case 'addProperties':
+        for (const value in object.extraData) {
+          Object.assign(stateCopy, { [value]: object.extraData[value] });
+        }
+        break;
+      case 'removeProperties':
+        for (const value of object.keysToRemove) {
+          delete stateCopy[value];
+        }
+        break;
+
+      case 'clear':
+        stateCopy = {};
+        break;
+
+      default:
+        throw new Error(`Unknown action type: ${object.type}`);
     }
+    result.push({ ...stateCopy });
   }
 
   return result;
