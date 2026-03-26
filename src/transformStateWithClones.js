b1a331ebffc742fc683eb107b32e21b84fc6331d
Drelya Liza <lizadrelya@gmail.com>
2026-03-26 16:41:06 +0200

    add solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,77 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const copyState = { ...state };
+  const history = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        addProperties(copyState, action.extraData);
+        history.push({ ...copyState });
+        break;
+      case 'removeProperties':
+        removeProperties(copyState, action.keysToRemove);
+        history.push({ ...copyState });
+        break;
+      case 'clear':
+        clearProperties(copyState);
+        history.push({ ...copyState });
+        break;
+    }
+  }
+
+  return history;
 }
+
+function addProperties(state, extraData) {
+  Object.assign(state, extraData);
+}
+
+function removeProperties(state, keysToRemove) {
+  for (const key of keysToRemove) {
+    delete state[key];
+  }
+}
+
+function clearProperties(state) {
+  for (const key in state) {
+    delete state[key];
+  }
+}
+
+// function transformState(state, actions) {
+//   for (const action of actions) {
+//     switch (action.type) {
+//       case 'addProperties':
+//         addProperties(state, action.extraData);
+//         break;
+
+//       case 'removeProperties':
+//         removeProperties(state, action.keysToRemove);
+//         break;
+
+//       case 'clear':
+//         clearProperties(state);
+//         break;
+//     }
+//   }
+// }
+
+// function addProperties(state, extraData) {
+//   Object.assign(state, extraData);
+// }
+
+// function removeProperties(state, keysToRemove) {
+//   for (const key of keysToRemove) {
+//     delete state[key];
+//   }
+// }
+
+// function clearProperties(state) {
+//   for (const key in state) {
+//     delete state[key];
+//   }
+// }
 
 module.exports = transformStateWithClones;
