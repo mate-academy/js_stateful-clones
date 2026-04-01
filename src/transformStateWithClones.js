8b90fc6c102ccd992c316a11835e4b598698452a
Dmytro Stelmashuk <dimon16295@gmail.com>
2026-04-01 21:52:43 +0300

    add solution task to js file

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let actualStateCopy = { ...state };
+  const historyOfStates = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        actualStateCopy = { ...actualStateCopy };
+        Object.assign(actualStateCopy, action.extraData);
+        break;
+
+      case 'removeProperties':
+        actualStateCopy = { ...actualStateCopy };
+
+        for (const key of action.keysToRemove) {
+          delete actualStateCopy[key];
+        }
+        break;
+
+      case 'clear':
+        actualStateCopy = {};
+        break;
+
+      default:
+        throw new Error('Unknown action');
+    }
+
+    historyOfStates.push(actualStateCopy);
+  }
+
+  return historyOfStates;
 }
 
 module.exports = transformStateWithClones;
