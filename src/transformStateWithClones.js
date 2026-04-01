2347f9470bb3555456f353120e244ebd4a4750ae
Назар Регалов <nazarregalov@MacBook-Air-Nazar.local>
2026-04-01 22:39:43 +0300

    Stateful-clones_solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let currentState = state;
+
+  for (const action of actions) {
+    let newState = { ...currentState };
+
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(newState, action.extraData);
+        result.push(newState);
+        break;
+
+      case 'removeProperties':
+        for (const k of action.keysToRemove) {
+          delete newState[k];
+        }
+        result.push(newState);
+        break;
+
+      case 'clear':
+        newState = {};
+        result.push(newState);
+        break;
+    }
+    currentState = newState;
+  }
+  return result;
 }
 
 module.exports = transformStateWithClones;
