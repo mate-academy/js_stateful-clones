3093d072736769085e40e73afd38fea40a79b183
Vadim <sergiyivanskiy@gmail.com>
2026-04-08 15:34:00 +0300

    add switch

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -13,18 +13,20 @@ function transformStateWithClones(state, actions) {
   for (const action of actions) {
     let nextState = { ...currentState };
 
-    if (action.type === 'clear') {
-      nextState = {};
-    }
+    switch (action.type) {
+      case 'clear':
+        nextState = {};
+        break;
 
-    if (action.type === 'addProperties') {
-      Object.assign(nextState, action.extraData);
-    }
+      case 'addProperties':
+        Object.assign(nextState, action.extraData);
+        break;
 
-    if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete nextState[key];
-      }
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete nextState[key];
+        }
+        break;
     }
 
     result.push(nextState);
