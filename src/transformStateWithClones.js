14ceca58232e57fd39a0d182477016bcf6a0beba
Danyliuk Yaroslav <unityismylove@gmail.com>
2026-05-02 17:12:20 +0300

    solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,50 @@
 'use strict';
 
 /**
- * @param {Object} state
+ * @param {Object} stateClone
  * @param {Object[]} actions
  *
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateClone = { ...state };
+  const stateHistory = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        addProperties(stateClone, action.extraData);
+        stateHistory.push({ ...stateClone });
+        break;
+
+      case 'removeProperties':
+        removeProperties(stateClone, action.keysToRemove);
+        stateHistory.push({ ...stateClone });
+        break;
+
+      case 'clear':
+        clearProperties(stateClone);
+        stateHistory.push({ ...stateClone });
+        break;
+    }
+  }
+
+  return stateHistory;
 }
 
+function addProperties(stateClone, extraData) {
+  Object.assign(stateClone, extraData);
+}
+
+function removeProperties(stateClone, keysToRemove) {
+  for (const key of keysToRemove) {
+    delete stateClone[key];
+  }
+}
+
+function clearProperties(stateClone) {
+  for (const key in stateClone) {
+    delete stateClone[key];
+  }
+}
 module.exports = transformStateWithClones;
