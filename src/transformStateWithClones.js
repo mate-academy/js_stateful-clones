89970f341f344e39542a305a91a2bbf5437e8055
Timur K <timurkakandzo@gmail.com>
2026-05-05 13:45:53 +0300

    final2

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,28 +7,40 @@
  * @return {Object[]}
  */
 
+/**
+ * @param {Object} state
+ * @param {Object[]} actions
+ *
+ * @return {Object[]}
+ */
 function transformStateWithClones(state, actions) {
   const result = [];
   let currentState = { ...state };
 
   for (const action of actions) {
-    if (action.type === 'clear') {
-      currentState = {};
+    switch (action.type) {
+      case 'clear':
+        currentState = {};
+        break;
+
+      case 'addProperties':
+        currentState = {
+          ...currentState,
+          ...action.extraData,
+        };
+        break;
+
+      case 'removeProperties':
+        currentState = { ...currentState };
+
+        for (const key of action.keysToRemove) {
+          delete currentState[key];
+        }
+        break;
+
+      default:
+        break;
     }
-
-    if (action.type === 'addProperties') {
-      currentState = { ...currentState, ...action.extraData };
-    }
-
-
-    if (action.type === 'removeProperties') {
-
-      currentState = { ...currentState };
-      for (const key of action.keysToRemove) {
-        delete currentState[key];
-      }
-    }
-
     result.push({ ...currentState });
   }
 
