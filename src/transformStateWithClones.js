ef98e3474199008c1c9339095a33542466811351
Iryna Kaida <kaidairyna@gmail.com>
2026-05-06 20:47:04 +0300

    Implement transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,27 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const results = [];
+  let currentState = structuredClone(state);
+  let next;
+
+  for (const action of actions) {
+    if (action.type === 'clear') {
+      next = {};
+    } else if (action.type === 'addProperties') {
+      next = { ...currentState, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      next = { ...currentState };
+
+      for (const key of action.keysToRemove) {
+        delete next[key];
+      }
+    }
+    results.push(next);
+    currentState = next;
+  }
+
+  return results;
 }
 
 module.exports = transformStateWithClones;
