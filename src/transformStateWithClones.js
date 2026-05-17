efab2409a2b70c726b6e6199024b6a9213d83ebf
Dmytro Makhanok <dmmhnk23@gmail.com>
2026-05-17 15:48:33 +0200

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,26 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let current = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      current = { ...current, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      current = { ...current };
+
+      for (const key of action.keysToRemove) {
+        delete current[key];
+      }
+    } else if (action.type === 'clear') {
+      current = {};
+    }
+
+    result.push({ ...current });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
