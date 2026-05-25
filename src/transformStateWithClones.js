1995b23c799b191ca66fa774c67ce321fcabce18
Kristina Kovalchuk <kristina36247@gmail.com>
2026-05-25 15:15:56 +0300

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -14,25 +14,18 @@ function transformStateWithClones(state, actions) {
     let nextState;
 
     if (action.type === 'clear') {
-      // Створюємо повністю порожній об'єкт
       nextState = {};
     } else if (action.type === 'addProperties') {
-      // Клонуємо поточний стан і додаємо нові властивості
       nextState = { ...currentState, ...action.extraData };
     } else if (action.type === 'removeProperties') {
-      // Спочатку клонуємо поточний стан
       nextState = { ...currentState };
 
-      // Потім видаляємо потрібні ключі з нового клону
       for (const key of action.keysToRemove) {
         delete nextState[key];
       }
     }
 
-    // Додаємо новий стан в історію
     history.push(nextState);
-
-    // Оновлюємо поточний стан для наступної ітерації циклу
     currentState = nextState;
   }
 
