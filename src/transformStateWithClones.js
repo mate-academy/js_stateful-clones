4ea437ee773721049a1d4c11cdbbb9f30cb8431d
IvanushkinVlad <vladivanushkin10@gmail.com>
2026-05-27 15:45:11 +0300

    Solve stateful clones task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,35 @@
  *
  * @return {Object[]}
  */
-function transformStateWithClones(state, actions) {
-  // write code here
-}
+const transformStateWithClones = (state, action) => {
+  let currentState = { ...state };
+  const newState = [];
+
+  for (let i = 0; i < action.length; i++) {
+    let clonedState = { ...currentState };
+
+    if (action[i].type === 'addProperties') {
+      clonedState = {
+        ...clonedState,
+        ...action[i].extraData,
+      };
+    }
+
+    if (action[i].type === 'removeProperties') {
+      for (const key of action[i].keysToRemove) {
+        delete clonedState[key];
+      }
+    }
+
+    if (action[i].type === 'clear') {
+      clonedState = {};
+    }
+
+    newState.push(clonedState);
+    currentState = clonedState;
+  }
+
+  return newState;
+};
 
 module.exports = transformStateWithClones;
