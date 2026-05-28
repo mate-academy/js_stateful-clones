a65d0cf207ee07db802d68bb9e76d626e567c5d0
Adel Pereira <nic.gis.office@gmail.com>
2026-05-28 14:08:21 +0100

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const fullHistory = [];
+  let prev = state;
+
+  for (const act of actions) {
+    let history = { ...prev };
+
+    switch (act.type) {
+      case 'addProperties':
+        for (const key in act.extraData) {
+          history[key] = act.extraData[key];
+        }
+        break;
+      case 'removeProperties':
+        for (const key of act.keysToRemove) {
+          delete history[key];
+        }
+        break;
+      case 'clear':
+        history = {};
+        break;
+    }
+    fullHistory.push(history);
+    prev = history;
+  }
+
+  return fullHistory;
 }
 
 module.exports = transformStateWithClones;
