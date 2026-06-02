27469e8a217c75c37210cfd2cd903e1108ae3582
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-06-02 11:14:19 -0500

    Add transformStateWithClones.js

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const allStates = [];
+  let copy = { ...state };
+
+  for (const action of actions) {
+    for (const entry of Object.entries(action)) {
+      if (entry[0] === 'extraData') {
+        Object.assign(copy, entry[1]);
+      }
+
+      if (entry[0] === 'keysToRemove') {
+        for (let i = 0; i < entry[1].length; i++) {
+          delete copy[entry[1][i]];
+        }
+      }
+
+      if (entry[1] === 'clear') {
+        copy = {};
+      }
+    }
+
+    allStates.push({ ...copy });
+  }
+
+  return allStates;
 }
 
 module.exports = transformStateWithClones;
