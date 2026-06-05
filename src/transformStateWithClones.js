5fb7c3f040ffb172c80ddfb8cf6b707deb020da5
Ibbott <ibbottmakeup@gmail.com>
2026-06-05 14:02:29 -0300

    Descrição das alterações

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,35 @@
 'use strict';
 
-/**
- * @param {Object} state
- * @param {Object[]} actions
- *
- * @return {Object[]}
- */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let currentState = { ...state };
+
+  actions.forEach((action) => {
+    switch (action.type) {
+      case 'addProperties':
+        currentState = {
+          ...currentState,
+          ...action.extraData,
+        };
+        break;
+
+      case 'removeProperties':
+        currentState = { ...currentState };
+
+        action.keysToRemove.forEach((key) => {
+          delete currentState[key];
+        });
+        break;
+
+      case 'clear':
+        currentState = {};
+        break;
+    }
+
+    result.push({ ...currentState });
+  });
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
