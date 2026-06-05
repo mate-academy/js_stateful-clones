b4ef50287fdc719fc1ae7792265594f852a93b2a
Oleksandr <oleksandr.sviderskyi@nure.ua>
2026-06-06 00:27:22 +0300

    task complete

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const clonedState = { ...state };
+  const result = [];
+
+  for (const obj in actions) {
+    for (const property in actions[obj]) {
+      if (actions[obj][property] === 'addProperties') {
+        for (const key in actions[obj].extraData) {
+          clonedState[key] = actions[obj].extraData[key];
+        }
+      }
+
+      if (actions[obj][property] === 'removeProperties') {
+        for (const key of actions[obj].keysToRemove) {
+          delete clonedState[key];
+        }
+      }
+
+      if (actions[obj][property] === 'clear') {
+        for (const key in clonedState) {
+          delete clonedState[key];
+        }
+      }
+    }
+
+    result.push({ ...clonedState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
