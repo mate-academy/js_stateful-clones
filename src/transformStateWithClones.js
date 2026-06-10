b397bb352ed7861a27990b816ebeb154389ab0f7
Marcin Dąbrowski <marcin.maroin.dabrowski@gmail.com>
2026-06-11 00:59:29 +0200

    Do Task transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,34 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  let currentState = state;
+  const history = [];
+
+  for (const action of actions) {
+    let nextState;
+
+    if (action.type === 'addProperties') {
+      nextState = { ...currentState, ...action.extraData };
+    } else if (action.type === 'clear') {
+      nextState = {};
+    } else if (action.type === 'removeProperties') {
+      nextState = { ...currentState };
+
+      for (const key of action.keysToRemove) {
+        delete nextState[key];
+      }
+    } else {
+      // unknown action: skip
+      continue;
+    }
+
+    history.push(nextState);
+    currentState = nextState;
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
