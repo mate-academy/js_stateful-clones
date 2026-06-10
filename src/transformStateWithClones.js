e49413fbefb48ab86244bcb07c102c46ba2d767f
Marcin Dąbrowski <marcin.maroin.dabrowski@gmail.com>
2026-06-11 01:19:11 +0200

    adding switch cases

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,25 +8,39 @@
  */
 
 function transformStateWithClones(state, actions) {
-  let currentState = state;
+  // accept undefined actions but require `state` as the first argument
+  const initialState = state || {};
+  let currentState = { ...initialState };
+
+  const actionList = Array.isArray(actions) ? actions : [];
+
   const history = [];
 
-  for (const action of actions) {
+  for (const action of actionList) {
+    if (!action || typeof action.type !== 'string') {
+      // unknown or malformed action: skip
+      continue;
+    }
+
     let nextState;
 
-    if (action.type === 'addProperties') {
-      nextState = { ...currentState, ...action.extraData };
-    } else if (action.type === 'clear') {
-      nextState = {};
-    } else if (action.type === 'removeProperties') {
-      nextState = { ...currentState };
+    switch (action.type) {
+      case 'addProperties':
+        nextState = { ...currentState, ...(action.extraData || {}) };
+        break;
+      case 'clear':
+        nextState = {};
+        break;
+      case 'removeProperties':
+        nextState = { ...currentState };
 
-      for (const key of action.keysToRemove) {
-        delete nextState[key];
-      }
-    } else {
-      // unknown action: skip
-      continue;
+        for (const key of action.keysToRemove || []) {
+          delete nextState[key];
+        }
+        break;
+      default:
+        // unknown action type: skip
+        continue;
     }
 
     history.push(nextState);
