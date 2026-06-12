c3ea6b32d0d3b2b077e6ce852813511d020b7045
Serhii <serezatkacik8@gmail.com>
2026-06-12 13:54:25 +0300

    performed by transformStateWithClones.

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,42 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'clear':
+        currentState = {};
+        break;
+
+      case 'addProperties':
+        currentState = {
+          ...currentState,
+          ...action.extraData,
+        };
+        break;
+
+      case 'removeProperties':
+        currentState = Object.keys(currentState)
+          .filter((key) => !action.keysToRemove.includes(key))
+          .reduce(
+            (newState, key) => ({
+              ...newState,
+              [key]: currentState[key],
+            }),
+            {},
+          );
+        break;
+
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
+    }
+
+    stateHistory.push(currentState);
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
