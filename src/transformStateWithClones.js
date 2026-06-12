8df8ba76571f1ed236ba636cf0898bda3c066622
Serhii <serezatkacik8@gmail.com>
2026-06-12 14:00:11 +0300

    Fix state history cloning

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -39,7 +39,7 @@ function transformStateWithClones(state, actions) {
         throw new Error(`Unknown action type: ${action.type}`);
     }
 
-    stateHistory.push(currentState);
+    stateHistory.push({ ...currentState });
   }
 
   return stateHistory;
