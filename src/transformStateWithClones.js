d19d41e2686ccfb0c98cf1f6a9514c85425ec0cb
student1232354 <tarasromanenko33@gmail.com>
2026-06-18 14:07:20 +0300

    first update

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const story = [];
+  let currentstate = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'clear') {
+      currentstate = {};
+    }
+
+    if (action.type === 'addProperties') {
+      currentstate = {
+        ...currentstate,
+        ...action.extraData,
+      };
+    }
+
+    if (action.type === 'removeProperties') {
+      const copy = { ...currentstate };
+
+      for (const needtodelete of action.keysToRemove) {
+        delete copy[needtodelete];
+      }
+      currentstate = copy;
+    }
+    story.push({ ...currentstate });
+  }
+
+  return story;
 }
 
 module.exports = transformStateWithClones;
