0f964fb2c998693ced77811cfc16647584f0e0ae
ALex <sasha.hivuk@gmail.com>
2026-06-25 15:00:30 +0300

    replaceAll

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,29 +8,36 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
-  const newObj = { ...state };
+  let stateCopy = { ...state };
   const arr = [];
 
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties':
-        Object.assign(newObj, action.extraData);
+        stateCopy = { ...stateCopy, ...action.extraData };
         break;
 
       case 'removeProperties':
-        for (const key of action.keysToRemove) {
-          delete newObj[key];
+        const cellForKey = {};
+
+        for (const key in stateCopy) {
+          if (!action.keysToRemove.includes(key)) {
+            cellForKey[key] = stateCopy[key];
+          }
         }
+
+        stateCopy = cellForKey;
         break;
 
       case 'clear':
-        for (const key in newObj) {
-          delete newObj[key];
-        }
+        stateCopy = {};
+        break;
+
+      default:
         break;
     }
 
-    arr.push({ ...newObj });
+    arr.push({ ...stateCopy });
   }
 
   return arr;
