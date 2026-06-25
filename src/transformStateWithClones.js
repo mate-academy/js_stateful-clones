f6c69528cca8d00190b5ff40b790bc1e6dc0e7e4
ALex <sasha.hivuk@gmail.com>
2026-06-25 11:29:12 +0300

    jsTransform

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,32 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const newObj = { ...state };
+  const arr = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(newObj, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete newObj[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in newObj) {
+          delete newObj[key];
+        }
+        break;
+    }
+
+    arr.push({ ...newObj });
+  }
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
