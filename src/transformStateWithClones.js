dd03699175c8cfc4fe9c438f344714dc282a20f7
Yehor Bohuslavskiy <boguslavskiyegor@gmail.com>
2026-06-25 18:16:45 +0200

    add sol

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,27 +11,29 @@ function transformStateWithClones(state, actions) {
   const result = [];
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(currentState, action.extraData);
-      result.push({ ...currentState });
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(currentState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete currentState[key];
+        }
+        break;
+
+      case 'clear':
+        currentState = {};
+        break;
+
+      default:
+        throw new Error('Unknown action type');
     }
 
-    if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete currentState[key];
-      }
-      result.push({ ...currentState });
-    }
+    const stateCopy = { ...currentState };
 
-    if (action.type === 'clear') {
-      for (const key in currentState) {
-        delete currentState[key];
-      }
-      result.push({ ...currentState });
-    }
+    result.push(stateCopy);
   }
-
-  currentState = {};
 
   return result;
 }
