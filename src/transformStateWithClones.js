869bd7836e9e7126dd70eb0ccf8448943e5e2f16
IBUPROFEN3000 <breslava6@gmail.com>
2026-07-05 18:00:06 +0200

    Task solving

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,37 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let historyOfStates = [];
+
+  for (let action of actions) {
+    let newState = { ...state};
+
+    if (action.type === 'addProperties') {
+      for (let key in action.extraData) {
+        newState[key] = action.extraData[key];
+      }
+
+    historyOfStates.push(newState);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (let key of action.keysToRemove) {
+        delete newState[key];
+      }
+
+    historyOfStates.push(newState);
+    }
+
+    if (action.type === 'clear') {
+      for (let key in newState) {
+        delete newState[key];
+      }
+
+    historyOfStates.push(newState);
+    }
+  }
+
+  return historyOfStates;
 }
 
 module.exports = transformStateWithClones;
