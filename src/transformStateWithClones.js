40ea8f1cbf59650a5790bc6237015c2fd352670c
Max <143881777+ZloyHoma@users.noreply.github.com>
2026-07-28 10:41:26 +0300

    need help

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,30 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const result = [];
+
+  for (const obj of actions) {
+    const action = obj.type;
+    const addition = obj.extraData;
+    const minus = obj.keysToRemove;
+
+    if (action === 'addProperties') {
+      Object.assign(state, addition);
+      result.push(state);
+    } else if (action === 'removeProperties') {
+      for (const key of minus) {
+        delete state[key];
+      }
+      result.push(state);
+    } else if (action === 'clear') {
+      for (const key1 in state) {
+        delete state[key1];
+      }
+      result.push(state);
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
