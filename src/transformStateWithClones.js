7f85871c2c9975e06e86280fcf2052d9a33783b8
Mykyta Fabrykator <mykyta.fabrykator@gmail.com>
2026-07-30 21:25:12 +0100

    feat: implement solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,51 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const results = [];
+
+  for (const action of actions) {
+    let temporaryState = {};
+
+    if (results.length) {
+      temporaryState = { ...results[results.length - 1] };
+    } else {
+      temporaryState = { ...state };
+    }
+
+    switch (action.type) {
+      case 'addProperties':
+        addProperties(temporaryState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        removeProperties(temporaryState, action.keysToRemove);
+        break;
+
+      default:
+        clearProperties(temporaryState);
+        break;
+    }
+
+    results.push(temporaryState);
+  }
+
+  return results;
+}
+
+function addProperties(state, extraData) {
+  Object.assign(state, extraData);
+}
+
+function removeProperties(state, keysToRemove) {
+  for (const key of keysToRemove) {
+    delete state[key];
+  }
+}
+
+function clearProperties(state) {
+  for (const key in state) {
+    delete state[key];
+  }
 }
 
 module.exports = transformStateWithClones;
