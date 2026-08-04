71e70a787c376aca40a8a6fc4c6a2f939ff0bbd5
kirushamatvieiev008 <mkiril087@gmail.com>
2026-08-04 22:53:17 +0300

    did the task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const historyArr = [];
+  let localState = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      localState = { ...localState, ...action.extraData };
+      historyArr.push(localState);
+    }
+
+    if (action.type === 'removeProperties') {
+      localState = { ...localState };
+
+      for (const key of action.keysToRemove) {
+        delete localState[key];
+      }
+      historyArr.push(localState);
+    }
+
+    if (action.type === 'clear') {
+      localState = {};
+      historyArr.push(localState);
+    }
+  }
+
+  return historyArr;
 }
 
 module.exports = transformStateWithClones;
