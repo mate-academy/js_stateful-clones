ed80bf2badd1b67408acd2c3f844ee1508f16e1d
skuzmenk <kuzmenko.oleksandr1123@vu.cdu.edu.ua>
2026-08-05 11:36:56 +0300

    Completed task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let currentState = { ...state };
+  const arr = [];
+
+  for (const action of actions) {
+    if (action.type === 'clear') {
+      currentState = {};
+    } else if (action.type === 'addProperties') {
+      currentState = {
+        ...currentState,
+        ...action.extraData,
+      };
+    } else if (action.type === 'removeProperties') {
+      currentState = { ...currentState };
+
+      for (const key of action.keysToRemove) {
+        delete currentState[key];
+      }
+    }
+
+    arr.push(currentState);
+  }
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
