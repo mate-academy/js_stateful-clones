b8f3a7b8e189d0fbfa71c256f5eb5672b0961ebf
Vladyslav Hlukhov <vlad.ua@gmail.com>
2026-08-05 13:12:11 +0300

    implement function transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let stateClone = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        for (const newKey of Object.keys(action.extraData)) {
+          stateClone[newKey] = action.extraData[newKey];
+        }
+        break;
+      case 'removeProperties':
+        for (const keyToRemove of action.keysToRemove) {
+          delete stateClone[keyToRemove];
+        }
+        break;
+      case 'clear':
+        stateClone = {};
+        break;
+    }
+    result.push({ ...stateClone });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
