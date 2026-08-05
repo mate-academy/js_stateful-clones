3d120a630a480b4e8ff468189fcf5ff6130d06a4
Oleksandr Konovalenko <enot333lol333@gmail.com>
2026-08-05 12:32:58 +0300

    Did the homework

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const history = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    const newState = { ...currentState };
+
+    if (action.type === 'addProperties') {
+      Object.assign(newState, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete newState[key];
+      }
+    }
+
+    if (action.type === 'clear') {
+      for (const key in newState) {
+        delete newState[key];
+      }
+    }
+
+    history.push(newState);
+    currentState = newState;
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
