864f48d930c0238077e27e518acdbfb74adb5805
Daniil <dan201103@gmail.com>
2026-08-06 11:14:38 +0300

    first commit

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const array = [];
+  let currentState = state;
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      currentState = { ...currentState, ...action.extraData };
+      array.push(currentState);
+    }
+
+    if (action.type === 'removeProperties') {
+      currentState = Object.keys(currentState).reduce((acc, key) => {
+        if (!action.keysToRemove.includes(key)) {
+          acc[key] = currentState[key];
+        }
+
+        return acc;
+      }, {});
+      array.push(currentState);
+    }
+
+    if (action.type === 'clear') {
+      currentState = {};
+      array.push(currentState);
+    }
+  }
+
+  return array;
 }
 
 module.exports = transformStateWithClones;
