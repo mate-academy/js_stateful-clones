22bdbed2e507f32f6b680108d7352c27f9a8a189
Oleh Puzin <olegpuzin77@icloud.com>
2026-08-10 00:05:33 +0300

    fixed another mistake

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -10,30 +10,23 @@ function transformStateWithClones(state, actions) {
   const stateCopy = { ...state };
   const resultActionsArr = [];
 
-  const addActionToArr = () => {
-    resultActionsArr.push({ ...stateCopy });
-  };
-
   for (let i = 0; i <= actions.length - 1; i++) {
     switch (actions[i].type) {
       case 'addProperties':
         Object.assign(stateCopy, actions[i].extraData);
-        addActionToArr();
         break;
       case 'removeProperties':
         actions[i].keysToRemove.forEach((key) => delete stateCopy[key]);
-        addActionToArr();
         break;
       case 'clear':
         for (const key in stateCopy) {
           delete stateCopy[key];
         }
-        addActionToArr();
         break;
-
       default:
         break;
     }
+    resultActionsArr.push({ ...stateCopy });
   }
 
   return resultActionsArr;
