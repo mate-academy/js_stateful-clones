c8e2d5adc9fb9738ac1eaa85d3fe00349feb558a
AchekcSs_ <achekcss@gmail.com>
2026-08-10 09:53:13 +0300

    fix: add error handling for default switch case

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -36,7 +36,7 @@ function transformStateWithClones(state, actions) {
       }
 
       default:
-        break;
+        throw new Error('Unknown action type: ' + action.type);
     }
 
     stateHistory.push({ ...currentState });
