bd8f29bc01d453e064b12a3f2ac092d6152a7aa6
Daniil <dan201103@gmail.com>
2026-08-12 11:09:38 +0300

    second commit

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -34,7 +34,7 @@ function transformStateWithClones(state, actions) {
       }
 
       default: {
-        break;
+        throw new Error(`Unexpected action type: "${action.type}"`);
       }
     }
 
