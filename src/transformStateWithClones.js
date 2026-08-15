2563938f31cafad09be341ea7dd7562a95dfe34c
Alexander <krevevrorsasha@gmail.com>
2026-08-15 14:17:52 +0200

    feat: implement transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,44 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const currentState = { ...state };
+  const result = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        addProperties(currentState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        removeProperties(currentState, action.keysToRemove);
+        break;
+
+      case 'clear':
+        clearProperties(currentState);
+        break;
+    }
+
+    result.push({ ...currentState });
+  }
+
+  return result;
+}
+
+function addProperties(copy, extraData) {
+  Object.assign(copy, extraData);
+}
+
+function removeProperties(copy, keysToRemove) {
+  for (const key of keysToRemove) {
+    delete copy[key];
+  }
+}
+
+function clearProperties(copy) {
+  for (const key of Object.keys(copy)) {
+    delete copy[key];
+  }
 }
 
 module.exports = transformStateWithClones;
