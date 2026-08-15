dc9a27b9ebcc33851f0903bec885cff65baaebfe
Vlad <supok2015@gmail.com>
2026-08-15 11:34:38 +0200

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,38 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  // создаем новый пустой обьект, в который будем добавлять новые состояния
+  let currentState = structuredClone(state);
+  // создаем копию обьекта, чтобы не изменять его напрямую
+
+  for (const action of actions) {
+    // перебираем все действия, которые нужно выполнить над состоянием
+    if (action.type === 'clear') {
+      // если действие - очистка состояния
+      currentState = {};
+      // создаем новый пустой объект
+    } else if (action.type === 'addProperties') {
+      // если действие - добавление свойств
+      currentState = { ...currentState, ...action.extraData };
+      // создаем новый объект, объединяя текущие свойства
+    } else if (action.type === 'removeProperties') {
+      // если действие - удаление свойств
+      currentState = { ...currentState };
+      // создаем новый объект, копируя текущие свойства, и..
+
+      for (const key of action.keysToRemove) {
+        // перебираем все ключи, которые нужно удалить
+        delete currentState[key];
+        // удаляем ключ из текущего состояния
+      }
+    }
+    result.push(currentState);
+    // добавляем текущие правки в пустой обьект
+  }
+
+  return result;
+  // возвращаем обьект всех состояний после применения действий
 }
 
 module.exports = transformStateWithClones;
