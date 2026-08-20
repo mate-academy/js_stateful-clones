c9187f0b483e50216473ea3e21f8c91bdaf37d2c
Delviiiii <vladbuld71@gmail.com>
2026-08-20 18:17:55 +0200

    fixed issues from the checklist

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,24 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const userInput = { ...state };
+  let stateCopy = { ...state };
   const logs = [];
 
   for (let i = 0; i < actions.length; i++) {
-    if (actions[i].type === 'addProperties') {
-      Object.assign(userInput, actions[i].extraData);
-      logs.push({ ...userInput });
-    } else if (actions[i].type === 'removeProperties') {
-      for (const key of actions[i].keysToRemove) {
-        delete userInput[key];
-      }
-      logs.push({ ...userInput });
-    } else if (actions[i].type === 'clear') {
-      for (const key in userInput) {
-        delete userInput[key];
-      }
-      logs.push({ ...userInput });
+    switch (actions[i].type) {
+      case 'addProperties':
+        Object.assign(stateCopy, actions[i].extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of actions[i].keysToRemove) {
+          delete stateCopy[key];
+        }
+        break;
+
+      case 'clear':
+        stateCopy = {};
+        break;
+
+      default:
+        break;
     }
+
+    logs.push({ ...stateCopy });
   }
 
   return logs;
