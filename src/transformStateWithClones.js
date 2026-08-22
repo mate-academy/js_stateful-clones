d9474afc49792ce30d9e81779f5e2e36a77de882
mukola-svg <mykolka.sviderok@email.com>
2026-08-22 23:25:12 +0300

    realy finish

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,29 @@
 'use strict';
 
-/**
- * @param {Object} state
- * @param {Object[]} actions
- *
- * @return {Object[]}
- */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  const copy = { ...state };
+
+  for (const key of actions) {
+    if (key.type === 'addProperties') {
+      Object.assign(copy, key.extraData);
+    }
+
+    if (key.type === 'removeProperties') {
+      for (const key2 of key.keysToRemove) {
+        delete copy[key2];
+      }
+    }
+
+    if (key.type === 'clear') {
+      for (const key3 in copy) {
+        delete copy[key3];
+      }
+    }
+    result.push({ ...copy });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
