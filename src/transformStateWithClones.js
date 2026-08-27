1f539af17b1bc43edc91413573f61d912e83e767
Pianist <kosigorek@gmail.com>
2026-08-27 12:56:51 +0300

    result

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,38 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  let currentState = { ...state };
+  const stateHistory = [];
+
+  for (const action of actions) {
+    let nextState;
+
+    switch (action.type) {
+      case 'addProperties':
+        nextState = { ...currentState, ...action.extraData };
+        break;
+
+      case 'removeProperties': {
+        const stateCopy = { ...currentState };
+
+        action.keysToRemove.forEach((key) => delete stateCopy[key]);
+        nextState = stateCopy;
+        break;
+      }
+
+      case 'clear':
+        nextState = {};
+        break;
+
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
+    }
+
+    currentState = nextState;
+    stateHistory.push(currentState);
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
