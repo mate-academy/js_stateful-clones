56e87b4eab73dec94a27c122cab3c1235a26e716
Maksyttaaa <maksytka.lll.327@gmail.com>
2026-09-02 01:01:00 +0300

    Added error output for unknown type

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -29,6 +29,9 @@ function transformStateWithClones(state, actions) {
           delete nextState[key];
         }
         break;
+
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
     }
 
     currentState = nextState;
