f2ac59792d52f06ae8412e6cad4d739ccdc6a9e0
Isabella Braga <isabella.m.braga.a@gmail.com>
2026-09-11 01:11:17 -0300

    Implement transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,33 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  let estadoAtual = { ...state };
+  const armario = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'clear':
+        estadoAtual = {};
+        break;
+
+      case 'addProperties':
+        estadoAtual = {
+          ...estadoAtual,
+          ...action.extraData,
+        };
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete estadoAtual[key];
+        }
+        break;
+    }
+
+    armario.push({ ...estadoAtual });
+  }
+
+  return armario;
 }
 
 module.exports = transformStateWithClones;
